bb9da047e4e9ba74d7e886f9a117e41f03c3a9d7
Kunal Patel <kpatel@apriori.com>
2019-05-31 15:46:07 -0400

    fix constant

--- utils/src/main/java/constants/Constants.java
+++ utils/src/main/java/constants/Constants.java
@@ -6,9 +6,9 @@ import java.util.logging.Level;
 
 public class Constants {
 
-    public static final String defaultCidURL = "https://cid-te.awsdev.apriori.com/apriori/cost/";
-    public static final String cidURL = (StringUtils.isEmpty(System.getProperty("url"))) ? defaultCidURL : System.getProperty("url");
+    public static final String defaultCidURL = "https://cid-te.awsdev.apriori.com/";
+    public static final String cidURL = (StringUtils.isEmpty(System.getProperty("url"))) ? defaultCidURL + "apriori/cost/" : System.getProperty("url");
     public static final Level consoleLogLevel = (StringUtils.isEmpty(System.getProperty("consoleloglevel"))) ? Level.OFF : Level.parse(System.getProperty("consoleloglevel"));
-    public static final String internalApiURL = Constants.cidURL.endsWith("/") ? Constants.defaultCidURL + "ch" : Constants.cidURL + "/ws";
+    public static final String internalApiURL = Constants.defaultCidURL + "/ws";
     public static final String GRID_SERVER_URL = "http://localhost:4444/wd/hub";
 }
