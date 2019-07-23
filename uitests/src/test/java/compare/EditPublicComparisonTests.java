6b241bb7c937ca78fb1401947e3e1b04cd784acd
cfrith <ciene.frith@gmail.com>
2019-07-23 10:03:05 +0100

    modified test data; removed unnecessary object creation;

--- uitests/src/test/java/compare/EditPublicComparisonTests.java
+++ uitests/src/test/java/compare/EditPublicComparisonTests.java
@@ -63,7 +63,8 @@ public class EditPublicComparisonTests extends TestBase {
             .enterComparisonName("DeletePrivateComparison10")
             .save();
 
-        new EvaluatePage(driver).publishScenario().openComparison("DeletePrivateComparison10");
+        new EvaluatePage(driver).publishScenario()
+            .openComparison("DeletePrivateComparison10");
 
         assertThat(new ComparePage(driver).getDescriptionText(), containsString("DeletePrivateComparison10"));
     }
