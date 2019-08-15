d31c90135971f86e7d2e7c595aca0196923d3a59
scrowe-ap <51759581+scrowe-ap@users.noreply.github.com>
2019-08-15 15:59:18 +0100

    Added test C645

--- uitests/src/test/java/evaluate/ProcessRoutingTests.java
+++ uitests/src/test/java/evaluate/ProcessRoutingTests.java
@@ -1,5 +1,6 @@
 package test.java.evaluate;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -7,18 +8,26 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import main.java.base.TestBase;
+import main.java.enums.ProcessGroupEnum;
 import main.java.enums.UsersEnum;
+import main.java.enums.VPEEnum;
 import main.java.pages.evaluate.EvaluatePage;
 import main.java.pages.evaluate.process.ProcessPage;
 import main.java.pages.login.LoginPage;
+import main.java.pages.settings.SettingsPage;
 import main.java.utils.FileResourceUtil;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
 public class ProcessRoutingTests extends TestBase {
+
+    private final String scenarioName = "AutoScenario" + LocalDateTime.now();
 
     private LoginPage loginPage;
     private ProcessPage processPage;
     private EvaluatePage evaluatePage;
+    private SettingsPage settingsPage;
 
     public ProcessRoutingTests() {
         super();
@@ -30,7 +39,7 @@ public class ProcessRoutingTests extends TestBase {
     public void testAlternateRoutingSelection() {
         loginPage = new LoginPage(driver);
         processPage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
-            .uploadFile("Alternate Routing", new FileResourceUtil().getResourceFile("Machining-DTC_Issue_SharpCorner_CurvedWall-CurvedSurface.CATPart"))
+            .uploadFile(scenarioName, new FileResourceUtil().getResourceFile("Machining-DTC_Issue_SharpCorner_CurvedWall-CurvedSurface.CATPart"))
             .costScenario()
             .openProcessDetails()
             .selectRoutingsButton()
@@ -46,14 +55,26 @@ public class ProcessRoutingTests extends TestBase {
     @Test
     @Description("C645 View detailed information about costed process")
     @Severity(SeverityLevel.CRITICAL)
-    public void testViewProcessDetails(){
+    public void testViewProcessDetails() {
         loginPage = new LoginPage(driver);
+
+
         processPage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
-                .uploadFile("Process Details", new FileResourceUtil().getResourceFile("PlasticMoulding.CATPart"))
+                .uploadFile(scenarioName, new FileResourceUtil().getResourceFile("PlasticMoulding.CATPart"))
+                //.openSettings()
+                //.save(EvaluatePage.class)
+                .selectProcessGroup(ProcessGroupEnum.PLASTIC_MOLDING.getProcessGroup())
+                .selectVPE(VPEEnum.APRIORI_USA.getVpe())
                 .costScenario()
                 .openProcessDetails();
 
-        
+        System.out.println(processPage.getSelectionTableDetails());
+        assertThat(processPage.getSelectionTableDetails(), containsString("Cycle Time (s): 29.67\n" +
+                "Piece Part Cost (HKD): 3.44\n" +
+                "Fully Burdened Cost (HKD): 6.48\n" +
+                "Total Capital Investments (HKD): 83,617.49"));
+
+
     }
 
 }
