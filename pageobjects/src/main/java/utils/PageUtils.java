gurijalaram/TestRailReporting

commit cffa7c21947e7f8deb7a432f04b730b24e1124ed

fix bugs:

pageobjects/src/main/java/utils/PageUtils.java
@@ -501,9 +501,6 @@ public class PageUtils {
             .ignoring(StaleElementReferenceException.class)
             .until((WebDriver driver) -> {
                 new Select(locator).selectByVisibleText(dropdownOption);
-                if (!new Select(locator).getFirstSelectedOption().equals(dropdownOption)) {
-                    new Select(locator).selectByVisibleText(dropdownOption);
-                }
                 return true;
             });
     }

build/pom.xml
@@ -669,7 +669,7 @@
 
 	<build>
 		<directory>target</directory>
-		<sourceDirectory>src</sourceDirectory>
+		<sourceDirectory>src/main/java</sourceDirectory>
 		<resources>
 			<resource>
 				<directory>src/main/resources</directory>
@@ -696,6 +696,12 @@
 					<groupId>org.sonarsource.scanner.maven</groupId>
 					<artifactId>sonar-maven-plugin</artifactId>
 					<version>3.6.0.1398</version>
+					<configuration>
+						<properties>
+							<sonar.sources>src/main</sonar.sources>
+							<sonar.tests>src/test</sonar.tests>
+						</properties>
+					</configuration>
 				</plugin>
 				<plugin>
 					<artifactId>maven-surefire-plugin</artifactId>

build/sonar-project.properties
@@ -3,8 +3,4 @@ sonar.projectName=Apriori QA Web Automation
 sonar.projectVersion=1.0
 
 sonar.modules=utils,apibase,pageobjects,internalapi,uitests
-
-sonar.sources=.
-sonar.tests=.
 sonar.scm.forceReloadAll=true
-sonar.exclusions=uitests/src/test/java/testsuites/*

pageobjects/src/main/java/header/EvaluateHeader.java
@@ -56,7 +56,7 @@ public class EvaluateHeader extends GenericHeader {
     public EvaluatePage costScenario(String costText) {
         pageUtils.waitForElementAndClick(costButton);
         pageUtils.waitForElementAndClick(dialogCostButton);
-        costText = costText == "Success" ? COST_UP_TO_DATE : costText;
+        costText = costText.equalsIgnoreCase("Success") ? COST_UP_TO_DATE : costText;
         getCostLabel();
         checkCostLabel(costText);
         return new EvaluatePage(driver);

utils/src/main/java/utils/Util.java
@@ -65,7 +65,8 @@ public class Util {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
-            logger.debug("InterruptedException", e);
+            Thread.currentThread().interrupt();
+            logger.debug("Failed to highlight element");
         }
         js.executeScript("arguments[0].setAttribute('style', '" + originalStyle + "');", element);
     }

commit a14080bb9b3472a05226bc96c79acfcdcb560c6d

re-added deleted code caused by rev:cffa7c21947e7f8deb7a432f04b730b24e1124ed

pageobjects/src/main/java/utils/PageUtils.java
@@ -501,6 +501,9 @@ public class PageUtils {
             .ignoring(StaleElementReferenceException.class)
             .until((WebDriver driver) -> {
                 new Select(locator).selectByVisibleText(dropdownOption);
+                if (!new Select(locator).getFirstSelectedOption().equals(dropdownOption)) {
+                    new Select(locator).selectByVisibleText(dropdownOption);
+                }
                 return true;
             });
     }