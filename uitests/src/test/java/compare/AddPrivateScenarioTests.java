171aa3bfe5eedd04a7e7a40ef514b43c5e1f56fc
mparker <mparker@apriori.com>
2019-08-16 16:52:00 +0100

    Added a test to include deleting public comparisons from comparison page using the filter Added testrail case numbers

--- uitests/src/test/java/compare/AddPrivateScenarioTests.java
+++ uitests/src/test/java/compare/AddPrivateScenarioTests.java
@@ -23,6 +23,7 @@ public class AddPrivateScenarioTests extends TestBase {
 
     private LoginPage loginPage;
     private ComparisonTablePage comparisonTablePage;
+    private ComparePage comparePage;
 
     public AddPrivateScenarioTests() {
         super();
@@ -50,4 +51,40 @@ public class AddPrivateScenarioTests extends TestBase {
 
         assertThat(comparisonTablePage.findComparison(testScenarioName, "Machining-DTC_Issue_SharpCorner_CurvedWall-CurvedSurface").isDisplayed(), Matchers.is(true));
     }
+
+    @Test
+    @Description("Testing the user can update comparisons")
+    @Severity(SeverityLevel.CRITICAL)
+    public void updatePrivateComparison() {
+
+        String testScenarioName = scenarioName;
+
+        loginPage = new LoginPage(driver);
+        comparePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("partbody_2.stp"))
+            .selectProcessGroup(ProcessGroupEnum.CASTING_DIE.getProcessGroup())
+            .costScenario()
+            .createNewScenario()
+            .enterScenarioName("Scenario 2")
+            .save()
+            .createNewComparison()
+            .enterComparisonName("Update Private Comparison")
+            .save(ComparePage.class)
+            .addScenario()
+            .filterCriteria()
+            .filterPrivateCriteria("Part", "Part Name", "Contains", "partbody_2")
+            .apply(ComparisonTablePage.class)
+            .selectComparison(testScenarioName,"partbody_2")
+            .apply()
+            .addScenario()
+            .filterCriteria()
+            .filterPrivateCriteria("Part", "Part Name", "Contains", "partbody_2")
+            .apply(ComparisonTablePage.class)
+            .selectComparison("Scenario 2","partbody_2")
+            .apply();
+
+
+        //assertThat(comparisonTablePage.findComparison(testScenarioName, "Machining-DTC_Issue_SharpCorner_CurvedWall-CurvedSurface").isDisplayed(), Matchers.is(true));
+    }
+
 }
