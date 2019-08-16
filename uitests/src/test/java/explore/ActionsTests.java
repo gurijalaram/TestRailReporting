9ff4233845007b1fb0b9913a9d1c88335a0842a4
mparker <mparker@apriori.com>
2019-08-16 14:14:45 +0100

    Updated comparison tests to include deleting from the explore page Added tags for test rail test cases

--- uitests/src/test/java/explore/ActionsTests.java
+++ uitests/src/test/java/explore/ActionsTests.java
@@ -1,5 +1,9 @@
 package test.java.explore;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
@@ -12,17 +16,9 @@ import main.java.pages.explore.ExplorePage;
 import main.java.pages.explore.ScenarioNotesPage;
 import main.java.pages.login.LoginPage;
 import main.java.utils.FileResourceUtil;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 public class ActionsTests extends TestBase {
 
@@ -41,18 +37,18 @@ public class ActionsTests extends TestBase {
 
         loginPage = new LoginPage(driver);
         loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
-                .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("M3CapScrew.CATPart"))
-                .selectProcessGroup(ProcessGroupEnum.PLASTIC_MOLDING.getProcessGroup())
-                .costScenario()
-                .publishScenario()
-                .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace())
-                .highlightScenario(testScenarioName, "M3CapScrew");
+            .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("M3CapScrew.CATPart"))
+            .selectProcessGroup(ProcessGroupEnum.PLASTIC_MOLDING.getProcessGroup())
+            .costScenario()
+            .publishScenario()
+            .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace())
+            .highlightScenario(testScenarioName, "M3CapScrew");
 
         explorePage = new ExplorePage(driver);
         explorePage.selectScenarioInfoNotes()
-                .enterScenarioInfoNotes("New", "Low", "Qa Description", "\u2022 QA Notes Test\n \u2022 MP Testing\n \u2022 Add and remove notes")
-                .save()
-                .highlightScenario(testScenarioName, "M3CapScrew");
+            .enterScenarioInfoNotes("New", "Low", "Qa Description", "\u2022 QA Notes Test\n \u2022 MP Testing\n \u2022 Add and remove notes")
+            .save()
+            .highlightScenario(testScenarioName, "M3CapScrew");
 
         explorePage = new ExplorePage(driver);
         scenarioNotesPage = explorePage.selectScenarioInfoNotes();
@@ -65,32 +61,32 @@ public class ActionsTests extends TestBase {
     @Test
     @Description("Validate status and cost maturity columns can be added")
     @Severity(SeverityLevel.NORMAL)
-    public void addStatusColumn(){
+    public void addStatusColumn() {
 
         String testScenarioName = scenarioName;
 
-            loginPage = new LoginPage(driver);
-            loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
-                    .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("M3CapScrew.CATPart"))
-                    .selectProcessGroup(ProcessGroupEnum.PLASTIC_MOLDING.getProcessGroup())
-                    .costScenario()
-                    .publishScenario()
-                    .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace())
-                    .highlightScenario(testScenarioName, "M3CapScrew");
+        loginPage = new LoginPage(driver);
+        loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("M3CapScrew.CATPart"))
+            .selectProcessGroup(ProcessGroupEnum.PLASTIC_MOLDING.getProcessGroup())
+            .costScenario()
+            .publishScenario()
+            .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace())
+            .highlightScenario(testScenarioName, "M3CapScrew");
 
-            explorePage = new ExplorePage(driver);
-            explorePage.selectScenarioInfoNotes()
-                    .enterScenarioInfoNotes("Analysis", "Medium", "Qa Description", "Adding QA notes")
-                    .save()
-                    .openColumnsTable()
-                    .addColumn(ColumnsEnum.COST_MATURITY.getColumns())
-                    .addColumn(ColumnsEnum.STATUS.getColumns())
-                    .selectSaveButton();
+        explorePage = new ExplorePage(driver);
+        explorePage.selectScenarioInfoNotes()
+            .enterScenarioInfoNotes("Analysis", "Medium", "Qa Description", "Adding QA notes")
+            .save()
+            .openColumnsTable()
+            .addColumn(ColumnsEnum.COST_MATURITY.getColumns())
+            .addColumn(ColumnsEnum.STATUS.getColumns())
+            .selectSaveButton();
 
-            assertThat(explorePage.getColumnHeaderNames(), hasItems(ColumnsEnum.STATUS.getColumns(), ColumnsEnum.COST_MATURITY.getColumns()));
-        }
-
-
+        assertThat(explorePage.getColumnHeaderNames(), hasItems(ColumnsEnum.STATUS.getColumns(), ColumnsEnum.COST_MATURITY.getColumns()));
     }
+
+
+}
 
 
