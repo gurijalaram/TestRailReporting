362c0b6521c6df32f4354390fc7ee970f1be53c6
smccaffrey <smccaffrey@apriori.com>
2019-08-27 16:34:36 +0100

    Removed Status assertion
    Added VPE assertion
    
    Added TestRail ID's
    
    Updated Workspace selections

--- uitests/src/test/java/explore/TableHeadersTests.java
+++ uitests/src/test/java/explore/TableHeadersTests.java
@@ -10,6 +10,7 @@ import main.java.enums.UsersEnum;
 import main.java.enums.WorkspaceEnum;
 import main.java.pages.explore.ExplorePage;
 import main.java.pages.login.LoginPage;
+import main.java.utils.TestRail;
 import org.junit.Test;
 
 public class TableHeadersTests extends TestBase {
@@ -22,6 +23,7 @@ public class TableHeadersTests extends TestBase {
     }
 
     @Test
+    @TestRail(testCaseId = {"C1096"})
     @Description("Test default list of column headers in the private workspace")
     public void testPrivateDefaultColumnHeaders() {
         loginPage = new LoginPage(driver);
@@ -29,42 +31,47 @@ public class TableHeadersTests extends TestBase {
             .selectWorkSpace(WorkspaceEnum.PRIVATE.getWorkspace());
 
         assertThat(explorePage.getColumnHeaderNames(), hasItems(ColumnsEnum.NAME_SCENARIO.getColumns(), ColumnsEnum.LOCKED_WORKSPACE.getColumns(),
-            ColumnsEnum.STATUS.getColumns(), ColumnsEnum.PROCESS_GROUP.getColumns(), ColumnsEnum.LAST_SAVED.getColumns()));
+            ColumnsEnum.PROCESS_GROUP.getColumns(), ColumnsEnum.VPE.getColumns(), ColumnsEnum.LAST_SAVED.getColumns()));
     }
 
     @Test
+    @TestRail(testCaseId = {"C1096"})
     @Description("Test default list of column headers in the public workspace")
     public void testPublicDefaultColumnHeaders() {
         loginPage = new LoginPage(driver);
         explorePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
-            .selectWorkSpace(WorkspaceEnum.PRIVATE.getWorkspace());
+            .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace());
 
         assertThat(explorePage.getColumnHeaderNames(), hasItems(ColumnsEnum.NAME_SCENARIO.getColumns(), ColumnsEnum.LOCKED_WORKSPACE.getColumns(),
-            ColumnsEnum.STATUS.getColumns(), ColumnsEnum.PROCESS_GROUP.getColumns(), ColumnsEnum.LAST_SAVED.getColumns()));
+            ColumnsEnum.PROCESS_GROUP.getColumns(), ColumnsEnum.VPE.getColumns(), ColumnsEnum.LAST_SAVED.getColumns()));
     }
 
     @Test
+    @TestRail(testCaseId = {"C1095"})
     @Description("Test added columns are displayed in the public workspace")
     public void testPublicAddColumnHeaders() {
         loginPage = new LoginPage(driver);
         explorePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace())
             .openColumnsTable()
             .addColumn(ColumnsEnum.TYPE.getColumns())
-            .selectSaveButton()
-            .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace());
+            .selectSaveButton();
+
 
         assertThat(explorePage.getColumnHeaderNames(), hasItems(ColumnsEnum.TYPE.getColumns()));
     }
 
     @Test
+    @TestRail(testCaseId = {"C1095"})
     @Description("Test added columns are displayed in the private workspace")
     public void testPrivateAddColumnHeaders() {
         loginPage = new LoginPage(driver);
         explorePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .selectWorkSpace(WorkspaceEnum.PRIVATE.getWorkspace())
             .openColumnsTable()
             .addColumn(ColumnsEnum.ASSIGNEE.getColumns())
-            .selectSaveButton()
-            .selectWorkSpace(WorkspaceEnum.PRIVATE.getWorkspace());
+            .selectSaveButton();
+
 
         assertThat(explorePage.getColumnHeaderNames(), hasItems(ColumnsEnum.ASSIGNEE.getColumns()));
     }
