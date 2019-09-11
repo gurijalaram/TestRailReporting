f659e7c3c085d00341158b35bb6305f1fdf1026b
cfrith <ciene.frith@gmail.com>
2019-09-11 11:23:27 +0100

    refactored costing method;

--- pageobjects/src/main/java/header/EvaluateHeader.java
+++ pageobjects/src/main/java/header/EvaluateHeader.java
@@ -58,10 +58,8 @@ public class EvaluateHeader extends GenericHeader {
     public EvaluatePage costScenario() {
         pageUtils.waitForElementAndClick(costButton);
         new CostingJobPage(driver).selectCostButton();
-        pageUtils.checkElementContains(costLabel, CostingLabelEnum.COSTING_IN_PROGRESS.getCostingText());
-        pageUtils.checkElementNotContain(costLabel, CostingLabelEnum.COSTING_IN_PROGRESS.getCostingText());
-        pageUtils.isElementDisplayed(loadingImage);
-        pageUtils.waitForElementNotDisplayed(loadingImage);
+        checkForCostLabel();
+        checkForImage();
         return new EvaluatePage(driver);
     }
 
@@ -72,5 +70,21 @@ public class EvaluateHeader extends GenericHeader {
      */
     public Boolean getCostLabel(String text) {
         return pageUtils.checkElementContains(costLabel, text);
+    }
+
+    /**
+     * Method to check for the loading image displayed/not displayed
+     */
+    private void checkForImage() {
+        pageUtils.isElementDisplayed(loadingImage);
+        pageUtils.waitForElementNotDisplayed(loadingImage);
+    }
+
+    /**
+     * Method to check cost label contains/doesn't contain text
+     */
+    private void checkForCostLabel() {
+        pageUtils.checkElementContains(costLabel, CostingLabelEnum.COSTING_IN_PROGRESS.getCostingText());
+        pageUtils.checkElementNotContain(costLabel, CostingLabelEnum.COSTING_IN_PROGRESS.getCostingText());
     }
 }
