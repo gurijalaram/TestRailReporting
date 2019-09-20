f6516cddb287ef566164731a65f88530a42d4597
cfrith <ciene.frith@gmail.com>
2019-09-20 15:26:23 +0100

    minor change;

--- pageobjects/src/main/java/pages/evaluate/process/secondaryprocess/SecondaryOptionsPage.java
+++ pageobjects/src/main/java/pages/evaluate/process/secondaryprocess/SecondaryOptionsPage.java
@@ -93,7 +93,6 @@ public class SecondaryOptionsPage extends SecondaryProcessPage {
     @FindBy(css = "input[data-ap-comp='numMasks.radioButtons.userOverride']")
     private WebElement maskFeatureRadioButton;
 
-
     private WebDriver driver;
     private PageUtils pageUtils;
 
