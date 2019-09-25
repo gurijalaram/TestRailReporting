843f58442ea0c219cda98fc6c238f5a5df141a3e
cfrith <ciene.frith@gmail.com>
2019-09-25 16:19:01 +0100

    added tolerances enum; changed edittoleance() visibility; reworked/refactored get,set,delete of all tolerances;

--- uitests/src/test/java/evaluate/designguidance/tolerance/ToleranceTests.java
+++ uitests/src/test/java/evaluate/designguidance/tolerance/ToleranceTests.java
@@ -1,11 +1,14 @@
 package test.java.evaluate.designguidance.tolerance;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import io.qameta.allure.Description;
 import main.java.base.TestBase;
 import main.java.enums.ProcessGroupEnum;
+import main.java.enums.ToleranceEnum;
 import main.java.enums.UsersEnum;
+import main.java.pages.evaluate.designguidance.tolerances.ToleranceEditPage;
 import main.java.pages.evaluate.designguidance.tolerances.TolerancePage;
 import main.java.pages.login.LoginPage;
 import main.java.utils.FileResourceUtil;
@@ -17,24 +20,26 @@ public class ToleranceTests extends TestBase {
 
     private LoginPage loginPage;
     private TolerancePage tolerancePage;
+    private ToleranceEditPage toleranceEditPage;
 
     public ToleranceTests() {
         super();
     }
 
     @Test
-    @TestRail(testCaseId = "389")
+    @TestRail(testCaseId = "")
     @Description("Tolerance can be edited")
     public void testEditTolerance() {
         loginPage = new LoginPage(driver);
-        tolerancePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+        toleranceEditPage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
             .uploadFile(new Util().getScenarioName(), new FileResourceUtil().getResourceFile("DTCCastingIssues.CATPART"))
             .selectProcessGroup(ProcessGroupEnum.CASTING_DIE.getProcessGroup())
             .costScenario()
             .openDesignGuidance()
             .openTolerancesTab()
-            .selectToleranceTypeAndGCD("Flatness", "PlanarFace:35");
+            .selectToleranceTypeAndGCD("Flatness", "PlanarFace:35")
+            .editTolerance();
 
-        assertThat(tolerancePage.)
+        assertThat(toleranceEditPage.isTolerance(ToleranceEnum.FLATNESS.getTolerance(), "0.50"), is(true));
     }
 }
