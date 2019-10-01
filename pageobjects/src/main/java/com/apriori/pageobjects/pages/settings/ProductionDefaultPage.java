040e2073cd272a72786d7d27b14454bb21c2fc9e
cfrith <ciene.frith@gmail.com>
2019-10-01 14:14:40 +0100

    added new pageobjects; added new enum for colours;

---
+++ pageobjects/src/main/java/com/apriori/pageobjects/pages/settings/ProductionDefaultPage.java
@@ -0,0 +1,35 @@
+package com.apriori.pageobjects.pages.settings;
+
+import com.apriori.pageobjects.utils.PageUtils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProductionDefaultPage extends LoadableComponent<ProductionDefaultPage> {
+
+    private final Logger logger = LoggerFactory.getLogger(ToleranceSettingsPage.class);
+
+    private WebDriver driver;
+    private PageUtils pageUtils;
+
+    public ProductionDefaultPage(WebDriver driver) {
+        this.driver = driver;
+        this.pageUtils = new PageUtils(driver);
+        logger.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
+        PageFactory.initElements(driver, this);
+        this.get();
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+
+    }
+}
