231772e3926499ac768209dd1eccc45474232316
cfrith <ciene.frith@gmail.com>
2019-10-08 14:30:31 +0100

    added new page objects;

---
+++ pageobjects/src/main/java/com/apriori/pageobjects/pages/login/PrivacyPolicyPage.java
@@ -0,0 +1,34 @@
+package com.apriori.pageobjects.pages.login;
+
+import com.apriori.pageobjects.utils.PageUtils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PrivacyPolicyPage extends LoadableComponent<PrivacyPolicyPage> {
+
+    private Logger logger = LoggerFactory.getLogger(PrivacyPolicyPage.class);
+
+    private WebDriver driver;
+    private PageUtils pageUtils;
+
+    public PrivacyPolicyPage(WebDriver driver) {
+        this.driver = driver;
+        this.pageUtils = new PageUtils(driver);
+        logger.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
+        PageFactory.initElements(driver, this);
+        this.get();
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+    }
+}
