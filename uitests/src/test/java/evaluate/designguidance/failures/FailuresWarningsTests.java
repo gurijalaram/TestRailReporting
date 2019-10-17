60e2c86dfd106ce7fd9e9b33831546dbc07ffe04
mparker <mparker@apriori.com>
2019-10-17 18:29:56 +0100

    Added customer smoke test to failures and warnings tab

--- uitests/src/test/java/evaluate/designguidance/failures/FailuresWarningsTests.java
+++ uitests/src/test/java/evaluate/designguidance/failures/FailuresWarningsTests.java
@@ -1,6 +1,8 @@
 package evaluate.designguidance.failures;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.apriori.pageobjects.pages.evaluate.designguidance.FailuresPage;
@@ -9,6 +11,7 @@ import com.apriori.pageobjects.pages.login.LoginPage;
 import com.apriori.pageobjects.pages.settings.SettingsPage;
 import com.apriori.pageobjects.pages.settings.ToleranceSettingsPage;
 import com.apriori.utils.FileResourceUtil;
+import com.apriori.utils.TestRail;
 import com.apriori.utils.Util;
 import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.enums.UsersEnum;
@@ -16,6 +19,8 @@ import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import testsuites.suiteinterface.CustomerSmokeTests;
 
 public class FailuresWarningsTests extends TestBase {
 
@@ -24,9 +29,11 @@ public class FailuresWarningsTests extends TestBase {
     private ToleranceSettingsPage toleranceSettingsPage;
     private FailuresPage failuresPage;
 
+    @Category(CustomerSmokeTests.class)
     @Test
-    @Description("Ensure that 'Failures/ Warnings tab includes: - Issue type & count - Messaging")
-    public void failedCostingCount() {
+    @TestRail(testCaseId = {"1592"})
+    @Description("Ensure that 'Failures/ Warnings tab includes: Messaging")
+    public void failedCostingMessaging() {
         loginPage = new LoginPage(driver);
         toleranceSettingsPage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
             .openSettings()
@@ -46,5 +53,30 @@ public class FailuresWarningsTests extends TestBase {
 
         failuresPage.selectIssueTypeAndGCD("Not Supported GCDs", "Not Supported GCDs");
         assertThat(failuresPage.getUncostedMessage(), containsString("Multiple bodies exist in the model. Only the largest body is used and the remainder are ignored"));
+    }
+
+    @Category(CustomerSmokeTests.class)
+    @Test
+    @TestRail(testCaseId = {"1592"})
+    @Description("Ensure that 'Failures/ Warnings tab includes: - Issue type & count")
+    public void failedCostingCount() {
+        loginPage = new LoginPage(driver);
+        toleranceSettingsPage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .openSettings()
+            .openTolerancesTab()
+            .selectUseCADModel();
+
+        settingsPage = new SettingsPage(driver);
+        failuresPage = settingsPage.save(ExplorePage.class)
+            .uploadFile(new Util().getScenarioName(), new FileResourceUtil().getResourceFile("DTCCastingIssues.catpart"))
+            .selectProcessGroup(ProcessGroupEnum.CASTING_DIE.getProcessGroup())
+            .costScenario(5)
+            .openDesignGuidance()
+            .openFailuresTab();
+
+        assertThat(failuresPage.getFailuresCell("Failed GCDs", "Count"), is(equalTo("3")));
+        assertThat(failuresPage.getFailuresCell("Failed GCDs", "Reason"), is(equalTo("Failed to cost")));
+        assertThat(failuresPage.getFailuresCell("Failed GCDs", "Not Supported GCDs"), is(equalTo("1")));
+        assertThat(failuresPage.getFailuresCell("Failed GCDs", "Detached Solid"), is(equalTo("1"))); Friday Check
     }
 }
