0552fc5b43715f25e205241f560efcbf6a4c56de
smccaffrey <smccaffrey@apriori.com>
2019-10-22 14:24:07 +0100

    SaveAs Public Comparison added

--- uitests/src/test/java/compare/SaveAsComparisonTests.java
+++ uitests/src/test/java/compare/SaveAsComparisonTests.java
@@ -1,8 +1,5 @@
 package compare;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import com.apriori.pageobjects.header.GenericHeader;
 import com.apriori.pageobjects.pages.compare.ComparePage;
 import com.apriori.pageobjects.pages.explore.ExplorePage;
@@ -12,10 +9,11 @@ import com.apriori.utils.Util;
 import com.apriori.utils.enums.UsersEnum;
 import com.apriori.utils.enums.WorkspaceEnum;
 import com.apriori.utils.web.driver.TestBase;
-
 import io.qameta.allure.Description;
-import io.qameta.allure.Issue;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class SaveAsComparisonTests extends TestBase {
@@ -59,5 +57,40 @@ public SaveAsComparisonTests() {
         assertThat(explorePage.findComparison(testSaveAsComparisonName).isDisplayed(), is(true));
 
     }
+    @Test
+    @TestRail(testCaseId = {"419"})
+    @Description("Test a public comparison can be have Save As performed on it")
+    public void testSaveAsPublicComparison() {
 
+        String testComparisonName = new Util().getComparisonName();
+        String testSaveAsComparisonName = new Util().getComparisonName();
+        String testSaveAsComparisonDescription = "Save As Comparison Description";
+
+        loginPage = new LoginPage(driver);
+        comparePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+                .createNewComparison()
+                .enterComparisonName(testComparisonName)
+                .save(ComparePage.class);
+
+        genericHeader = new GenericHeader(driver);
+
+        comparePage = genericHeader.publishScenario()
+                .selectWorkSpace(WorkspaceEnum.COMPARISONS.getWorkspace())
+                .openComparison(testComparisonName);
+
+        genericHeader = new GenericHeader(driver);
+
+        comparePage = genericHeader.saveAs()
+                .inputName(testSaveAsComparisonName)
+                .inputDescription(testSaveAsComparisonDescription)
+                .selectCreateButton();
+
+        genericHeader = new GenericHeader(driver);
+
+        explorePage = genericHeader.selectExploreButton()
+                .selectWorkSpace(WorkspaceEnum.COMPARISONS.getWorkspace());
+
+        assertThat(explorePage.findComparison(testSaveAsComparisonName).isDisplayed(), is(true));
+
+    }
 }
