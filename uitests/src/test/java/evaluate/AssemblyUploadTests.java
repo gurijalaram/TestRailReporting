0d0bacac83ec79545b8978dfd3f8e36a314cc220
mparker <mparker@apriori.com>
2019-10-24 11:50:59 +0100

    Imported junit after

--- uitests/src/test/java/evaluate/AssemblyUploadTests.java
+++ uitests/src/test/java/evaluate/AssemblyUploadTests.java
@@ -10,12 +10,14 @@ import com.apriori.utils.TestRail;
 import com.apriori.utils.Util;
 import com.apriori.utils.enums.AssemblyProcessGroupEnum;
 import com.apriori.utils.enums.CostingLabelEnum;
+import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.enums.UsersEnum;
+import com.apriori.utils.enums.WorkspaceEnum;
 import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
-
 import io.qameta.allure.Issue;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AssemblyUploadTests extends TestBase {
@@ -39,5 +41,57 @@ public class AssemblyUploadTests extends TestBase {
             .costScenario();
 
         assertThat(evaluatePage.getCostLabel(CostingLabelEnum.COSTING_INCOMPLETE.getCostingText()), is(true));
+    }
+
+    @BeforeClass
+    public void costAssembly() {
+
+        String scenarioName = new Util().getScenarioName();
+
+        loginPage = new LoginPage(driver);
+        evaluatePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .uploadFile(scenarioName, new FileResourceUtil().getResourceFile("Assembly2.stp"))
+            .selectProcessGroup(AssemblyProcessGroupEnum.ASSEMBLY.getProcessGroup())
+            .costScenario()
+            .selectExploreButton()
+            .selectWorkSpace(WorkspaceEnum.PRIVATE.getWorkspace())
+            .openScenario(scenarioName, "PART0001")
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario()
+            .selectExploreButton()
+            .openScenario(scenarioName, "PART0002")
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario()
+            .selectExploreButton()
+            .openScenario(scenarioName, "PART0003")
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario()
+            .selectExploreButton()
+            .openScenario(scenarioName, "PART0004")
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario()
+            .selectExploreButton()
+            .openScenario(scenarioName, "PART0005A")
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario()
+            .selectExploreButton()
+            .openScenario(scenarioName, "PART0005B")
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario()
+            .selectExploreButton()
+            .openAssembly(scenarioName, "ASSY03A")
+            .costScenario()
+            .selectExploreButton()
+            .openAssembly(scenarioName, "ASSY03")
+            .costScenario()
+            .selectExploreButton()
+            .openAssembly(scenarioName, "ASSY02")
+            .costScenario()
+            .selectExploreButton()
+            .openAssembly(scenarioName, "ASSEMBLY01")
+            .costScenario()
+            .selectExploreButton()
+            .openAssembly(scenarioName, "Assembly2")
+            .costScenario();
     }
 }
