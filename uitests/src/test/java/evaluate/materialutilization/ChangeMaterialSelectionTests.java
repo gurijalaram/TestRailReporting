f594577e75d1d89cbaf1429caddd058666153345
smccaffrey <smccaffrey@apriori.com>
2019-10-28 13:57:27 +0000

    Material Selection Regression Tests expanded Test Parts added

--- uitests/src/test/java/evaluate/materialutilization/ChangeMaterialSelectionTests.java
+++ uitests/src/test/java/evaluate/materialutilization/ChangeMaterialSelectionTests.java
@@ -243,4 +243,62 @@ public class ChangeMaterialSelectionTests extends TestBase {
 
         assertThat(evaluatePage.isMaterialInfo("Nylon, Type 46"), is(true));
     }
+
+    @Test
+    @TestRail(testCaseId = {"864", "866", "867"})
+    @Description("Test making changes to the Material for Sheet Metal, the change is respected and the scenario can be cost")
+    public void changeMaterialSelectionTestSheetMetal() {
+        loginPage = new LoginPage(driver);
+        evaluatePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .uploadFile(new Util().getScenarioName(), new FileResourceUtil().getResourceFile("bracket_basic.prt"))
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.isMaterialInfo("Steel, Cold Worked, AISI 1020"), is(true));
+
+        new EvaluatePage(driver).openMaterialCompositionTable()
+            .selectMaterialComposition("Inconel 625")
+            .apply()
+            .costScenario();
+
+        assertThat(evaluatePage.isMaterialInfo("Inconel 625"), is(true));
+    }
+    @Test
+    @TestRail(testCaseId = {"864", "866", "867"})
+    @Description("Test making changes to the Material for Sheet Metal - Hydroforming, the change is respected and the scenario can be cost")
+    public void changeMaterialSelectionTestSheetMetalHydroforming() {
+        loginPage = new LoginPage(driver);
+        evaluatePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .uploadFile(new Util().getScenarioName(), new FileResourceUtil().getResourceFile("Hydroform Sample Banana.stp"))
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL_HYDROFORMING.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.isMaterialInfo("Aluminum, Stock, ANSI 2017"), is(true));
+
+        new EvaluatePage(driver).openMaterialCompositionTable()
+            .selectMaterialComposition("Copper, Stock, UNS C11000")
+            .apply()
+            .costScenario();
+
+        assertThat(evaluatePage.isMaterialInfo("Copper, Stock, UNS C11000"), is(true));
+    }
+    @Test
+    @TestRail(testCaseId = {"864", "866", "867"})
+    @Description("Test making changes to the Material for Sheet Metal Transfer Die, the change is respected and the scenario can be cost")
+    public void changeMaterialSelectionTestSheetMetalTransferDie() {
+        loginPage = new LoginPage(driver);
+        evaluatePage = loginPage.login(UsersEnum.CID_TE_USER.getUsername(), UsersEnum.CID_TE_USER.getPassword())
+            .uploadFile(new Util().getScenarioName(), new FileResourceUtil().getResourceFile("bracket_basic.prt"))
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL_TRANSFER_DIE.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.isMaterialInfo("Steel, Cold Worked, AISI 1020"), is(true));
+
+        new EvaluatePage(driver).openMaterialCompositionTable()
+            .selectMaterialComposition("C60E")
+            .apply()
+            .costScenario();
+
+        assertThat(evaluatePage.isMaterialInfo("C60E"), is(true));
+    }
 }
