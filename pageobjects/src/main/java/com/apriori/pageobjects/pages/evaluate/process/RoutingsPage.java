c4207d64fa8b8eeb2bec38e87ed098e6522a0018
cfrith <ciene.frith@gmail.com>
2019-10-30 11:04:57 +0000

    optimized getroutings method;

--- pageobjects/src/main/java/com/apriori/pageobjects/pages/evaluate/process/RoutingsPage.java
+++ pageobjects/src/main/java/com/apriori/pageobjects/pages/evaluate/process/RoutingsPage.java
@@ -13,10 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author cfrith
@@ -29,9 +26,6 @@ public class RoutingsPage extends LoadableComponent<RoutingsPage> {
     @FindBy(css = "div[data-ap-comp='routingSelectionTable'] td")
     private WebElement routingTableCell;
 
-    @FindBy(css = "div[data-ap-comp='routingSelectionTable'] td")
-    private List<WebElement> routingTableCells;
-
     @FindBy(css = "label[data-ap-field='lastCostedLabel']")
     private WebElement costedRouting;
 
@@ -43,6 +37,9 @@ public class RoutingsPage extends LoadableComponent<RoutingsPage> {
 
     @FindBy(css = "div[data-ap-comp='routingSelectionTable'] div.v-grid-scroller-vertical")
     private WebElement routingScroller;
+
+    @FindBy(css = "div[data-ap-comp='routingSelectionTable'] tr.v-grid-row-has-data")
+    private List<WebElement> routingTableRows;
 
     @FindBy(css = "button.btn.btn-primary")
     private WebElement applyButton;
@@ -119,21 +116,9 @@ public class RoutingsPage extends LoadableComponent<RoutingsPage> {
      * @return list of routings
      */
     public List<String> getRoutings() {
-        return routingTableCells.stream().map(WebElement::getText).collect(Collectors.toList());
-    }
-
-    public Map<String, List<String>> getRoutingDetailsByColumn(String columnName) {
-        String[] header = driver.findElement(By.cssSelector("div[data-ap-comp='routingSelectionTable'] thead")).getAttribute("innerText").split("\n");
-        List<WebElement> rows = driver.findElements(By.cssSelector("div[data-ap-comp='routingSelectionTable'] tr.v-grid-row-has-data"));
-
-        Map<String, List<String>> firstColumn = new HashMap<>();
-        List<String> firstColumnCell = new ArrayList<>();
-
-        for (WebElement routingRow : rows) {
-            firstColumnCell.add(Arrays.asList(routingRow.getAttribute("innerText").split("\n")).get(0));
-            firstColumn.put(header[0], firstColumnCell);
-        }
-        return firstColumn;
+        List<String> routingCell = new ArrayList<>();
+        routingTableRows.forEach(routingRow -> routingCell.add(Arrays.asList(routingRow.getAttribute("innerText").split("\n")).get(0)));
+        return routingCell;
     }
 
     /**
