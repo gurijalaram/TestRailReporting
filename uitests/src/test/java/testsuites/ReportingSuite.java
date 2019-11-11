791bb50650e1b3bd370294a3dd92ff567f960ca7
Kunal Patel <kpatel@apriori.com>
2019-11-11 09:28:51 -0500

    add testrunid

--- uitests/src/test/java/testsuites/ReportingSuite.java
+++ uitests/src/test/java/testsuites/ReportingSuite.java
@@ -1,12 +1,12 @@
 package testsuites;
 
+import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.ConcurrentSuiteRunner;
 import login.ReportLoginTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//TODO: add project ID for TestRail
-//@ProjectRunID("999")
+@ProjectRunID("261")
 @RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses ({
     ReportLoginTests.class
