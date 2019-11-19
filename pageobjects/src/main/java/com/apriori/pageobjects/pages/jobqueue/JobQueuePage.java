e2a535846479754d63df7eb697acf1577526061a
cfrith <ciene.frith@gmail.com>
2019-11-19 09:25:14 +0000

    changed method name;

--- pageobjects/src/main/java/com/apriori/pageobjects/pages/jobqueue/JobQueuePage.java
+++ pageobjects/src/main/java/com/apriori/pageobjects/pages/jobqueue/JobQueuePage.java
@@ -70,7 +70,7 @@ public class JobQueuePage extends LoadableComponent<JobQueuePage> {
      * @param icon         - icon can be 'okay' or 'stop'
      * @return webelement
      */
-    public WebElement checkJobQueueActionComplete(String scenarioName, String jobType, String icon) {
+    public WebElement checkJobQueueActionStatus(String scenarioName, String jobType, String icon) {
         By jobStatus = By.xpath("//a[@title='" + scenarioName + "']/ancestor::tr//div[.='" + jobType + "']/ancestor::tr//img[@src='" + icon + "18.png']");
         return pageUtils.waitForElementToAppear(jobStatus, 2);
     }
