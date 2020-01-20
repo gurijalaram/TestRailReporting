5a408802f05ad3ecc02fcf711104fe9745c5035c
bhegan613 <fEJ3ANB12145>
2020-01-20 14:24:13 +0000

    Revert "Updated id of manage menu option to fix a couple of failing tests"
    This reverts commit df599611a8c3e04c8b514f0c35e937793dcd26bc.

--- pageobjects/src/main/java/com/apriori/pageobjects/admin/header/PageHeader.java
+++ pageobjects/src/main/java/com/apriori/pageobjects/admin/header/PageHeader.java
@@ -45,7 +45,7 @@ public class PageHeader extends LoadableComponent<PageHeader> {
     @FindBy(id = "help")
     private WebElement helpButton;
 
-    @FindBy(id = "manage")
+    @FindBy(id = "main_manage")
     private WebElement manageMenuOption;
 
     @FindBy(id = "help.cost-insight_rep")
