f31a9fdd075566615ffe5cf436604580cb513d8f
mparker <mparker@apriori.com>
2020-01-21 14:02:04 +0000

    Cost model changes and jiras added

--- pageobjects/src/main/java/com/apriori/pageobjects/utils/PageUtils.java
+++ pageobjects/src/main/java/com/apriori/pageobjects/utils/PageUtils.java
@@ -645,7 +645,7 @@ public class PageUtils {
      */
     public void waitForElementAndClick(WebElement locator) {
         waitForElementToBeClickable(locator);
-        new WebDriverWait(driver, BASIC_WAIT_TIME_IN_SECONDS / 2)
+        new WebDriverWait(driver, BASIC_WAIT_TIME_IN_SECONDS)
             .ignoreAll(ignoredWebDriverExceptions)
             .until((WebDriver webDriver) -> {
                 locator.click();
