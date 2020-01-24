b71980a87a99c75a184cd30db25506b33289a247
bhegan613 <fEJ3ANB12145>
2020-01-24 17:09:54 +0000

    Removed hard wait again

--- pageobjects/src/main/java/com/apriori/pageobjects/reports/pages/view/reports/GenericReportPage.java
+++ pageobjects/src/main/java/com/apriori/pageobjects/reports/pages/view/reports/GenericReportPage.java
@@ -134,7 +134,7 @@ public class GenericReportPage extends ReportsPageHeader {
      */
     public GenericReportPage setAssembly(String assemblyName) {
         currentAssemblyElement.click();
-        pageUtils.waitFor(1000);
+        //pageUtils.waitFor(1000);
         pageUtils.checkElementAttribute(currentAssemblyElement, "className", "jr-mSingleselect-input jr jr-isFocused");
         if (!currentAssemblyElement.getAttribute("title").equals(assemblyName)) {
             if (assemblyName.equals("TOP-LEVEL (Initial)")) {
