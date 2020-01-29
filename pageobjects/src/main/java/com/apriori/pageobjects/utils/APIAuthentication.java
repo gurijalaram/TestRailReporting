683b894710868d7e6beb473256e86c77af3ca167
cfrith <ciene.frith@gmail.com>
2020-01-29 15:00:17 +0000

    implemented passivemap to easily cache token;

--- pageobjects/src/main/java/com/apriori/pageobjects/utils/APIAuthentication.java
+++ pageobjects/src/main/java/com/apriori/pageobjects/utils/APIAuthentication.java
@@ -5,17 +5,29 @@ import com.apriori.apibase.http.builder.common.response.common.AuthenticateJSON;
 import com.apriori.apibase.http.builder.service.HTTPRequest;
 import com.apriori.utils.constants.Constants;
 
+import org.apache.commons.collections4.map.PassiveExpiringMap;
+
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class APIAuthentication {
 
     private static String accessToken = null;
+    private static int expireTime;
     private String username;
     private String password;
 
     public APIAuthentication(String username) {
         this.username = username;
         this.password = this.username.split("@")[0];
+    }
+
+    public RequestEntity requestAuthorisation(String endPoint) {
+        return new HTTPRequest()
+            .unauthorized()
+            .customizeRequest()
+            .setHeaders(new APIAuthentication(this.username).initAuthorizationHeader())
+            .setEndpoint(Constants.getBaseUrl() + endPoint);
     }
 
     /**
@@ -25,7 +37,7 @@ public class APIAuthentication {
      */
     private HashMap<String, String> initAuthorizationHeader() {
         return new HashMap<String, String>() {{
-                put("Authorization", "Bearer " + getAccessToken());
+                put("Authorization", "Bearer " + getCachedToken());
                 put("apriori.tenantgroup", "default");
                 put("apriori.tenant", "default");
                 put("Content-Type", "application/vnd.apriori.v1+json");
@@ -45,11 +57,23 @@ public class APIAuthentication {
             : accessToken;
     }
 
-    public RequestEntity requestAuthorisation(String endPoint) {
-        return new HTTPRequest()
-            .unauthorized()
-            .customizeRequest()
-            .setHeaders(new APIAuthentication(this.username).initAuthorizationHeader())
-            .setEndpoint(Constants.getBaseUrl() + endPoint);
+    private int getTimeToLive() {
+        return expireTime = expireTime < 1
+            ? ((AuthenticateJSON) new HTTPRequest().defaultFormAuthorization(this.username, this.password)
+                .customizeRequest()
+                .setReturnType(AuthenticateJSON.class)
+                .setEndpoint(Constants.getBaseUrl() + "ws/auth/token")
+                .setAutoLogin(false)
+                .commitChanges()
+                .connect()
+                .post()).getExpiresIn()
+                : expireTime;
+    }
+
+    private String getCachedToken() {
+        PassiveExpiringMap<String, String> tokenCache = new PassiveExpiringMap<>(TimeUnit.SECONDS.toMillis(getTimeToLive()));
+        tokenCache.put("Token", getAccessToken());
+
+        return tokenCache.isEmpty() ? tokenCache.put("Token", getAccessToken()) : tokenCache.get("Token");
     }
 }
