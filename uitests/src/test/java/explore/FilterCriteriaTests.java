4f14d5be78130697704db8aed02aa0b246b72662
smccaffrey <smccaffrey@apriori.com>
2020-01-31 14:17:40 +0000

    Filter tests updated with assertions

--- uitests/src/test/java/explore/FilterCriteriaTests.java
+++ uitests/src/test/java/explore/FilterCriteriaTests.java
@@ -3,6 +3,9 @@ package explore;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import com.apriori.pageobjects.header.GenericHeader;
+import com.apriori.pageobjects.pages.compare.ComparePage;
+import com.apriori.pageobjects.pages.evaluate.EvaluatePage;
 import com.apriori.pageobjects.pages.evaluate.PublishPage;
 import com.apriori.pageobjects.pages.explore.ExplorePage;
 import com.apriori.pageobjects.pages.login.CIDLoginPage;
@@ -22,6 +25,8 @@ public class FilterCriteriaTests extends TestBase {
 
     private CIDLoginPage loginPage;
     private ExplorePage explorePage;
+    private PublishPage publishPage;
+    private GenericHeader genericHeader;
 
     public FilterCriteriaTests() {
         super();
@@ -38,8 +43,6 @@ public class FilterCriteriaTests extends TestBase {
         loginPage = new CIDLoginPage(driver);
         explorePage = loginPage.login(UserUtil.getUser())
             .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("SheetMetal.prt"))
-            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL_TRANSFER_DIE.getProcessGroup())
-            .costScenario()
             .selectExploreButton();
 
         explorePage = new ExplorePage(driver);
@@ -54,48 +57,62 @@ public class FilterCriteriaTests extends TestBase {
     @Issue("AP-56845")
     @Description("Test private criteria attribute")
     public void testPrivateCriteriaAttribute() {
+
+        String testScenarioName = new Util().getScenarioName();
+
         loginPage = new CIDLoginPage(driver);
         loginPage.login(UserUtil.getUser())
-            .filterCriteria()
-            .filterPrivateCriteria("Part", "Process Group", "is", "Casting")
+            .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("Casting.prt"))
+            .selectProcessGroup(ProcessGroupEnum.CASTING_DIE.getProcessGroup())
+            .costScenario()
+            .selectExploreButton();
+
+        explorePage = new ExplorePage(driver);
+        explorePage.filterCriteria()
+            .filterPrivateCriteria("Part", "Process Group", "is", "Casting - Die")
             .apply(ExplorePage.class);
-        //Assert.assertTrue();
+
+        Assert.assertThat(explorePage.getListOfScenarios(testScenarioName, "Casting"), is(equalTo(1)));
     }
 
     @Test
     @Issue("AP-56845")
     @Description("Test private criteria part contains")
     public void testPrivateCriteriaContains() {
-        loginPage = new CIDLoginPage(driver);
-        loginPage.login(UserUtil.getUser())
-            .filterCriteria()
-            .filterPrivateCriteria("Part", "Part Name", "Contains", "Test")
-            .apply(ExplorePage.class);
-        //Assert.assertTrue();
-    }
 
-    @Test
-    @Issue("AP-56845")
-    @Description("Test private criteria value option")
-    public void testPrivateCriteriaPartValue() {
+        String testScenarioName = new Util().getScenarioName();
+
         loginPage = new CIDLoginPage(driver);
         loginPage.login(UserUtil.getUser())
-            .filterCriteria()
-            .filterPrivateCriteria("Part", "Status", "is", "Waiting")
+            .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("CurvedWall.CATPart"))
+            .selectExploreButton();
+
+        explorePage = new ExplorePage(driver);
+        explorePage.filterCriteria()
+            .filterPrivateCriteria("Part", "Part Name", "Contains", "Wall")
             .apply(ExplorePage.class);
-        //Assert.assertFalse();
+
+        Assert.assertThat(explorePage.getListOfScenarios(testScenarioName, "CurvedWall"), is(equalTo(1)));
     }
 
     @Test
     @Issue("AP-56845")
     @Description("Test private criteria assembly")
     public void testPrivateCriteriaAssembly() {
+
+        String testScenarioName = new Util().getScenarioName();
+
         loginPage = new CIDLoginPage(driver);
         loginPage.login(UserUtil.getUser())
-            .filterCriteria()
-            .filterPrivateCriteria("Assembly", "Part Name", "Contains", "15136")
+            .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("Piston_assembly.stp"))
+            .selectExploreButton();
+
+        explorePage = new ExplorePage(driver);
+        explorePage.filterCriteria()
+            .filterPrivateCriteria("Assembly", "Part Name", "Contains", "Piston_assembly")
             .apply(ExplorePage.class);
-        //Assert.assertTrue();
+
+        Assert.assertThat(explorePage.getListOfScenarios(testScenarioName, "Piston_assembly"), is(equalTo(1)));
     }
 
     @Test
@@ -115,13 +132,12 @@ public class FilterCriteriaTests extends TestBase {
     @Issue("AP-56845")
     @Description("Test public criteria part")
     public void testPublicCriteriaPart() {
+
         String testScenarioName = new Util().getScenarioName();
 
         loginPage = new CIDLoginPage(driver);
         explorePage = loginPage.login(UserUtil.getUser())
             .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("Push Pin.stp"))
-            .selectProcessGroup(ProcessGroupEnum.ADDITIVE_MANUFACTURING.getProcessGroup())
-            .costScenario()
             .publishScenario(PublishPage.class)
             .selectPublishButton();
 
@@ -137,23 +153,51 @@ public class FilterCriteriaTests extends TestBase {
     @Issue("AP-56845")
     @Description("Test public criteria assembly description")
     public void testPublicCriteriaAssemblyDesc() {
+
+        String testScenarioName = new Util().getScenarioName();
+
         loginPage = new CIDLoginPage(driver);
         loginPage.login(UserUtil.getUser())
-            .filterCriteria()
-            .filterPublicCriteria("Assembly", "Description", "Contains", "Test")
+            .uploadFile(testScenarioName, new FileResourceUtil().getResourceFile("Piston_assembly.stp"))
+            .selectScenarioInfoNotes()
+            .enterScenarioInfoNotes("Complete", "High", "Test Description", "Test Notes")
+            .save(EvaluatePage.class)
+            .openJobQueue()
+            .checkJobQueueActionStatus("piston_assembly", testScenarioName, "Update", "okay")
+            .closeJobQueue(EvaluatePage.class)
+            .publishScenario(PublishPage.class)
+            .selectPublishButton();
+
+        explorePage = new ExplorePage(driver);
+        explorePage.filterCriteria()
+            .filterPublicCriteria("Assembly", "Description", "Contains", "Test Description")
             .apply(ExplorePage.class);
-        //Assert.assertFalse();
+
+        Assert.assertThat(explorePage.getListOfScenarios(testScenarioName, "Piston_assembly"), is(equalTo(1)));
     }
 
     @Test
     @Issue("AP-56845")
     @Description("Test public criteria comparison")
     public void testPublicCriteriaComparison() {
+
+        String testComparisonName = new Util().getScenarioName();
+
         loginPage = new CIDLoginPage(driver);
         loginPage.login(UserUtil.getUser())
-            .filterCriteria()
-            .filterPublicCriteria("Comparison", "Cost Maturity", "is", "clear selections")
+            .createNewComparison()
+            .enterComparisonName(testComparisonName)
+            .save(ComparePage.class);
+
+        genericHeader = new GenericHeader(driver);
+        genericHeader.publishScenario(PublishPage.class)
+            .selectPublishButton();
+
+        explorePage = new ExplorePage(driver);
+        explorePage.filterCriteria()
+            .filterPublicCriteria("Comparison", "Part Name", "Contains", testComparisonName)
             .apply(ExplorePage.class);
-        //Assert.assertTrue();
+
+        Assert.assertThat(explorePage.getListOfScenarios("Initial", testComparisonName), is(equalTo(1)));
     }
 }
