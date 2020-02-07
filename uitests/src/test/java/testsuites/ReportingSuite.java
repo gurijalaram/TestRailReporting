cc901f8340fa82194ba341d44e936dffe3844697
bhegan613 <fEJ3ANB12145>
2020-02-07 11:54:06 +0000

    Reverted suite to include all reports tests

--- uitests/src/test/java/testsuites/ReportingSuite.java
+++ uitests/src/test/java/testsuites/ReportingSuite.java
@@ -12,6 +12,9 @@ import org.junit.runners.Suite;
 @ProjectRunID("261")
 @RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses ({
+    LoginTests.class,
+    NavigationTests.class,
+    AssemblyDetailsReportTests.class,
     MachiningDtcReportTests.class
 })
 public class ReportingSuite {
