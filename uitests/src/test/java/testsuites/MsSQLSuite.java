d6d5dcbe99df920f648b7fefe5c2560ec8a0b866
bhegan613 <fEJ3ANB12145>
2020-02-28 17:13:57 +0000

    Excluding mysql test

--- uitests/src/test/java/testsuites/MsSQLSuite.java
+++ uitests/src/test/java/testsuites/MsSQLSuite.java
@@ -6,9 +6,11 @@ import testsuites.suiteinterface.MsSQLTest;
 import cireporttests.login.LoginTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import testsuites.suiteinterface.MySQLTest;
 
 @RunWith(ConcurrentSuiteRunner.class)
 @Categories.IncludeCategory(MsSQLTest.class)
+@Categories.ExcludeCategory(MySQLTest.class)
 @Suite.SuiteClasses ({
         LoginTests.class
 })
