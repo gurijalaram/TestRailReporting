da7489265b5dfcfb6900a857286318a195b21455
cfrith <ciene.frith@gmail.com>
2020-03-03 14:13:19 +0000

    removed comments;

--- internalapi/src/test/java/com/apriori/internalapi/edc/suite/TestSuiteAPI.java
+++ internalapi/src/test/java/com/apriori/internalapi/edc/suite/TestSuiteAPI.java
@@ -1,6 +1,8 @@
 package com.apriori.internalapi.edc.suite;
 
-import com.apriori.internalapi.services.CdsUsers;
+import com.apriori.internalapi.edc.AccountsTest;
+import com.apriori.internalapi.edc.BillOfMaterialsTest;
+import com.apriori.internalapi.edc.PartOfMaterialsTest;
 import com.apriori.utils.runner.ConcurrentSuiteRunner;
 
 import org.junit.runner.RunWith;
@@ -8,10 +10,9 @@ import org.junit.runners.Suite;
 
 @RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses({
-//    PartOfMaterialsTest.class,
-//    BillOfMaterialsTest.class,
-//    AccountsTest.class,
-    CdsUsers.class
+    PartOfMaterialsTest.class,
+    BillOfMaterialsTest.class,
+    AccountsTest.class,
 })
 public class TestSuiteAPI {
 }
