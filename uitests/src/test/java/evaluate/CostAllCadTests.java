d4c35ca012dc64e40f8d0fcb59c403efe4b845c6
mparker <mparker@apriori.com>
2020-03-09 10:56:06 +0000

    Cost model changes and jiras added

--- uitests/src/test/java/evaluate/CostAllCadTests.java
+++ uitests/src/test/java/evaluate/CostAllCadTests.java
@@ -53,7 +53,7 @@ public class CostAllCadTests extends TestBase {
             .expandDropdown("Piece Part Cost")
             .expandDropdown("Total Variable Costs");
 
-        assertThat(costDetailsPage.getCostContribution("Material Cost "), containsString("14.41"));
+        assertThat(costDetailsPage.getCostContribution("Material Cost "), containsString("16.22"));
         assertThat(costDetailsPage.getCostContribution("Labor "), containsString("4.94"));
         assertThat(costDetailsPage.getCostContribution("Direct Overhead "), containsString("1.35"));
     }
