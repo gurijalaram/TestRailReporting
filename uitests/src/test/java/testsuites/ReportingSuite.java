e22610064f5f75d65724416589d88a336262047d
Ben <bhegan@apriori.com>
2020-03-10 10:30:29 +0000

    Reduced test suite size for faster results

--- uitests/src/test/java/testsuites/ReportingSuite.java
+++ uitests/src/test/java/testsuites/ReportingSuite.java
@@ -16,13 +16,7 @@ import org.junit.runners.Suite;
 @ProjectRunID("261")
 @RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses({
-    LoginTests.class,
-    NavigationTests.class,
-    AssemblyDetailsReportTests.class,
-    MachiningDtcReportTests.class,
-    CastingDtcReportTests.class,
-    CastingDtcComparisonReportTests.class,
-    CastingDtcDetailsReportTests.class
+    MachiningDtcReportTests.class
 })
 public class ReportingSuite {
 }
