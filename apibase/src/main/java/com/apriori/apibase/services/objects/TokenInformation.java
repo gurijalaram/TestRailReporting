4df348c91f9955de6e77978d4ff25c0cb9432472
dmorrow-apr <59620328+dmorrow-apr@users.noreply.github.com>
2020-03-12 11:30:03 -0400

    [BA-966] ATS Tests and supporting framework code (#443)
    * [BA-966] ATS Tests and supporting framework code
    
    * [BA-966] Fixes for comments in PR
    
    * [BA-966] Fixed typo
    
    * add regex checking of jwt token
    
    Co-authored-by: Kunal Patel <kpatel@apriori.com>

---
+++ apibase/src/main/java/com/apriori/apibase/services/objects/TokenInformation.java
@@ -0,0 +1,41 @@
+
+package com.apriori.apibase.services.objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TokenInformation {
+
+    @JsonProperty
+    private String issuer;
+
+    @JsonProperty
+    private String subject;
+
+    @JsonProperty
+    private Claims claims;
+
+    public TokenInformation setIssuer(String issuer) {
+        this.issuer = issuer;
+        return this;
+    }
+
+    public String getIssuer() {
+        return this.issuer;
+    }
+
+    public TokenInformation setSubject(String subject) {
+        this.subject =  subject;
+        return this;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public TokenInformation setNameAndEmail(String name, String email) {
+        this.claims = new Claims();
+        this.claims.setName(name);
+        this.claims.setEmail(email);
+        return this;
+    }
+}
