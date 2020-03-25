ffe2847db85cd552d07855e730a2fc9b57f6ff17
smccaffrey <smccaffrey@apriori.com>
2020-03-25 12:57:26 +0000

    DecimalPlaceTests added to

--- uitests/src/test/java/settings/DecimalPlaceTests.java
+++ uitests/src/test/java/settings/DecimalPlaceTests.java
@@ -42,7 +42,7 @@ public class DecimalPlaceTests extends TestBase {
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
     @Test
-    @TestRail(testCaseId = {"3730", "3738"})
+    @TestRail(testCaseId = {"3730", "3738", "3764"})
     @Description("User can change the default Displayed Decimal Places")
     public void changeDecimalPlaceDefaultsMax() {
 
@@ -68,4 +68,76 @@ public class DecimalPlaceTests extends TestBase {
         assertThat(evaluatePage.getBurdenedCost("8.377654"), is(true));
         assertThat(evaluatePage.getCapitalInvestment(), is("51,265.177987"));
     }
+
+    @Category({CustomerSmokeTests.class, SmokeTests.class})
+    @Test
+    @TestRail(testCaseId = {"3730", "3738", "3764"})
+    @Description("User can change the default Displayed Decimal Places to the minimum")
+    public void changeDecimalPlaceDefaultsMin() {
+
+        resourceFile = new FileResourceUtil().getResourceFile("bracket_basic.prt");
+        String testScenarioName = new Util().getScenarioName();
+
+        loginPage = new CIDLoginPage(driver);
+        currentUser = UserUtil.getUser();
+
+        loginPage.login(currentUser)
+            .openSettings()
+            .changeDecimalPlaces(DecimalPlaceEnum.ZERO.getDecimalPlaces());
+        new SettingsPage(driver).save(ExplorePage.class)
+            .uploadFile(testScenarioName, resourceFile)
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.isFinishMass("0.701755"), is(true));
+        assertThat(evaluatePage.isUtilization("95.000000"), is(true));
+        assertThat(evaluatePage.getCycleTimeCount(), is("92.698712"));
+        assertThat(evaluatePage.getMaterialCost(), is("2.227365"));
+        assertThat(evaluatePage.getPartCost(), is("6.513465"));
+        assertThat(evaluatePage.getBurdenedCost("8.377654"), is(true));
+        assertThat(evaluatePage.getCapitalInvestment(), is("51,265.177987"));
+    }
+
+    @Category({CustomerSmokeTests.class, SmokeTests.class})
+    @Test
+    @TestRail(testCaseId = {"3730", "3738", "3792", "3764"})
+    @Description("User can change the default Displayed Decimal Places multiple times and rounding adjusts")
+    public void changeDecimalPlaceDefaultsUpdates() {
+
+        resourceFile = new FileResourceUtil().getResourceFile("bracket_basic.prt");
+        String testScenarioName = new Util().getScenarioName();
+
+        loginPage = new CIDLoginPage(driver);
+        currentUser = UserUtil.getUser();
+
+        loginPage.login(currentUser)
+            .openSettings()
+            .changeDecimalPlaces(DecimalPlaceEnum.FIVE.getDecimalPlaces());
+        new SettingsPage(driver).save(ExplorePage.class)
+            .uploadFile(testScenarioName, resourceFile)
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.isFinishMass("0.701755"), is(true));
+        assertThat(evaluatePage.isUtilization("95.000000"), is(true));
+        assertThat(evaluatePage.getCycleTimeCount(), is("92.698712"));
+        assertThat(evaluatePage.getMaterialCost(), is("2.227365"));
+        assertThat(evaluatePage.getPartCost(), is("6.513465"));
+        assertThat(evaluatePage.getBurdenedCost("8.377654"), is(true));
+        assertThat(evaluatePage.getCapitalInvestment(), is("51,265.177987"));
+
+        evaluatePage.openSettings()
+            .changeDecimalPlaces(DecimalPlaceEnum.FOUR.getDecimalPlaces());
+        new SettingsPage(driver).save(ExplorePage.class);
+
+        assertThat(evaluatePage.isFinishMass("0.701755"), is(true));
+        assertThat(evaluatePage.isUtilization("95.000000"), is(true));
+        assertThat(evaluatePage.getCycleTimeCount(), is("92.698712"));
+        assertThat(evaluatePage.getMaterialCost(), is("2.227365"));
+        assertThat(evaluatePage.getPartCost(), is("6.513465"));
+        assertThat(evaluatePage.getBurdenedCost("8.377654"), is(true));
+        assertThat(evaluatePage.getCapitalInvestment(), is("51,265.177987"));
+    }
+
+
 }
