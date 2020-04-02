8fb92d9fc39190f823a7f41c051f3de8320aa99f
dmorrow-apr <59620328+dmorrow-apr@users.noreply.github.com>
2020-04-02 14:09:02 -0400

    [BA-1002] Create generic pagination object and schema (#481)
    * [BA-1002] Create generic pagination object and schema
    
    * [BA-1002] Fixed checkstyle issue

--- apibase/src/main/java/com/apriori/apibase/services/cds/objects/Applications.java
+++ apibase/src/main/java/com/apriori/apibase/services/cds/objects/Applications.java
@@ -1,20 +1,12 @@
 package com.apriori.apibase.services.cds.objects;
 
+import com.apriori.apibase.services.Pagination;
 import com.apriori.utils.http.enums.Schema;
 
 import java.util.List;
 
 @Schema(location = "CdsApplicationsSchema.json")
-public class Applications {
-    private Boolean isFirstPage;
-    private Boolean isLastPage;
-    private Integer pageNumber;
-    private Integer pageSize;
-    private Integer pageItemCount;
-    private Integer totalItemCount;
-    private Integer totalPageCount;
-    private Boolean hasNextPage;
-    private Boolean hasPreviousPage;
+public class Applications extends Pagination {
     private List<Application> items;
     private Applications response;
 
@@ -27,101 +19,6 @@ public class Applications {
         return this;
     }
 
-    public Applications setIsFirstPage(Boolean isFirstPage) {
-        this.isFirstPage = isFirstPage;
-        return this;
-    }
-
-    public Boolean getIsFirstPage() {
-        return this.isFirstPage;
-    }
-
-    public Applications setIsLastPage(Boolean isLastPage) {
-        this.isLastPage = isLastPage;
-        return this;
-
-    }
-
-    public Boolean getIsLastPage() {
-        return this.isLastPage;
-
-    }
-
-    public Applications setPageNumber(Integer pageNumber) {
-        this.pageNumber = pageNumber;
-        return this;
-
-    }
-
-    public Integer getPageNumber() {
-        return this.pageNumber;
-
-    }
-
-    public Applications setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-        return this;
-
-    }
-
-    public Integer getPageSize() {
-        return this.pageSize;
-
-    }
-
-    public Applications setPageItemCount(Integer pageItemCount) {
-        this.pageItemCount = pageItemCount;
-        return this;
-
-    }
-
-    public Integer getPageItemCount() {
-        return this.pageItemCount;
-
-    }
-
-    public Applications setTotalItemCount(Integer totalItemCount) {
-        this.totalItemCount = totalItemCount;
-        return this;
-
-    }
-
-    public Integer getTotalItemCount() {
-        return this.totalItemCount;
-
-    }
-
-    public Applications setTotalPageCount(Integer totalPageCount) {
-        this.totalPageCount = totalPageCount;
-        return this;
-    }
-
-    public Integer getTotalPageCount() {
-        return this.totalPageCount;
-
-    }
-
-    public Applications setHasNextPage(Boolean hasNextPage) {
-        this.hasNextPage = hasNextPage;
-        return this;
-
-    }
-
-    public Boolean getHasNextPage() {
-        return this.hasNextPage;
-
-    }
-
-    public Applications setHasPreviousPage(Boolean hasPreviousPage) {
-        this.hasPreviousPage = hasPreviousPage;
-        return this;
-
-    }
-
-    public Boolean getHasPreviousPage() {
-        return this.hasPreviousPage;
-    }
-
     public Applications setItems(List<Application> items) {
         this.items = items;
         return this;
@@ -130,6 +27,4 @@ public class Applications {
     public List<Application> getItems() {
         return this.items;
     }
-
-
 }
--- apibase/src/main/java/com/apriori/apibase/services/cds/objects/Users.java
+++ apibase/src/main/java/com/apriori/apibase/services/cds/objects/Users.java
@@ -1,20 +1,12 @@
 package com.apriori.apibase.services.cds.objects;
 
+import com.apriori.apibase.services.Pagination;
 import com.apriori.utils.http.enums.Schema;
 
 import java.util.List;
 
 @Schema(location = "CdsUsersSchema.json")
-public class Users {
-    private Boolean isFirstPage;
-    private Boolean isLastPage;
-    private Integer pageNumber;
-    private Integer pageSize;
-    private Integer pageItemCount;
-    private Integer totalItemCount;
-    private Integer totalPageCount;
-    private Boolean hasNextPage;
-    private Boolean hasPreviousPage;
+public class Users extends Pagination {
     private List<User> items;
     private Users response;
 
@@ -25,101 +17,6 @@ public class Users {
     public Users setResponse(Users response) {
         this.response = response;
         return this;
-    }
-
-    public Users setIsFirstPage(Boolean isFirstPage) {
-        this.isFirstPage = isFirstPage;
-        return this;
-    }
-
-    public Boolean getIsFirstPage() {
-        return this.isFirstPage;
-    }
-
-    public Users setIsLastPage(Boolean isLastPage) {
-        this.isLastPage = isLastPage;
-        return this;
-
-    }
-
-    public Boolean getIsLastPage() {
-        return this.isLastPage;
-
-    }
-
-    public Users setPageNumber(Integer pageNumber) {
-        this.pageNumber = pageNumber;
-        return this;
-
-    }
-
-    public Integer getPageNumber() {
-        return this.pageNumber;
-
-    }
-
-    public Users setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-        return this;
-
-    }
-
-    public Integer getPageSize() {
-        return this.pageSize;
-
-    }
-
-    public Users setPageItemCount(Integer pageItemCount) {
-        this.pageItemCount = pageItemCount;
-        return this;
-
-    }
-
-    public Integer getPageItemCount() {
-        return this.pageItemCount;
-
-    }
-
-    public Users setTotalItemCount(Integer totalItemCount) {
-        this.totalItemCount = totalItemCount;
-        return this;
-
-    }
-
-    public Integer getTotalItemCount() {
-        return this.totalItemCount;
-
-    }
-
-    public Users setTotalPageCount(Integer totalPageCount) {
-        this.totalPageCount = totalPageCount;
-        return this;
-    }
-
-    public Integer getTotalPageCount() {
-        return this.totalPageCount;
-
-    }
-
-    public Users setHasNextPage(Boolean hasNextPage) {
-        this.hasNextPage = hasNextPage;
-        return this;
-
-    }
-
-    public Boolean getHasNextPage() {
-        return this.hasNextPage;
-
-    }
-
-    public Users setHasPreviousPage(Boolean hasPreviousPage) {
-        this.hasPreviousPage = hasPreviousPage;
-        return this;
-
-    }
-
-    public Boolean getHasPreviousPage() {
-        return this.hasPreviousPage;
     }
 
     public Users setItems(List<User> items) {
