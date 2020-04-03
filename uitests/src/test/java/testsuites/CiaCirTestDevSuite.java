75f8354f085c1fb150605ef0999a347e5eec38ea
Ben <bhegan@apriori.com>
2020-04-03 10:38:34 +0100

    Removed unwanted classes from suite

--- uitests/src/test/java/testsuites/CiaCirTestDevSuite.java
+++ uitests/src/test/java/testsuites/CiaCirTestDevSuite.java
@@ -1,12 +1,6 @@
 package testsuites;
 
-import cireporttests.login.LoginTests;
-import cireporttests.navigation.NavigationTests;
-import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcComparisonReportTests;
-import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcDetailsReportTests;
 import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcReportTests;
-import cireporttests.ootbreports.dtcmetrics.machiningdtc.MachiningDtcReportTests;
-import cireporttests.ootbreports.general.assemblydetails.AssemblyDetailsReportTests;
 import com.apriori.utils.runner.CategorySuiteRunner;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
@@ -16,14 +10,7 @@ import testsuites.suiteinterface.CiaCirTestDevTest;
 @RunWith(CategorySuiteRunner.class)
 @Categories.IncludeCategory(CiaCirTestDevTest.class)
 @Suite.SuiteClasses({
-        LoginTests.class,
-        NavigationTests.class,
-        ciadmintests.navigation.NavigationTests.class,
-        AssemblyDetailsReportTests.class,
-        MachiningDtcReportTests.class,
-        CastingDtcReportTests.class,
-        CastingDtcComparisonReportTests.class,
-        CastingDtcDetailsReportTests.class
+        CastingDtcReportTests.class
 })
 public class CiaCirTestDevSuite {
 }
