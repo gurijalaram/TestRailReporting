0998cdf14e5487fb228177386143e01312caf53c
Kunal Patel <kpatel@apriori.com>
2020-04-29 11:51:49 -0400

    fix login test

--- ciconnect/src/test/java/LoginTests.java
+++ ciconnect/src/test/java/LoginTests.java
@@ -1,23 +1,21 @@
 import com.apriori.utils.web.driver.TestBase;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTests extends TestBase {
 
-    public static final String cicQA = "https://cic.qa.awsdev.apriori.com/Thingworx/Runtime/index.html#master=PLMC_MasterMashup_Master&mashup=PLMC_JobManagement_MU";
+    public static final String cicQA = "cic.qa.awsdev.apriori.com/Thingworx/Runtime/index.html#master=PLMC_MasterMashup_Master&mashup=PLMC_JobManagement_MU";
 
     public LoginTests() {
         super();
     }
 
     public void login(String uname, String pwd){
-        String URL = "http://" + uname + ":" + pwd + "@" + cicQA;
+        String URL = "https://" + uname + ":" + pwd + "@" + cicQA;
         driver.get(URL);
     }
 
     @Test
     public void testLogin(){
-        driver = new ChromeDriver();
         login("kpatel@apriori.com", "CostInsight2019");
     }
 }
