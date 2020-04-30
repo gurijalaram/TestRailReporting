2543639572fcec0f3f1d07c730f13994e19157c3
moyaparker <mparker@apriori.com>
2020-04-30 13:30:33 +0100

    added dfm tests

--- uitests/src/test/java/evaluate/designguidance/dtc/DFMRiskTests.java
+++ uitests/src/test/java/evaluate/designguidance/dtc/DFMRiskTests.java
@@ -6,7 +6,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.apriori.pageobjects.pages.evaluate.EvaluatePage;
 import com.apriori.pageobjects.pages.login.CIDLoginPage;
-import com.apriori.utils.AfterTestUtil;
+import com.apriori.pageobjects.toolbars.GenericHeader;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.Util;
@@ -17,7 +17,6 @@ import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
-import org.junit.After;
 import org.junit.Test;
 
 import java.io.File;
@@ -27,19 +26,13 @@ public class DFMRiskTests extends TestBase {
     private CIDLoginPage loginPage;
     private EvaluatePage evaluatePage;
     private UserCredentials currentUser;
+    private GenericHeader genericHeader;
 
     private File resourceFile;
     private File cadResourceFile;
 
     public DFMRiskTests() {
         super();
-    }
-
-    @After
-    public void resetSettings() {
-        if (currentUser != null) {
-            new AfterTestUtil().resetAllSettings(currentUser.getUsername());
-        }
     }
 
     @Test
@@ -194,8 +187,94 @@ public class DFMRiskTests extends TestBase {
         assertThat(evaluatePage.getDfmRisk(), is("High"));
 
         evaluatePage.uploadCadFile(cadResourceFile);
-        //assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.TRANSLATING.getCostingText()), is(true));
+        assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.TRANSLATING.getCostingText()), is(true));
         assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.COSTING_UP_TO_DATE.getCostingText()), is(true));
+
+        evaluatePage.costScenario();
+        assertThat(evaluatePage.getDFMRiskIcon(), containsString("dtc-low-risk-icon"));
+        assertThat(evaluatePage.getDfmRisk(), is("Low"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"3864"})
+    @Description("Validate DFM Risk can be REDUCED for SHEET METAL")
+    public void dfmReducedSheetMetal() {
+
+        String file = "bracketdfm.SLDPRT";
+        resourceFile = new FileResourceUtil().getResourceFile(file);
+        cadResourceFile = new FileResourceUtil().getResourceCadFile(file);
+        loginPage = new CIDLoginPage(driver);
+        currentUser = UserUtil.getUser();
+
+        evaluatePage = loginPage.login(currentUser)
+            .uploadFile(new Util().getScenarioName(), resourceFile)
+            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.getDFMRiskIcon(), containsString("dtc-medium-risk-icon"));
+        assertThat(evaluatePage.getDfmRisk(), is("Medium"));
+
+        evaluatePage.uploadCadFile(cadResourceFile);
+        assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.TRANSLATING.getCostingText()), is(true));
+        assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.COSTING_UP_TO_DATE.getCostingText()), is(true));
+        assertThat(evaluatePage.getDFMRiskIcon(), containsString("dtc-low-risk-icon"));
+        assertThat(evaluatePage.getDfmRisk(), is("Low"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"3872"})
+    @Description("Validate DFM Risk can be REDUCED for DIE CAST")
+    public void dfmReducedDieCast() {
+
+        String file = "manifold.prt.1";
+        resourceFile = new FileResourceUtil().getResourceFile(file);
+        cadResourceFile = new FileResourceUtil().getResourceCadFile(file);
+        loginPage = new CIDLoginPage(driver);
+        currentUser = UserUtil.getUser();
+
+        evaluatePage = loginPage.login(currentUser)
+            .uploadFile(new Util().getScenarioName(), resourceFile)
+            .selectProcessGroup(ProcessGroupEnum.CASTING_DIE.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.getDFMRiskIcon(), containsString("dtc-medium-risk-icon"));
+        assertThat(evaluatePage.getDfmRisk(), is("Medium"));
+
+        evaluatePage.uploadCadFile(cadResourceFile);
+        assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.TRANSLATING.getCostingText()), is(true));
+        assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.COSTING_UP_TO_DATE.getCostingText()), is(true));
+
+        evaluatePage.openDesignGuidance()
+            .closePanel();
+        assertThat(evaluatePage.getDFMRiskIcon(), containsString("dtc-low-risk-icon"));
+        assertThat(evaluatePage.getDfmRisk(), is("Low"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"3874"})
+    @Description("Validate DFM Risk can be REDUCED for SAND CAST")
+    public void dfmReducedSandCast() {
+
+        String file = "SandCastBox.SLDPRT";
+        resourceFile = new FileResourceUtil().getResourceFile(file);
+        cadResourceFile = new FileResourceUtil().getResourceCadFile(file);
+        loginPage = new CIDLoginPage(driver);
+        currentUser = UserUtil.getUser();
+
+        evaluatePage = loginPage.login(currentUser)
+            .uploadFile(new Util().getScenarioName(), resourceFile)
+            .selectProcessGroup(ProcessGroupEnum.CASTING_SAND.getProcessGroup())
+            .costScenario();
+
+        assertThat(evaluatePage.getDFMRiskIcon(), containsString("dtc-high-risk-icon"));
+        assertThat(evaluatePage.getDfmRisk(), is("High"));
+
+        evaluatePage.uploadCadFile(cadResourceFile);
+        assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.TRANSLATING.getCostingText()), is(true));
+        assertThat(new EvaluatePage(driver).getCostLabel(CostingLabelEnum.COSTING_UP_TO_DATE.getCostingText()), is(true));
+
+        evaluatePage.openDesignGuidance()
+            .closePanel();
         assertThat(evaluatePage.getDFMRiskIcon(), containsString("dtc-low-risk-icon"));
         assertThat(evaluatePage.getDfmRisk(), is("Low"));
     }
