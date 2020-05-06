aeec0706eeb4e027e317b0f286404fb027f4bf29
cfrith <ciene.frith@gmail.com>
2020-05-06 13:35:54 +0100

    refactored api authorization to its own class;

--- utils/src/main/java/com/apriori/utils/Util.java
+++ utils/src/main/java/com/apriori/utils/Util.java
@@ -16,8 +16,6 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collector;
@@ -86,26 +84,6 @@ public class Util {
             Collections.shuffle(collected);
             return collected.stream();
         });
-    }
-
-
-    public static Map<String, String> getTokenAuthorizationForm(final String token) {
-        return new HashMap<String, String>() {{
-                put("Authorization", "Bearer " + token);
-                put("apriori.tenantGroup", "default");
-                put("apriori.tenant", "default");
-            }};
-    }
-
-    public static Map<String, String> getDefaultAuthorizationForm(final String username, final String password) {
-        return new HashMap<String, String>() {{
-                put("grant_type", "password");
-                put("client_id", "apriori-web-cost");
-                put("client_secret", "donotusethiskey");
-                put("scope", "tenantGroup%3Ddefault%20tenant%3Ddefault");
-                put("username", username);
-                put("password", password);
-            }};
     }
 
     /**
