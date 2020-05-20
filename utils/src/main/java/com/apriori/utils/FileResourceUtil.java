21eefafd821459ef1d43dc31ed4f93f88240d2cf
cfrith <ciene.frith@gmail.com>
2020-05-20 15:18:26 +0100

    moved method to end of class;

--- utils/src/main/java/com/apriori/utils/FileResourceUtil.java
+++ utils/src/main/java/com/apriori/utils/FileResourceUtil.java
@@ -83,48 +83,6 @@ public class FileResourceUtil {
         }
     }
 
-    private static File createTempDir() {
-        File baseDir = new File(System.getProperty("java.io.tmpdir"));
-        String baseName = System.currentTimeMillis() + "-";
-
-        for (int counter = 0; counter < TEMP_DIR_ATTEMPTS; counter++) {
-            File tempDir = new File(baseDir, baseName + counter);
-            if (tempDir.mkdirs()) {
-                return tempDir;
-            }
-        }
-        throw new IllegalStateException(
-            "Failed to create directory within "
-                + TEMP_DIR_ATTEMPTS
-                + " attempts (tried "
-                + baseName
-                + "0 to "
-                + baseName
-                + (TEMP_DIR_ATTEMPTS - 1)
-                + ')');
-    }
-
-    private static File createTempDir(String path) {
-        File baseDir = new File(System.getProperty("java.io.tmpdir"));
-        String baseName = System.currentTimeMillis() + "-";
-
-        for (int counter = 0; counter < TEMP_DIR_ATTEMPTS; counter++) {
-            File tempDir = new File(baseDir, baseName + counter + File.separator + path);
-            if (tempDir.mkdirs()) {
-                return tempDir;
-            }
-        }
-        throw new IllegalStateException(
-            "Failed to create directory within "
-                + TEMP_DIR_ATTEMPTS
-                + " attempts (tried "
-                + baseName
-                + "0 to "
-                + baseName
-                + (TEMP_DIR_ATTEMPTS - 1)
-                + ')');
-    }
-
     /**
      * Get file from resource folder
      * @param resourceFileName - the file name
@@ -183,5 +141,47 @@ public class FileResourceUtil {
             GenerateStringUtil.logger.error(String.format("Resource file: %s was not found", resourceFileName));
             throw new IllegalArgumentException();
         }
+    }
+
+    private static File createTempDir() {
+        File baseDir = new File(System.getProperty("java.io.tmpdir"));
+        String baseName = System.currentTimeMillis() + "-";
+
+        for (int counter = 0; counter < TEMP_DIR_ATTEMPTS; counter++) {
+            File tempDir = new File(baseDir, "Automation-" + baseName + counter);
+            if (tempDir.mkdirs()) {
+                return tempDir;
+            }
+        }
+        throw new IllegalStateException(
+            "Failed to create directory within "
+                + TEMP_DIR_ATTEMPTS
+                + " attempts (tried "
+                + baseName
+                + "0 to "
+                + baseName
+                + (TEMP_DIR_ATTEMPTS - 1)
+                + ')');
+    }
+
+    private static File createTempDir(String path) {
+        File baseDir = new File(System.getProperty("java.io.tmpdir"));
+        String baseName = System.currentTimeMillis() + "-";
+
+        for (int counter = 0; counter < TEMP_DIR_ATTEMPTS; counter++) {
+            File tempDir = new File(baseDir, "Automation-" + baseName + counter + File.separator + path);
+            if (tempDir.mkdirs()) {
+                return tempDir;
+            }
+        }
+        throw new IllegalStateException(
+            "Failed to create directory within "
+                + TEMP_DIR_ATTEMPTS
+                + " attempts (tried "
+                + baseName
+                + "0 to "
+                + baseName
+                + (TEMP_DIR_ATTEMPTS - 1)
+                + ')');
     }
 }
