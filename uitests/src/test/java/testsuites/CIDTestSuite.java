fcd4bfc47dc4aaad8a1938331f111cc203eaaca9
cfrith <ciene.frith@gmail.com>
2020-05-28 09:43:42 +0100

    commented tests;

--- uitests/src/test/java/testsuites/CIDTestSuite.java
+++ uitests/src/test/java/testsuites/CIDTestSuite.java
@@ -3,58 +3,17 @@ package testsuites;
 import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.CategorySuiteRunner;
 
-import compare.AddScenarioTests;
-import compare.DeleteComparisonTests;
-import compare.EditPublicComparisonTests;
-import compare.PublishComparisonTests;
-import compare.SaveAsComparisonTests;
-import evaluate.AssemblyUploadTests;
-import evaluate.CostAllCadTests;
-import evaluate.DeletePrivateScenarioTests;
-import evaluate.DeletePublicScenarioTests;
-import evaluate.DeleteScenarioIterationsTests;
 import evaluate.ListOfVPETests;
-import evaluate.ListProcessGroupTests;
-import evaluate.NewScenarioNameTests;
-import evaluate.ProcessGroupsTests;
-import evaluate.ProcessRoutingTests;
-import evaluate.PsoEditTests;
-import evaluate.PublishExistingCostedTests;
-import evaluate.PublishNewCostedTests;
-import evaluate.ReCostScenarioTests;
-import evaluate.ReferencePanelTests;
-import evaluate.RevertScenarioTests;
-import evaluate.SecondaryProcessTests;
-import evaluate.TwoModelMachiningTests;
-import evaluate.designguidance.dtc.DTCCastingTests;
-import evaluate.designguidance.dtc.DTCMachiningTests;
-import evaluate.designguidance.dtc.DTCPlasticMouldingTests;
-import evaluate.designguidance.dtc.SheetMetalDTCTests;
-import evaluate.designguidance.failures.FailuresWarningsTests;
-import evaluate.designguidance.geometry.GeometryTests;
-import evaluate.designguidance.thread.ThreadTests;
-import evaluate.designguidance.tolerance.ToleranceTests;
-import evaluate.materialutilization.ChangeMaterialSelectionTests;
-import evaluate.materialutilization.ChangeStockSelectionTests;
-import evaluate.materialutilization.MaterialPMITests;
-import evaluate.materialutilization.MaterialStockTests;
-import evaluate.materialutilization.PartNestingTests;
-import explore.ActionsTests;
-import explore.FilterCriteriaTests;
-import explore.PreviewPanelTests;
-import explore.TableHeadersTests;
-import login.LoginTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import settings.SettingsTests;
 import testsuites.suiteinterface.SmokeTests;
 
 @ProjectRunID("177")
 @RunWith(CategorySuiteRunner.class)
 @Categories.ExcludeCategory(SmokeTests.class)
 @Suite.SuiteClasses({
-    AddScenarioTests.class,
+    /*ddScenarioTests.class,
     DeleteComparisonTests.class,
     EditPublicComparisonTests.class,
     DTCCastingTests.class,
@@ -71,9 +30,9 @@ import testsuites.suiteinterface.SmokeTests;
     CostAllCadTests.class,
     DeletePrivateScenarioTests.class,
     DeletePublicScenarioTests.class,
-    DeleteScenarioIterationsTests.class,
+    DeleteScenarioIterationsTests.class,*/
     ListOfVPETests.class,
-    ListProcessGroupTests.class,
+    /*ListProcessGroupTests.class,
     NewScenarioNameTests.class,
     ProcessGroupsTests.class,
     ProcessRoutingTests.class,
@@ -95,7 +54,7 @@ import testsuites.suiteinterface.SmokeTests;
     ReferencePanelTests.class,
     MaterialPMITests.class,
     RevertScenarioTests.class,
-    TwoModelMachiningTests.class,
+    TwoModelMachiningTests.class,*/
 })
 public class CIDTestSuite {
 }
