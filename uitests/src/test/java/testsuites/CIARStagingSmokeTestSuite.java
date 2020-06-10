53d3db1a1dc470e75514b9c5fbc07c2d3bf9da4f
Ben <bhegan@apriori.com>
2020-06-10 16:22:20 +0100

    Removed some tests from suite

--- uitests/src/test/java/testsuites/CIARStagingSmokeTestSuite.java
+++ uitests/src/test/java/testsuites/CIARStagingSmokeTestSuite.java
@@ -4,11 +4,6 @@ import com.apriori.utils.runner.CategorySuiteRunner;
 
 import ciadmintests.navigation.NavigationTests;
 import cireporttests.login.LoginTests;
-import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcComparisonReportTests;
-import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcDetailsReportTests;
-import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcReportTests;
-import cireporttests.ootbreports.dtcmetrics.machiningdtc.MachiningDtcReportTests;
-import cireporttests.ootbreports.general.assemblydetails.AssemblyDetailsReportTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -19,12 +14,7 @@ import testsuites.suiteinterface.CIARStagingSmokeTest;
 @Suite.SuiteClasses({
     NavigationTests.class,
     cireporttests.navigation.NavigationTests.class,
-    LoginTests.class,
-    AssemblyDetailsReportTests.class,
-    MachiningDtcReportTests.class,
-    CastingDtcReportTests.class,
-    CastingDtcDetailsReportTests.class,
-    CastingDtcComparisonReportTests.class
+    LoginTests.class
 })
 
 public class CIARStagingSmokeTestSuite {
