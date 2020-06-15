510bdcb10361faba9b19d0024d842ec273ad3057
vzarovnyi <vzarovnyi@apriori.com>
2020-06-15 17:34:15 +0300

    Migrated FMS data

---
+++ microservices/fms/src/main/java/com.apriori.fms/controller/FileManagementController.java
@@ -0,0 +1,70 @@
+package com.apriori.fms.controller;
+
+import com.apriori.fms.entity.response.FileResponse;
+import com.apriori.fms.entity.response.FilesResponse;
+import com.apriori.utils.FileResourceUtil;
+import com.apriori.utils.constants.Constants;
+import com.apriori.utils.http.builder.common.entity.RequestEntity;
+import com.apriori.utils.http.builder.dao.GenericRequestUtil;
+import com.apriori.utils.http.builder.service.RequestAreaApi;
+import com.apriori.utils.http.utils.FormParams;
+import com.apriori.utils.http.utils.MultiPartFiles;
+import com.apriori.utils.http.utils.ResponseWrapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileManagementService {
+
+    private static String finalUrl = "https://%s/files/";
+
+    public static ResponseWrapper<FilesResponse> getFiles(String token) {
+        return getFileInfo(FilesResponse.class,  Constants.getFmsServiceHost(), token, null);
+    }
+
+    public static ResponseWrapper<FileResponse> getFileByIdentity(String token, String fileIdentity) {
+        return getFileInfo(FileResponse.class,  Constants.getFmsServiceHost(), token, fileIdentity);
+    }
+
+    public static <T> ResponseWrapper<T> getFileInfo(Class klass, String url, String token,  String fileIdentity) {
+        String requestUrl = String.format(finalUrl, url);
+
+        if (fileIdentity != null) {
+            requestUrl = requestUrl.concat(fileIdentity);
+        }
+
+        RequestEntity requestEntity = RequestEntity.init(requestUrl, klass)
+                .setHeaders(initHeaders(token, false));
+
+        return  GenericRequestUtil.get(requestEntity, new RequestAreaApi());
+    }
+
+
+    public static ResponseWrapper<FileResponse> uploadFile(String token, String fileName) {
+
+        RequestEntity requestEntity = RequestEntity.init(String.format(finalUrl, Constants.getFmsServiceHost()), FileResponse.class)
+                .setHeaders(initHeaders(token, true))
+                .setMultiPartFiles(new MultiPartFiles()
+                        .use("data", FileResourceUtil.getResourceAsFile(fileName))
+                )
+                .setFormParams(new FormParams()
+                        .use("filename", fileName)
+                        .use("folder", "QAAutomationFolder")
+                );
+
+        return GenericRequestUtil.postMultipart(requestEntity, new RequestAreaApi());
+    }
+
+    private static Map<String, String> initHeaders(String token, boolean addMultiPartFile) {
+        Map<String, String> headers = new HashMap<String, String>() {{
+                put("Authorization", "Bearer " + token);
+                put("ap-cloud-context", Constants.getAtsAuthTargetCloudContext());
+            }};
+
+        if (addMultiPartFile) {
+            headers.put("Content-Type", "multipart/form-data");
+        }
+        return headers;
+    }
+
+}
