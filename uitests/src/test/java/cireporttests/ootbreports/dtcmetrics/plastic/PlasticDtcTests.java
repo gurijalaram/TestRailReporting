ab472a616c6cf2a050289450ed236f66b2bef3f2
Ben <bhegan@apriori.com>
2020-06-30 16:58:59 +0100

    Created test file

---
+++ uitests/src/test/java/cireporttests/ootbreports/dtcmetrics/plastic/PlasticDtcTests.java
@@ -0,0 +1,35 @@
+package cireporttests.ootbreports.dtcmetrics.plastic;
+
+import com.apriori.pageobjects.reports.pages.login.ReportsLoginPage;
+import com.apriori.pageobjects.reports.pages.view.ViewRepositoryPage;
+import com.apriori.pageobjects.reports.pages.view.enums.CastingReportsEnum;
+import com.apriori.utils.TestRail;
+import com.apriori.utils.web.driver.TestBase;
+import io.qameta.allure.Description;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PlasticDtcTests extends TestBase {
+
+    private ViewRepositoryPage repository;
+
+    public PlasticDtcTests() { super(); }
+
+    @Test
+    @TestRail(testCaseId = "1343")
+    @Description("Test Plastic DTC Reports Availability")
+    public void testPlasticDtcReportAvailability() {
+        repository = new ReportsLoginPage(driver)
+            .login()
+            .navigateToViewRepositoryPage()
+            .navigateToCastingFolder();
+
+        CastingReportsEnum[] reportNames = CastingReportsEnum.values();
+        for (CastingReportsEnum report : reportNames) {
+            assertThat(repository.getReportName(report.getReportName()), is(equalTo(report.getReportName())));
+        }
+    }
+}
