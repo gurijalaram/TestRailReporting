b6033c79107c9046b97ef50eca1f94b314dc2e96
cfrith <ciene.frith@gmail.com>
2020-07-02 14:38:28 +0100

    updated javadocs;

--- utils/src/main/java/com/apriori/utils/FileResourceUtil.java
+++ utils/src/main/java/com/apriori/utils/FileResourceUtil.java
@@ -48,7 +48,7 @@ public class FileResourceUtil {
     /**
      * Gets resource file from specified path
      *
-     * Subfolders should be separated by a comma eg. cad-files, files
+     * Subfolders should be separated by a comma eg. a folder structure of [cad-file > multipartfiles > files] would be represented as "cad-files, multipartfiles, files"
      *
      * @param fileName - the file name
      * @return file object
