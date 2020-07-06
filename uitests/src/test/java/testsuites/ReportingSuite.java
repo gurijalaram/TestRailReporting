7c8dd24bcbf7fc09f8187c2a040ea2dfb213fa72
Ben <bhegan@apriori.com>
2020-07-06 11:06:49 +0100

    Checkstyle issues fixed

--- uitests/src/test/java/testsuites/ReportingSuite.java
+++ uitests/src/test/java/testsuites/ReportingSuite.java
@@ -1,16 +1,16 @@
 package testsuites;
 
-import ciadmintests.navigation.AdminNavigationTests;
-import cireporttests.ootbreports.dtcmetrics.plastic.PlasticDtcTests;
 import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.ConcurrentSuiteRunner;
 
+import ciadmintests.navigation.AdminNavigationTests;
 import cireporttests.login.LoginTests;
 import cireporttests.navigation.ReportsNavigationTests;
 import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcComparisonReportTests;
 import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcDetailsReportTests;
 import cireporttests.ootbreports.dtcmetrics.casting.CastingDtcReportTests;
 import cireporttests.ootbreports.dtcmetrics.machiningdtc.MachiningDtcReportTests;
+import cireporttests.ootbreports.dtcmetrics.plastic.PlasticDtcTests;
 import cireporttests.ootbreports.general.assemblydetails.AssemblyDetailsReportTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
