6181d5391370ea26ce868cd02913d06c5247189b
bhegan613 <57667693+bhegan613@users.noreply.github.com>
2020-07-15 13:40:17 +0100

    Updated ExportSetEnum.java

--- uibase/src/main/java/com/apriori/pageobjects/reports/pages/view/enums/ExportSetEnum.java
+++ uibase/src/main/java/com/apriori/pageobjects/reports/pages/view/enums/ExportSetEnum.java
@@ -1,8 +1,8 @@
 package com.apriori.pageobjects.reports.pages.view.enums;
 
 public enum ExportSetEnum {
-    TOP_LEVEL("top-level"),
-    PISTON_ASSEMBLY("Piston Assembly"),
+    TOP_LEVEL("-top-level"),
+    PISTON_ASSEMBLY("-Piston Assembly"),
     MACHINING_DTC_DATASET("DTC_MachiningDataset"),
     CASTING_DTC("DTC-Casting"),
     ROLL_UP_A("ROLL-UP A");
