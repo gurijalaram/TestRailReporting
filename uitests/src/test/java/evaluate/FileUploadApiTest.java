36ab1ed9693d5691bc4108bc05bd58fd01291fc4
cfrith <ciene.frith@gmail.com>
2020-07-30 16:41:29 +0100

    added pragmatists library for parameterization; modified test;

--- uitests/src/test/java/evaluate/FileUploadApiTest.java
+++ uitests/src/test/java/evaluate/FileUploadApiTest.java
@@ -9,17 +9,19 @@ import com.apriori.utils.FileUploadResources;
 import com.apriori.utils.json.utils.JsonManager;
 
 import io.qameta.allure.Description;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.IdentityMapper;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.Reader;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
+@RunWith(JUnitParamsRunner.class)
 public class FileUploadApiTest extends TestUtil {
 
     private static PropertyStore propertyStore;
@@ -33,23 +35,29 @@ public class FileUploadApiTest extends TestUtil {
     }
 
     @Test
+    @FileParameters(value = "classpath:test-parts2.csv", mapper = CustomMapper.class)
     @Description("Upload, cost and publish a part using CID API")
-    public void createFileUpload() {
+    public void createFileUpload(String fileName, String scenarioName, String processGroup, String materialName) {
         Object fileObject = JsonManager.deserializeJsonFromFile(FileResourceUtil.getResourceAsFile("CreatePartData.json").getPath(), NewPartRequest.class);
 
-        List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(FileResourceUtil.getResourceAsFile("test-parts2.csv")))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",(?=([^\\\"]|\\\"[^\\\"]*\\\")*$)");
-                records.add(Arrays.asList(values));
-            }
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+        new FileUploadResources().uploadCostPublishApi(token, fileObject, fileName, scenarioName, processGroup, materialName);
+    }
 
-        for (List<String> record : records) {
-            new FileUploadResources().uploadCostPublishApi(token, fileObject, record.get(0), record.get(1), record.get(2), record.get(3).replace("\"", ""));
+    public static class CustomMapper extends IdentityMapper {
+        @Override
+        public Object[] map(Reader reader) {
+            Object[] map = super.map(reader);
+            List<Object> result = new LinkedList<>();
+            for (Object lineObj : map) {
+                String line = lineObj.toString();
+                String[] index = line.split(",(?=([^\\\"]|\\\"[^\\\"]*\\\")*$)");
+                String fileName = index[0].replace("\"", "");
+                String scenarioName = index[1].replace("\"", "");
+                String processGroup = index[2].replace("\"", "");
+                String materialName = index[3].replace("\"", "");
+                result.add(new Object[] {fileName, scenarioName, processGroup, materialName});
+            }
+            return result.toArray();
         }
     }
 }
