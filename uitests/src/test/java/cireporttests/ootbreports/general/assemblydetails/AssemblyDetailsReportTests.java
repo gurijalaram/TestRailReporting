0499a8c5e93d002438c1ef1b336ffc3e55051d42
cfrith <ciene.frith@gmail.com>
2020-08-13 11:49:49 +0100

    fixed issues after merge;

--- uitests/src/test/java/cireporttests/ootbreports/general/assemblydetails/AssemblyDetailsReportTests.java
+++ uitests/src/test/java/cireporttests/ootbreports/general/assemblydetails/AssemblyDetailsReportTests.java
@@ -5,8 +5,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import com.apriori.pageobjects.pages.evaluate.ComponentsPage;
-import com.apriori.pageobjects.pages.explore.ExplorePage;
 import com.apriori.pageobjects.reports.pages.login.ReportsLoginPage;
 import com.apriori.pageobjects.reports.pages.view.reports.AssemblyDetailsReportPage;
 import com.apriori.pageobjects.reports.pages.view.reports.GenericReportPage;
