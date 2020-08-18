8cff0975b5418d6e21e4db4ae6f463c43003218e
cfrith <ciene.frith@gmail.com>
2020-08-18 12:53:37 +0100

    added method to import a file; added locators;

--- cas/src/main/java/newcustomer/users/ImportPage.java
+++ cas/src/main/java/newcustomer/users/ImportPage.java
@@ -4,14 +4,27 @@ import com.apriori.utils.PageUtils;
 
 import customeradmin.NavToolbar;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+
 public class ImportPage extends LoadableComponent<ImportPage> {
 
     private final Logger logger = LoggerFactory.getLogger(ImportPage.class);
+
+    @FindBy(css = "input[type='file']")
+    private WebElement fileInput;
+
+    @FindBy(xpath = "//button[.='Load']")
+    private WebElement loadButton;
+
+    @FindBy(xpath = "//button[.='Refresh']")
+    private WebElement refreshButton;
 
     private WebDriver driver;
     private PageUtils pageUtils;
@@ -33,6 +46,17 @@ public class ImportPage extends LoadableComponent<ImportPage> {
 
     @Override
     protected void isLoaded() throws Error {
+        pageUtils.waitForElementAppear(loadButton);
+    }
 
+    /**
+     * Import File
+     *
+     * @param filePath - the file path
+     * @return current page object
+     */
+    public ImportPage importFile(File filePath) {
+        fileInput.sendKeys(filePath.getAbsolutePath());
+        return this;
     }
 }
