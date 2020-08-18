08a5a1ddbeed245cae84a86e5f284bc74022edb1
cfrith <ciene.frith@gmail.com>
2020-08-18 17:15:26 +0100

    reverting changes;

--- cid/src/test/java/evaluate/FileUploadApiTest.java
+++ cid/src/test/java/evaluate/FileUploadApiTest.java
@@ -7,6 +7,7 @@ import com.apriori.apibase.utils.FileUploadResources;
 import com.apriori.apibase.utils.TestUtil;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.json.utils.JsonManager;
+import com.apriori.utils.users.UserUtil;
 
 import io.qameta.allure.Description;
 import junitparams.FileParameters;
@@ -26,7 +27,7 @@ public class FileUploadApiTest extends TestUtil {
 
     private static PropertyStore propertyStore;
 
-    private final HashMap<String, String> token = new APIAuthentication().initAuthorizationHeaderNoContent("cfrith@apriori.com");
+    private final HashMap<String, String> token = new APIAuthentication().initAuthorizationHeaderNoContent(UserUtil.getUser().getUsername());
 
     @BeforeClass
     public static void testSetup() {
@@ -35,7 +36,7 @@ public class FileUploadApiTest extends TestUtil {
     }
 
     @Test
-    @FileParameters(value = "classpath:test-parts2.csv", mapper = CustomMapper.class, encoding = "ISO-8859-1")
+    @FileParameters(value = "classpath:auto_api_upload.csv", mapper = CustomMapper.class, encoding = "ISO-8859-1")
     @Description("Upload, cost and publish a part using CID API")
     public void createDataUploadApi(String fileName, String scenarioName, String processGroup) {
         Object fileObject = JsonManager.deserializeJsonFromFile(FileResourceUtil.getResourceAsFile("CreatePartData.json").getPath(), NewPartRequest.class);
