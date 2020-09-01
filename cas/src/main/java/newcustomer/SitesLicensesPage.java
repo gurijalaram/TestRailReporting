b44752f3fdb46d015d7d5a2230b26ee80d5be82c
cfrith <ciene.frith@gmail.com>
2020-09-01 10:44:57 +0100

    added new locators and method to select sublicense and get submodules;

--- cas/src/main/java/newcustomer/SitesLicensesPage.java
+++ cas/src/main/java/newcustomer/SitesLicensesPage.java
@@ -3,6 +3,7 @@ package newcustomer;
 import com.apriori.utils.PageUtils;
 
 import customeradmin.NavToolbar;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,12 +12,18 @@ import org.openqa.selenium.support.ui.LoadableComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SitesLicensesPage extends LoadableComponent<SitesLicensesPage> {
 
     private final Logger logger = LoggerFactory.getLogger(SitesLicensesPage.class);
 
     @FindBy(xpath = "//h5[.='Loaded Licenses']")
     private WebElement licenseHeader;
+
+    @FindBy(css = "ul[class='list-group list-group-flush'] li")
+    private List<WebElement> submodules;
 
     private WebDriver driver;
     private PageUtils pageUtils;
@@ -39,5 +46,25 @@ public class SitesLicensesPage extends LoadableComponent<SitesLicensesPage> {
     @Override
     protected void isLoaded() throws Error {
         pageUtils.waitForElementAppear(licenseHeader);
+    }
+
+    /**
+     * Selects the sub-license name
+     *
+     * @param licenseName - license name
+     * @return current page object
+     */
+    public SitesLicensesPage selectSubLicense(String licenseName) {
+        By license = By.xpath(String.format("//td[.='%s']", licenseName));
+        pageUtils.waitForElementAndClick(license);
+        return this;
+    }
+
+    /**
+     * Gets list of sub modules
+     * @return list
+     */
+    public List<String> getListOfSubModules() {
+        return submodules.stream().map(WebElement::getText).collect(Collectors.toList());
     }
 }
