66624820b02a346a8c5752e60a1c9880c52c469f
cfrith <ciene.frith@gmail.com>
2020-09-07 13:06:03 +0100

    modified TestBase;

--- utils/src/main/java/com/apriori/utils/web/driver/TestBase.java
+++ utils/src/main/java/com/apriori/utils/web/driver/TestBase.java
@@ -99,7 +99,7 @@ public class TestBase extends TestHelper {
 
     private String getBrowserType(String browserProperty) {
         if (browserProperty == null || browserProperty.isEmpty()) {
-            return "firefox";
+            return "chrome";
         }
         return StringUtils.lowerCase(browserProperty);
     }
