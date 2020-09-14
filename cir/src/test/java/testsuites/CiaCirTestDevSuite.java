a98fb1fb06035509567bed6d06b27197e2a35594
Ben <bhegan@apriori.com>
2020-09-14 15:17:27 +0100

    Added Machining class to suite

--- cir/src/test/java/testsuites/CiaCirTestDevSuite.java
+++ cir/src/test/java/testsuites/CiaCirTestDevSuite.java
@@ -3,6 +3,7 @@ package testsuites;
 import com.apriori.utils.runner.CategorySuiteRunner;
 
 import ootbreports.dtcmetrics.casting.CastingDtcReportTests;
+import ootbreports.dtcmetrics.machiningdtc.MachiningDtcReportTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -11,7 +12,8 @@ import testsuites.suiteinterface.CiaCirTestDevTest;
 @RunWith(CategorySuiteRunner.class)
 @Categories.IncludeCategory(CiaCirTestDevTest.class)
 @Suite.SuiteClasses({
-        CastingDtcReportTests.class
+        CastingDtcReportTests.class,
+        MachiningDtcReportTests.class
 })
 
 public class CiaCirTestDevSuite {
