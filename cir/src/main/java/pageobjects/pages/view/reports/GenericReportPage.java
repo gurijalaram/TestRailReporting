01b9995dfcbceb860e5a60bc1408cfec57e7ab65
Ben <bhegan@apriori.com>
2020-09-18 16:11:55 +0100

    Removed unused import

--- cir/src/main/java/pageobjects/pages/view/reports/GenericReportPage.java
+++ cir/src/main/java/pageobjects/pages/view/reports/GenericReportPage.java
@@ -1,6 +1,5 @@
 package pageobjects.pages.view.reports;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
