6a9a0293c7caaf0f41ba8f4933a79008a85f6e8d
Ben <bhegan@apriori.com>
2020-10-21 13:10:53 +0100

    Removed TE reference

--- utils/src/main/java/com/apriori/utils/constants/Constants.java
+++ utils/src/main/java/com/apriori/utils/constants/Constants.java
@@ -67,8 +67,8 @@ public class Constants {
             ProcessGroupEnum.TWO_MODEL_MACHINING.getProcessGroup()
     );
 
-    public static final String CID_TE_HEADER_TEXT = "CI Design (TE)";
     public static final String CID_AUT_HEADER_TEXT = "CI Design AUTOMATION";
+    public static final String CID_QA_HEADER_TEXT = "CI Design QUAIL";
 
     public static final String ARROW_DOWN = "arrow_down";
     public static final String PAGE_DOWN = "page_down";
