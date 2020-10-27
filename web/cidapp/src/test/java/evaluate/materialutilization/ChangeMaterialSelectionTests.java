69ec5b5225c448a0b4fee00fb209d686705ed5d2
moyaparker <mparker@apriori.com>
2020-10-27 11:09:28 +0000

    added test class to suite

--- web/cidapp/src/test/java/evaluate/materialutilization/ChangeMaterialSelectionTests.java
+++ web/cidapp/src/test/java/evaluate/materialutilization/ChangeMaterialSelectionTests.java
@@ -35,76 +35,6 @@ public class ChangeMaterialSelectionTests extends TestBase {
     }
 
     @Test
-    @Category(SmokeTests.class)
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Additive Manufacturing, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestAdditive() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("BasicScenario_Additive.prt.1");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.ADDITIVE_MANUFACTURING.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Aluminum AlSi10Mg"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("Duraform ProX GF")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Duraform ProX GF"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Bar & Tube Fab, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestBarandTubeFab() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("BasicScenario_BarAndTube.prt");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.BAR_TUBE_FAB.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Steel, Hot Worked, AISI 1010"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("Stainless Steel, Stock, AISI 316")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Stainless Steel, Stock, AISI 316"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Casting, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestCasting() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("Casting.prt");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.CASTING.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Aluminum, Cast, ANSI AL380.0"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("ISO JMB 800-1")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("ISO JMB 800-1"), is(true));
-    }
-
-    @Test
     @TestRail(testCaseId = {"864", "866", "867"})
     @Description("Test making changes to the Material for Sand Casting, the change is respected and the scenario can be cost")
     public void changeMaterialSelectionTestSandCasting() {
@@ -115,6 +45,10 @@ public class ChangeMaterialSelectionTests extends TestBase {
         evaluatePage = loginPage.login(UserUtil.getUser())
             .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
             .selectProcessGroup(ProcessGroupEnum.CASTING_SAND.getProcessGroup())
+            .openMaterialSelectorTable()
+            .search("ANSI AL380")
+            .selectMaterial("Aluminum, Cast, ANSI AL380.0")
+            .select()
             .costScenario(5);
 
         assertThat(evaluatePage.getMaterialInfo("Aluminum, Cast, ANSI AL380.0"), is(true));
@@ -152,29 +86,6 @@ public class ChangeMaterialSelectionTests extends TestBase {
 
     @Test
     @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Forging, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestForging() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("BasicScenario_Forging.stp");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.FORGING.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Steel, Cold Worked, AISI 1010"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("Titanium, Ti-5Al-2.5Sn")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Titanium, Ti-5Al-2.5Sn"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
     @Description("Test making changes to the Material for Plastic Molding, the change is respected and the scenario can be cost")
     public void changeMaterialSelectionTestPlasticMolding() {
 
@@ -194,75 +105,6 @@ public class ChangeMaterialSelectionTests extends TestBase {
             .costScenario();
 
         assertThat(evaluatePage.getMaterialInfo("PET 30% Glass"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Powder Metal, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestPowderMetal() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("Powder Metal.stp");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.POWDER_METAL.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("F-0005"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("FLN2-4405")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("FLN2-4405"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Rapid Prototyping, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestRapidPrototyping() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("Plastic moulded cap DFM.CATPart");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.RAPID_PROTOTYPING.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Default"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("870 Black")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("870 Black"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Roto & Blow Molding, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestRotoBlowMolding() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("Plastic moulded cap DFM.CATPart");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.ROTO_BLOW_MOLDING.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Polyethylene, High Density (HDPE)"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("Nylon, Type 46")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Nylon, Type 46"), is(true));
     }
 
     @Test
@@ -288,75 +130,6 @@ public class ChangeMaterialSelectionTests extends TestBase {
         assertThat(evaluatePage.getMaterialInfo("Inconel 625"), is(true));
     }
 
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Sheet Metal - Hydroforming, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestSheetMetalHydroforming() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("Hydroform Sample Banana.stp");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL_HYDROFORMING.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Aluminum, Stock, ANSI 2017"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("Copper, Stock, UNS C11000")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Copper, Stock, UNS C11000"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Sheet Metal Transfer Die, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestSheetMetalTransferDie() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("bracket_basic.prt");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.SHEET_METAL_TRANSFER_DIE.getProcessGroup())
-            .selectVPE(VPEEnum.APRIORI_USA.getVpe())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Steel, Cold Worked, AISI 1020"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("C60E")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("C60E"), is(true));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"864", "866", "867"})
-    @Description("Test making changes to the Material for Sheet Plastic, the change is respected and the scenario can be cost")
-    public void changeMaterialSelectionTestSheetPlastic() {
-
-        resourceFile = FileResourceUtil.getResourceAsFile("2038646_Sheet_plastic.prt.1");
-
-        loginPage = new CidAppLoginPage(driver);
-        evaluatePage = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
-            .selectProcessGroup(ProcessGroupEnum.SHEET_PLASTIC.getProcessGroup())
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Polyethylene, HDPE Extrusion Sheet"), is(true));
-
-        evaluatePage.openMaterialSelectorTable()
-            .selectMaterial("Acrylic, PMMA")
-            .select()
-            .costScenario();
-
-        assertThat(evaluatePage.getMaterialInfo("Acrylic, PMMA"), is(true));
-    }
 
 //    @Test
 //    @Category(SmokeTests.class)
