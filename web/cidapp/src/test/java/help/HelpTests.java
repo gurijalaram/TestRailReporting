7d8a7d77427a7035b75759a0300cdfb308d1b378
cfrith <ciene.frith@gmail.com>
2020-11-03 15:59:52 +0000

    ported HelpTests to newcid project;

---
+++ web/cidapp/src/test/java/help/HelpTests.java
@@ -0,0 +1,134 @@
+package help;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.apriori.utils.FileResourceUtil;
+import com.apriori.utils.GenerateStringUtil;
+import com.apriori.utils.TestRail;
+import com.apriori.utils.enums.ProcessGroupEnum;
+import com.apriori.utils.users.UserUtil;
+import com.apriori.utils.web.driver.TestBase;
+
+import io.qameta.allure.Description;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import pageobjects.evaluate.EvaluatePage;
+import pageobjects.help.HelpDocPage;
+import pageobjects.help.HelpPage;
+import pageobjects.login.CidAppLoginPage;
+import testsuites.suiteinterface.SmokeTests;
+
+import java.io.File;
+
+public class HelpTests extends TestBase {
+
+    private CidAppLoginPage loginPage;
+    private HelpPage helpPage;
+    private HelpDocPage helpDocPage;
+
+    private File resourceFile;
+
+    @Test
+    @Category(SmokeTests.class)
+    @TestRail(testCaseId = {"263"})
+    @Description("Be able to access help information in the application header")
+    public void onlineHelpTest() {
+        loginPage = new CidAppLoginPage(driver);
+        helpPage = loginPage.login(UserUtil.getUser())
+            .goToHelp()
+            .clickOnlineHelp();
+
+        assertThat(helpPage.getChildPageTitle(), containsString("Cost Insight Design:User Guide"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"264"})
+    @Description("Have links to a detailed help pages in relevant areas of the UI")
+    public void moreInputsHelp() {
+
+        resourceFile = FileResourceUtil.getResourceAsFile("PowderMetalShaft.stp");
+
+        loginPage = new CidAppLoginPage(driver);
+        helpDocPage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.POWDER_METAL.getProcessGroup())
+            .costScenario()
+            .openMoreInputs()
+            .clickHelp();
+
+        assertThat(helpDocPage.getChildPageTitle(), containsString("More Inputs"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"264"})
+    @Description("Have links to a detailed help pages in relevant areas of the UI")
+    public void materialUtilHelp() {
+
+        resourceFile = FileResourceUtil.getResourceAsFile("PowderMetalShaft.stp");
+
+        loginPage = new CidAppLoginPage(driver);
+        helpDocPage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.POWDER_METAL.getProcessGroup())
+            .costScenario()
+            .openMaterialUtilization()
+            .clickHelp();
+
+        assertThat(helpDocPage.getChildPageTitle(), containsString("Material & Utilization Details"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"264"})
+    @Description("Have links to a detailed help pages in relevant areas of the UI")
+    public void designGuidanceHelp() {
+
+        resourceFile = FileResourceUtil.getResourceAsFile("PowderMetalShaft.stp");
+
+        loginPage = new CidAppLoginPage(driver);
+        helpDocPage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.POWDER_METAL.getProcessGroup())
+            .costScenario()
+            .openDesignGuidance()
+            .clickHelp();
+
+        assertThat(helpDocPage.getChildPageTitle(), containsString("Design Guidance Details"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"264"})
+    @Description("Have links to a detailed help pages in relevant areas of the UI")
+    public void processDetailsHelp() {
+
+        resourceFile = FileResourceUtil.getResourceAsFile("PowderMetalShaft.stp");
+
+        loginPage = new CidAppLoginPage(driver);
+        helpDocPage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.POWDER_METAL.getProcessGroup())
+            .costScenario()
+            .openProcessDetails()
+            .clickHelp();
+
+        assertThat(helpDocPage.getChildPageTitle(), containsString("Process Details"));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"264"})
+    @Description("Have links to a detailed help pages in relevant areas of the UI")
+    public void costResultsHelp() {
+
+        resourceFile = FileResourceUtil.getResourceAsFile("PowderMetalShaft.stp");
+
+        loginPage = new CidAppLoginPage(driver);
+        helpDocPage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndOk(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.POWDER_METAL.getProcessGroup())
+            .costScenario()
+            .openCostDetails()
+            .clickHelp();
+
+        assertThat(helpDocPage.getChildPageTitle(), containsString("Cost Details"));
+    }
+}
