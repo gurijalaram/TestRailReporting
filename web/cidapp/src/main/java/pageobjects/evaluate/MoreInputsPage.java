9b29bcdd50ef73144fa9a21aa1a7e78eba708c31
cfrith <ciene.frith@gmail.com>
2020-11-03 16:30:51 +0000

    added MoreInputsPage object;

---
+++ web/cidapp/src/main/java/pageobjects/evaluate/MoreInputsPage.java
@@ -0,0 +1,38 @@
+package pageobjects.evaluate;
+
+import com.apriori.utils.PageUtils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author cfrith
+ */
+
+public class MoreInputsPage extends LoadableComponent<MoreInputsPage> {
+
+    private final Logger logger = LoggerFactory.getLogger(MoreInputsPage.class);
+
+    private WebDriver driver;
+    private PageUtils pageUtils;
+
+    public MoreInputsPage(WebDriver driver) {
+        this.driver = driver;
+        this.pageUtils = new PageUtils(driver);
+        logger.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
+        PageFactory.initElements(driver, this);
+        this.get();
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+    }
+}
