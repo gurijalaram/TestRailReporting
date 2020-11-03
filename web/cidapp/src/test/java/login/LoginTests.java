daf2715e81a7968d89ceb4b31f39eb94a50d6534
cfrith <ciene.frith@gmail.com>
2020-11-03 12:24:13 +0000

    ported welcome message test;

--- web/cidapp/src/test/java/login/LoginTests.java
+++ web/cidapp/src/test/java/login/LoginTests.java
@@ -6,6 +6,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.apriori.utils.GenerateStringUtil;
+import com.apriori.utils.TestRail;
 import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
@@ -14,11 +15,13 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import pageobjects.explore.ExplorePage;
 import pageobjects.login.CidAppLoginPage;
+import pageobjects.login.PrivacyPolicyPage;
 import testsuites.suiteinterface.SmokeTests;
 
 public class LoginTests extends TestBase {
 
     private static String loginPageErrorMessage = "We're sorry, something went wrong when attempting to log in.";
+    private PrivacyPolicyPage privacyPolicyPage;
 
     public LoginTests() {
         super();
@@ -83,4 +86,57 @@ public class LoginTests extends TestBase {
             "Proactively notify your team of manufacturability issues and enable them to optimize their designs faster."));
         assertThat(loginPage.isLogoDisplayed(), is(true));
     }
+
+    /*@Category(SmokeTests.class)
+    @Test
+    @TestRail(testCaseId = {"1574"})
+    @Description("Validate forgotten password link")
+    public void forgotPassword() {
+
+        loginPage = new CidAppLoginPage(driver);
+        forgottenPasswordPage = loginPage.forgottenPassword();
+
+        assertThat(forgottenPasswordPage.getResetPassword(), containsString("Reset your password"));
+    }*/
+
+    @Category(SmokeTests.class)
+    @Test
+    @TestRail(testCaseId = {"1575"})
+    @Description("Validate Welcome Message")
+    public void welcomeMessage() {
+
+        loginPage = new CidAppLoginPage(driver);
+        assertThat(loginPage.getWelcomeText(), containsString("Welcome! This login page provides access to aPriori's web applications, support portal and customer community. Access to these web services is available only to aPriori licensed customers, partners and employees"));
+
+        privacyPolicyPage = loginPage.privacyPolicy();
+
+        assertThat(privacyPolicyPage.getChildWindowURL(), containsString("https://www.apriori.com/privacy-policy"));
+        assertThat(privacyPolicyPage.getPageHeading(), containsString("APRIORI TECHNOLOGIES, INC. PRIVACY POLICY"));
+    }
+
+    /*@Category({CustomerSmokeTests.class, SmokeTests.class})
+    @Test
+    @TestRail(testCaseId = {"1590", "1583", "1180"})
+    @Description("Validate CAD association remains and attributes can be updated between CID sessions.")
+    public void cadConnectionRemains() {
+
+        resourceFile = FileResourceUtil.getResourceAsFile("225_gasket-1-solid1.prt.1");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+
+        loginPage = new CidAppLoginPage(driver);
+        loginPage.login(UserUtil.getUser())
+            .uploadComponentAndOk(scenarioName, resourceFile, EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.STOCK_MACHINING.getProcessGroup())
+            .costScenario()
+            .publishScenario(PublishPage.class)
+            .selectPublishButton()
+            .openAdminDropdown()
+            .selectLogOut();
+
+        loginPage = new CidAppLoginPage(driver);
+        evaluatePage = loginPage.login(UserUtil.getUser())
+            .openScenario(scenarioName, "225_gasket-1-solid1");
+
+        assertThat(evaluatePage.isCADConnectionStatus(), is("CAD file connected"));
+    }*/
 }
