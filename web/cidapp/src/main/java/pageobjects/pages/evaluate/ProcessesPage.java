0ec6778948a98c0b2e360499723d33f3fb34cf1e
cfrith <ciene.frith@gmail.com>
2020-11-04 10:24:24 +0000

    added ProcessesPage object;

---
+++ web/cidapp/src/main/java/pageobjects/pages/evaluate/ProcessesPage.java
@@ -0,0 +1,35 @@
+package pageobjects.pages.evaluate;
+
+import com.apriori.utils.PageUtils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProcessesPage extends LoadableComponent<ProcessesPage> {
+
+    private final Logger logger = LoggerFactory.getLogger(ProcessesPage.class);
+
+    private WebDriver driver;
+    private PageUtils pageUtils;
+
+    public ProcessesPage(WebDriver driver) {
+        this.driver = driver;
+        this.pageUtils = new PageUtils(driver);
+        logger.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
+        PageFactory.initElements(driver, this);
+        this.get();
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+
+    }
+}
