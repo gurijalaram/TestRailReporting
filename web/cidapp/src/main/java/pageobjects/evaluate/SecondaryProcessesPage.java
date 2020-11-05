ac82509c2be8127ce5278139420d34a3b1345a26
cfrith <ciene.frith@gmail.com>
2020-11-05 10:31:32 +0000

    changed method return type;

--- web/cidapp/src/main/java/pageobjects/evaluate/SecondaryProcessesPage.java
+++ web/cidapp/src/main/java/pageobjects/evaluate/SecondaryProcessesPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pageobjects.common.ModalDialogController;
 
 public class SecondaryProcessesPage extends LoadableComponent<SecondaryProcessesPage> {
 
@@ -19,10 +20,12 @@ public class SecondaryProcessesPage extends LoadableComponent<SecondaryProcesses
 
     private WebDriver driver;
     private PageUtils pageUtils;
+    private ModalDialogController modalDialogController;
 
     public SecondaryProcessesPage(WebDriver driver) {
         this.driver = driver;
         this.pageUtils = new PageUtils(driver);
+        this.modalDialogController = new ModalDialogController(driver);
         LOGGER.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
         PageFactory.initElements(driver, this);
         this.get();
@@ -36,5 +39,23 @@ public class SecondaryProcessesPage extends LoadableComponent<SecondaryProcesses
     @Override
     protected void isLoaded() throws Error {
         pageUtils.waitForElementAppear(processTree);
+    }
+
+    /**
+     * Selects the submit button
+     *
+     * @return generic page object
+     */
+    public EvaluatePage submit() {
+        return modalDialogController.submit(EvaluatePage.class);
+    }
+
+    /**
+     * Select the cancel button
+     *
+     * @return generic page object
+     */
+    public EvaluatePage cancel() {
+        return modalDialogController.cancel(EvaluatePage.class);
     }
 }
