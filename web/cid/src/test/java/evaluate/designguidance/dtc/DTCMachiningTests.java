2f800573651d2a041e9f9ff63688a00e4f6ee4e6
smccaffrey <smccaffrey@apriori.com>
2020-11-05 17:05:57 +0000

    Regression tests update

--- web/cid/src/test/java/evaluate/designguidance/dtc/DTCMachiningTests.java
+++ web/cid/src/test/java/evaluate/designguidance/dtc/DTCMachiningTests.java
@@ -119,7 +119,7 @@ public class DTCMachiningTests extends TestBase {
     @Description("Testing DTC Machining Side Milling L/D Ratio")
     public void testDTCSideMilling() {
 
-        resourceFile = FileResourceUtil.getResourceAsFile("Machining-DTC_Issue_SideMillingLengthDia3.SLDPRT");
+        resourceFile = FileResourceUtil.getResourceAsFile("Deep hole.CATPart");
         currentUser = UserUtil.getUser();
 
         loginPage = new CidLoginPage(driver);
@@ -129,7 +129,7 @@ public class DTCMachiningTests extends TestBase {
             .costScenario()
             .openDesignGuidance()
             .openGuidanceTab()
-            .selectIssueTypeAndGCD("Machining Issues", "Side Milling L/D", "CurvedWall:3");
+            .selectIssueTypeAndGCD("Machining Issues", "Side Milling L/D", "CurvedWall:6");
 
         assertThat(guidancePage.getGuidanceMessage(), containsString("Required tool exceeds the max L/D Ratio"));
     }
