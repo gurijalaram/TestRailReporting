c7148f24ec33bf32f66101201f867b7f3d6fd349
cfrith <ciene.frith@gmail.com>
2020-11-23 12:46:03 +0000

    minor formatting;

--- web/cia/src/main/java/utils/Constants.java
+++ web/cia/src/main/java/utils/Constants.java
@@ -9,10 +9,6 @@ import java.util.Properties;
 
 public class Constants {
 
-    public static final String DEFAULT_ENVIRONMENT_KEY = "env";
-    public static final String DEFAULT_ENVIRONMENT_VALUE = "cia-qa";
-    public static final String ENVIRONMENT = System.getProperty(DEFAULT_ENVIRONMENT_KEY, DEFAULT_ENVIRONMENT_VALUE);
-
     public static final String REPORTS_LAST_SUFFIX = "flow.html?_flowId=homeFlow";
     public static final String CIA_USER_GUIDE_URL_SUBSTRING = "CI_ADMIN_USER_GUIDE";
     public static final String SCENARIO_EXPORT_CHAPTER_URL_PART_ONE = "https://www.apriori.com/Collateral/Documents/English-US/online_help/apriori-platform/";
@@ -21,6 +17,9 @@ public class Constants {
     public static final String SCENARIO_EXPORT_CHAPTER_PAGE_TITLE = "2 Scenario and System Data Exports";
     public static final String REPORTS_URL_SUFFIX = "jasperserver-pro/";
 
+    public static final String DEFAULT_ENVIRONMENT_KEY = "env";
+    public static final String DEFAULT_ENVIRONMENT_VALUE = "cia-qa";
+    public static final String ENVIRONMENT = System.getProperty(DEFAULT_ENVIRONMENT_KEY, DEFAULT_ENVIRONMENT_VALUE);
     private static final Properties PROPERTIES = new Properties();
     private static final File INPUT_STREAM;
 
