0dd91d17280d085bf46363d816417c218fe33769
cfrith <ciene.frith@gmail.com>
2020-12-08 17:29:25 +0000

    modified logic in all Constants files to allow env to take a default value or a value from command line;

--- web/cid/src/main/java/com/utils/Constants.java
+++ web/cid/src/main/java/com/utils/Constants.java
@@ -11,14 +11,14 @@ public class Constants {
 
     public static final String DEFAULT_ENVIRONMENT_KEY = "env";
     public static final String DEFAULT_ENVIRONMENT_VALUE = "cid-qa";
-    public static final String ENVIRONMENT = System.getProperty(DEFAULT_ENVIRONMENT_KEY, DEFAULT_ENVIRONMENT_VALUE);
+    public static String environment;
     private static final Properties PROPERTIES = new Properties();
     private static final File INPUT_STREAM;
 
     static {
-        System.setProperty(DEFAULT_ENVIRONMENT_KEY, ENVIRONMENT);
+        environment = environment == null ? System.getProperty(DEFAULT_ENVIRONMENT_KEY, DEFAULT_ENVIRONMENT_VALUE) : System.setProperty(DEFAULT_ENVIRONMENT_KEY, environment);
 
-        INPUT_STREAM = FileResourceUtil.getResourceAsFile(DEFAULT_ENVIRONMENT_VALUE.concat(".properties"));
+        INPUT_STREAM = FileResourceUtil.getResourceAsFile(environment.concat(".properties"));
 
         try {
             PROPERTIES.load(new FileInputStream(INPUT_STREAM));
