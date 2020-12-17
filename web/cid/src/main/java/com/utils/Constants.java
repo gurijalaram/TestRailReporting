a6382a839a37c82a139d5ca9183d4f226e6d722d
cfrith <ciene.frith@gmail.com>
2020-12-17 18:37:53 +0000

    modified Constants file;

--- web/cid/src/main/java/com/utils/Constants.java
+++ web/cid/src/main/java/com/utils/Constants.java
@@ -9,16 +9,23 @@ import java.util.Properties;
 
 public class Constants {
 
+    public static final String DEFAULT_BASE_URL_KEY = "url";
     public static final String DEFAULT_ENVIRONMENT_KEY = "env";
     public static final String DEFAULT_ENVIRONMENT_VALUE = "cid-qa";
-    public static String environment;
+    public static final String EVALUATE_TOOLTIP_TEXT = "There is no active scenario.\n- Select one in Explore\nOR\n- Use New to create a new one.";
+    public static final String COMPARE_TOOLTIP_TEXT = "There is no active comparison.\n- Select one in Explore\nOR\n- Use New to create a new one.";
+    public static final String ARROW_DOWN = "arrow_down";
+    public static final String PAGE_DOWN = "page_down";
+    public static final String HORIZONTAL_SCROLL = "horizontal_scroll";
     private static final Properties PROPERTIES = new Properties();
     private static final File INPUT_STREAM;
+    public static String environment;
+    private static String baseUrl;
 
     static {
         environment = environment == null ? System.getProperty(DEFAULT_ENVIRONMENT_KEY, DEFAULT_ENVIRONMENT_VALUE) : System.setProperty(DEFAULT_ENVIRONMENT_KEY, environment);
 
-        INPUT_STREAM = FileResourceUtil.getResourceAsFile(environment.concat(".properties"));
+        INPUT_STREAM = FileResourceUtil.getResourceAsFile(DEFAULT_ENVIRONMENT_VALUE.concat(".properties"));
 
         try {
             PROPERTIES.load(new FileInputStream(INPUT_STREAM));
@@ -29,9 +36,11 @@ public class Constants {
 
     /**
      * Get default url
+     *
      * @return string
      */
     public static String getDefaultUrl() {
-        return PROPERTIES.getProperty("url.default").concat(PROPERTIES.getProperty("url.additional"));
+        baseUrl = System.getProperty(DEFAULT_BASE_URL_KEY) == null ? PROPERTIES.getProperty("url.default") : System.getProperty(DEFAULT_BASE_URL_KEY);
+        return baseUrl.concat(PROPERTIES.getProperty("url.additional"));
     }
 }
