590e3d46afdc1c7cee4fce743260d349a7b31f8a
cfrith <ciene.frith@gmail.com>
2021-01-05 12:16:57 +0000

    populated edc Constants file;

--- microservices/edc/src/main/java/com.apriori.edc/utils/Constants.java
+++ microservices/edc/src/main/java/com.apriori.edc/utils/Constants.java
@@ -1,4 +1,30 @@
 package com.apriori.edc.utils;
 
+import com.apriori.utils.FileResourceUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Constants {
+
+    public static final String DEFAULT_ENVIRONMENT_KEY = "env";
+    public static final String DEFAULT_ENVIRONMENT_VALUE = "edc";
+    public static final String ENVIRONMENT = System.getProperty(DEFAULT_ENVIRONMENT_KEY, DEFAULT_ENVIRONMENT_VALUE);
+    private static final Properties PROPERTIES = new Properties();
+    private static final File INPUT_STREAM;
+
+    static {
+        System.setProperty(DEFAULT_ENVIRONMENT_KEY, ENVIRONMENT);
+
+        INPUT_STREAM = FileResourceUtil.getResourceAsFile(DEFAULT_ENVIRONMENT_VALUE.concat(".properties"));
+
+        try {
+            PROPERTIES.load(new FileInputStream(INPUT_STREAM));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
