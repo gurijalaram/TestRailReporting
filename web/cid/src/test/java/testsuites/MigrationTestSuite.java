f0e694dc7e64ca2157362b719d4e69e7020e69b5
cfrith <ciene.frith@gmail.com>
2021-01-08 12:24:30 +0000

    added MigrationTestSuite to project; modified test to include new suite category;

---
+++ web/cid/src/test/java/testsuites/MigrationTestSuite.java
@@ -0,0 +1,31 @@
+package testsuites;
+
+
+import com.apriori.utils.runner.CategorySuiteRunner;
+
+import com.compare.EditPublicComparisonTests;
+import com.evaluate.ProcessGroupsTests;
+import com.evaluate.ProcessRoutingTests;
+import com.evaluate.SecondaryProcessTests;
+import com.evaluate.designguidance.tolerance.ToleranceTests;
+import com.explore.ActionsTests;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import testsuites.suiteinterface.MigrationTests;
+
+@RunWith(CategorySuiteRunner.class)
+@Categories.IncludeCategory(MigrationTests.class)
+@Suite.SuiteClasses({
+    ProcessGroupsTests.class,
+    ProcessRoutingTests.class,
+    EditPublicComparisonTests.class,
+    ActionsTests.class,
+    SecondaryProcessTests.class,
+    ToleranceTests.class,
+    ProcessGroupsTests.class,
+
+})
+
+public class MigrationTestSuite {
+}
