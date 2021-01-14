76f15a24a35719676253c8b16aa612ab5ef9b0f3
cfrith <ciene.frith@gmail.com>
2021-01-14 16:36:30 +0000

    updated tests after porting over to newcid...wip;

--- web/cidapp/src/test/java/com/evaluate/NewScenarioNameTests.java
+++ web/cidapp/src/test/java/com/evaluate/NewScenarioNameTests.java
@@ -1,11 +1,16 @@
 package com.evaluate;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import com.apriori.pageobjects.pages.evaluate.EvaluatePage;
 import com.apriori.pageobjects.pages.explore.ExplorePage;
 import com.apriori.pageobjects.pages.login.CidAppLoginPage;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.TestRail;
+import com.apriori.utils.enums.CostingLabelEnum;
 import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
@@ -46,78 +51,73 @@ public class NewScenarioNameTests extends TestBase {
             .enterScenarioName(testScenarioName)
             .submit(EvaluatePage.class);
 
-//        assertThat(evaluatePage.getCurrentScenarioName(testScenarioName), is(true));
+        assertThat(evaluatePage.getCurrentScenarioName(), is(testScenarioName));
     }
 
-//    @Category({CustomerSmokeTests.class, SmokeTests.class})
-//    @Test
-//    @TestRail(testCaseId = {"1576", "1586", "1587", "1589"})
-//    @Description("Test entering a new scenario name shows the correct name on the evaluate page after the scenario is published")
-//    public void testPublishEnterNewScenarioName() {
-//
-//        resourceFile = FileResourceUtil.getCloudFile(ProcessGroupEnum.WITHOUT_PG, "partbody_2.stp");
-//        String testScenarioName = new GenerateStringUtil().generateScenarioName();
-//        String testNewScenarioName = new GenerateStringUtil().generateScenarioName();
-//
-//        loginPage = new CidAppLoginPage(driver);
-//        evaluatePage = loginPage.login(UserUtil.getUser())
-//            .uploadComponentAndSubmit(testScenarioName, resourceFile, EvaluatePage.class);
-//
-//        assertThat(evaluatePage.isCostLabel(CostingLabelEnum.READY_TO_COST.getCostingText()), is(true));
-//
-//        evaluatePage.costScenario()
-//            .publishScenario(PublishPage.class)
-//            .selectPublishButton()
-//            .selectWorkSpace(WorkspaceEnum.PUBLIC.getWorkspace())
-//            .highlightScenario(testScenarioName, "partbody_2");
-//
-//        explorePage = new ExplorePage(driver);
-//        evaluatePage = explorePage.createNewScenario()
-//            .enterScenarioName(testNewScenarioName)
-//            .save(EvaluatePage.class);
-//
-//        assertThat(evaluatePage.getCurrentScenarioName(testNewScenarioName), is(true));
-//    }
-//
-//    @Category({CustomerSmokeTests.class, SmokeTests.class})
-//    @Test
-//    @TestRail(testCaseId = {"1588"})
-//    @Description("Ensure a previously uploaded CAD File of the same name can be uploaded subsequent times with a different scenario name")
-//    public void multipleUpload() {
-//        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.CASTING_DIE;
-//
-//        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, "MultiUpload.stp");
-//        String scenarioA = new GenerateStringUtil().generateScenarioName();
-//        String scenarioB = new GenerateStringUtil().generateScenarioName();
-//        String scenarioC = new GenerateStringUtil().generateScenarioName();
-//
-//        loginPage = new CidAppLoginPage(driver);
-//        explorePage = loginPage.login(UserUtil.getUser())
-//            .uploadComponentAndSubmit(scenarioA, resourceFile, EvaluatePage.class)
-//            .selectProcessGroup(processGroupEnum.getProcessGroup())
-//            .costScenario()
-//            .publishScenario(PublishPage.class)
-//            .selectPublishButton()
-//            .refreshCurrentPage()
-//            .uploadFileAndOk(scenarioB, FileResourceUtil.getCloudFile(processGroupEnum, "MultiUpload.stp"), EvaluatePage.class)
-//            .selectProcessGroup(ProcessGroupEnum.STOCK_MACHINING.getProcessGroup())
-//            .costScenario()
-//            .publishScenario(PublishPage.class)
-//            .selectPublishButton()
-//            .refreshCurrentPage()
-//            .uploadFileAndOk(scenarioC, FileResourceUtil.getCloudFile(processGroupEnum, "MultiUpload.stp"), EvaluatePage.class)
-//            .selectProcessGroup(ProcessGroupEnum.PLASTIC_MOLDING.getProcessGroup())
-//            .costScenario()
-//            .publishScenario(PublishPage.class)
-//            .selectPublishButton()
+    @Category({CustomerSmokeTests.class, SmokeTests.class})
+    @Test
+    @TestRail(testCaseId = {"1576", "1586", "1587", "1589"})
+    @Description("Test entering a new scenario name shows the correct name on the evaluate page after the scenario is published")
+    public void testPublishEnterNewScenarioName() {
+
+        resourceFile = FileResourceUtil.getCloudFile(ProcessGroupEnum.WITHOUT_PG, "partbody_2.stp");
+        String testScenarioName = new GenerateStringUtil().generateScenarioName();
+        String testNewScenarioName = new GenerateStringUtil().generateScenarioName();
+
+        loginPage = new CidAppLoginPage(driver);
+        evaluatePage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndSubmit(testScenarioName, resourceFile, EvaluatePage.class);
+
+        assertThat(evaluatePage.isCostLabel(CostingLabelEnum.READY_TO_COST.getCostingText()), is(true));
+
+        evaluatePage.costScenario()
+            .publishScenario()
+            .publish(ExplorePage.class)
+            .highlightComponent(testScenarioName, "partbody_2")
+            .createScenario()
+            .enterScenarioName(testNewScenarioName)
+            .submit(EvaluatePage.class);
+
+        assertThat(evaluatePage.getCurrentScenarioName(), is(testNewScenarioName));
+    }
+
+    @Category({CustomerSmokeTests.class, SmokeTests.class})
+    @Test
+    @TestRail(testCaseId = {"1588"})
+    @Description("Ensure a previously uploaded CAD File of the same name can be uploaded subsequent times with a different scenario name")
+    public void multipleUpload() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.CASTING_DIE;
+
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, "MultiUpload.stp");
+        String scenarioA = new GenerateStringUtil().generateScenarioName();
+        String scenarioB = new GenerateStringUtil().generateScenarioName();
+        String scenarioC = new GenerateStringUtil().generateScenarioName();
+
+        loginPage = new CidAppLoginPage(driver);
+        explorePage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndSubmit(scenarioA, resourceFile, EvaluatePage.class)
+            .selectProcessGroup(processGroupEnum.getProcessGroup())
+            .costScenario()
+            .publishScenario()
+            .publish(EvaluatePage.class)
+            .uploadComponentAndSubmit(scenarioB, FileResourceUtil.getCloudFile(processGroupEnum, "MultiUpload.stp"), EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.STOCK_MACHINING.getProcessGroup())
+            .costScenario()
+            .publishScenario()
+            .publish(EvaluatePage.class)
+            .uploadComponentAndSubmit(scenarioC, FileResourceUtil.getCloudFile(processGroupEnum, "MultiUpload.stp"), EvaluatePage.class)
+            .selectProcessGroup(ProcessGroupEnum.PLASTIC_MOLDING.getProcessGroup())
+            .costScenario()
+            .publishScenario()
+            .publish(ExplorePage.class);
 //            .filter()
 //            .setWorkspace("Public")
 //            .setScenarioType("Part")
 //            .setRowOne("Part Name", "Contains", "MultiUpload")
 //            .apply(ExplorePage.class);
-//
-//        assertThat(explorePage.getListOfScenarios(scenarioA, "MultiUpload"), equalTo(1));
-//        assertThat(explorePage.getListOfScenarios(scenarioB, "MultiUpload"), equalTo(1));
-//        assertThat(explorePage.getListOfScenarios(scenarioC, "MultiUpload"), equalTo(1));
-//    }
+
+        assertThat(explorePage.getListOfComponents(scenarioA, "MultiUpload"), equalTo(1));
+        assertThat(explorePage.getListOfComponents(scenarioB, "MultiUpload"), equalTo(1));
+        assertThat(explorePage.getListOfComponents(scenarioC, "MultiUpload"), equalTo(1));
+    }
 }
