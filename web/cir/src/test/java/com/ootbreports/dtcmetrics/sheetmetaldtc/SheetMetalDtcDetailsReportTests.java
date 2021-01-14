516d41d6ca19edc6047719e2844741e72f03548d
Ben <bhegan@apriori.com>
2021-01-14 17:22:39 +0000

    Tidied up code

--- web/cir/src/test/java/com/ootbreports/dtcmetrics/sheetmetaldtc/SheetMetalDtcDetailsReportTests.java
+++ web/cir/src/test/java/com/ootbreports/dtcmetrics/sheetmetaldtc/SheetMetalDtcDetailsReportTests.java
@@ -25,7 +25,6 @@ import com.navigation.CommonReportTests;
 import io.qameta.allure.Description;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import testsuites.suiteinterface.CiaCirTestDevTest;
 import testsuites.suiteinterface.ReportsSmokeTest;
 import testsuites.suiteinterface.ReportsTest;
 import utils.Constants;
