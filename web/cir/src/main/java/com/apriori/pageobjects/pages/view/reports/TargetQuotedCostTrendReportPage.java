e75140f55ec763985f281130a765558e7ecebff1
Ben <bhegan@apriori.com>
2021-01-27 15:46:19 +0000

    Two more tests

--- web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/TargetQuotedCostTrendReportPage.java
+++ web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/TargetQuotedCostTrendReportPage.java
@@ -1,7 +1,10 @@
 package com.apriori.pageobjects.pages.view.reports;
 
 import com.apriori.utils.PageUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,24 @@ import org.slf4j.LoggerFactory;
 public class TargetQuotedCostTrendReportPage extends GenericReportPage {
 
     private final Logger LOGGER = LoggerFactory.getLogger(TargetQuotedCostTrendReportPage.class);
+
+    @FindBy(xpath = "//div[@id='projectRollup']//a")
+    private WebElement projectRollupDropdown;
+
+    @FindBy(xpath = "(//div[@class='jr-mSingleselect-dropdownContainer jr'])[1]//ul")
+    private WebElement projectRollupDropdownOptionList;
+
+    @FindBy(xpath = "//div[@id='projectName']//a")
+    private WebElement projectNameDropdown;
+
+    @FindBy(xpath = "(//div[@class='jr-mSingleselect-dropdownContainer jr'])[2]//ul")
+    private WebElement projectNameDropdownOptionList;
+
+    @FindBy(xpath = "//div[@class='highcharts_parent_container']/div")
+    private WebElement chartDiv;
+
+    @FindBy(xpath = "//span[contains(text(), 'Project Name:')]/../following-sibling::td[2]/span")
+    private WebElement projectNameAboveChart;
 
     private PageUtils pageUtils;
     private WebDriver driver;
@@ -21,4 +42,74 @@ public class TargetQuotedCostTrendReportPage extends GenericReportPage {
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     * Gets project rollup dropdown item count
+     * @return String
+     */
+    public String getProjectRollupDropdownItemCount() {
+        pageUtils.waitForElementToAppear(projectRollupDropdown);
+        return projectRollupDropdownOptionList.getAttribute("childElementCount");
+    }
+
+    /**
+     * Gets project rollup dropdown option text
+     * @param optionIndex index of item text to get
+     * @return String
+     */
+    public String getProjectRollupDropdownOptionText(String optionIndex) {
+        By locator = By.xpath(
+                String.format("(//div[@class='jr-mSingleselect-dropdownContainer jr'])[1]//ul/li[%s]", optionIndex));
+        return driver.findElement(locator).getAttribute("title");
+    }
+
+    /**
+     * Gets project name dropdown item count
+     * @return String
+     */
+    public String getProjectNameDropdownItemCount() {
+        pageUtils.waitForElementToAppear(projectNameDropdown);
+        return projectNameDropdownOptionList.getAttribute("childElementCount");
+    }
+
+    /**
+     * Gets project name dropdown option text
+     * @param optionIndex index of item text to get
+     * @return String
+     */
+    public String getProjectNameDropdownOptionText(String optionIndex) {
+        By locator = By.xpath(
+                String.format("(//div[@class='jr-mSingleselect-dropdownContainer jr'])[2]//ul/li[%s]", optionIndex));
+        return driver.findElement(locator).getAttribute("title");
+    }
+
+    /**
+     * Checks if chart is displayed and enabled
+     * @return boolean
+     */
+    public boolean isChartDisplayedAndEnabled() {
+        pageUtils.waitForElementToAppear(chartDiv);
+        return chartDiv.isDisplayed() && chartDiv.isEnabled();
+    }
+
+    /**
+     * Selects project from dropdown
+     * @param projectToSelect String
+     */
+    public void selectProject(String projectToSelect) {
+        By locator = By.xpath(String.format("//li[@title='%s']/div/a", projectToSelect));
+        if (!projectNameDropdown.getAttribute("title").equals(projectToSelect)) {
+            projectNameDropdown.click();
+            pageUtils.waitForElementToAppear(locator);
+            driver.findElement(locator).click();
+        }
+    }
+
+    /**
+     * Gets project name from above chart
+     * @return String
+     */
+    public String getProjectNameAboveChart() {
+        pageUtils.waitForElementToAppear(projectNameAboveChart);
+        return projectNameAboveChart.getText();
+    }
 }
--- web/cir/src/test/java/com/ootbreports/targetquotedcosttrend/TargetAndQuotedCostTrendTests.java
+++ web/cir/src/test/java/com/ootbreports/targetquotedcosttrend/TargetAndQuotedCostTrendTests.java
@@ -1,7 +1,13 @@
 package com.ootbreports.targetquotedcosttrend;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.apriori.pageobjects.pages.login.ReportsLoginPage;
 import com.apriori.pageobjects.pages.view.reports.GenericReportPage;
 import com.apriori.pageobjects.pages.view.reports.ScenarioComparisonReportPage;
+import com.apriori.pageobjects.pages.view.reports.TargetQuotedCostTrendReportPage;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.enums.reports.ReportNamesEnum;
 import com.apriori.utils.web.driver.TestBase;
@@ -16,6 +22,7 @@ import utils.Constants;
 
 public class TargetAndQuotedCostTrendTests extends TestBase {
 
+    private TargetQuotedCostTrendReportPage targetQuotedCostTrendReportPage;
     private ScenarioComparisonReportPage scenarioComparisonReportPage;
     private InputControlsTests inputControlsTests;
     private GenericReportPage genericReportPage;
@@ -59,4 +66,50 @@ public class TargetAndQuotedCostTrendTests extends TestBase {
         );
     }
 
+    @Test
+    @Category({ReportsTest.class, CiaCirTestDevTest.class})
+    @TestRail(testCaseId = "3355")
+    @Description("Validate Projects Rollup drop-down Input Control functionality")
+    public void testProjectRollupDropdown() {
+        targetQuotedCostTrendReportPage = new ReportsLoginPage(driver)
+                .login()
+                .navigateToLibraryPage()
+                .navigateToReport(ReportNamesEnum.TARGET_AND_QUOTED_COST_TREND.getReportName(),
+                        TargetQuotedCostTrendReportPage.class);
+
+        assertThat(targetQuotedCostTrendReportPage.getProjectRollupDropdownItemCount(), is(equalTo("2")));
+        assertThat(targetQuotedCostTrendReportPage.getProjectRollupDropdownOptionText("1"),
+                is(equalTo("AC CYCLE TIME VT 1")));
+        assertThat(targetQuotedCostTrendReportPage.getProjectRollupDropdownOptionText("2"),
+                is(equalTo("CYCLE TIME VALUE TRACKING ROLLUP")));
+
+        targetQuotedCostTrendReportPage.clickOk();
+        assertThat(targetQuotedCostTrendReportPage.isChartDisplayedAndEnabled(), is(equalTo(true)));
+    }
+
+    @Test
+    @Category({ReportsTest.class, CiaCirTestDevTest.class})
+    @TestRail(testCaseId = "3356")
+    @Description("Validate Project Name drop-down Input Control functionality")
+    public void testProjectNameDropdown() {
+        targetQuotedCostTrendReportPage = new ReportsLoginPage(driver)
+                .login()
+                .navigateToLibraryPage()
+                .navigateToReport(ReportNamesEnum.TARGET_AND_QUOTED_COST_TREND.getReportName(),
+                        TargetQuotedCostTrendReportPage.class);
+
+        int itemCount = Integer.parseInt(targetQuotedCostTrendReportPage.getProjectNameDropdownItemCount());
+        assertThat(itemCount, is(equalTo(4)));
+
+        for (int i = 0; i == itemCount; i++) {
+            assertThat(targetQuotedCostTrendReportPage.getProjectNameDropdownOptionText(String.valueOf(i)), is(equalTo(String.format("PROJECT %d", i))));
+        }
+
+        String projectToSelect = "PROJECT 4";
+        targetQuotedCostTrendReportPage.selectProject(projectToSelect);
+        targetQuotedCostTrendReportPage.clickOk();
+
+        assertThat(targetQuotedCostTrendReportPage.isChartDisplayedAndEnabled(), is(equalTo(true)));
+        assertThat(targetQuotedCostTrendReportPage.getProjectNameAboveChart(), is(equalTo(projectToSelect)));
+    }
 }
