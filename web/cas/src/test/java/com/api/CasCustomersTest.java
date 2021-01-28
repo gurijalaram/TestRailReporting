76fbfb9db86a88418fa56b46af5e4ace64f45887
nvalieieva <nvalieieva@apriori.com>
2021-01-28 17:29:38 +0200

    - deleted test - changed name and description of test - added assertions to the existing test

--- web/cas/src/test/java/com/api/CasCustomersTest.java
+++ web/cas/src/test/java/com/api/CasCustomersTest.java
@@ -3,7 +3,7 @@ package com.api;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 import com.apriori.apibase.services.cas.objects.Customers;
 import com.apriori.apibase.utils.APIAuthentication;
@@ -22,8 +22,8 @@ public class CasCustomersTest extends TestUtil {
     private String token;
 
     @Test
-    @Description("Get a list of CAS customers sorted by name and verify that OK status returned")
-    public void getCustomersStatus() {
+    @Description("Get a list of CAS customers sorted by name")
+    public void getCustomersByName() {
         String apiUrl = String.format(Constants.getApiUrl(), "customers?sortBy[ASC]=name");
 
         token = new JwtTokenUtil().retrieveJwtToken(Constants.getSecretKey(),
@@ -38,24 +38,7 @@ public class CasCustomersTest extends TestUtil {
             new APIAuthentication().initAuthorizationHeaderContent(token));
 
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
-    }
-
-    @Test
-    @Description("Get a list of CAS customers sorted by name and verify that total item more or equal than 1")
-    public void getCustomersTotalItems() {
-        String apiUrl = String.format(Constants.getApiUrl(), "customers?sortBy[ASC]=name");
-
-        token = new JwtTokenUtil().retrieveJwtToken(Constants.getSecretKey(),
-                Constants.getCasServiceHost(),
-                HttpStatus.SC_CREATED,
-                Constants.getCasTokenUsername(),
-                Constants.getCasTokenEmail(),
-                Constants.getCasTokenIssuer(),
-                Constants.getCasTokenSubject());
-
-        ResponseWrapper<Customers> response = new CommonRequestUtil().getCommonRequest(apiUrl, true, Customers.class,
-                new APIAuthentication().initAuthorizationHeaderContent(token));
-
-        assertTrue(response.getResponseEntity().getResponse().getTotalItemCount() >= 1);
+        assertThat(response.getResponseEntity().getResponse().getTotalItemCount(), greaterThanOrEqualTo(1));
+        assertThat((int) response.getResponseEntity().getResponse().getItems().stream().filter(items -> items.getIdentity() != null).count(), greaterThanOrEqualTo(1));
     }
 }
