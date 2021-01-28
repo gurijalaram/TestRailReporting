b294dc4c2e3924b6f55ec954b3b80459f37c8f60
Ben <bhegan@apriori.com>
2021-01-28 10:44:28 +0000

    Sorted suite

--- web/cir/src/test/java/testsuites/CiaCirTestDevSuite.java
+++ web/cir/src/test/java/testsuites/CiaCirTestDevSuite.java
@@ -2,10 +2,7 @@ package testsuites;
 
 import com.apriori.utils.runner.CategorySuiteRunner;
 
-import com.ootbreports.general.assemblydetails.AssemblyDetailsReportTests;
 import com.ootbreports.targetquotedcosttrend.TargetAndQuotedCostTrendTests;
-import com.ootbreports.targetquotedcosttrend.TargetAndQuotedCostValueTrackingDetailsTests;
-import com.ootbreports.targetquotedcosttrend.TargetAndQuotedCostValueTrackingTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -14,9 +11,7 @@ import testsuites.suiteinterface.CiaCirTestDevTest;
 @RunWith(CategorySuiteRunner.class)
 @Categories.IncludeCategory(CiaCirTestDevTest.class)
 @Suite.SuiteClasses({
-        TargetAndQuotedCostTrendTests.class,
-        TargetAndQuotedCostValueTrackingTests.class,
-        TargetAndQuotedCostValueTrackingDetailsTests.class
+        TargetAndQuotedCostTrendTests.class
 })
 
 public class CiaCirTestDevSuite {
