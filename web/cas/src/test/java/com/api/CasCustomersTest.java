73e0464bf11d3f1aa5db4527c1e0f5de14d48e2b
nvalieieva <nvalieieva@apriori.com>
2021-02-03 16:09:47 +0200

    - added new test

--- web/cas/src/test/java/com/api/CasCustomersTest.java
+++ web/cas/src/test/java/com/api/CasCustomersTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 import com.apriori.apibase.services.cas.objects.Customers;
+import com.apriori.apibase.services.cds.objects.Customer;
 import com.apriori.apibase.utils.APIAuthentication;
 import com.apriori.apibase.utils.CommonRequestUtil;
 import com.apriori.apibase.utils.JwtTokenUtil;
@@ -39,5 +40,33 @@ public class CasCustomersTest extends TestUtil {
 
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
         assertThat(response.getResponseEntity().getResponse().getTotalItemCount(), is(greaterThanOrEqualTo(1)));
+    }
+
+    @Test
+    @Description("Get the Customer identified by its identity")
+    public void getCustomersByIdentity() {
+        String apiUrl = String.format(Constants.getApiUrl(), "customers");
+
+        token = new JwtTokenUtil().retrieveJwtToken(Constants.getSecretKey(),
+                Constants.getCasServiceHost(),
+                HttpStatus.SC_CREATED,
+                Constants.getCasTokenUsername(),
+                Constants.getCasTokenEmail(),
+                Constants.getCasTokenIssuer(),
+                Constants.getCasTokenSubject());
+
+        ResponseWrapper<Customers> response = new CommonRequestUtil().getCommonRequest(apiUrl, true, Customers.class,
+                new APIAuthentication().initAuthorizationHeaderContent(token));
+
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
+
+       String identity = response.getResponseEntity().getResponse().getItems().get(0).getIdentity();
+
+       String identityEndpoint = apiUrl + "/" + identity;
+
+       ResponseWrapper<Customer> responseIdentity = new CommonRequestUtil().getCommonRequest(identityEndpoint, true, Customer.class,
+                new APIAuthentication().initAuthorizationHeaderContent(token));
+
+        assertThat(responseIdentity.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
     }
 }
