324ad6b41e091856a96e679a06802e8c489620b9
nvalieieva <nvalieieva@apriori.com>
2021-02-09 15:48:04 +0200

    -added test for not existing identity; -added test for not existing name; -added json schemas for each test; -added pojos for each test.

---
+++ apibase/src/main/java/com/apriori/apibase/services/cas/objects/NotFoundNameResponse.java
@@ -0,0 +1,29 @@
+package com.apriori.apibase.services.cas.objects;
+
+import com.apriori.apibase.services.Pagination;
+import com.apriori.utils.http.enums.Schema;
+
+import java.util.List;
+
+@Schema(location = "cas/NotFoundName.json")
+public class NotFoundNameResponse extends Pagination {
+    private NotFoundNameResponse response;
+    private List<NotFoundName> items;
+
+    public NotFoundNameResponse getResponse() {
+        return response;
+    }
+
+    public void setResponse(NotFoundNameResponse response) {
+        this.response = response;
+    }
+
+    public List<NotFoundName> getItems(){
+        return this.items;
+    }
+
+    public NotFoundNameResponse setItems(List<NotFoundName> items){
+        this.items = items;
+        return this;
+    }
+}
