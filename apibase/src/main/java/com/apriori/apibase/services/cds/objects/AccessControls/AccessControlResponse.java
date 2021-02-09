7f60eba7a000ae3675497473de59612fff702a67
moyaparker <mparker@apriori.com>
2021-02-09 10:20:46 +0000

    Added assertion for outofcontext

--- apibase/src/main/java/com/apriori/apibase/services/cds/objects/AccessControls/AccessControlResponse.java
+++ apibase/src/main/java/com/apriori/apibase/services/cds/objects/AccessControls/AccessControlResponse.java
@@ -1,20 +1,9 @@
 package com.apriori.apibase.services.cds.objects.AccessControls;
 
 import com.apriori.apibase.services.Pagination;
-import com.apriori.apibase.services.cds.objects.AccessControls.AccessControlItems;
-import com.apriori.apibase.services.cds.objects.Customer;
-import com.apriori.apibase.services.cds.objects.Customers;
 import com.apriori.utils.http.enums.Schema;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @Schema(location = "cds/CdsAccessControlsSchema.json")
 public class AccessControlResponse extends Pagination {
@@ -33,7 +22,7 @@ public class AccessControlResponse extends Pagination {
 
     public AccessControlResponse setItems(List<AccessControlItems> items) {
         this.items = items;
-       return this;
+        return this;
     }
 
     public List<AccessControlItems> getItems() {
--- microservices/cds/src/test/java/com/apriori/cds/tests/CdsAccessControls.java
+++ microservices/cds/src/test/java/com/apriori/cds/tests/CdsAccessControls.java
@@ -2,27 +2,20 @@ package com.apriori.cds.tests;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 import com.apriori.apibase.services.cds.objects.AccessControls.AccessControlResponse;
-import com.apriori.cds.entity.response.User;
-import com.apriori.cds.entity.response.Users;
 import com.apriori.cds.tests.utils.CdsTestUtil;
 import com.apriori.cds.utils.Constants;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.http.utils.ResponseWrapper;
 
 import io.qameta.allure.Description;
-import org.apache.commons.validator.routines.EmailValidator;
 import org.apache.http.HttpStatus;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
 
 public class CdsAccessControls extends CdsTestUtil {
     private String url;
@@ -42,7 +35,6 @@ public class CdsAccessControls extends CdsTestUtil {
 
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
         assertThat(response.getResponseEntity().getResponse().getTotalItemCount(), is(greaterThanOrEqualTo(1)));
-       // assertThat(response.getResponseEntity().getResponse().getItems().get(0).getOutOfContext(), is(not(emptyString())));
+        assertThat(response.getResponseEntity().getResponse().getItems().get(0).getOutOfContext(), is(notNullValue()));
     }
-
 }
