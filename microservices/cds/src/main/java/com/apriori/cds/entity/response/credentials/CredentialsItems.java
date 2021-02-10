96c39aa4d8b42b8d387ce2e8bb5bfc4658a8fbce
moyaparker <mparker@apriori.com>
2021-02-10 09:47:51 +0000

    Modified tests follow layout of other tests. Also in order to make the tests run on multiple environments I removed the static Identity Added a test to validate the credentials endpoint

---
+++ microservices/cds/src/main/java/com/apriori/cds/entity/response/credentials/CredentialsItems.java
@@ -0,0 +1,81 @@
+
+package com.apriori.cds.entity.response.credentials;
+
+import com.apriori.utils.http.enums.Schema;
+
+import java.util.List;
+
+@Schema(location = "cds/credentialsSchema.json")
+public class CredentialsItems {
+
+    private String identity;
+    private String userIdentity;
+    private String email;
+    private String passwordHash;
+    private String passwordSalt;
+    private List<String> passwordHashHistory = null;
+    private List<String> passwordSaltHistory = null;
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public CredentialsItems setIdentity(String identity) {
+        this.identity = identity;
+        return this;
+    }
+
+    public String getUserIdentity() {
+        return userIdentity;
+    }
+
+    public CredentialsItems setUserIdentity(String userIdentity) {
+        this.userIdentity = userIdentity;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CredentialsItems setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public CredentialsItems setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+        return this;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public CredentialsItems setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+        return this;
+    }
+
+    public List<String> getPasswordHashHistory() {
+        return passwordHashHistory;
+    }
+
+    public CredentialsItems setPasswordHashHistory(List<String> passwordHashHistory) {
+        this.passwordHashHistory = passwordHashHistory;
+        return this;
+    }
+
+    public List<String> getPasswordSaltHistory() {
+        return passwordSaltHistory;
+    }
+
+    public CredentialsItems setPasswordSaltHistory(List<String> passwordSaltHistory) {
+        this.passwordSaltHistory = passwordSaltHistory;
+        return this;
+    }
+}
