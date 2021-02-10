538677aa059e23b527f4bbe8376019569b7ddd2d
cfrith <ciene.frith@gmail.com>
2021-02-10 11:49:46 +0000

    added new pagination schema...wip;

---
+++ apibase/src/main/java/com/apriori/apibase/services/Pagination.java
@@ -0,0 +1,108 @@
+package com.apriori.apibase.services;
+
+import com.apriori.utils.http.enums.Schema;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Schema(location = "PaginationSchema.json")
+public class Pagination {
+    @JsonProperty
+    private Boolean isFirstPage;
+    @JsonProperty
+    private Boolean isLastPage;
+    @JsonProperty
+    private Integer pageNumber;
+    @JsonProperty
+    private Integer pageSize;
+    @JsonProperty
+    private Integer pageItemCount;
+    @JsonProperty
+    private Integer totalItemCount;
+    @JsonProperty
+    private Integer totalPageCount;
+    @JsonProperty
+    private Boolean hasNextPage;
+    @JsonProperty
+    private Boolean hasPreviousPage;
+
+    public Pagination setIsFirstPage(Boolean isFirstPage) {
+        this.isFirstPage = isFirstPage;
+        return this;
+    }
+
+    public Boolean getIsFirstPage() {
+        return this.isFirstPage;
+    }
+
+    public Pagination setIsLastPage(Boolean isLastPage) {
+        this.isLastPage = isLastPage;
+        return this;
+    }
+
+    public Boolean getIsLastPage() {
+        return this.isLastPage;
+    }
+
+    public Pagination setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+        return this;
+    }
+
+    public Integer getPageNumber() {
+        return this.pageNumber;
+    }
+
+    public Pagination setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+        return this;
+    }
+
+    public Integer getPageSize() {
+        return this.pageSize;
+    }
+
+    public Pagination setPageItemCount(Integer pageItemCount) {
+        this.pageItemCount = pageItemCount;
+        return this;
+    }
+
+    public Integer getPageItemCount() {
+        return this.pageItemCount;
+    }
+
+    public Pagination setTotalItemCount(Integer totalItemCount) {
+        this.totalItemCount = totalItemCount;
+        return this;
+    }
+
+    public Integer getTotalItemCount() {
+        return this.totalItemCount;
+    }
+
+    public Pagination setTotalPageCount(Integer totalPageCount) {
+        this.totalPageCount = totalPageCount;
+        return this;
+    }
+
+    public Integer getTotalPageCount() {
+        return this.totalPageCount;
+    }
+
+    public Pagination setHasNextPage(Boolean hasNextPage) {
+        this.hasNextPage = hasNextPage;
+        return this;
+    }
+
+    public Boolean getHasNextPage() {
+        return this.hasNextPage;
+    }
+
+    public Pagination setHasPreviousPage(Boolean hasPreviousPage) {
+        this.hasPreviousPage = hasPreviousPage;
+        return this;
+    }
+
+    public Boolean getHasPreviousPage() {
+        return this.hasPreviousPage;
+    }
+}
