55a85ff960bc0cde21988846cdfe2c554f037186
cfrith <ciene.frith@gmail.com>
2021-02-11 12:52:30 +0000

    renamed class; added json property to field; updated path to schema;

---
+++ apibase/src/main/java/com/apriori/apibase/services/common/objects/AccessControls/AccessControls.java
@@ -0,0 +1,34 @@
+package com.apriori.apibase.services.common.objects.AccessControls;
+
+import com.apriori.apibase.services.common.objects.Pagination;
+import com.apriori.utils.http.enums.Schema;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@Schema(location = "common/CdsAccessControlsSchema.json")
+public class AccessControls extends Pagination {
+    @JsonProperty
+    private AccessControls response;
+    @JsonProperty
+    private List<AccessControl> items;
+
+    public AccessControls getResponse() {
+        return response;
+    }
+
+    public AccessControls setResponse(AccessControls response) {
+        this.response = response;
+        return this;
+    }
+
+    public AccessControls setItems(List<AccessControl> items) {
+        this.items = items;
+        return this;
+    }
+
+    public List<AccessControl> getItems() {
+        return this.items;
+    }
+}
