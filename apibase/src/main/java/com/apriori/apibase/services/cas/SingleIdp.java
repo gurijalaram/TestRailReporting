49e25f5d3670e805840987a4c0267cf9945bfd1b
cfrith <ciene.frith@gmail.com>
2021-02-16 17:25:39 +0000

    changed variable type;

--- apibase/src/main/java/com/apriori/apibase/services/cas/SingleIdp.java
+++ apibase/src/main/java/com/apriori/apibase/services/cas/SingleIdp.java
@@ -26,7 +26,7 @@ public class SingleIdp {
     private IdentityProviderPlatform identityProviderPlatform;
     private String displayName;
     private String description;
-    private List<Object> idpDomains = null;
+    private List<String> idpDomains = null;
     private String signInUrl;
     private String signingCertificate;
     private String signingCertificateExpiresAt;
@@ -118,11 +118,11 @@ public class SingleIdp {
         return this;
     }
 
-    public List<Object> getIdpDomains() {
+    public List<String> getIdpDomains() {
         return idpDomains;
     }
 
-    public SingleIdp setIdpDomains(List<Object> idpDomains) {
+    public SingleIdp setIdpDomains(List<String> idpDomains) {
         this.idpDomains = idpDomains;
         return this;
     }
