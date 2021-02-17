86766ec0b84ae4bf0c0961b3aa2959f1865a271c
cfrith <ciene.frith@gmail.com>
2021-02-17 17:44:31 +0000

    removed setters from response;

--- apibase/src/main/java/com/apriori/apibase/services/common/objects/IdentityProviderResponse.java
+++ apibase/src/main/java/com/apriori/apibase/services/common/objects/IdentityProviderResponse.java
@@ -40,161 +40,71 @@ public class IdentityProviderResponse {
         return response;
     }
 
-    public IdentityProviderResponse setResponse(IdentityProviderResponse response) {
-        this.response = response;
-        return this;
-    }
-
     public String getIdentity() {
         return identity;
-    }
-
-    public IdentityProviderResponse setIdentity(String identity) {
-        this.identity = identity;
-        return this;
     }
 
     public String getCreatedBy() {
         return createdBy;
     }
 
-    public IdentityProviderResponse setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-        return this;
-    }
-
     public LocalDateTime getCreatedAt() {
         return createdAt;
-    }
-
-    public IdentityProviderResponse setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-        return this;
     }
 
     public IdentityProviderPlatform getIdentityProviderPlatform() {
         return identityProviderPlatform;
     }
 
-    public IdentityProviderResponse setIdentityProviderPlatform(IdentityProviderPlatform identityProviderPlatform) {
-        this.identityProviderPlatform = identityProviderPlatform;
-        return this;
-    }
-
     public Boolean getActive() {
         return active;
-    }
-
-    public IdentityProviderResponse setActive(Boolean active) {
-        this.active = active;
-        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public IdentityProviderResponse setName(String name) {
-        this.name = name;
-        return this;
-    }
-
     public String getDisplayName() {
         return displayName;
-    }
-
-    public IdentityProviderResponse setDisplayName(String displayName) {
-        this.displayName = displayName;
-        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public IdentityProviderResponse setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
     public List<String> getIdpDomains() {
         return idpDomains;
-    }
-
-    public IdentityProviderResponse setIdpDomains(List<String> idpDomains) {
-        this.idpDomains = idpDomains;
-        return this;
     }
 
     public String getSignInUrl() {
         return signInUrl;
     }
 
-    public IdentityProviderResponse setSignInUrl(String signInUrl) {
-        this.signInUrl = signInUrl;
-        return this;
-    }
-
     public String getSigningCertificate() {
         return signingCertificate;
-    }
-
-    public IdentityProviderResponse setSigningCertificate(String signingCertificate) {
-        this.signingCertificate = signingCertificate;
-        return this;
     }
 
     public String getSigningCertificateExpiresAt() {
         return signingCertificateExpiresAt;
     }
 
-    public IdentityProviderResponse setSigningCertificateExpiresAt(String signingCertificateExpiresAt) {
-        this.signingCertificateExpiresAt = signingCertificateExpiresAt;
-        return this;
-    }
-
     public Boolean getSignRequest() {
         return signRequest;
-    }
-
-    public IdentityProviderResponse setSignRequest(Boolean signRequest) {
-        this.signRequest = signRequest;
-        return this;
     }
 
     public AttributeMappings getAttributeMappings() {
         return attributeMappings;
     }
 
-    public IdentityProviderResponse setAttributeMappings(AttributeMappings attributeMappings) {
-        this.attributeMappings = attributeMappings;
-        return this;
-    }
-
     public String getSignRequestAlgorithm() {
         return signRequestAlgorithm;
-    }
-
-    public IdentityProviderResponse setSignRequestAlgorithm(String signRequestAlgorithm) {
-        this.signRequestAlgorithm = signRequestAlgorithm;
-        return this;
     }
 
     public String getSignRequestAlgorithmDigest() {
         return signRequestAlgorithmDigest;
     }
 
-    public IdentityProviderResponse setSignRequestAlgorithmDigest(String signRequestAlgorithmDigest) {
-        this.signRequestAlgorithmDigest = signRequestAlgorithmDigest;
-        return this;
-    }
-
     public String getProtocolBinding() {
         return protocolBinding;
-    }
-
-    public IdentityProviderResponse setProtocolBinding(String protocolBinding) {
-        this.protocolBinding = protocolBinding;
-        return this;
     }
 }
