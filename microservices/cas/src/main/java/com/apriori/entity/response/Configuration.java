77e5c6e8d281cb4b38fb0ea2d0ab0ed3afe2b586
nvalieieva <nvalieieva@apriori.com>
2021-02-18 18:29:02 +0200

    -added test to get a list of all aP Versions -added new schemas and pojo

---
+++ microservices/cas/src/main/java/com/apriori/entity/response/Configuration.java
@@ -0,0 +1,38 @@
+package com.apriori.entity.response;
+
+import com.apriori.utils.http.enums.Schema;
+import com.apriori.utils.json.deserializers.DateTimeDeserializer_yyyyMMddTHHmmZ;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(location = "cas/ConfigurationSchema.json")
+public class Configuration {
+    private String identity;
+    @JsonSerialize(using = ToStringSerializer.class)
+    @JsonDeserialize(using = DateTimeDeserializer_yyyyMMddTHHmmZ.class)
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private String version;
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
