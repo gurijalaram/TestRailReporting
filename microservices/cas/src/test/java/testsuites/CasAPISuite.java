c2ace9631995a983a512b9d23a85198dad3a82cc
nvalieieva <nvalieieva@apriori.com>
2021-02-19 15:10:34 +0200

    added cas test suites

---
+++ microservices/cas/src/test/java/testsuites/CasAPISuite.java
@@ -0,0 +1,20 @@
+package testsuites;
+
+import com.apriori.tests.*;
+import com.apriori.utils.ProjectRunID;
+import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/*@ProjectRunID("361")*/
+@RunWith(ConcurrentSuiteRunner.class)
+@Suite.SuiteClasses({
+        ApplicationsTests.class,
+        CasConfigurationsTests.class,
+        CasCustomersTests.class,
+        CasDeploymentsTests.class,
+        CasIdentityProvidersTests.class,
+        CasSitesTests.class
+})
+public class CasAPISuite {
+}
