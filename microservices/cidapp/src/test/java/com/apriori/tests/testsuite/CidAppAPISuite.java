34995048fa1b91b34a009a11024919d3e471a5e8
cfrith <ciene.frith@gmail.com>
2021-02-24 09:57:54 +0000

    renamed class and changed functionality of methods;

--- microservices/cidapp/src/test/java/com/apriori/tests/testsuite/CidAppAPISuite.java
+++ microservices/cidapp/src/test/java/com/apriori/tests/testsuite/CidAppAPISuite.java
@@ -1,6 +1,6 @@
 package com.apriori.tests.testsuite;
 
-import com.apriori.tests.UploadComponentTests;
+import com.apriori.tests.ComponentsControllerTests;
 import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.ConcurrentSuiteRunner;
 
@@ -10,7 +10,7 @@ import org.junit.runners.Suite;
 @ProjectRunID("361")
 @RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses({
-    UploadComponentTests.class
+    ComponentsControllerTests.class
 })
 public class CidAppAPISuite {
 
