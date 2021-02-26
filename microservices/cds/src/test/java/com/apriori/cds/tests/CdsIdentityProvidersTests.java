6a16d028f36dee530c2c5673be3457191d3761fa
cfrith <ciene.frith@gmail.com>
2021-02-26 17:19:19 +0000

    added test to delete idp;

--- microservices/cds/src/test/java/com/apriori/cds/tests/CdsIdentityProvidersTests.java
+++ microservices/cds/src/test/java/com/apriori/cds/tests/CdsIdentityProvidersTests.java
@@ -1,6 +1,7 @@
 package com.apriori.cds.tests;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
@@ -61,5 +62,37 @@ public class CdsIdentityProvidersTests extends CdsTestUtil {
         ResponseWrapper<IdentityProviderResponse> response = addSaml(identityProviderEndpoint, IdentityProviderResponse.class, userIdentity, userName);
 
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_CREATED)));
+    }
+
+    @Test
+    @TestRail(testCaseId = " ")
+    @Description("Deletes identity provider")
+    public void deleteCustomerIdentityProvider() {
+        String customersEndpoint = String.format(url, "customers");
+
+        String customerName = generateStringUtil.generateCustomerName();
+        String cloudRef = generateStringUtil.generateCloudReference();
+        String salesForceId = generateStringUtil.generateSalesForceId();
+        String emailPattern = "\\S+@".concat(customerName);
+        String userName = generateStringUtil.generateUserName();
+
+        ResponseWrapper<Customer> customer = addCustomer(customersEndpoint, Customer.class, customerName, cloudRef, salesForceId, emailPattern);
+        String customerIdentity = customer.getResponseEntity().getResponse().getIdentity();
+        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
+        String usersEndpoint = String.format(url, String.format("customers/%s", customerIdentity.concat("/users")));
+
+        ResponseWrapper<User> user = addUser(usersEndpoint, User.class, userName, customerName);
+        String userIdentity = user.getResponseEntity().getResponse().getIdentity();
+
+        String identityProviderEndpoint = String.format(url, String.format("customers/%s/identity-providers", customerIdentity));
+
+        ResponseWrapper<IdentityProviderResponse> identityProviderResponse = addSaml(identityProviderEndpoint, IdentityProviderResponse.class, userIdentity, userName);
+        String identityProviderIdentity = identityProviderResponse.getResponseEntity().getResponse().getIdentity();
+
+        String deleteIdentityProviderEndpoint = String.format(url, String.format("customers/%s/identity-providers/%s", customerIdentity, identityProviderIdentity));
+        ResponseWrapper<String> deleteResponse = delete(deleteIdentityProviderEndpoint);
+
+        assertThat(deleteResponse.getStatusCode(), is(equalTo(HttpStatus.SC_NO_CONTENT)));
+        assertThat(deleteResponse.getResponseEntity(), is(emptyString()));
     }
 }
