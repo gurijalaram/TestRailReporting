9b70241769231eb99eaed148841dbc63091d2cd7
cfrith <ciene.frith@gmail.com>
2021-03-03 12:28:39 +0000

    added testrail id's to tests;

--- microservices/cds/src/test/java/testsuites/CdsAPISuite.java
+++ microservices/cds/src/test/java/testsuites/CdsAPISuite.java
@@ -16,13 +16,13 @@ import com.apriori.cds.tests.CdsRolesTests;
 import com.apriori.cds.tests.CdsSitesTests;
 import com.apriori.cds.tests.CdsUsersTests;
 import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.CategorySuiteRunner;
+import com.apriori.utils.runner.ConcurrentSuiteRunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @ProjectRunID("641")
-@RunWith(CategorySuiteRunner.class)
+@RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses({
     ApVersionsTests.class,
     CdsAccessControlsTests.class,
