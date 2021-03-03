7fd4acd6b65dbc936c7bbc4241fa410df606567d
Ben <bhegan@apriori.com>
2021-03-03 16:09:04 +0000

    Changed suite

--- web/cir/src/test/java/com/ootbreports/scenariocomparison/ScenarioComparisonReportTests.java
+++ web/cir/src/test/java/com/ootbreports/scenariocomparison/ScenarioComparisonReportTests.java
@@ -21,7 +21,6 @@ import com.navigation.CommonReportTests;
 import io.qameta.allure.Description;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import testsuites.suiteinterface.CiaCirTestDevTest;
 import testsuites.suiteinterface.ReportsSmokeTest;
 import testsuites.suiteinterface.ReportsTest;
 import utils.Constants;
@@ -61,7 +60,7 @@ public class ScenarioComparisonReportTests extends TestBase {
     }
 
     @Test
-    @Category({ReportsTest.class, CiaCirTestDevTest.class})
+    @Category(ReportsTest.class)
     @TestRail(testCaseId = "3245")
     @Description("Validate report is available by search")
     public void testReportAvailabilityBySearch() {
