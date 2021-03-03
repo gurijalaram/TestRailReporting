d80851940330396425bff9c96c8be0192e6301cb
Ben <bhegan@apriori.com>
2021-03-03 16:24:17 +0000

    Removed a couple of unused imports

--- web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/ScenarioComparisonReportPage.java
+++ web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/ScenarioComparisonReportPage.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.net.www.content.text.Generic;
 
 import java.math.BigDecimal;
 
