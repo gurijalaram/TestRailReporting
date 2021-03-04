f552ea0ee6320503368e94e34dc2169494b99b99
nvalieieva <nvalieieva@apriori.com>
2021-03-04 20:03:54 +0200

    -undo accidental change

--- microservices/cis/src/main/java/com.apriori.cis/entity/response/Batch.java
+++ microservices/cis/src/main/java/com.apriori.cis/entity/response/Batch.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 import java.time.LocalDateTime;
 
-@Schema(location = "PostBatchSchema.json")
+@Schema(location = "CustomerBatchSchema.json")
 public class Batch {
     
     private String identity;
