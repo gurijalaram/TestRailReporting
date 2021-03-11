18e14eb8df5e9f9700ba502d3e22742d655fa4fe
cfrith <ciene.frith@gmail.com>
2021-03-11 13:34:30 +0000

    removed unused field;

--- microservices/ats/src/main/java/com/apriori/ats/entity/request/ResetAutoUsers.java
+++ microservices/ats/src/main/java/com/apriori/ats/entity/request/ResetAutoUsers.java
@@ -4,17 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResetAutoUsers {
-    private ResetAutoUsers response;
     private String password;
-
-    public ResetAutoUsers getResponse() {
-        return response;
-    }
-
-    public ResetAutoUsers setResponse(ResetAutoUsers response) {
-        this.response = response;
-        return this;
-    }
 
     public String getPassword() {
         return password;
