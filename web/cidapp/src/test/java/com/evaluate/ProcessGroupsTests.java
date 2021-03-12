b4ed8b2288d5d2d7af0b6efa2773b6587027a4e7
cfrith <ciene.frith@gmail.com>
2021-03-12 16:41:51 +0000

    added ignore tag to tests;

--- web/cidapp/src/test/java/com/evaluate/ProcessGroupsTests.java
+++ web/cidapp/src/test/java/com/evaluate/ProcessGroupsTests.java
@@ -15,6 +15,7 @@ import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import testsuites.suiteinterface.CustomerSmokeTests;
@@ -34,7 +35,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @TestRail(testCaseId = {"5441", "6061"})
     @Description("Testing process group Forging")
     public void testProcessGroupForging() {
@@ -53,7 +55,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @TestRail(testCaseId = {"5441"})
     @Description("Testing process group Stock Machining")
     public void testProcessGroupStockMachining() {
@@ -72,7 +75,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Bar and Tube")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupBarTube() {
@@ -92,7 +96,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Casting")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupDieCasting() {
@@ -192,7 +197,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Machining-Gage Parts")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupMachiningGageParts() {
@@ -213,7 +219,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Machining-Milling-4 Axis Mill")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupMachining4AxisMill() {
@@ -253,7 +260,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Machining-Milling-Mill Turn")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupMachiningMillTurn() {
@@ -333,7 +341,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Rough Milling")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupRoughMilling() {
@@ -393,7 +402,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Blow Molding")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupBlowMolding() {
@@ -413,7 +423,8 @@ public class ProcessGroupsTests extends TestBase {
     }
 
     @Category({CustomerSmokeTests.class, SmokeTests.class})
-    //@Test
+    @Ignore
+    @Test
     @Description("Testing process group Powder Metal")
     @TestRail(testCaseId = {"6061"})
     public void testProcessGroupPowderMetal() {
