facb59ad82c15c779aa02821822535ac5fcfa4c1
cfrith <ciene.frith@gmail.com>
2021-04-01 17:08:08 +0100

    commented out runid;

--- microservices/cidapp/src/test/java/testsuite/CidApiSuite.java
+++ microservices/cidapp/src/test/java/testsuite/CidApiSuite.java
@@ -1,13 +1,12 @@
 package testsuite;
 
 import com.apriori.tests.ComponentImageTests;
-import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.ConcurrentSuiteRunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@ProjectRunID("361")
+//@ProjectRunID("361")
 @RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses({
     ComponentImageTests.class
--- microservices/cidapp/src/test/java/testsuite/ControllersAPISuite.java
+++ microservices/cidapp/src/test/java/testsuite/ControllersAPISuite.java
@@ -2,13 +2,12 @@ package testsuite;
 
 import com.apriori.tests.controllers.ComponentsControllerTests;
 import com.apriori.tests.controllers.IterationsControllerTests;
-import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.ConcurrentSuiteRunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@ProjectRunID("361")
+//@ProjectRunID("361")
 @RunWith(ConcurrentSuiteRunner.class)
 @Suite.SuiteClasses({
     ComponentsControllerTests.class,
