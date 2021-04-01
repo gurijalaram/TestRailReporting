e9084de7c9fe02d30ea1faeb5a89189b10bc5740
cfrith <ciene.frith@gmail.com>
2021-04-01 17:06:37 +0100

    added missing setters/getters;

--- microservices/cidapp/src/main/java/com/apriori/entity/response/scenarios/ScenarioRepresentationResponse.java
+++ microservices/cidapp/src/main/java/com/apriori/entity/response/scenarios/ScenarioRepresentationResponse.java
@@ -31,4 +31,143 @@ public class ScenarioRepresentationResponse {
     private Boolean published;
     private String ownedByName;
     private String createdByName;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public ScenarioRepresentationResponse setIdentity(String identity) {
+        this.identity = identity;
+        return this;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public ScenarioRepresentationResponse setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public ScenarioRepresentationResponse setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+        return this;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public ScenarioRepresentationResponse setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    public String getCustomerIdentity() {
+        return customerIdentity;
+    }
+
+    public ScenarioRepresentationResponse setCustomerIdentity(String customerIdentity) {
+        this.customerIdentity = customerIdentity;
+        return this;
+    }
+
+    public String getScenarioName() {
+        return scenarioName;
+    }
+
+    public ScenarioRepresentationResponse setScenarioName(String scenarioName) {
+        this.scenarioName = scenarioName;
+        return this;
+    }
+
+    public String getScenarioType() {
+        return scenarioType;
+    }
+
+    public ScenarioRepresentationResponse setScenarioType(String scenarioType) {
+        this.scenarioType = scenarioType;
+        return this;
+    }
+
+    public String getScenarioState() {
+        return scenarioState;
+    }
+
+    public ScenarioRepresentationResponse setScenarioState(String scenarioState) {
+        this.scenarioState = scenarioState;
+        return this;
+    }
+
+    public String getPreviousScenarioState() {
+        return previousScenarioState;
+    }
+
+    public ScenarioRepresentationResponse setPreviousScenarioState(String previousScenarioState) {
+        this.previousScenarioState = previousScenarioState;
+        return this;
+    }
+
+    public String getLastAction() {
+        return lastAction;
+    }
+
+    public ScenarioRepresentationResponse setLastAction(String lastAction) {
+        this.lastAction = lastAction;
+        return this;
+    }
+
+    public String getOwnedBy() {
+        return ownedBy;
+    }
+
+    public ScenarioRepresentationResponse setOwnedBy(String ownedBy) {
+        this.ownedBy = ownedBy;
+        return this;
+    }
+
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public ScenarioRepresentationResponse setLocked(Boolean locked) {
+        this.locked = locked;
+        return this;
+    }
+
+    public Boolean getPublished() {
+        return published;
+    }
+
+    public ScenarioRepresentationResponse setPublished(Boolean published) {
+        this.published = published;
+        return this;
+    }
+
+    public String getOwnedByName() {
+        return ownedByName;
+    }
+
+    public ScenarioRepresentationResponse setOwnedByName(String ownedByName) {
+        this.ownedByName = ownedByName;
+        return this;
+    }
+
+    public String getCreatedByName() {
+        return createdByName;
+    }
+
+    public ScenarioRepresentationResponse setCreatedByName(String createdByName) {
+        this.createdByName = createdByName;
+        return this;
+    }
 }
