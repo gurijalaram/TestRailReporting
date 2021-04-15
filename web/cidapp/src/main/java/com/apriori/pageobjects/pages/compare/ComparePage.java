065f9fd47fb8c72a8d5ca5299e60d703ec4bdb32
cfrith <ciene.frith@gmail.com>
2021-04-15 10:04:18 +0100

    added new locators; added method to enter description;

--- web/cidapp/src/main/java/com/apriori/pageobjects/pages/compare/ComparePage.java
+++ web/cidapp/src/main/java/com/apriori/pageobjects/pages/compare/ComparePage.java
@@ -3,8 +3,11 @@ package com.apriori.pageobjects.pages.compare;
 import com.apriori.pageobjects.common.StatusIcon;
 import com.apriori.pageobjects.navtoolbars.CompareToolbar;
 import com.apriori.utils.PageUtils;
+import com.apriori.utils.enums.StatusIconEnum;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,12 @@ import org.slf4j.LoggerFactory;
 public class ComparePage extends CompareToolbar {
 
     private static final Logger logger = LoggerFactory.getLogger(ComparePage.class);
+
+    @FindBy(css = "[data-rbd-droppable-id='header-sections']")
+    private WebElement headerSections;
+
+    @FindBy(css = "[placeholder='Description']")
+    private WebElement descriptionInput;
 
     private PageUtils pageUtils;
     private WebDriver driver;
@@ -24,5 +33,28 @@ public class ComparePage extends CompareToolbar {
         this.pageUtils = new PageUtils(driver);
         logger.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
         PageFactory.initElements(driver, this);
+        pageUtils.waitForElementAppear(headerSections);
+    }
+
+    /**
+     * Checks icon is displayed
+     *
+     * @param icon - the icon
+     * @return true/false
+     */
+    public boolean isIconDisplayed(StatusIconEnum icon) {
+        return statusIcon.isIconDisplayed(icon);
+    }
+
+    /**
+     * Inputs the description
+     *
+     * @param description - the description
+     * @return current page object
+     */
+    public ComparePage inputDescription(String description) {
+        descriptionInput.clear();
+        descriptionInput.sendKeys(description);
+        return this;
     }
 }
