46e752edf18922e3823113edda08997090e34060
Ben <bhegan@apriori.com>
2021-05-06 17:02:43 +0100

    Trial of a fix

--- web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/GenericReportPage.java
+++ web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/GenericReportPage.java
@@ -1007,6 +1007,7 @@ public class GenericReportPage extends ReportsPageHeader {
      * @return current page object
      */
     public GenericReportPage waitForCorrectExportSetListCount(String listName, String expectedCount) {
+        pageUtils.waitFor(1000);
         waitForCorrectAvailableSelectedCount(listName, "Available: ", expectedCount);
         waitForCorrectAvailableSelectedCount(listName, "Selected: ", expectedCount);
         return this;
