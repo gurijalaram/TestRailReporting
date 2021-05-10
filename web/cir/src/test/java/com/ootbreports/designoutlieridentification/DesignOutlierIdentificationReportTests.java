856d0be644c3387d40998e8691de7bf6fc11effb
Ben <bhegan@apriori.com>
2021-05-10 17:07:23 +0100

    Fix compile error

--- web/cir/src/test/java/com/ootbreports/designoutlieridentification/DesignOutlierIdentificationReportTests.java
+++ web/cir/src/test/java/com/ootbreports/designoutlieridentification/DesignOutlierIdentificationReportTests.java
@@ -17,7 +17,6 @@ import com.navigation.CommonReportTests;
 import io.qameta.allure.Description;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import testsuites.suiteinterface.CiaCirTestDevTest;
 import testsuites.suiteinterface.ReportsTest;
 
 import java.math.BigDecimal;
@@ -79,7 +78,7 @@ public class DesignOutlierIdentificationReportTests extends TestBase {
     }
 
     @Test
-    @Category({ReportsTest.class)
+    @Category(ReportsTest.class)
     @TestRail(testCaseId = {"7385"})
     @Description("Verify mass metric - rough mass - Design Outlier Identification Report")
     public void testMassMetricRoughMass() {
