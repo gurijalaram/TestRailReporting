b6ceb429d66ef54a55c89c8905423e8d5e253402
vzarovnyi <vzarovnyi@apriori.com>
2021-05-11 15:25:55 +0300

    Updated base tests

--- microservices/vds/src/test/java/com/apriori/vds/tests/ConfigurationsTest.java
+++ microservices/vds/src/test/java/com/apriori/vds/tests/ConfigurationsTest.java
@@ -5,9 +5,12 @@ import com.apriori.utils.http2.builder.common.entity.RequestEntity;
 import com.apriori.utils.http2.builder.service.HTTP2Request;
 import com.apriori.utils.http2.utils.RequestEntityUtil;
 import com.apriori.vds.entity.enums.VDSAPIEnum;
+import com.apriori.vds.entity.response.access.control.AccessControlGroupItems;
+import com.apriori.vds.tests.util.VDSRequestEntityUtil;
 import com.apriori.vds.tests.util.VDSTestUtil;
 
 import io.qameta.allure.Description;
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -18,30 +21,36 @@ public class ConfigurationsTest extends VDSTestUtil {
     @TestRail(testCaseId = {"7929"})
     @Description("Returns a list of CustomerConfigurations for a customer.")
     public void getConfigurations() {
-        RequestEntity requestEntity = RequestEntityUtil.init(VDSAPIEnum.GET_CONFIGURATIONS, null);
+        RequestEntity requestEntity = VDSRequestEntityUtil.initWithSharedSecret(VDSAPIEnum.GET_CONFIGURATIONS, null);
 
-        HTTP2Request.build(requestEntity).get();
+        validateResponseCodeByExpectingAndRealCode(HttpStatus.SC_OK,
+            HTTP2Request.build(requestEntity).get().getStatusCode()
+        );
     }
 
     @Test
     @TestRail(testCaseId = {"7930"})
     @Description("Get a specific CustomerConfiguration.")
     public void getConfigurationsByIdentity() {
-        RequestEntity requestEntity =
-            RequestEntityUtil.init(VDSAPIEnum.GET_CONFIGURATIONS_BY_IDENTITY, null)
+        RequestEntity requestEntity = VDSRequestEntityUtil.initWithSharedSecret(VDSAPIEnum.GET_CONFIGURATIONS_BY_IDENTITY, null)
             .inlineVariables(Collections.singletonList(""));
 
-        HTTP2Request.build(requestEntity).get();
+        validateResponseCodeByExpectingAndRealCode(HttpStatus.SC_OK,
+            HTTP2Request.build(requestEntity).get().getStatusCode()
+        );
     }
 
     @Test
     @TestRail(testCaseId = {"7931"})
     @Description("Replaces a CustomerConfiguration for a customer. Creates it if it is missing.")
     public void putConfiguration() {
-        RequestEntity requestEntity =
-            RequestEntityUtil.init(VDSAPIEnum.PUT_CONFIGURATION, null)
-                .inlineVariables(Collections.singletonList(""));
+        RequestEntity requestEntity = VDSRequestEntityUtil.initWithSharedSecret(VDSAPIEnum.PUT_CONFIGURATION, null)
+            .customBody(
+            " 'customerConfiguration':{ 'configurationType':'UDAS' }");
 
-        HTTP2Request.build(requestEntity).get();
+
+        validateResponseCodeByExpectingAndRealCode(HttpStatus.SC_OK,
+            HTTP2Request.build(requestEntity).put().getStatusCode()
+        );
     }
 }
