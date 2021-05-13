a6171200600d79db312c014f5443708cdcffa77c
vzarovnyi <vzarovnyi@apriori.com>
2021-05-13 11:38:18 +0300

    Removed not used data

--- utils/src/main/java/com/apriori/utils/http2/builder/service/ConnectionManager.java
+++ utils/src/main/java/com/apriori/utils/http2/builder/service/ConnectionManager.java
@@ -77,84 +77,6 @@ class ConnectionManager<T> {
 
     }
 
-    public static Object postMultiPartFormData(String uri, Map<String, String> params, Class klass, String cloudContext) throws IOException {
-        return postMultiPartFormData(uri, params, klass, null, cloudContext);
-
-    }
-
-    public static Object postMultiPartFormData(String uri, Map<String, String> params, Class klass, File file, String cloudContext)
-        throws IOException {
-        URL url = new URL(uri);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-        conn.setRequestMethod("POST");
-        String boundary = "----------------------------544615151549871231842369";
-        conn.setDoOutput(true);
-        conn.setRequestProperty("Content-Type",
-            "multipart/form-data; boundary=" + boundary);
-        conn.setRequestProperty("ap-cloud-context", cloudContext);
-
-        OutputStream outputStream = conn.getOutputStream();
-        BufferedWriter bodyWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-
-        for (Object key : params.keySet()) {
-            bodyWriter.write("--" + boundary + "\r\n");
-            bodyWriter.write("Content-Disposition: form-data; name=\"" + key + "\"\r\n\r\n" + params.get(key));
-            bodyWriter.write("\r\n");
-            bodyWriter.flush();
-        }
-
-        if (file != null) {
-
-            bodyWriter.write("--" + boundary + "\r\n");
-            bodyWriter.write("Content-Disposition: form-data; name=\"attachment\"; filename=\""
-                + file.getName() + "\"");
-            bodyWriter.write("\r\n\r\n");
-            bodyWriter.flush();
-
-            InputStream istreamFile = new FileInputStream(file);
-            Integer bytesRead;
-            byte[] buffer = new byte[1024];
-            while ((bytesRead = istreamFile.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-
-            outputStream.flush();
-        }
-
-
-        bodyWriter.write("--" + boundary + "-\r\n");
-        bodyWriter.flush();
-
-        outputStream.close();
-        bodyWriter.close();
-
-        int status = conn.getResponseCode();
-
-        InputStream inputStream;
-        if (status != HttpStatus.SC_CREATED) {
-            inputStream = conn.getErrorStream();
-            Assert.fail("Error code was not expected (" + status + ")");
-        } else {
-            inputStream = conn.getInputStream();
-        }
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
-            inputStream, StandardCharsets.UTF_8));
-
-        String line;
-        StringBuilder sb = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            sb.append(line);
-            sb.append(System.getProperty("line.separator"));
-        }
-
-        reader.close();
-
-        Object response = JsonManager.deserializeJsonFromString(sb.toString(), klass);
-        return response;
-    }
-
     private RequestSpecification createRequestSpecification() {
         RequestSpecBuilder builder = new RequestSpecBuilder();
 
@@ -268,14 +190,6 @@ class ConnectionManager<T> {
                 .followRedirection(false)
                 .build();
 
-            //                //RequestEntityUtil.init(AuthEndpointEnum.POST_AUTH, AuthenticateJSON.class)
-            //                .addXwwwwFormUrlEncoded(
-            //                    AuthorizationFormUtil.getDefaultAuthorizationForm(requestEntity.userAuthenticationEntity().getEmailAddress(),
-            //                        requestEntity.userAuthenticationEntity().getPassword()
-            //                ))
-            //                .autoLogin(false)
-            //                .followRedirection(false);
-
             String authToken =
                 ((AuthenticateJSON) HTTP2Request.build(authEntity).post()
                     .getResponseEntity()
@@ -286,19 +200,6 @@ class ConnectionManager<T> {
 
         return authTokens.get(requestEntity.userAuthenticationEntity().getEmailAddress());
 
-    }
-
-    private UserAuthenticationEntity initUserConnectionData(final String username, final String password) {
-        return new UserAuthenticationEntity(
-            username,
-            password,
-            null,
-            "password",
-            "apriori-web-cost",
-            "donotusethiskey",
-            "tenantGroup%3Ddefault%20tenant%3Ddefault",
-            false
-        );
     }
 
     private <T> ResponseWrapper<T> resultOf(ValidatableResponse response) {
