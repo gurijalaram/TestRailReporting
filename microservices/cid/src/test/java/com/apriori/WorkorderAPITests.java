b3f026cde64ca93e6535e91eb5834035782a03aa
Ben <bhegan@apriori.com>
2021-05-18 14:29:23 +0100

    Added issue tag and removed comments

--- microservices/cid/src/test/java/com/apriori/WorkorderAPITests.java
+++ microservices/cid/src/test/java/com/apriori/WorkorderAPITests.java
@@ -19,6 +19,7 @@ import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.json.utils.JsonManager;
 
 import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -34,6 +35,7 @@ public class WorkorderAPITests {
     }
 
     @Test
+    @Issue("AP-69600")
     @Category(CidAPITest.class)
     @TestRail(testCaseId = {"6933"})
     @Description("Upload a part, load CAD Metadata, and generate part images")
@@ -64,6 +66,7 @@ public class WorkorderAPITests {
     }
 
     @Test
+    @Issue("AP-69600")
     @Category(CidAPITest.class)
     @TestRail(testCaseId = {"7697"})
     @Description("Get image after each iteration - Upload, Cost, Publish")
@@ -95,6 +98,7 @@ public class WorkorderAPITests {
     }
 
     @Test
+    @Issue("AP-69600")
     @Category(CidAPITest.class)
     @TestRail(testCaseId = {"7710"})
     @Description("Upload a part, load CAD Metadata, and generate assembly images")
@@ -102,49 +106,42 @@ public class WorkorderAPITests {
         FileUploadResources fileUploadResources = new FileUploadResources();
         String scenarioName = new GenerateStringUtil().generateScenarioName();
 
-        // upload component one - 3574727
         FileResponse fileResponseComponentOne = fileUploadResources.initialisePartUpload(
                 "3574727.prt",
                 ProcessGroupEnum.ASSEMBLY.getProcessGroup()
         );
-        FileUploadOutputs fileUploadOutputsComponentOne = fileUploadResources.uploadPart(
+        fileUploadResources.uploadPart(
                 fileResponseComponentOne,
                 scenarioName
         );
 
-        // upload component two - 3574875
         FileResponse fileResponseComponentTwo = fileUploadResources.initialisePartUpload(
                 "3574875.prt",
                 ProcessGroupEnum.ASSEMBLY.getProcessGroup()
         );
-        FileUploadOutputs fileUploadOutputsComponentTwo = fileUploadResources.uploadPart(
+        fileUploadResources.uploadPart(
                 fileResponseComponentTwo,
                 scenarioName
         );
 
-        // upload assembly
         FileResponse fileResponseAssembly = fileUploadResources.initialisePartUpload(
                 "PatternThreadHoles.asm",
                 ProcessGroupEnum.ASSEMBLY.getProcessGroup()
         );
-        FileUploadOutputs fileUploadOutputsAssembly = fileUploadResources.uploadPart(
+        fileUploadResources.uploadPart(
                 fileResponseAssembly,
                 scenarioName
         );
 
-        // load cad metadata for component one - pin
         LoadCadMetadataOutputs loadCadMetadataOutputsComponentOne = fileUploadResources.loadCadMetadata(
                 fileResponseComponentOne);
 
-        // load cad metadata for component two - small ring
         LoadCadMetadataOutputs loadCadMetadataOutputsComponentTwo = fileUploadResources.loadCadMetadata(
                 fileResponseComponentTwo);
 
-        // load cad metadata for assembly
         LoadCadMetadataOutputs loadCadMetadataOutputsAssembly = fileUploadResources.loadCadMetadata(
                 fileResponseAssembly);
 
-        // generate assembly images call
         GenerateAssemblyImagesOutputs generateAssemblyImagesOutputs = fileUploadResources.generateAssemblyImages(
                 fileResponseAssembly,
                 loadCadMetadataOutputsAssembly,
@@ -154,7 +151,6 @@ public class WorkorderAPITests {
 
         ArrayList<String> images = generateAssemblyImagesOutputs.getGeneratedWebImages();
 
-        // assertions
         for (String image : images) {
             fileUploadResources.imageValidation(image);
         }
