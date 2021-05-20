f91ff63ac74f793ea26286476fd373260beba2b1
cfrith <ciene.frith@gmail.com>
2021-05-20 09:13:30 +0100

    minor change;

--- web/cidapp/src/test/java/testsuites/CIDSmokeTestSuite.java
+++ web/cidapp/src/test/java/testsuites/CIDSmokeTestSuite.java
@@ -11,33 +11,33 @@ import org.junit.runners.Suite;
 @ProjectRunID("562")
 @RunWith(CategorySuiteRunner.class)
 @Suite.SuiteClasses({
-//    CostAllCadTests.class,
-//    CostScenarioTests.class,
+    /*CostAllCadTests.class,
+    CostScenarioTests.class,*/
     ListOfDigitalFactoryTests.class,
-//    ListProcessGroupTests.class,
-//    NewScenarioNameTests.class,
-//    ProcessGroupsTests.class,
-//    ReCostScenarioTests.class,
-//    SecondaryProcessTests.class,
-//    ChangeMaterialSelectionTests.class,
-//    MaterialStockTests.class,
-//    UploadComponentTests.class,
-//    UploadTests.class,
-//    HelpTests.class,
-//    LoginTests.class,
-//    DecimalPlaceTests.class,
-//    UploadAssembliesTests.class,
-//    PublishExistingCostedTests.class,
-//    PreviewPanelTests.class,
-//    DFMRiskTests.class,
-//    DeleteTests.class,
-//    DFMRiskTests.class,
+    /*ListProcessGroupTests.class,
+    NewScenarioNameTests.class,
+    ProcessGroupsTests.class,
+    ReCostScenarioTests.class,
+    SecondaryProcessTests.class,
+    ChangeMaterialSelectionTests.class,
+    MaterialStockTests.class,
+    UploadComponentTests.class,
+    UploadTests.class,
+    HelpTests.class,
+    LoginTests.class,
+    DecimalPlaceTests.class,
+    UploadAssembliesTests.class,
+    PublishExistingCostedTests.class,
+    PreviewPanelTests.class,
+    DFMRiskTests.class,
+    DeleteTests.class,
+    DFMRiskTests.class,*/
     PublishTests.class,
-//    SheetMetalDTCTests.class,
-//    DTCPlasticMouldingTests.class,
-//    SheetMetalDTCTests.class,
-//    ActionsTests.class,
-//    TableHeadersTests.class
+    /*SheetMetalDTCTests.class,
+    DTCPlasticMouldingTests.class,
+    SheetMetalDTCTests.class,
+    ActionsTests.class,
+    TableHeadersTests.class*/
 })
 public class CIDSmokeTestSuite {
 }
