b97e38f257a87298ec9dba23b17b00004b996382
Ben <bhegan@apriori.com>
2021-05-21 16:10:16 +0100

    Removed unnecessary wait

--- web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/ScenarioComparisonReportPage.java
+++ web/cir/src/main/java/com/apriori/pageobjects/pages/view/reports/ScenarioComparisonReportPage.java
@@ -96,10 +96,10 @@ public class ScenarioComparisonReportPage extends GenericReportPage {
         By filteredLocator = By.xpath("((//div[@title='Scenarios to Compare']//ul)[1]/li[contains(@title, '(Initial)')])[1]");
         pageUtils.waitForElementToAppear(filteredLocator);
 
-        GenericReportPage genericReportPage = new GenericReportPage(driver);
+        /*GenericReportPage genericReportPage = new GenericReportPage(driver);
         String expectedCount = driver.getCurrentUrl().contains("01") ? "149" : "150";
         genericReportPage.waitForCorrectAvailableSelectedCount(ListNameEnum.SCENARIOS_TO_COMPARE.getListName(),
-                "Available: ", expectedCount);
+                "Available: ", expectedCount);*/
     }
 
     /**
