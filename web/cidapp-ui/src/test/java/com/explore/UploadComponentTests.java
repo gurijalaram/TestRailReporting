a37259f82473710f3d4b87611635ce48843a9178
moyaparker <mparker@apriori.com>
2021-05-26 08:50:52 +0100

    updated tests to work with 21.1

--- web/cidapp-ui/src/test/java/com/explore/UploadComponentTests.java
+++ web/cidapp-ui/src/test/java/com/explore/UploadComponentTests.java
@@ -1,18 +1,19 @@
 package com.explore;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import com.apriori.pageobjects.navtoolbars.EvaluateToolbar;
+import com.apriori.pageobjects.pages.explore.ExplorePage;
 import com.apriori.pageobjects.pages.login.CidAppLoginPage;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
-import com.apriori.utils.enums.NewCostingLabelEnum;
 import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import testsuites.suiteinterface.SmokeTests;
@@ -23,6 +24,7 @@ public class UploadComponentTests extends TestBase {
 
     private File resourceFile;
     private CidAppLoginPage loginPage;
+    private ExplorePage explorePage;
     private EvaluateToolbar evaluateToolbar;
 
     @Test
@@ -32,11 +34,13 @@ public class UploadComponentTests extends TestBase {
         final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.CASTING;
 
         resourceFile = FileResourceUtil.getCloudFile(processGroupEnum,"Casting.prt");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
 
         loginPage = new CidAppLoginPage(driver);
-        evaluateToolbar = loginPage.login(UserUtil.getUser())
-            .uploadComponentAndSubmit(new GenerateStringUtil().generateScenarioName(), resourceFile, EvaluateToolbar.class);
+        explorePage = loginPage.login(UserUtil.getUser())
+            .uploadComponentAndSubmit(scenarioName, resourceFile, ExplorePage.class)
+            .inputFilter("Recent");
 
-        assertThat(evaluateToolbar.isCostLabel(NewCostingLabelEnum.CREATED), is(true));
+        assertThat(explorePage.getListOfScenarios("CASTING", scenarioName), Matchers.is(equalTo(1)));
     }
 }
