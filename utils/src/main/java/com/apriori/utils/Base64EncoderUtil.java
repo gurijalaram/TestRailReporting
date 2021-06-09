a3f1682888a4e4f1634defee9da57e057b23f5d5
cfrith <ciene.frith@gmail.com>
2021-06-09 09:21:12 +0100

    minor change;

--- utils/src/main/java/com/apriori/utils/Base64EncoderUtil.java
+++ utils/src/main/java/com/apriori/utils/Base64EncoderUtil.java
@@ -8,6 +8,9 @@ import org.apache.commons.io.FileUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * @author cfrith
+ */
 public class Base64EncoderUtil {
     /**
      * Encodes file to base64
