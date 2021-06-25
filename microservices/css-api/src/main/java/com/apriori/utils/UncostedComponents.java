ad9b1a9fa098beb039148f1ed5f7c96ddbc4813e
cfrith <ciene.frith@gmail.com>
2021-06-25 11:17:26 +0100

    changed filtering;

--- microservices/css-api/src/main/java/com/apriori/utils/UncostedComponents.java
+++ microservices/css-api/src/main/java/com/apriori/utils/UncostedComponents.java
@@ -81,7 +81,8 @@ public class UncostedComponents {
                     if (scenarioRepresentation.getResponseEntity().getItems().get(0).getScenarioState().equals(verifiedState.toUpperCase())) {
                         Assert.assertEquals("The component response should be okay.", HttpStatus.SC_OK, scenarioRepresentation.getStatusCode());
 
-                        return scenarioRepresentation.getResponseEntity().getItems().stream().filter(x -> !x.getComponentType().equals("UNKNOWN")).collect(Collectors.toList());
+                        return scenarioRepresentation.getResponseEntity().getItems().stream().filter(x -> x.getComponentType().equals("PARTS")
+                            && x.getComponentType().equals("ROLL_UP")).collect(Collectors.toList());
                     }
                 }
             } while (currentCount++ <= attemptsCount);
