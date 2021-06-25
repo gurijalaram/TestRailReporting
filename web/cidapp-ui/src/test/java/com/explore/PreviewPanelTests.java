7c6091ec6f6e610fa001e21c2708fa24a88301f9
cfrith <ciene.frith@gmail.com>
2021-06-25 12:02:50 +0100

    fixed call to pg;

--- web/cidapp-ui/src/test/java/com/explore/PreviewPanelTests.java
+++ web/cidapp-ui/src/test/java/com/explore/PreviewPanelTests.java
@@ -47,7 +47,7 @@ public class PreviewPanelTests extends TestBase {
         loginPage = new CidAppLoginPage(driver);
         previewPage = loginPage.login(currentUser)
             .uploadComponentAndOpen(partName, testScenarioName, resourceFile, currentUser)
-            .selectProcessGroup(processGroupEnum.PLASTIC_MOLDING.getProcessGroup())
+            .selectProcessGroup(processGroupEnum.getProcessGroup())
             .openMaterialSelectorTable()
             .search("ABS,10")
             .selectMaterial("ABS, 10% Glass")
