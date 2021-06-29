6104e136a5dae5b6b896169b350ee902fb85adf0
cfrith <ciene.frith@gmail.com>
2021-06-29 12:48:09 +0100

    refactor adding customer to before class; separated tests into 2 classes;

--- microservices/cds-api/src/test/java/com/apriori/cds/tests/CdsCustomersTests.java
+++ microservices/cds-api/src/test/java/com/apriori/cds/tests/CdsCustomersTests.java
@@ -1,6 +1,5 @@
 package com.apriori.cds.tests;
 
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,26 +7,19 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 
-import com.apriori.cds.objects.response.Applications;
 import com.apriori.cds.objects.response.Customer;
 import com.apriori.cds.objects.response.Customers;
 import com.apriori.cds.tests.utils.CdsTestUtil;
 import com.apriori.cds.utils.Constants;
 import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.TestRail;
-import com.apriori.utils.http.builder.common.entity.RequestEntity;
-import com.apriori.utils.http.builder.dao.GenericRequestUtil;
-import com.apriori.utils.http.builder.service.RequestAreaApi;
 import com.apriori.utils.http.utils.ResponseWrapper;
 
 import io.qameta.allure.Description;
 import org.apache.http.HttpStatus;
-import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
 
 public class CdsCustomersTests {
     private String url;
@@ -76,71 +68,5 @@ public class CdsCustomersTests {
         customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
 
         assertThat(customer.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"3278"})
-    @Description("Get customer by Identity")
-    public void getCustomerByIdentity() {
-        String customerName = generateStringUtil.generateCustomerName();
-        String cloudRef = generateStringUtil.generateCloudReference();
-        String salesForceId = generateStringUtil.generateSalesForceId();
-        String emailPattern = "\\S+@".concat(customerName);
-
-        ResponseWrapper<Customer> customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
-        assertThat(customer.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
-
-        customerIdentity = customer.getResponseEntity().getResponse().getIdentity();
-        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
-
-        ResponseWrapper<Customer> response = cdsTestUtil.getCommonRequest(customerIdentityEndpoint, Customer.class);
-        assertThat(response.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
-        assertThat(response.getResponseEntity().getResponse().getEmailRegexPatterns(), is(Arrays.asList(emailPattern + ".com", emailPattern + ".co.uk")));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"5957"})
-    @Description("Get customer applications")
-    public void getCustomersApplications() {
-        String customerName = generateStringUtil.generateCustomerName();
-        String cloudRef = generateStringUtil.generateCloudReference();
-        String salesForceId = generateStringUtil.generateSalesForceId();
-        String emailPattern = "\\S+@".concat(customerName);
-
-        ResponseWrapper<Customer> customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
-        assertThat(customer.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
-
-        customerIdentity = customer.getResponseEntity().getResponse().getIdentity();
-        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
-        String applicationsEndpoint = String.format(url, String.format("customers/%s/applications", customerIdentity));
-
-        ResponseWrapper<Applications> response = cdsTestUtil.getCommonRequest(applicationsEndpoint, Applications.class);
-        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
-        assertThat(response.getResponseEntity().getResponse().getTotalItemCount(), CoreMatchers.is(equalTo(0)));
-    }
-
-    @Test
-    @TestRail(testCaseId = {"5305"})
-    @Description("Update customer info by id")
-    public void updateCustomerInfoId() {
-        String customerName = generateStringUtil.generateCustomerName();
-        String cloudRef = generateStringUtil.generateCloudReference();
-        String salesForceId = generateStringUtil.generateSalesForceId();
-        String emailPattern = "\\S+@".concat(customerName);
-        String updatedEmailPattern = "\\S+@".concat(generateStringUtil.generateCustomerName());
-
-        ResponseWrapper<Customer> customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
-
-        customerIdentity = customer.getResponseEntity().getResponse().getIdentity();
-        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
-
-        RequestEntity requestEntity = RequestEntity.init(customerIdentityEndpoint, Customer.class)
-            .setHeaders("Content-Type", "application/json")
-            .setBody("customer",
-                new Customer().setEmailRegexPatterns(Arrays.asList(updatedEmailPattern + ".com", updatedEmailPattern + ".co.uk")));
-
-        ResponseWrapper<Customer> updatedEmail = GenericRequestUtil.patch(requestEntity, new RequestAreaApi());
-
-        assertThat(updatedEmail.getResponseEntity().getResponse().getEmailRegexPatterns(), hasItem(updatedEmailPattern + ".com"));
     }
 }
---
+++ microservices/cds-api/src/test/java/com/apriori/cds/tests/CdsGetCustomerTests.java
@@ -0,0 +1,103 @@
+package com.apriori.cds.tests;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import com.apriori.cds.objects.response.Applications;
+import com.apriori.cds.objects.response.Customer;
+import com.apriori.cds.tests.utils.CdsTestUtil;
+import com.apriori.cds.utils.Constants;
+import com.apriori.utils.GenerateStringUtil;
+import com.apriori.utils.TestRail;
+import com.apriori.utils.http.builder.common.entity.RequestEntity;
+import com.apriori.utils.http.builder.dao.GenericRequestUtil;
+import com.apriori.utils.http.builder.service.RequestAreaApi;
+import com.apriori.utils.http.utils.ResponseWrapper;
+
+import io.qameta.allure.Description;
+import org.apache.http.HttpStatus;
+import org.hamcrest.CoreMatchers;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class CdsGetCustomerTests {
+
+    private static GenerateStringUtil generateStringUtil = new GenerateStringUtil();
+    private static CdsTestUtil cdsTestUtil = new CdsTestUtil();
+    private static ResponseWrapper<Customer> customer;
+    private static String customerName;
+    private static String cloudRef;
+    private static String salesForceId;
+    private static String emailPattern;
+    private static String customerIdentity;
+    private static String url;
+    private static String customerIdentityEndpoint;
+    private static String updatedEmailPattern;
+
+    @BeforeClass
+    public static void setDetails() {
+        url = Constants.getServiceUrl();
+
+        customerName = generateStringUtil.generateCustomerName();
+        cloudRef = generateStringUtil.generateCloudReference();
+        salesForceId = generateStringUtil.generateSalesForceId();
+        emailPattern = "\\S+@".concat(customerName);
+        updatedEmailPattern = "\\S+@".concat(generateStringUtil.generateCustomerName());
+
+        customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
+        customerIdentity = customer.getResponseEntity().getResponse().getIdentity();
+        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        if (customerIdentityEndpoint != null) {
+            cdsTestUtil.delete(customerIdentityEndpoint);
+        }
+    }
+
+    @Test
+    @TestRail(testCaseId = {"3278"})
+    @Description("Get customer by Identity")
+    public void getCustomerByIdentity() {
+        assertThat(customer.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
+
+        ResponseWrapper<Customer> response = cdsTestUtil.getCommonRequest(customerIdentityEndpoint, Customer.class);
+        assertThat(response.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
+        assertThat(response.getResponseEntity().getResponse().getEmailRegexPatterns(), is(Arrays.asList(emailPattern + ".com", emailPattern + ".co.uk")));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"5957"})
+    @Description("Get customer applications")
+    public void getCustomersApplications() {
+        assertThat(customer.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
+
+        String applicationsEndpoint = String.format(url, String.format("customers/%s/applications", customerIdentity));
+
+        ResponseWrapper<Applications> response = cdsTestUtil.getCommonRequest(applicationsEndpoint, Applications.class);
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
+        assertThat(response.getResponseEntity().getResponse().getTotalItemCount(), CoreMatchers.is(equalTo(0)));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"5305"})
+    @Description("Update customer info by id")
+    public void updateCustomerInfoId() {
+        assertThat(customer.getResponseEntity().getResponse().getName(), is(equalTo(customerName)));
+
+        RequestEntity requestEntity = RequestEntity.init(customerIdentityEndpoint, Customer.class)
+            .setHeaders("Content-Type", "application/json")
+            .setBody("customer",
+                new Customer().setEmailRegexPatterns(Arrays.asList(updatedEmailPattern + ".com", updatedEmailPattern + ".co.uk")));
+
+        ResponseWrapper<Customer> updatedEmail = GenericRequestUtil.patch(requestEntity, new RequestAreaApi());
+
+        assertThat(updatedEmail.getResponseEntity().getResponse().getEmailRegexPatterns(), hasItem(updatedEmailPattern + ".com"));
+    }
+}
