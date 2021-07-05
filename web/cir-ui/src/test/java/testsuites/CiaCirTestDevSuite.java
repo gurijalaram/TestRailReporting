d50bcf4819ba4aa2e699929fcaeb5f03f0d1b71f
Ben <bhegan@apriori.com>
2021-07-05 17:09:40 +0100

    Fixed test suite

--- web/cir-ui/src/test/java/testsuites/CiaCirTestDevSuite.java
+++ web/cir-ui/src/test/java/testsuites/CiaCirTestDevSuite.java
@@ -2,8 +2,7 @@ package testsuites;
 
 import com.apriori.utils.runner.CategorySuiteRunner;
 
-import com.ootbreports.designoutlieridentification.DesignOutlierIdentificationDetailsReportTests;
-import com.ootbreports.designoutlieridentification.DesignOutlierIdentificationReportTests;
+import com.ootbreports.targetquotedcosttrend.TargetAndQuotedCostTrendReportTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -12,8 +11,7 @@ import testsuites.suiteinterface.CiaCirTestDevTest;
 @RunWith(CategorySuiteRunner.class)
 @Categories.IncludeCategory(CiaCirTestDevTest.class)
 @Suite.SuiteClasses({
-        DesignOutlierIdentificationReportTests.class,
-        DesignOutlierIdentificationDetailsReportTests.class
+        TargetAndQuotedCostTrendReportTests.class
 })
 
 public class CiaCirTestDevSuite {
