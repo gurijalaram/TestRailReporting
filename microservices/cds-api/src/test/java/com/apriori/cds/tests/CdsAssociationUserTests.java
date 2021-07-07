5b8850080908b38ed004f56864fe9d973e6a9e3e
cfrith <ciene.frith@gmail.com>
2021-07-07 08:59:30 +0100

    refactor 'customer' to before class;

--- microservices/cds-api/src/test/java/com/apriori/cds/tests/CdsAssociationUserTests.java
+++ microservices/cds-api/src/test/java/com/apriori/cds/tests/CdsAssociationUserTests.java
@@ -16,29 +16,42 @@ import com.apriori.utils.http.utils.ResponseWrapper;
 
 import io.qameta.allure.Description;
 import org.apache.http.HttpStatus;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.stream.Collectors;
 
-public class CdsAssociationUserTests  {
-    private String url;
+public class CdsAssociationUserTests {
+    private static String customerAssociationUserIdentity;
+    private static String customerAssociationUserIdentityEndpoint;
+    private static GenerateStringUtil generateStringUtil = new GenerateStringUtil();
+    private static CdsTestUtil cdsTestUtil = new CdsTestUtil();
+    private static ResponseWrapper<Customer> customer;
+    private static String customerName;
+    private static String cloudRef;
+    private static String salesForceId;
+    private static String emailPattern;
+    private static String customerIdentity;
+    private static String url;
+    private static String customerIdentityEndpoint;
 
-    private String customerIdentity;
-    private String customerIdentityEndpoint;
-    private String customerAssociationUserIdentity;
-    private String customerAssociationUserIdentityEndpoint;
-    private GenerateStringUtil generateStringUtil = new GenerateStringUtil();
-    private CdsTestUtil cdsTestUtil = new CdsTestUtil();
-
-    @Before
-    public void setServiceUrl() {
+    @BeforeClass
+    public static void setDetails() {
         url = Constants.getServiceUrl();
+
+        customerName = generateStringUtil.generateCustomerName();
+        cloudRef = generateStringUtil.generateCloudReference();
+        salesForceId = generateStringUtil.generateSalesForceId();
+        emailPattern = "\\S+@".concat(customerName);
+
+        customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
+        customerIdentity = customer.getResponseEntity().getIdentity();
+        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
     }
 
-    @After
-    public void cleanUp() {
+    @AfterClass
+    public static void cleanUp() {
         if (customerAssociationUserIdentityEndpoint != null) {
             cdsTestUtil.delete(customerAssociationUserIdentityEndpoint);
         }
@@ -52,19 +65,8 @@ public class CdsAssociationUserTests  {
     @TestRail(testCaseId = {"5959"})
     @Description("Get customer association for apriori Internal")
     public void addCustomerUserAssociation() {
-
-        String customerName = generateStringUtil.generateCustomerName();
-        String cloudRef = generateStringUtil.generateCloudReference();
-        String salesForceId = generateStringUtil.generateSalesForceId();
-        String emailPattern = "\\S+@".concat(customerName);
         String aPStaffIdentity = Constants.getUserIdentity();
         String aPCustomerIdentity = Constants.getAPrioriInternalCustomerIdentity();
-
-        ResponseWrapper<Customer> customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
-        assertThat(customer.getResponseEntity().getName(), is(equalTo(customerName)));
-
-        customerIdentity = customer.getResponseEntity().getIdentity();
-        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
 
         String associationsEndpoint = String.format(url + "&pageSize=5000", String.format("customers/%s/customer-associations", aPCustomerIdentity));
         ResponseWrapper<CustomerAssociationResponse> response = cdsTestUtil.getCommonRequest(associationsEndpoint, CustomerAssociationResponse.class);
@@ -80,19 +82,8 @@ public class CdsAssociationUserTests  {
     @TestRail(testCaseId = {"5965"})
     @Description("Get users associated for customer")
     public void getAssociationUsers() {
-
-        String customerName = generateStringUtil.generateCustomerName();
-        String cloudRef = generateStringUtil.generateCloudReference();
-        String salesForceId = generateStringUtil.generateSalesForceId();
-        String emailPattern = "\\S+@".concat(customerName);
         String aPStaffIdentity = Constants.getUserIdentity();
         String aPCustomerIdentity = Constants.getAPrioriInternalCustomerIdentity();
-
-        ResponseWrapper<Customer> customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
-        assertThat(customer.getResponseEntity().getName(), is(equalTo(customerName)));
-
-        customerIdentity = customer.getResponseEntity().getIdentity();
-        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
 
         String associationsEndpoint = String.format(url + "&pageSize=5000", String.format("customers/%s/customer-associations", aPCustomerIdentity));
         ResponseWrapper<CustomerAssociationResponse> response = cdsTestUtil.getCommonRequest(associationsEndpoint, CustomerAssociationResponse.class);
@@ -113,19 +104,8 @@ public class CdsAssociationUserTests  {
     @TestRail(testCaseId = {"5964"})
     @Description("Get user details for association")
     public void getAssociationByUserIdentity() {
-
-        String customerName = generateStringUtil.generateCustomerName();
-        String cloudRef = generateStringUtil.generateCloudReference();
-        String salesForceId = generateStringUtil.generateSalesForceId();
-        String emailPattern = "\\S+@".concat(customerName);
         String aPStaffIdentity = Constants.getUserIdentity();
         String aPCustomerIdentity = Constants.getAPrioriInternalCustomerIdentity();
-
-        ResponseWrapper<Customer> customer = cdsTestUtil.addCustomer(customerName, cloudRef, salesForceId, emailPattern);
-        assertThat(customer.getResponseEntity().getName(), is(equalTo(customerName)));
-
-        customerIdentity = customer.getResponseEntity().getIdentity();
-        customerIdentityEndpoint = String.format(url, String.format("customers/%s", customerIdentity));
 
         String associationsEndpoint = String.format(url + "&pageSize=5000", String.format("customers/%s/customer-associations", aPCustomerIdentity));
         ResponseWrapper<CustomerAssociationResponse> response = cdsTestUtil.getCommonRequest(associationsEndpoint, CustomerAssociationResponse.class);
