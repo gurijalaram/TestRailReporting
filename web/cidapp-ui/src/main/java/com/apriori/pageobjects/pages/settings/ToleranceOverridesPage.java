d8cf356398875bedfb5eaddd6545f3d230ce7d59
cfrith <ciene.frith@gmail.com>
2021-07-08 12:00:54 +0100

    added assertion to isloaded;

--- web/cidapp-ui/src/main/java/com/apriori/pageobjects/pages/settings/ToleranceOverridesPage.java
+++ web/cidapp-ui/src/main/java/com/apriori/pageobjects/pages/settings/ToleranceOverridesPage.java
@@ -1,16 +1,23 @@
 package com.apriori.pageobjects.pages.settings;
 
+import static org.junit.Assert.assertTrue;
+
 import com.apriori.pageobjects.common.ModalDialogController;
 import com.apriori.pageobjects.pages.evaluate.EvaluatePage;
 import com.apriori.utils.PageUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
 
 @Slf4j
 public class ToleranceOverridesPage extends LoadableComponent<ToleranceOverridesPage> {
+
+    @FindBy(css = ".section-header .left")
+    private WebElement sectionHeader;
 
     private WebDriver driver;
     private PageUtils pageUtils;
@@ -31,7 +38,7 @@ public class ToleranceOverridesPage extends LoadableComponent<ToleranceOverrides
 
     @Override
     protected void isLoaded() throws Error {
-
+        assertTrue("Section header is not displayed", sectionHeader.getAttribute("textContext").contains("Geometric Tolerance"));
     }
 
     /**
