dc296e3d8a6dd078d2d9fab0a7f6e4590873c53c
cfrith <ciene.frith@gmail.com>
2021-07-13 11:43:06 +0100

    optimized import;

--- web/cidapp-ui/src/test/java/com/evaluate/UploadAssembliesTests.java
+++ web/cidapp-ui/src/test/java/com/evaluate/UploadAssembliesTests.java
@@ -19,12 +19,12 @@ import com.apriori.utils.enums.NewCostingLabelEnum;
 import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
+import com.utils.AssemDfmIconEnum;
 import io.qameta.allure.Description;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import testsuites.suiteinterface.IgnoreTests;
-import utils.AssemDfmIconEnum;
 
 import java.io.File;
 
