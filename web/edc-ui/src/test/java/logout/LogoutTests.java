354ed5fd0fd5e92744223503e9473156202f4741
David Ogunseyin <dogunseyin@apriori.com>
2021-08-11 14:01:26 +0100

    Added Test case id and Description to the test

--- web/edc-ui/src/test/java/logout/LogoutTests.java
+++ web/edc-ui/src/test/java/logout/LogoutTests.java
@@ -4,10 +4,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.apriori.pageobjects.pages.login.EdcAppLoginPage;
+import com.apriori.utils.TestRail;
 import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
+import io.qameta.allure.Description;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import testsuites.suiteinterface.SmokeTests;
 
 public class LogoutTests extends TestBase {
 
@@ -15,6 +19,9 @@ public class LogoutTests extends TestBase {
     private EdcAppLoginPage edcPage;
 
     @Test
+    @Category(SmokeTests.class)
+    @TestRail(testCaseId = {"8922"})
+    @Description("Test Logout successfully")
     public void testLogout() {
         String pageTitle = "aPriori Single Sign-On";
         loginPage = new EdcAppLoginPage(driver);
