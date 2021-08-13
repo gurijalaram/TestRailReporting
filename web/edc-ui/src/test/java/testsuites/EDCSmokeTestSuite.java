bdda89dadd24845b7ff21e452b562cb3ca6cc950
David Ogunseyin <dogunseyin@apriori.com>
2021-08-13 16:37:03 +0100

    Deleted IgnoreTests from EDC

--- web/edc-ui/src/test/java/testsuites/EDCSmokeTestSuite.java
+++ web/edc-ui/src/test/java/testsuites/EDCSmokeTestSuite.java
@@ -7,7 +7,6 @@ import com.login.LoginTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import testsuites.suiteinterface.IgnoreTests;
 import testsuites.suiteinterface.SmokeTests;
 
 @ProjectRunID("610")
