60d5aca42557f3367bd86905fdba540432cee5c4
cfrith <ciene.frith@gmail.com>
2021-08-20 16:50:21 +0100

    moved field to correct class;

--- apibase/src/main/java/com/apriori/apibase/services/Pagination.java
+++ apibase/src/main/java/com/apriori/apibase/services/Pagination.java
@@ -5,7 +5,6 @@ import com.apriori.utils.http.enums.Schema;
 
 @Schema(location = "common/PaginationSchema.json")
 public class Pagination {
-    private Boolean isRestrictedByAccessControl;
     private Boolean isFirstPage;
     private Boolean isLastPage;
     private Integer pageNumber;
@@ -15,15 +14,6 @@ public class Pagination {
     private Integer totalPageCount;
     private Boolean hasNextPage;
     private Boolean hasPreviousPage;
-
-    public Boolean getRestrictedByAccessControl() {
-        return isRestrictedByAccessControl;
-    }
-
-    public Pagination setRestrictedByAccessControl(Boolean restrictedByAccessControl) {
-        isRestrictedByAccessControl = restrictedByAccessControl;
-        return this;
-    }
 
     public Boolean getIsFirstPage() {
         return isFirstPage;
