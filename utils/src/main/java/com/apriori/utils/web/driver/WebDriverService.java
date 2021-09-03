382ce8fd76d866a6c682dbfaa73f70fc851815b1
cfrith <ciene.frith@gmail.com>
2021-09-03 16:19:26 +0100

    minor change;

--- utils/src/main/java/com/apriori/utils/web/driver/WebDriverService.java
+++ utils/src/main/java/com/apriori/utils/web/driver/WebDriverService.java
@@ -1,7 +1,6 @@
 package com.apriori.utils.web.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
