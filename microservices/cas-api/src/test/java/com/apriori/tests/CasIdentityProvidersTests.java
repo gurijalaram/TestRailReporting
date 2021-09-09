a9df6f243066a25d5f0d2c9093150b169240cccd
cfrith <cfrith@apriori.com>
2021-09-09 14:42:22 +0100

    implemented new approach for building api requests;

--- microservices/cas-api/src/test/java/com/apriori/tests/CasIdentityProvidersTests.java
+++ microservices/cas-api/src/test/java/com/apriori/tests/CasIdentityProvidersTests.java
@@ -7,13 +7,14 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 import com.apriori.apibase.services.cas.IdentityProviders;
 import com.apriori.apibase.utils.APIAuthentication;
-import com.apriori.apibase.utils.CommonRequestUtil;
 import com.apriori.apibase.utils.TestUtil;
 import com.apriori.ats.utils.JwtTokenUtil;
 import com.apriori.cas.enums.CASAPIEnum;
 import com.apriori.entity.response.SingleIdp;
+import com.apriori.tests.utils.CasTestUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.http.utils.ResponseWrapper;
+import com.apriori.utils.http2.builder.service.HTTP2Request;
 
 import io.qameta.allure.Description;
 import org.apache.http.HttpStatus;
@@ -33,20 +34,21 @@ public class CasIdentityProvidersTests extends TestUtil {
     @TestRail(testCaseId = {"5646", "5647"})
     @Description("Get IDPs for customer and get IDP by identity")
     public void getIdpCustomer() {
-        String apiUrl = CASAPIEnum.GET_IDENTITY_PROVIDERS.getEndpointString();
 
-        ResponseWrapper<IdentityProviders> response = new CommonRequestUtil().getCommonRequest(apiUrl, true, IdentityProviders.class,
-            new APIAuthentication().initAuthorizationHeaderContent(token));
+        ResponseWrapper<IdentityProviders> response = HTTP2Request.build(CasTestUtil.getCommonRequest(CASAPIEnum.GET_CUSTOMER_ID, true, IdentityProviders.class,
+            new APIAuthentication().initAuthorizationHeaderContent(token))
+            .inlineVariables("L2H992828N8M/deployments"))
+            .get();
 
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
         assertThat(response.getResponseEntity().getResponse().getTotalItemCount(), is(greaterThanOrEqualTo(1)));
 
         String identity = response.getResponseEntity().getResponse().getItems().get(0).getIdentity();
 
-        String identityProvidersUrl = apiUrl + identity;
-
-        ResponseWrapper<SingleIdp> responseIdentity = new CommonRequestUtil().getCommonRequest(identityProvidersUrl, true, SingleIdp.class,
-            new APIAuthentication().initAuthorizationHeaderContent(token));
+        ResponseWrapper<SingleIdp> responseIdentity = HTTP2Request.build(CasTestUtil.getCommonRequest(CASAPIEnum.GET_CUSTOMER_DEPLOYMENT, true, SingleIdp.class,
+            new APIAuthentication().initAuthorizationHeaderContent(token))
+            .inlineVariables(identity))
+            .get();
 
         assertThat(responseIdentity.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
         assertThat(responseIdentity.getResponseEntity().getResponse().getIdentity(), is(equalTo(identity)));
