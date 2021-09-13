de19f4307be2521e327b40caa6c10a953cf26e8c
cfrith <cfrith@apriori.com>
2021-09-13 11:47:55 +0100

    used new request approach for test;

--- microservices/cas-api/src/test/java/com/apriori/tests/CasConfigurationsTests.java
+++ microservices/cas-api/src/test/java/com/apriori/tests/CasConfigurationsTests.java
@@ -5,15 +5,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
-import com.apriori.apibase.utils.APIAuthentication;
 import com.apriori.apibase.utils.TestUtil;
 import com.apriori.ats.utils.JwtTokenUtil;
 import com.apriori.cas.enums.CASAPIEnum;
 import com.apriori.entity.response.Configurations;
-import com.apriori.tests.utils.CasTestUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.http.utils.ResponseWrapper;
 import com.apriori.utils.http2.builder.service.HTTP2Request;
+import com.apriori.utils.http2.utils.RequestEntityUtil;
 
 import io.qameta.allure.Description;
 import org.apache.http.HttpStatus;
@@ -27,15 +26,15 @@ public class CasConfigurationsTests extends TestUtil {
     @Before
     public void getToken() {
         token = new JwtTokenUtil().retrieveJwtToken();
+        RequestEntityUtil.useTokenForRequests(token);
     }
 
     @Test
     @TestRail(testCaseId = {"5660"})
     @Description("Returns a list of all aP Versions.")
     public void getConfigurationsTest() {
-        ResponseWrapper<Configurations> configurationsResponse = HTTP2Request.build(CasTestUtil.getCommonRequest(CASAPIEnum.GET_CONFIGURATIONS, true, Configurations.class,
-                new APIAuthentication().initAuthorizationHeaderContent(token)))
-            .get();
+        ResponseWrapper<Configurations> configurationsResponse = HTTP2Request.build(RequestEntityUtil.init(CASAPIEnum.GET_CONFIGURATIONS, Configurations.class)
+            .token(token)).get();
 
         assertThat(configurationsResponse.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
         assertThat(configurationsResponse.getResponseEntity().getTotalItemCount(), is(greaterThanOrEqualTo(1)));
