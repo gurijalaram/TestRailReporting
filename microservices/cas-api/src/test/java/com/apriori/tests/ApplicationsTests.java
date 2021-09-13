e35db7fcc4aae199227af809f9b36bfd21345d68
cfrith <cfrith@apriori.com>
2021-09-13 11:41:59 +0100

    used new request approach for test;

--- microservices/cas-api/src/test/java/com/apriori/tests/ApplicationsTests.java
+++ microservices/cas-api/src/test/java/com/apriori/tests/ApplicationsTests.java
@@ -5,14 +5,13 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
-import com.apriori.apibase.utils.APIAuthentication;
 import com.apriori.ats.utils.JwtTokenUtil;
 import com.apriori.cas.enums.CASAPIEnum;
 import com.apriori.entity.response.Applications;
-import com.apriori.tests.utils.CasTestUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.http.utils.ResponseWrapper;
 import com.apriori.utils.http2.builder.service.HTTP2Request;
+import com.apriori.utils.http2.utils.RequestEntityUtil;
 
 import io.qameta.allure.Description;
 import org.apache.http.HttpStatus;
@@ -26,14 +25,15 @@ public class ApplicationsTests {
     @Before
     public void getToken() {
         token = new JwtTokenUtil().retrieveJwtToken();
+        RequestEntityUtil.useTokenForRequests(token);
     }
 
     @Test
     @TestRail(testCaseId = {"5659"})
     @Description("Returns a list of applications for the customer.")
     public void getCustomerApplications() {
-        ResponseWrapper<Applications> responseApplications = HTTP2Request.build(CasTestUtil.getCommonRequest(CASAPIEnum.GET_CUSTOMER, true, Applications.class,
-            new APIAuthentication().initAuthorizationHeaderContent(token))).get();
+        ResponseWrapper<Applications> responseApplications = HTTP2Request.build(RequestEntityUtil.init(CASAPIEnum.GET_CUSTOMER, Applications.class)
+            .token(token)).get();
 
         assertThat(responseApplications.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
         assertThat(responseApplications.getResponseEntity().getTotalItemCount(), is(greaterThanOrEqualTo(1)));
