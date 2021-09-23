19ffef44628b180b6c3f0f3c1ffb5b812132d7ef
dmorrow-apr <59620328+dmorrow-apr@users.noreply.github.com>
2021-09-23 11:43:48 -0400

    Ba 2037 multipart (#1287)
    * [BA-3037] Fixed create part functionality and updated Costing Multiple Part scenario
    
    * Using propeties context to get the number of parts to cost
    
    * Filtered out 2-Model Machining
    
    * Fixed Multi Part test
    
    * checkstyle fix
    
    * Update config.yml
    
    Co-authored-by: TurboFry3 <55706035+TurboFry3@users.noreply.github.com>

--- microservices/bcs-api/src/main/java/com/apriori/bcs/utils/Constants.java
+++ microservices/bcs-api/src/main/java/com/apriori/bcs/utils/Constants.java
@@ -7,11 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Constants {
 
-    public static final int MAX_NUMBER_OF_PARTS = 100;
-    public static final int MULTIPART_POLLING_INTERVALS = 25;
+    public static final int MULTIPART_POLLING_INTERVALS = 50;
     public static final int MULTIPART_POLLING_WAIT = 10000;
     public static final int MULTIPART_THREAD_COUNT = 100;
-    public static final int POLLING_TIMEOUT = 25;
+    public static final int BATCH_POLLING_TIMEOUT = 25;
     public static final String SECRET_KEY = PropertiesContext.get("${env}.secret_key");
 }
 
