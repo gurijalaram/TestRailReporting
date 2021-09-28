7be43bed3adaaf5aa10154bbfd702e29a6c3e102
cfrith <cfrith@apriori.com>
2021-09-28 14:58:29 +0100

    modified variables; added csv file;

--- microservices/cidapp-api/src/test/java/com/apriori/tests/FileUploadAPI.java
+++ microservices/cidapp-api/src/test/java/com/apriori/tests/FileUploadAPI.java
@@ -7,7 +7,6 @@ import com.apriori.cidappapi.entity.response.scenarios.ScenarioResponse;
 import com.apriori.cidappapi.utils.CidAppTestUtil;
 import com.apriori.css.entity.response.Item;
 import com.apriori.utils.FileResourceUtil;
-import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.enums.DigitalFactoryEnum;
 import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.http.utils.ResponseWrapper;
@@ -15,32 +14,58 @@ import com.apriori.utils.users.UserCredentials;
 import com.apriori.utils.users.UserUtil;
 
 import io.qameta.allure.Description;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.IdentityMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.File;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
+@RunWith(JUnitParamsRunner.class)
 public class FileUploadAPI {
 
     private CidAppTestUtil cidAppTestUtil = new CidAppTestUtil();
 
     @Test
+    @FileParameters(value = "classpath:auto_api_upload.csv", mapper = CustomMapper.class, encoding = "ISO-8859-1")
     @Description("Upload, cost and publish a part")
-    public void uploadCostPublish() {
+    public void uploadCostPublish(String componentName, String scenarioName, String processGroup) {
 
-        String componentName = "Casting";
-        File resourceFile = FileResourceUtil.getCloudFile(ProcessGroupEnum.CASTING_DIE, componentName + ".prt");
-        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        String[] component = componentName.split("\\.", 2);
+        ProcessGroupEnum pg = ProcessGroupEnum.fromString(processGroup);
+        File resourceFile = FileResourceUtil.getCloudFile(pg, component[0] + "." + component[1]);
         String mode = "manual";
-        String materialName = "Aluminum, Cast, ANSI AL380.0";
+        String materialName = "Use Default";
         UserCredentials currentUser = UserUtil.getUser();
 
         Item componentResponse = cidAppTestUtil.postCssComponents(componentName, scenarioName, resourceFile, currentUser);
 
-        cidAppTestUtil.postCostScenario(componentName, scenarioName, componentResponse.getComponentIdentity(), componentResponse.getScenarioIdentity(), ProcessGroupEnum.CASTING_DIE, DigitalFactoryEnum.APRIORI_USA, mode, materialName, currentUser);
+        cidAppTestUtil.postCostScenario(componentName, scenarioName, componentResponse.getComponentIdentity(), componentResponse.getScenarioIdentity(), pg, DigitalFactoryEnum.APRIORI_USA, mode, materialName, currentUser);
 
         ResponseWrapper<ScenarioResponse> publishResponse = cidAppTestUtil.postPublishScenario(componentResponse, componentResponse.getComponentIdentity(), componentResponse.getScenarioIdentity(), currentUser);
 
         assertThat(publishResponse.getResponseEntity().getLastAction(), is("PUBLISH"));
         assertThat(publishResponse.getResponseEntity().getPublished(), is(true));
+    }
+
+    public static class CustomMapper extends IdentityMapper {
+        @Override
+        public Object[] map(Reader reader) {
+            Object[] map = super.map(reader);
+            List<Object> result = new ArrayList<>();
+            for (Object lineObj : map) {
+                String line = lineObj.toString();
+                String[] index = line.split(",(?=([^\\\"]|\\\"[^\\\"]*\\\")*$)");
+                String fileName = index[0].replace("\"", "");
+                String scenarioName = index[1].replace("\"", "");
+                String processGroup = index[2].replace("\"", "");
+                result.add(new Object[] {fileName, scenarioName, processGroup});
+            }
+            return result.toArray();
+        }
     }
 }
