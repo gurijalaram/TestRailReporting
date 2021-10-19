121b7c80b805ff5ab592eb61dd8b5028267b1b95
cfrith <cfrith@apriori.com>
2021-10-19 10:40:04 +0100

    removed desiredcaps for the newer and more favoured mutablecaps; moved chrome/ff options to separate class;

--- utils/src/main/java/com/apriori/utils/web/driver/WebDriverService.java
+++ utils/src/main/java/com/apriori/utils/web/driver/WebDriverService.java
@@ -1,31 +1,30 @@
 package com.apriori.utils.web.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.security.InvalidParameterException;
 
 /**
  * @author cfrith
  */
 
+@Slf4j
 public class WebDriverService extends BrowserManager {
 
     private static final Logger logger = LoggerFactory.getLogger(WebDriverService.class);
 
     private WebDriver result;
-    private DesiredCapabilities dc = new DesiredCapabilities();
+    private MutableCapabilities capabilities = new MutableCapabilities();
     private BrowserTypes browser;
     private Proxy proxy;
     private String downloadPath;
@@ -42,42 +41,37 @@ public class WebDriverService extends BrowserManager {
     public WebDriver startService() {
         setDownloadFolder(downloadPath);
         setProxy(proxy);
-        logInfo(dc);
 
         try {
             switch (browser) {
                 case CHROME:
-                    WebDriverManager.chromedriver().setup();
-                    System.setProperty("webdriver.chrome.logfile", System.getProperty("java.io.tmpdir") + File.separator + "chromedriver.log");
-                    System.setProperty("webdriver.chrome.verboseLogging", "true");
+                    log.info("Starting ChromeDriver........ ");
 
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    chromeOptions.setCapability("browser.download.dir", downloadPath);
-                    chromeOptions.setCapability("intl.accept.languages", locale);
-                    chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-                    chromeOptions.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+                    WebDriverManager.chromedriver().setup();
+
+                    ChromeOptions chromeOptions = new ChromeDriverOptions(downloadPath, locale).getChromeOptions();
+                    capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 
                     result = new ChromeDriver(chromeOptions);
-                    logger.info("Full list of Capabilities: " + ((ChromeDriver) result).getCapabilities().toString());
+                    log.info("Full list of Capabilities: " + ((ChromeDriver) result).getCapabilities().toString());
                     break;
 
                 case FIREFOX:
-                    WebDriverManager.firefoxdriver().setup();
+                    log.info("Starting GeckoDriver........ ");
 
-                    FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    firefoxOptions.getBrowserName();
-                    firefoxOptions.setCapability("browser.download.dir", downloadPath);
-                    firefoxOptions.setCapability("intl.accept_languages", locale);
+                    WebDriverManager.firefoxdriver().browserVersion("78.15.0esr").setup();
 
-                    result = new FirefoxDriver(firefoxOptions);
-                    logger.info("Full list of Capabilities: " + ((FirefoxDriver) result).getCapabilities().toString());
+                    result = new FirefoxDriver(new FirefoxDriverOptions(downloadPath, locale).getFirefoxOptions());
+                    log.info("Full list of Capabilities: " + ((FirefoxDriver) result).getCapabilities().toString());
                     break;
 
                 case EDGE:
+                    log.info("Starting EdgeDriver........ ");
+
                     WebDriverManager.edgedriver().setup();
 
                     result = new EdgeDriver();
-                    logger.info("Full list of Capabilities: " + ((EdgeDriver) result).getCapabilities().toString());
+                    log.info("Full list of Capabilities: " + ((EdgeDriver) result).getCapabilities().toString());
                     break;
 
                 default:
