70be63ea2c6249cae394c1cb23fa5102a7f921f4
Anthony Bonta <abonta@apriori.com>
2021-11-03 12:56:43 -0500

    chore: cleanup

--- web/cas-ui/src/test/java/com/customer/NewCustomerTests.java
+++ web/cas-ui/src/test/java/com/customer/NewCustomerTests.java
@@ -19,7 +19,6 @@ import java.util.TimeZone;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
