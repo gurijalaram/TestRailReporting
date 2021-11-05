eaaa4134002d03fceff1acbdc77cadfe2f5a9174
Anthony Bonta <abonta@apriori.com>
2021-11-05 13:27:08 -0500

    feat: adding ability for tests to be marked sanity

---
+++ web/cas-ui/src/test/java/testsuites/SanityTestSuite.java
@@ -0,0 +1,20 @@
+package testsuites;
+
+import com.apriori.utils.ProjectRunID;
+import com.apriori.utils.runner.CategorySuiteRunner;
+import com.customer.NewCustomerTests;
+import com.login.LoginTests;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import testsuites.categories.SanityTest;
+
+@ProjectRunID("874")
+@RunWith(CategorySuiteRunner.class)
+@Categories.IncludeCategory({SanityTest.class})
+@Suite.SuiteClasses({
+    NewCustomerTests.class,
+    LoginTests.class
+})
+public class SanityTestSuite {
+}
