138623dbe1fa492c5d1d020e79700e3b45e6cb4c
Anthony Bonta <abonta@apriori.com>
2021-11-05 13:28:03 -0500

    feat: the main login test is now part of the sanity suite

--- web/cas-ui/src/test/java/com/login/LoginTests.java
+++ web/cas-ui/src/test/java/com/login/LoginTests.java
@@ -13,6 +13,8 @@ import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import testsuites.categories.SanityTest;
 
 public class LoginTests extends TestBase {
 
@@ -25,6 +27,7 @@ public class LoginTests extends TestBase {
     }
 
     @Test
+    @Category(SanityTest.class)
     @Description("Test successful login")
     public void testLogin() {
 
