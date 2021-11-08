76df937f78cb7dbecc4a2d6668638b91ffdf7c61
Anthony Bonta <abonta@apriori.com>
2021-11-08 15:22:36 -0600

    chore: import cleanup

--- web/cas-ui/src/test/java/testsuites/SmokeTestSuite.java
+++ web/cas-ui/src/test/java/testsuites/SmokeTestSuite.java
@@ -1,5 +1,6 @@
 package testsuites;
 
+import com.apriori.testsuites.categories.SmokeTest;
 import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.CategorySuiteRunner;
 
@@ -10,8 +11,6 @@ import com.login.LoginTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
-import com.apriori.testsuites.categories.SmokeTest;
 
 @ProjectRunID("874")
 @RunWith(CategorySuiteRunner.class)
