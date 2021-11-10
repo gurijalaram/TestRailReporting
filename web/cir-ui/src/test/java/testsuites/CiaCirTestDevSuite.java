aaddff737c5197b1548a5b6a508b7e440b1ec440
bhegan613 <bhegan@apriori.com>
2021-11-10 10:20:33 +0000

    Fixed suite

--- web/cir-ui/src/test/java/testsuites/CiaCirTestDevSuite.java
+++ web/cir-ui/src/test/java/testsuites/CiaCirTestDevSuite.java
@@ -2,21 +2,7 @@ package testsuites;
 
 import com.apriori.utils.runner.CategorySuiteRunner;
 
-import com.ootbreports.componentcost.ComponentCostReportTests;
-import com.ootbreports.dtcmetrics.castingdtc.CastingDtcComparisonReportTests;
-import com.ootbreports.dtcmetrics.castingdtc.CastingDtcDetailsReportTests;
-import com.ootbreports.dtcmetrics.castingdtc.CastingDtcReportTests;
-import com.ootbreports.dtcmetrics.machiningdtc.MachiningDtcComparisonReportTests;
-import com.ootbreports.dtcmetrics.machiningdtc.MachiningDtcDetailsReportTests;
-import com.ootbreports.dtcmetrics.machiningdtc.MachiningDtcReportTests;
-import com.ootbreports.dtcmetrics.plasticdtc.PlasticDtcComparisonReportTests;
-import com.ootbreports.dtcmetrics.plasticdtc.PlasticDtcDetailsReportTests;
-import com.ootbreports.dtcmetrics.plasticdtc.PlasticDtcReportTests;
-import com.ootbreports.dtcmetrics.sheetmetaldtc.SheetMetalDtcComparisonReportTests;
-import com.ootbreports.dtcmetrics.sheetmetaldtc.SheetMetalDtcDetailsReportTests;
-import com.ootbreports.dtcmetrics.sheetmetaldtc.SheetMetalDtcReportTests;
-import com.ootbreports.general.assemblydetails.AssemblyDetailsReportTests;
-import com.ootbreports.scenariocomparison.ScenarioComparisonReportTests;
+import com.ootbreports.costoutlieridentification.CostOutlierIdentificationDetailsReportTests;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -25,14 +11,7 @@ import testsuites.suiteinterface.CiaCirTestDevTest;
 @RunWith(CategorySuiteRunner.class)
 @Categories.IncludeCategory(CiaCirTestDevTest.class)
 @Suite.SuiteClasses({
-        AssemblyDetailsReportTests.class,
-        CastingDtcReportTests.class,
-        MachiningDtcReportTests.class,
-        PlasticDtcReportTests.class,
-        SheetMetalDtcComparisonReportTests.class,
-        SheetMetalDtcDetailsReportTests.class,
-        SheetMetalDtcReportTests.class,
-        ScenarioComparisonReportTests.class
+        CostOutlierIdentificationDetailsReportTests.class
 })
 
 public class CiaCirTestDevSuite {
