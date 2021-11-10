5c01a342940bc6d28142c733a943563aa68dcae3
Anthony Bonta <abonta@apriori.com>
2021-11-10 14:25:06 -0600

    feat: the permissions page now loads the permissions list

--- web/cas-ui/src/main/java/com/apriori/customer/systemconfiguration/SystemConfigurationPermissionsPage.java
+++ web/cas-ui/src/main/java/com/apriori/customer/systemconfiguration/SystemConfigurationPermissionsPage.java
@@ -2,13 +2,20 @@ package com.apriori.customer.systemconfiguration;
 
 import com.apriori.utils.web.components.EagerPageComponent;
 
+import com.apriori.utils.web.components.SelectionTreeComponent;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.slf4j.LoggerFactory;
 
 /**
  * Represents the page object model of the system configuration permissions page.
  */
 public class SystemConfigurationPermissionsPage extends EagerPageComponent<SystemConfigurationPermissionsPage> {
+    @FindBy(css = ".system-configuration-permissions .selection-tree")
+    private WebElement permissionsListRoot;
+    private SelectionTreeComponent permissionsList;
 
     /**
      * Initializes a new instance of this object.
@@ -17,6 +24,16 @@ public class SystemConfigurationPermissionsPage extends EagerPageComponent<Syste
      */
     public SystemConfigurationPermissionsPage(WebDriver driver) {
         super(driver, LoggerFactory.getLogger(SystemConfigurationPermissionsPage.class));
+        permissionsList = new SelectionTreeComponent(getDriver(), permissionsListRoot);
+    }
+
+    /**
+     * Gets the permissions list.
+     *
+     * @return The permissions list.
+     */
+    public SelectionTreeComponent getPermissionsList() {
+        return permissionsList;
     }
 
     /**
@@ -26,6 +43,6 @@ public class SystemConfigurationPermissionsPage extends EagerPageComponent<Syste
      */
     @Override
     protected void isLoaded() throws Error {
-        // TODO
+        getPageUtils().waitForElementToAppear(permissionsListRoot);
     }
 }
