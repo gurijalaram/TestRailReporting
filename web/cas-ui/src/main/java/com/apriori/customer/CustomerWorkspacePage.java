d3bbed75dcb8ec85e63939450c2c0eeffe66e133
Anthony Bonta <abonta@apriori.com>
2021-11-10 11:43:13 -0600

    refactor: split the CustomerProfilePage into the CustomerWorkspacePage which manages the tabs

---
+++ web/cas-ui/src/main/java/com/apriori/customer/CustomerWorkspacePage.java
@@ -0,0 +1,184 @@
+package com.apriori.customer;
+
+import com.apriori.customer.systemconfiguration.SystemConfigurationPage;
+import com.apriori.newcustomer.CustomerProfilePage;
+import com.apriori.newcustomer.InfrastructurePage;
+import com.apriori.newcustomer.SitesLicensesPage;
+import com.apriori.newcustomer.users.UsersListPage;
+import com.apriori.utils.properties.PropertiesContext;
+import com.apriori.utils.web.components.EagerPageComponent;
+
+import org.apache.commons.lang.StringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+public class CustomerWorkspacePage extends EagerPageComponent<CustomerWorkspacePage> {
+    private static Logger logger = LoggerFactory.getLogger(CustomerWorkspacePage.class);
+
+    @FindBy(xpath = "//a[.='Profile']")
+    private WebElement profileTab;
+
+    @FindBy(xpath = "//a[.='Users']")
+    private WebElement usersTab;
+
+    @FindBy(xpath = "//a[.='Sites & Licenses']")
+    private WebElement siteLicenseTab;
+
+    @FindBy(xpath = "//a[.='Infrastructure']")
+    private WebElement infrastructureTab;
+
+    @FindBy(xpath = "//a[.='Security']")
+    private WebElement securityTab;
+
+    @FindBy(xpath = "//a[.='System Configuration']")
+    private WebElement systemConfigurationTab;
+
+    /**
+     * Initializes a new instance of this object.
+     *
+     * @param driver The web driver that the page exists on.
+     */
+    public CustomerWorkspacePage(WebDriver driver) {
+        super(driver, logger);
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        getPageUtils().waitForElementAppear(profileTab);
+        getPageUtils().waitForElementAppear(usersTab);
+        getPageUtils().waitForElementToAppear(siteLicenseTab);
+        getPageUtils().waitForElementToAppear(infrastructureTab);
+        getPageUtils().waitForElementToAppear(systemConfigurationTab);
+    }
+
+    /**
+     * Gets a value that indicates if the profile tab is displayed.
+     *
+     * @return True if the profile tab is displayed.
+     */
+    public boolean isProfileTabDisplayed() {
+        return profileTab.isDisplayed();
+    }
+
+    /**
+     * Gets a value that indicates if the users tab is enabled and can be clicked.
+     *
+     * @return A value that indicates if the users tab is enabled
+     */
+    public boolean isUsersTabEnabled() {
+        return !StringUtils.equalsIgnoreCase("true", usersTab.getAttribute("disabled"));
+    }
+
+    /**
+     * Gets a value that indicates if the sites & licenses tab is enabled and can be clicked.
+     *
+     * @return A value that indicates if the sites & licenses tab is enabled
+     */
+    public boolean isSitesAndLicensesEnabled() {
+        return !StringUtils.equalsIgnoreCase("true", siteLicenseTab.getAttribute("disabled"));
+    }
+
+    /**
+     * Gets a value that indicates if the infrastructure tab is enabled and can be clicked.
+     *
+     * @return A value that indicates if the infrastructure tab is enabled
+     */
+    public boolean isInfrastructureEnabled() {
+        return !StringUtils.equalsIgnoreCase("true", infrastructureTab.getAttribute("disabled"));
+    }
+
+    /**
+     * Gets a value that indicates if the security tab is enabled and can be clicked.
+     *
+     * @return A value that indicates if the security tab is enabled
+     */
+    public boolean isSecurityEnabled() {
+        return !StringUtils.equalsIgnoreCase("true", securityTab.getAttribute("disabled"));
+    }
+
+    /**
+     * Gets a value that indicates if the system configuration tab is enabled and can be clicked.
+     *
+     * @return A value that indicates if the system configuration tab is enabled
+     */
+    public boolean isSystemConfigurationEnabled() {
+        return !StringUtils.equalsIgnoreCase("true", systemConfigurationTab.getAttribute("disabled"));
+    }
+
+    private <T> T goToTab(WebElement tab, Supplier<T> factory) {
+        // It's possible that this is the only tab that can be viewed;
+        // it may be disabled - this is true for new customers.  If we are already on
+        // the tab, then we can just return the profile page.
+
+        boolean isTabActive = getPageUtils().doesElementHaveClass(tab, "active");
+
+        if (!isTabActive) {
+            getPageUtils().waitForElementAndClick(tab);
+        }
+
+        return factory.get();
+    }
+
+    /**
+     * Go to the profile tab.
+     *
+     * @return The profile page.
+     */
+    public CustomerProfilePage goToProfile() {
+        return goToTab(profileTab, () -> new CustomerProfilePage(getDriver()));
+    }
+
+    /**
+     * Go to users tab
+     *
+     * @return new page object
+     */
+    public UsersListPage goToUsersList() {
+        return goToTab(usersTab, () -> new UsersListPage(getDriver()));
+    }
+
+    /**
+     * Go to sites and licenses tab
+     *
+     * @return new page object
+     */
+    public SitesLicensesPage goToSitesLicenses() {
+        return goToTab(siteLicenseTab, () -> new SitesLicensesPage(getDriver()));
+    }
+
+    /**
+     * Go to infrastructure tab
+     *
+     * @return new page object
+     */
+    public InfrastructurePage goToInfrastructure() {
+        return goToTab(infrastructureTab, () -> new InfrastructurePage(getDriver()));
+    }
+
+    /**
+     * Go to the system configuration tab.
+     *
+     * @return The POM for the system configuration.
+     */
+    public SystemConfigurationPage goToSystemConfiguration() {
+        return goToTab(systemConfigurationTab, () -> new SystemConfigurationPage(getDriver()));
+    }
+
+    /**
+     * Attempts to discover the customer identity.
+     *
+     * @return The customer identity for an existing customer, "new" for a new customer,
+     *         and the empty string if it cannot determine the identity.
+     */
+    public String findCustomerIdentity() {
+        String baseUrl = PropertiesContext.get("${env}.cas.ui_url");
+        String url = getDriver().getCurrentUrl().replace(String.format("%s/customers/", baseUrl), "");
+        return Arrays.stream(url.split("/")).findFirst().orElse("");
+    }
+}
