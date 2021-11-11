5466c95145c4ddad5b1d824d10fe08a30c7704df
Anthony Bonta <abonta@apriori.com>
2021-11-11 12:42:30 -0600

    feat: A common component can now be scrolled into view

--- utils/src/main/java/com/apriori/utils/web/components/CommonComponent.java
+++ utils/src/main/java/com/apriori/utils/web/components/CommonComponent.java
@@ -13,8 +13,8 @@ import org.openqa.selenium.WebElement;
  * to them.
  */
 public abstract class CommonComponent {
-    private WebDriver driver;
-    private WebElement root;
+    private final WebDriver driver;
+    private final WebElement root;
     private PageUtils pageUtils;
 
     /**
@@ -22,10 +22,8 @@ public abstract class CommonComponent {
      *
      * @param driver The overall global web driver that is querying different pages.
      * @param root The root element to attach for this component.
-     *
-     * @exception java.lang.Error Occurs if there is a problem loading child components.
      */
-    protected CommonComponent(WebDriver driver, WebElement root) {
+    protected CommonComponent(final WebDriver driver, final WebElement root) {
         this.driver = driver;
         this.root = root;
     }
@@ -35,7 +33,7 @@ public abstract class CommonComponent {
      *
      * @return The web driver for this component.
      */
-    protected WebDriver getDriver() {
+    protected final WebDriver getDriver() {
         return this.driver;
     }
 
@@ -44,7 +42,7 @@ public abstract class CommonComponent {
      *
      * @return The root element that contains this component.
      */
-    protected WebElement getRoot() {
+    protected final WebElement getRoot() {
         return root;
     }
 
@@ -59,5 +57,15 @@ public abstract class CommonComponent {
     protected final PageUtils getPageUtils() {
         this.pageUtils = this.pageUtils == null ? new PageUtils(getDriver()) : this.pageUtils;
         return this.pageUtils;
+    }
+
+    /**
+     * Scrolls this item into view if it exists within
+     * a parent container with a scrollbar.
+     *
+     * This aligns the component to the top of the scroll container at most.
+     */
+    protected final void scrollIntoView() {
+        getPageUtils().scrollWithJavaScript(this.getRoot(), true);
     }
 }
