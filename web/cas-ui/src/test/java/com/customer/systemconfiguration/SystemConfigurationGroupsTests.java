35999471a6e1efb4e4b5b2c22323c1d746cf4d9a
Anthony Bonta <abonta@apriori.com>
2021-11-11 12:45:49 -0600

    tests: added necessary tests to validate IDs-84

--- web/cas-ui/src/test/java/com/customer/systemconfiguration/SystemConfigurationGroupsTests.java
+++ web/cas-ui/src/test/java/com/customer/systemconfiguration/SystemConfigurationGroupsTests.java
@@ -2,6 +2,7 @@ package com.customer.systemconfiguration;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 import com.apriori.customer.systemconfiguration.SystemConfigurationGroupsPage;
 import com.apriori.login.CasLoginPage;
@@ -12,17 +13,25 @@ import com.apriori.utils.web.components.SelectionTreeItemComponent;
 import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class SystemConfigurationGroupsTests extends TestBase {
     private SystemConfigurationGroupsPage systemConfigurationGroupsPage;
 
     @Before
     public void setup() {
+
         systemConfigurationGroupsPage = new CasLoginPage(driver)
             .login(UserUtil.getUser())
             .openAprioriInternal()
@@ -30,16 +39,113 @@ public class SystemConfigurationGroupsTests extends TestBase {
             .goToGroupsPage();
     }
 
-
-    @Test
-    @Category(SmokeTest.class)
-    @Description("Test that the groups are displayed and the first one is selected.")
-    @TestRail(testCaseId = {"9880", "9883", "9881"})
-    public void testValidateShouldDisplayGroupsForTheCustomer() {
+    private SelectionTreeItemComponent validateThereIsAtLeastOneGroup() {
 
         List<SelectionTreeItemComponent> groups = systemConfigurationGroupsPage.getGroupsTree().getHierarchy();
         SelectionTreeItemComponent firstGroup = groups.stream().findFirst().orElse(null);
-        boolean isSelected = firstGroup == null || firstGroup.isSelected();
-        assertThat(isSelected, is(true));
+        assertThat("There are no groups to verify anything on", firstGroup, is(notNullValue()));
+        return firstGroup;
+    }
+
+    private void validateFirstGroupIsSelected(SoftAssertions soft) {
+
+        SelectionTreeItemComponent firstGroup = validateThereIsAtLeastOneGroup();
+        boolean isSelected = firstGroup.isSelected();
+        soft.assertThat(isSelected)
+            .overridingErrorMessage("The first group is not selected.")
+            .isTrue();
+    }
+
+    private void validateAllGroupsAreExpanded(SoftAssertions soft) {
+
+        List<SelectionTreeItemComponent> all = systemConfigurationGroupsPage.getGroupsTree().getFlatHierarchy();
+        List<SelectionTreeItemComponent> expandable = all.stream().filter(SelectionTreeItemComponent::isExpandable).collect(Collectors.toList());
+        boolean areAllGroupsExpanded = expandable.stream().allMatch(SelectionTreeItemComponent::isExpanded);
+        soft.assertThat(areAllGroupsExpanded)
+            .overridingErrorMessage("There are groups that are not expanded by default.")
+            .isFalse();
+    }
+
+    @Test
+    @Description("Test that the groups are displayed and the first one is selected.")
+    @TestRail(testCaseId = {"9880", "9883", "9881", "9903"})
+    public void testValidateShouldDisplayGroupsForTheCustomer() {
+
+        SoftAssertions soft = new SoftAssertions();
+        validateFirstGroupIsSelected(soft);
+        validateAllGroupsAreExpanded(soft);
+        soft.assertAll();
+    }
+
+    private void validateRootsCollapseAndExpand(List<SelectionTreeItemComponent> groups, SoftAssertions soft) {
+
+        for (SelectionTreeItemComponent group : groups) {
+            List<SelectionTreeItemComponent> children = group.getChildren()
+                .stream()
+                .filter(SelectionTreeItemComponent::isExpandable)
+                .collect(Collectors.toList());
+
+            validateRootsCollapseAndExpand(children, soft);
+
+            String groupName = group.getText();
+            boolean expanded = group.collapse().isExpanded();
+
+            soft.assertThat(expanded)
+                .overridingErrorMessage("The group, %s, did not collapse properly.", groupName)
+                .isFalse();
+
+            expanded = group.expand().isExpanded();
+
+            soft.assertThat(expanded)
+                .overridingErrorMessage("The group, %s, did not expand properly.", groupName)
+                .isTrue();
+        }
+    }
+
+    @Test
+    @Description("Test that the groups can be expanded and collapsed.")
+    @TestRail(testCaseId = {"9905"})
+    public void testValidateGroupsCanBeExpandedAndCollapsed() {
+
+        SoftAssertions soft = new SoftAssertions();
+        List<SelectionTreeItemComponent> roots = systemConfigurationGroupsPage
+                .getGroupsTree()
+                .getHierarchy().stream()
+                .filter(SelectionTreeItemComponent::isExpandable)
+                .collect(Collectors.toList());
+
+        validateRootsCollapseAndExpand(roots, soft);
+        soft.assertAll();
+    }
+
+    private void validateHeaderChangesToReflectTheSelectedGroup(SoftAssertions soft) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(500));
+        List<SelectionTreeItemComponent> groups = systemConfigurationGroupsPage.getGroupsTree().getFlatHierarchy();
+        int someGroupInTheMiddle = groups.size() / 2;
+        SelectionTreeItemComponent groupToSelect = groups.get(someGroupInTheMiddle);
+
+        String groupName = groupToSelect.select().getText();
+        String detailsHeader = systemConfigurationGroupsPage.getDetailsHeader();
+        final String detailsGroupName = detailsHeader.replace("GROUP DETAILS -", "").trim();
+
+        try {
+            wait.until((d) -> groupName.toUpperCase().startsWith(detailsGroupName.toUpperCase()));
+            soft.succeeded();
+        } catch (TimeoutException e) {
+            soft.fail("Expected the header for selected group, %s, to be GROUP DETAILS - %s.\nActual value was %s.", groupName, groupName.toUpperCase(), detailsHeader);
+        }
+    }
+
+    @Test
+    @Category(SmokeTest.class)
+    @TestRail(testCaseId = {"9906"})
+    public void testValidateGroupSelectionUpdatesTheSelectedDetails() {
+
+        // TODO: when a group selection changes, put the rest of the test cases in this method to verify the different data sets
+        SoftAssertions soft = new SoftAssertions();
+        validateThereIsAtLeastOneGroup();
+        validateHeaderChangesToReflectTheSelectedGroup(soft);
+        soft.assertAll();
     }
 }
