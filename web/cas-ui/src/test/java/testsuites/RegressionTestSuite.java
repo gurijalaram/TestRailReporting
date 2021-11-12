9367025a6c5fe00b1f23a6261defb0174196c90c
cfrith <cfrith@apriori.com>
2021-11-12 12:17:32 +0000

    cleanup: removed login test from suites after merge;

--- web/cas-ui/src/test/java/testsuites/RegressionTestSuite.java
+++ web/cas-ui/src/test/java/testsuites/RegressionTestSuite.java
@@ -6,8 +6,6 @@ import com.apriori.utils.runner.CategorySuiteRunner;
 import com.customer.NewCustomerTests;
 import com.customer.systemconfiguration.SystemConfigurationGroupsTests;
 import com.customer.systemconfiguration.SystemConfigurationPermissionsTests;
-import com.login.LoginTests;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -15,7 +13,6 @@ import org.junit.runners.Suite;
 @RunWith(CategorySuiteRunner.class)
 @Suite.SuiteClasses({
     NewCustomerTests.class,
-    LoginTests.class,
     SystemConfigurationGroupsTests.class,
     SystemConfigurationPermissionsTests.class
 })
--- web/cas-ui/src/test/java/testsuites/SmokeTestSuite.java
+++ web/cas-ui/src/test/java/testsuites/SmokeTestSuite.java
@@ -7,8 +7,6 @@ import com.apriori.utils.runner.CategorySuiteRunner;
 import com.customer.NewCustomerTests;
 import com.customer.systemconfiguration.SystemConfigurationGroupsTests;
 import com.customer.systemconfiguration.SystemConfigurationPermissionsTests;
-import com.login.LoginTests;
-
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -18,7 +16,6 @@ import org.junit.runners.Suite;
 @Categories.IncludeCategory({SmokeTest.class})
 @Suite.SuiteClasses({
     NewCustomerTests.class,
-    LoginTests.class,
     SystemConfigurationGroupsTests.class,
     SystemConfigurationPermissionsTests.class
 })
