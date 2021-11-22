a0139129d83ef47b98b58a0c2bb600d65e094db2
cfrith <cfrith@apriori.com>
2021-11-22 12:38:24 +0000

    task: added ep to enum;

--- microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/enums/CidAppAPIEnum.java
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/enums/CidAppAPIEnum.java
@@ -29,7 +29,10 @@ public enum CidAppAPIEnum implements ExternalEndpointEnum {
     POST_PUBLISH_SCENARIO("components/%s/scenarios/%s/publish"),
 
     //USER
-    GET_CURRENT_USER("users/current");
+    GET_CURRENT_USER("users/current"),
+
+    //PEOPLE
+    GET_CURRENT_PEOPLE("/people?username[EQ]=%s");
 
     private final String endpoint;
 
