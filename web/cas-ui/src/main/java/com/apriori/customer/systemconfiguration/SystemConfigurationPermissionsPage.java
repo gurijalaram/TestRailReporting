fc56d56dc082863ceaf808dba3d23fbec8481653
Luke McClure <lukemcclure71@gmail.com>
2021-11-23 13:01:16 +0000

    Build out SystemConfigurationPermissionsPage Add SystemConfigurationPermissionsTests

--- web/cas-ui/src/main/java/com/apriori/customer/systemconfiguration/SystemConfigurationPermissionsPage.java
+++ web/cas-ui/src/main/java/com/apriori/customer/systemconfiguration/SystemConfigurationPermissionsPage.java
@@ -1,13 +1,18 @@
 package com.apriori.customer.systemconfiguration;
 
 import com.apriori.utils.web.components.EagerPageComponent;
-
 import com.apriori.utils.web.components.SelectionTreeComponent;
 
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.time.Duration;
+import java.util.function.Function;
 
 /**
  * Represents the page object model of the system configuration permissions page.
@@ -16,6 +21,22 @@ import org.openqa.selenium.support.FindBy;
 public final class SystemConfigurationPermissionsPage extends EagerPageComponent<SystemConfigurationPermissionsPage> {
     @FindBy(css = ".system-configuration-permissions .selection-tree")
     private WebElement permissionsListRoot;
+
+    @FindBy(css = ".system-configuration-permissions .right-details .card-header .left")
+    private WebElement permissionDetailsHeader;
+
+    @FindBy(className = "left-tree-title-count")
+    private WebElement permissionNumberLabel;
+
+    @FindBy(css = ".edit-read-form .card-body")
+    private WebElement permissionContent;
+
+    @FindBy(name = "cslRule")
+    private WebElement cslRule;
+
+    @FindBy(name = "expression")
+    private WebElement javaScriptRule;
+
     private final SelectionTreeComponent permissionsList;
 
     /**
@@ -35,6 +56,190 @@ public final class SystemConfigurationPermissionsPage extends EagerPageComponent
      */
     public SelectionTreeComponent getPermissionsList() {
         return permissionsList;
+    }
+
+    /**
+     * Gets the label for a value in a given section.
+     *
+     * @param name The name of the label to search for.
+     * @param panel The parent search context element.
+     * @return The web element found or null if no such element appears within a reasonable timeframe.
+     */
+    private WebElement getLabel(String name, WebElement panel) {
+        try {
+            By query = By.xpath(String.format("//label[.='%s']", name));
+            getPageUtils().waitForCondition(() -> panel.findElements(query).size() > 0, Duration.ofMillis(500));
+            return panel.findElement(query);
+        } catch (TimeoutException | NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Gets the value element given a name.
+     * @param name The name of the field element.
+     * @param panel The panel that the value is expected to be in.
+     *
+     * @return The parsed value or null if the value cannot be parsed.
+     */
+    private <T> T getValue(String name, WebElement panel, Function<String, T> parse) {
+        try {
+            By elementQuery = By.className(String.format("read-field-%s", name));
+            getPageUtils().waitForCondition(() -> panel.findElements(elementQuery).size() > 0, Duration.ofMillis(500));
+            // It's possible that underneath the value there is a loading indicator.  Wait for that to go away.
+            WebElement value = panel.findElement(elementQuery);
+            By loaderQuery = By.className("read-field-loading");
+            getPageUtils().waitForCondition(() -> value.findElements(loaderQuery).size() == 0, Duration.ofSeconds(10));
+            return parse.apply(value.getText());
+        } catch (TimeoutException | NoSuchElementException | IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Gets the label for the description.
+     *
+     * @return The label for the description.
+     */
+    public WebElement getDescriptionLabel() {
+        return getLabel("Description: ", permissionContent);
+    }
+
+    /**
+     * Gets the description of the group.
+     *
+     * @return The description value or null if it cannot be found.
+     */
+    public String getDescription() {
+        return getValue("description", permissionContent, (v) -> v);
+    }
+
+    /**
+     * Gets the label for the Resource.
+     *
+     * @return The label for the Resource.
+     */
+    public WebElement getResourceLabel() {
+        return getLabel("Resource: ", permissionContent);
+    }
+
+    /**
+     * Gets the Resource of the group.
+     *
+     * @return The Resource value or null if it cannot be found.
+     */
+    public String getResource() {
+        return getValue("resource", permissionContent, (v) -> v);
+    }
+
+    /**
+     * Gets the label for the Action.
+     *
+     * @return The label for the Action.
+     */
+    public WebElement getActionLabel() {
+        return getLabel("Action: ", permissionContent);
+    }
+
+    /**
+     * Gets the actions of the group.
+     *
+     * @return The actions value or null if it cannot be found.
+     */
+    public String getActions() {
+        return getValue("actions", permissionContent, (v) -> v);
+    }
+
+    /**
+     * Gets the label for the Grant.
+     *
+     * @return The label for the Grant.
+     */
+    public WebElement getGrantLabel() {
+        return getLabel("Grant: ", permissionContent);
+    }
+
+    /**
+     * Gets the Grant of the group.
+     *
+     * @return The Grant value or null if it cannot be found.
+     */
+    public String getGrant() {
+        return getValue("grant", permissionContent, (v) -> v);
+    }
+
+    /**
+     * Gets the label for the Deny.
+     *
+     * @return The label for the Deny.
+     */
+    public WebElement getDenyLabel() {
+        return getLabel("Deny: ", permissionContent);
+    }
+
+    /**
+     * Gets the Deny of the group.
+     *
+     * @return The Deny value or null if it cannot be found.
+     */
+    public String getDeny() {
+        return getValue("deny", permissionContent, (v) -> v);
+    }
+
+    /**
+     * Gets the label for the CSL Rule.
+     *
+     * @return The label for the CSL Rule.
+     */
+    public WebElement getCSLRuleLabel() {
+        return getLabel("CSL Rule: ", permissionContent);
+    }
+
+    /**
+     * Gets the CSL Rule of the group.
+     *
+     * @return The CSL Rule value or null if it cannot be found.
+     */
+    public WebElement getCSLRule() {
+        return cslRule;
+    }
+
+    /**
+     * Gets the label for the JS Rule.
+     *
+     * @return The label for the JS Rule.
+     */
+    public WebElement getJSRuleLabel() {
+        return getLabel("JavaScript:", permissionContent);
+    }
+
+    /**
+     * Gets the JS Rule of the group.
+     *
+     * @return The JS Rule value or null if it cannot be found.
+     */
+    public WebElement getJSRule() {
+        return javaScriptRule;
+    }
+
+    /**
+     * Gets the count of permissions.
+     *
+     * @return The permissions count.
+     */
+    public Integer getPermissionLabelCount() {
+        String bracketedCount = permissionNumberLabel.getText();
+        String count = bracketedCount.substring(bracketedCount.indexOf("(") + 1, bracketedCount.indexOf(")"));
+        return Integer.parseInt(count);
+    }
+
+    /**
+     * Gets the header element of the details panel.
+     *
+     * @return The header text of the details panel.
+     */
+    public String getDetailsHeader() {
+        return permissionDetailsHeader.getText();
     }
 
     /**
--- web/cas-ui/src/test/java/com/customer/systemconfiguration/SystemConfigurationPermissionsTests.java
+++ web/cas-ui/src/test/java/com/customer/systemconfiguration/SystemConfigurationPermissionsTests.java
@@ -1,9 +1,14 @@
 package com.customer.systemconfiguration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
 import com.apriori.customer.systemconfiguration.SystemConfigurationPermissionsPage;
 import com.apriori.login.CasLoginPage;
 import com.apriori.testsuites.categories.SmokeTest;
 import com.apriori.utils.ListUtils;
+import com.apriori.utils.PageUtils;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.users.UserUtil;
 import com.apriori.utils.web.components.SelectionTreeItemComponent;
@@ -14,7 +19,9 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.openqa.selenium.TimeoutException;
 
+import java.time.Duration;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,15 +58,135 @@ public class SystemConfigurationPermissionsTests extends TestBase {
             .isTrue();
     }
 
+    private void testValidateNumberOfPermissionsDisplayed(SoftAssertions soft) {
+        Integer numberOfPermissions = systemConfigurationPermissionsPage
+            .getPermissionsList()
+            .getHierarchy()
+            .size();
+        Integer labelCount = systemConfigurationPermissionsPage.getPermissionLabelCount();
+        soft.assertThat(labelCount)
+            .overridingErrorMessage("Expected the displayed number of permissions to be %s.\nActual" +
+                    " value was %s.", numberOfPermissions, labelCount)
+            .isEqualTo(numberOfPermissions);
+    }
+
     @Test
     @Category(SmokeTest.class)
-    @Description("System Configuration Permissions should be displayed and sorted in alphabetical order.  The first permission should also be selected.")
-    @TestRail(testCaseId = {"9880", "9883", "9882"})
+    @Description("System Configuration Permissions should be displayed and sorted in alphabetical order, the number " +
+        "of permissions should be displayed.  The first permission should be selected and details shown correctly.")
+    @TestRail(testCaseId = {"9880", "9883", "9882", "9967", "9965", "9987"})
     public void testValidateShouldDisplayPermissions() {
 
         SoftAssertions soft = new SoftAssertions();
         testValidateThatPermissionsAreSortedInAscendingOrder(soft);
         testValidateFirstPermissionIsSelected(soft);
+        validateSelectedPermissionInformation(soft);
+        testValidateNumberOfPermissionsDisplayed(soft);
+        soft.assertAll();
+    }
+
+    private SelectionTreeItemComponent validateThereIsAtLeastOnePermission() {
+
+        List<SelectionTreeItemComponent> permissions =
+            systemConfigurationPermissionsPage.getPermissionsList().getHierarchy();
+        SelectionTreeItemComponent firstPermission = permissions.stream().findFirst().orElse(null);
+        assertThat("There are no permissions to verify anything on", firstPermission, is(notNullValue()));
+        return firstPermission;
+    }
+
+    private void validateHeaderChangesToReflectTheSelectedPermission(SoftAssertions soft) {
+
+        PageUtils utils = new PageUtils(driver);
+        List<SelectionTreeItemComponent> permissions =
+            systemConfigurationPermissionsPage.getPermissionsList().getFlatHierarchy();
+        int somePermissionInTheMiddle = permissions.size() / 2;
+        SelectionTreeItemComponent permissionsToSelect = permissions.get(somePermissionInTheMiddle);
+        String permissionName = permissionsToSelect.select().getText();
+
+        try {
+            utils.waitForCondition(() -> {
+                String detailsHeader = systemConfigurationPermissionsPage.getDetailsHeader();
+                return permissionName.equalsIgnoreCase(detailsHeader);
+            }, Duration.ofMillis(500));
+            soft.succeeded();
+        } catch (TimeoutException e) {
+            String detailsHeader = systemConfigurationPermissionsPage.getDetailsHeader();
+            soft.fail("Expected the header for selected permission, %s, to be %s.\nActual value was %s.", permissionName,
+                permissionName.toUpperCase(), detailsHeader);
+        }
+    }
+
+    private void validateSelectedPermissionInformation(SoftAssertions soft) {
+
+        List<SelectionTreeItemComponent> permissions =
+            systemConfigurationPermissionsPage.getPermissionsList().getFlatHierarchy();
+        int somePermissionInTheMiddle = permissions.size() / 2;
+        SelectionTreeItemComponent permissionsToSelect = permissions.get(somePermissionInTheMiddle);
+        permissionsToSelect.select();
+
+        // The values need to be compared with those of the api once the api is actually ready.
+        // TODO: Get the permission by id from the api and compare those values once ready
+        soft.assertThat(systemConfigurationPermissionsPage.getResourceLabel())
+            .overridingErrorMessage("The resource label was not found.")
+            .isNotNull();
+        soft.assertThat(systemConfigurationPermissionsPage.getResource())
+            .overridingErrorMessage("The resource value was not found.")
+            .isNotNull();
+
+        soft.assertThat(systemConfigurationPermissionsPage.getDescriptionLabel())
+            .overridingErrorMessage("The description label was not found.")
+            .isNotNull();
+        soft.assertThat(systemConfigurationPermissionsPage.getDescription())
+            .overridingErrorMessage("The description value was not found.")
+            .isNotNull();
+
+        soft.assertThat(systemConfigurationPermissionsPage.getActionLabel())
+            .overridingErrorMessage("The action label was not found.")
+            .isNotNull();
+        soft.assertThat(systemConfigurationPermissionsPage.getActions())
+            .overridingErrorMessage("The action value was not found.")
+            .isNotNull();
+
+        soft.assertThat(systemConfigurationPermissionsPage.getGrantLabel())
+            .overridingErrorMessage("The grant label was not found.")
+            .isNotNull();
+        soft.assertThat(systemConfigurationPermissionsPage.getGrant())
+            .overridingErrorMessage("The grant value was not found.")
+            .isNotNull();
+
+        soft.assertThat(systemConfigurationPermissionsPage.getDenyLabel())
+            .overridingErrorMessage("The deny label was not found.")
+            .isNotNull();
+        soft.assertThat(systemConfigurationPermissionsPage.getDeny())
+            .overridingErrorMessage("The deny value was not found.")
+            .isNotNull();
+
+        soft.assertThat(systemConfigurationPermissionsPage.getCSLRuleLabel())
+            .overridingErrorMessage("The CSL Rule label was not found.")
+            .isNotNull();
+        soft.assertThat(systemConfigurationPermissionsPage.getCSLRule())
+            .overridingErrorMessage("The CSL Rule value was not found.")
+            .isNotNull();
+
+        soft.assertThat(systemConfigurationPermissionsPage.getJSRuleLabel())
+            .overridingErrorMessage("The JavaScript Rule label was not found.")
+            .isNotNull();
+        soft.assertThat(systemConfigurationPermissionsPage.getJSRule())
+            .overridingErrorMessage("The JavaScript Rule value was not found.")
+            .isNotNull();
+    }
+
+    @Test
+    @Category(SmokeTest.class)
+    @Description("The details page should reflect when a permission is selected.")
+    @TestRail(testCaseId = {"9966"})
+    public void testValidatePermissionSelectionUpdatesTheSelectedDetails() {
+
+        // TODO: when a permission selection changes, put the rest of the test cases in this method to verify the
+        //  different data sets
+        SoftAssertions soft = new SoftAssertions();
+        validateThereIsAtLeastOnePermission();
+        validateHeaderChangesToReflectTheSelectedPermission(soft);
         soft.assertAll();
     }
 }
