1b0ea78035f3967a53022d21eae186d0384fb1a0
David Ogunseyin <dogunseyin@apriori.com>
2021-11-23 11:20:59 +0000

    BA-2017 Build objects and methods to enable search by component name

---
+++ web/cis-ui/src/test/java/com/explore/SearchTests.java
@@ -0,0 +1,37 @@
+package com.explore;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.apriori.pageobjects.pages.explore.ExplorePage;
+import com.apriori.pageobjects.pages.login.CisLoginPage;
+import com.apriori.utils.TestRail;
+import com.apriori.utils.users.UserUtil;
+import com.apriori.utils.web.driver.TestBase;
+
+import io.qameta.allure.Description;
+import org.junit.Test;
+
+public class SearchTests extends TestBase {
+
+    public SearchTests() {
+        super();
+    }
+
+    private CisLoginPage loginPage;
+    private ExplorePage explorePage;
+
+    @Test
+    @TestRail(testCaseId = "9556")
+    @Description("Component search by name on preset filters")
+    public void testSearchByComponentName() {
+        String componentName = "ASSEMBLY2";
+        loginPage = new CisLoginPage(driver);
+        explorePage = loginPage.login(UserUtil.getUser())
+            .selectPresetFilter("All")
+            .enterKeySearch(componentName);
+
+        assertThat(explorePage.getPresetFilterType(), is(equalTo("All")));
+    }
+}
