401c4a4dbb49e60d793d848a514d80e5890d6ac4
bhegan613 <bhegan@apriori.com>
2021-11-24 11:51:14 +0000

    Fixed another test

--- web/cir-ui/src/test/java/com/inputcontrols/InputControlsTests.java
+++ web/cir-ui/src/test/java/com/inputcontrols/InputControlsTests.java
@@ -697,6 +697,8 @@ public class InputControlsTests extends TestBase {
 
         for (String buttonName : buttonNames) {
             genericReportPage.clickListPanelButton(listName, buttonName);
+            genericReportPage.waitForCorrectAvailableSelectedCount(ListNameEnum.LAST_MODIFIED_BY.getListName(),
+                    "Selected: ", availableValue);
 
             if (buttonName.equals("Select All") || buttonName.equals("Invert")) {
                 genericReportPage.waitForCorrectAvailableSelectedCount(listName, "Selected: ", availableValue);
