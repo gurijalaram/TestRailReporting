b2229a1b92b562e450e583b705b672620f8f386e
Anthony Bonta <abonta@apriori.com>
2021-11-30 11:43:47 -0600

    test: added enable/disable test

--- web/cas-ui/src/test/java/com/customer/users/UsersStaffAssociationTests.java
+++ web/cas-ui/src/test/java/com/customer/users/UsersStaffAssociationTests.java
@@ -2,11 +2,14 @@ package com.customer.users;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 import com.apriori.cds.enums.CDSAPIEnum;
 import com.apriori.cds.objects.response.Customer;
+import com.apriori.cds.objects.response.Customers;
 import com.apriori.cds.objects.response.User;
+import com.apriori.cds.objects.response.Users;
 import com.apriori.cds.utils.CdsTestUtil;
 
 import com.apriori.customer.users.StaffAddModal;
@@ -17,6 +20,7 @@ import com.apriori.utils.Obligation;
 import com.apriori.utils.PageUtils;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.users.UserUtil;
+import com.apriori.utils.web.components.CheckboxComponent;
 import com.apriori.utils.web.components.PaginatorComponent;
 import com.apriori.utils.web.components.SourceListComponent;
 import com.apriori.utils.web.components.TableComponent;
@@ -32,28 +36,44 @@ import org.junit.experimental.categories.Category;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UsersStaffAssociationTests extends TestBase {
+    private static final String STAFF_TEST_CUSTOMER = "Staff Association Test Customer";
     private static final String STAFF_TEST_USER = "staff-test-user";
-    private List<User> sourceUsers;
     private Customer targetCustomer;
     private Customer aprioriInternal;
+    private List<User> sourceUsers;
     private CdsTestUtil cdsTestUtil;
     private StaffPage staffPage;
 
     @Before
     public void setup() {
+        Map<String, Object> existingUsers = Collections.singletonMap("username[CN]", STAFF_TEST_USER);
+        Map<String, Object> existingCustomer = Collections.singletonMap("name[EQ]", STAFF_TEST_CUSTOMER);
         String now = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-        String customerName = String.format("Test Customer %s", now);
+        String salesforce = StringUtils.leftPad(now, 15, "0");
         String email = "test.com";
 
         cdsTestUtil = new CdsTestUtil();
         aprioriInternal = cdsTestUtil.getAprioriInternal();
-        targetCustomer = cdsTestUtil.addCustomer(customerName, now, StringUtils.leftPad(now, 15, "0"), email).getResponseEntity();
-        sourceUsers = new ArrayList<>();
+        sourceUsers = new ArrayList<>(cdsTestUtil.find(
+            CDSAPIEnum.GET_USERS_BY_CUSTOMER_ID,
+            Users.class,
+            existingUsers,
+            Collections.emptyMap(),
+            aprioriInternal.getIdentity()
+        ));
+
+        targetCustomer = cdsTestUtil.findFirst(CDSAPIEnum.GET_CUSTOMERS, Customers.class, existingCustomer, Collections.emptyMap());
+        targetCustomer = targetCustomer == null
+            ? cdsTestUtil.addCustomer(STAFF_TEST_CUSTOMER, now, salesforce, email).getResponseEntity()
+            : targetCustomer;
+
         staffPage = new CasLoginPage(driver)
             .login(UserUtil.getUser())
             .openCustomer(targetCustomer.getIdentity())
@@ -63,21 +83,15 @@ public class UsersStaffAssociationTests extends TestBase {
 
     @After
     public void teardown() {
-        sourceUsers.forEach((user) -> cdsTestUtil.delete(
-            CDSAPIEnum.DELETE_USERS_BY_CUSTOMER_USER_IDS,
-            aprioriInternal.getIdentity(),
-            user.getIdentity()));
-
-        cdsTestUtil.delete(
-            CDSAPIEnum.DELETE_CUSTOMER_BY_ID,
-            targetCustomer.getIdentity());
+        sourceUsers.forEach((user) -> cdsTestUtil.delete(CDSAPIEnum.DELETE_USERS_BY_CUSTOMER_USER_IDS, aprioriInternal.getIdentity(), user.getIdentity()));
+        cdsTestUtil.delete(CDSAPIEnum.DELETE_CUSTOMER_BY_ID, targetCustomer.getIdentity());
     }
 
     private void populateStaffTestUsers(int count) {
         String now = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-        for (int i = 0; i < count; ++i) {
+        for (int i = sourceUsers.size(); i < count; ++i) {
             String identity = aprioriInternal.getIdentity();
-            String username = String.format("%s-%s-%d", STAFF_TEST_USER, now, i);
+            String username = String.format("%s-%s-%s", STAFF_TEST_USER, now, i);
             User added = cdsTestUtil.addUser(identity, username, "apriori").getResponseEntity();
             sourceUsers.add(added);
         }
@@ -86,7 +100,8 @@ public class UsersStaffAssociationTests extends TestBase {
     private long checkEveryOtherItem(List<TableRowComponent> rows, long pageSize) {
         long count = 0;
         for (int i = 0; i < pageSize; i += 2, ++count) {
-            rows.get(i).check(true);
+            final TableRowComponent row = rows.get(i);
+            Obligation.mandatory(row::getCheck, "The check cell is missing").check(true);
         }
         return count;
     }
@@ -94,7 +109,7 @@ public class UsersStaffAssociationTests extends TestBase {
     @Test
     @Description("Verify staff users can be added to the list.")
     @Category(SmokeTest.class)
-    @TestRail(testCaseId = {"5587", "5588", "5591"})
+    @TestRail(testCaseId = {"5587", "5588", "5589", "5590", "5591"})
     public void testVerifyUsersCanBeAddedToTheList() {
 
         populateStaffTestUsers(21);
@@ -109,25 +124,29 @@ public class UsersStaffAssociationTests extends TestBase {
         PageUtils utils = new PageUtils(getDriver());
         long pageSize = 10;
         long selected = 0;
-        SourceListComponent list = addModal.getUsers();
-        TableComponent userTable = Obligation.mandatory(list::getTable, "The users table is missing from the add modal.");
+        SourceListComponent userCandidates = addModal.getCandidates();
+        TableComponent userCandidatesTable = Obligation.mandatory(userCandidates::getTable, "The candidates table is missing from the add modal.");
 
-        PaginatorComponent paginator = Obligation.mandatory(list::getPaginator, "The users table is missing pagination.");
+        PaginatorComponent paginator = Obligation.mandatory(userCandidates::getPaginator, "The users table is missing pagination.");
         paginator.getPageSize().select(String.format("%d", pageSize));
-        Obligation.mandatory(list::getSearch, "The user search functionality is missing.").search(STAFF_TEST_USER);
-        utils.waitForCondition(list::isStable, PageUtils.DURATION_LOADING);
+        Obligation.mandatory(userCandidates::getSearch, "The user search functionality is missing.").search(STAFF_TEST_USER);
+        utils.waitForCondition(userCandidates::isStable, PageUtils.DURATION_LOADING);
 
-        selected += checkEveryOtherItem(userTable.getRows().collect(Collectors.toList()), pageSize);
+        selected += checkEveryOtherItem(userCandidatesTable.getRows().collect(Collectors.toList()), pageSize);
         paginator.clickNextPage();
-        utils.waitForCondition(list::isStable, PageUtils.DURATION_LOADING);
+        utils.waitForCondition(userCandidates::isStable, PageUtils.DURATION_LOADING);
 
-        selected += checkEveryOtherItem(userTable.getRows().collect(Collectors.toList()), pageSize);
+        selected += checkEveryOtherItem(userCandidatesTable.getRows().collect(Collectors.toList()), pageSize);
         paginator.clickNextPage();
-        utils.waitForCondition(list::isStable, PageUtils.DURATION_LOADING);
+        utils.waitForCondition(userCandidates::isStable, PageUtils.DURATION_LOADING);
 
-        userTable.getRows().findFirst().ifPresent((r) -> r.check(true));
+        userCandidatesTable.getRows().findFirst().ifPresent((row) -> Obligation.mandatory(row::getCheck, "The check cell is missing").check(true));
         ++selected;
-        paginator.clickFirstPage();
+        paginator.clickFirstPage().getPageSize().select("50").select("100");
+        utils.waitForCondition(userCandidates::isStable, PageUtils.DURATION_LOADING);
+
+        long expected = userCandidatesTable.getRows().filter((row) -> Obligation.mandatory(row::getCheck, "The check cell is missing").isChecked()).count();
+        assertThat("The selection is not holding across pages.", expected, is(equalTo(expected)));
 
         StaffPage updated = addModal.clickPrimary()
             .clickSecondary()
@@ -138,8 +157,7 @@ public class UsersStaffAssociationTests extends TestBase {
 
         SourceListComponent staffList = updated.getStaffAssociationList();
         Obligation.mandatory(staffList::getSearch, "The staff list search functionality is missing.").search(STAFF_TEST_USER);
-        utils.waitForCondition(staffList::isStable, PageUtils.DURATION_LOADING);
-        Obligation.mandatory(staffList::getPaginator, "The staff list paginator is missing.").getPageSize().select("100");
+        Obligation.mandatory(staffList::getPaginator, "The staff list paginator is missing.").getPageSize().select("50").select("100");
         utils.waitForCondition(staffList::isStable, PageUtils.DURATION_LOADING);
         TableComponent staffTable = Obligation.mandatory(staffList::getTable, "The staff list table is missing.");
         long count = staffTable.getRows().count();
@@ -152,5 +170,35 @@ public class UsersStaffAssociationTests extends TestBase {
     public void testVerifyAssociatedStaffCanBeDisabledAndEnabled() {
 
         populateStaffTestUsers(2);
+
+        PageUtils utils = new PageUtils(getDriver());
+
+        StaffAddModal addModal = staffPage.clickAddFromList();
+        SourceListComponent candidates = addModal.getCandidates();
+        TableComponent candidatesTable = Obligation.mandatory(candidates::getTable, "The candidate table is missing.");
+        Obligation.mandatory(candidates::getSearch, "The candidate search feature is missing.").search(STAFF_TEST_USER);
+        utils.waitForCondition(candidates::isStable, PageUtils.DURATION_LOADING);
+        Obligation.mandatory(candidatesTable::getCheckHeader, "The candidates checkbox header is missing.").check(true);
+        StaffPage updated = addModal.clickPrimary().clickPrimary();
+
+        SourceListComponent staffList = updated.getStaffAssociationList();
+        TableComponent staffTable = Obligation.mandatory(staffList::getTable, "The staff list table is missing");
+        CheckboxComponent checkHeader = Obligation.mandatory(staffTable::getCheckHeader, "The staff list check header is missing.");
+        Obligation.mandatory(staffList::getSearch, "Staff list search is missing").search(STAFF_TEST_USER);
+
+        long count = staffTable.getRows().count();
+        assertThat("The candidates were not added.", count, is(greaterThan(0L)));
+
+        checkHeader.check(true);
+        updated.clickDisableButton();
+        utils.waitForCondition(candidates::isStable, PageUtils.DURATION_LOADING);
+        long disabled = staffTable.getRows().filter((row) -> row.getCell("deletedAt").hasValue("No")).count();
+        assertThat("The associated users were not disabled.", disabled, is(equalTo(count)));
+
+        checkHeader.check(true);
+        updated.clickEnableButton();
+        utils.waitForCondition(candidates::isStable, PageUtils.DURATION_LOADING);
+        long enabled = staffTable.getRows().filter((row) -> row.getCell("deletedAt").hasValue("Yes")).count();
+        assertThat("The associated users were not enabled.", enabled, is(equalTo(count)));
     }
 }
