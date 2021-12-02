6a98c0423323c23d5cb0c5cdb740e2b9d791decc
Anthony Bonta <abonta@apriori.com>
2021-12-02 11:08:08 -0600

    feat: fix model schemas

--- apibase/src/main/java/com/apriori/apibase/services/cas/Customer.java
+++ apibase/src/main/java/com/apriori/apibase/services/cas/Customer.java
@@ -1,5 +1,6 @@
 package com.apriori.apibase.services.cas;
 
+import com.apriori.apibase.utils.CreatableModel;
 import com.apriori.utils.http.enums.Schema;
 import com.apriori.utils.json.deserializers.DateTimeDeserializer_yyyyMMddTHHmmssSSSZ;
 
@@ -22,6 +23,7 @@ import java.util.List;
 @AllArgsConstructor
 @JsonRootName("response")
 @Data
+@CreatableModel("customer")
 @Builder
 public class Customer {
     private String identity;
