d671a1826ebf797426827bb232c58dc45b31b1da
David Ogunseyin <dogunseyin@apriori.com>
2021-12-07 14:01:46 +0000

    BA-2066 Added assertions to the test, optional to the json file and refactored the LineItemUtil class

--- microservices/edc-api/src/test/java/com/apriori/edcapi/tests/LineItemsTest.java
+++ microservices/edc-api/src/test/java/com/apriori/edcapi/tests/LineItemsTest.java
@@ -1,17 +1,21 @@
 package com.apriori.edcapi.tests;
 
 import static com.apriori.edcapi.utils.BillOfMaterialsUtil.postBillOfMaterials;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 import com.apriori.ats.utils.JwtTokenUtil;
-import com.apriori.edcapi.entity.response.line.items.LineItemsItemsResponse;
 import com.apriori.edcapi.entity.response.line.items.LineItemsResponse;
 import com.apriori.edcapi.utils.LineItemsUtil;
+import com.apriori.utils.TestRail;
 import com.apriori.utils.http.utils.RequestEntityUtil;
-import com.apriori.utils.http.utils.ResponseWrapper;
 
-import org.apache.http.HttpStatus;
+import io.qameta.allure.Description;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.List;
 
 public class LineItemsTest extends LineItemsUtil {
 
@@ -25,8 +29,19 @@ public class LineItemsTest extends LineItemsUtil {
     }
 
     @Test
+    @TestRail(testCaseId = "9417")
+    @Description("GET List the line items in a bill of materials matching a specified query")
     public void testGetLineItems() {
-        ResponseWrapper<LineItemsItemsResponse> response = getAllLineItems(identity);
-        validateResponseCodeByExpectingAndRealCode(HttpStatus.SC_OK, response.getStatusCode());
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        List<LineItemsResponse> allLineItems = getAllLineItems(identity);
+        assertThat(allLineItems.get(0).getStatus(), is("Incomplete"));
+        assertThat(allLineItems.get(1).getQuantity(), is(1));
+
+        softAssertions.assertThat(allLineItems.contains(identity));
+        softAssertions.assertThat(allLineItems.get(8).getLineItemsPart().get(1).getRohsVersion());
+        softAssertions.assertThat(allLineItems.size());
+        softAssertions.assertAll();
     }
 }
