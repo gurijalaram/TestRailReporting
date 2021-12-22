6c59e4494ecbe235e35758082c75420ef9afbbdf
cfrith <cfrith@apriori.com>
2021-12-22 15:00:45 +0000

    refactor: favoured builder pattern over get/set; refactor: added var type in schema and entity;

--- microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/response/componentiteration/CostingInput.java
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/response/componentiteration/CostingInput.java
@@ -1,91 +1,52 @@
 package com.apriori.cidappapi.entity.response.componentiteration;
 
+import com.apriori.css.entity.response.Thumbnail;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CostingInput {
     private String identity;
     private Integer annualVolume;
     private Integer batchSize;
     private CustomAttributes customAttributes;
     private String materialName;
+    private String machiningMode;
+    private String materialMode;
     private String processGroupName;
     private Double productionLife;
     private String vpeName;
-
-    public String getIdentity() {
-        return identity;
-    }
-
-    public CostingInput setIdentity(String identity) {
-        this.identity = identity;
-        return this;
-    }
-
-    public Integer getAnnualVolume() {
-        return annualVolume;
-    }
-
-    public CostingInput setAnnualVolume(Integer annualVolume) {
-        this.annualVolume = annualVolume;
-        return this;
-    }
-
-    public Integer getBatchSize() {
-        return batchSize;
-    }
-
-    public CostingInput setBatchSize(Integer batchSize) {
-        this.batchSize = batchSize;
-        return this;
-    }
-
-    public CustomAttributes getCustomAttributes() {
-        return customAttributes;
-    }
-
-    public CostingInput setCustomAttributes(CustomAttributes customAttributes) {
-        this.customAttributes = customAttributes;
-        return this;
-    }
-
-    public String getMaterialName() {
-        return materialName;
-    }
-
-    public CostingInput setMaterialName(String materialName) {
-        this.materialName = materialName;
-        return this;
-    }
-
-    public String getProcessGroupName() {
-        return processGroupName;
-    }
-
-    public CostingInput setProcessGroupName(String processGroupName) {
-        this.processGroupName = processGroupName;
-        return this;
-    }
-
-    public Double getProductionLife() {
-        return productionLife;
-    }
-
-    public CostingInput setProductionLife(Double productionLife) {
-        this.productionLife = productionLife;
-        return this;
-    }
-
-    public String getVpeName() {
-        return vpeName;
-    }
-
-    public CostingInput setVpeName(String vpeName) {
-        this.vpeName = vpeName;
-        return this;
-    }
+    private ProcessSetupOptions processSetupOptions;
+    private SecondaryProcesses secondaryProcesses;
+    private SecondaryDigitalFactories secondaryDigitalFactories;
+    private Threads threads;
+    private Tolerances tolerances;
+    private Thumbnail thumbnail;
+    private String materialUtilizationMode;
 
     static class CustomAttributes {
+    }
 
+    static class ProcessSetupOptions {
+    }
+
+    static class SecondaryProcesses {
+    }
+
+    static class SecondaryDigitalFactories {
+    }
+
+    static class Threads {
+    }
+
+    static class Tolerances {
     }
 }
