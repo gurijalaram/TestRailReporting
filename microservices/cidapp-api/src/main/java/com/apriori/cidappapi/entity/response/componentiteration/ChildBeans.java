8951e3dea6bdb5a94989729755621020e242aa40
cfrith <cfrith@apriori.com>
2022-01-04 09:44:26 +0000

    refactor: added non-null tags to entities;

--- microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/response/componentiteration/ChildBeans.java
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/response/componentiteration/ChildBeans.java
@@ -1,5 +1,6 @@
 package com.apriori.cidappapi.entity.response.componentiteration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
