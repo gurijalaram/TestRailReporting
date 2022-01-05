b042c5c8c50cd5ec2eb1ed02bd0bdd29a2d1b0e1
cfrith <cfrith@apriori.com>
2022-01-05 15:59:34 +0000

    task: modified enum to include customisation; task: added new entities and schema for customisation call;

---
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/response/customizations/CustomizationItems.java
@@ -0,0 +1,15 @@
+package com.apriori.cidappapi.entity.response.customizations;
+
+import com.apriori.apibase.services.bcs.objects.ProcessGroups;
+
+import java.util.List;
+
+public class CustomizationItems {
+    private List<CustomAttributes> customAttributes;
+    private String customerIdentity;
+    private List<DigitalFactories> digitalFactories;
+    private List<ExchangeRates> exchangeRates;
+    private String identity;
+    private List<ProcessGroups> processGroups;
+    private List<SiteVariables> siteVariables;
+}
