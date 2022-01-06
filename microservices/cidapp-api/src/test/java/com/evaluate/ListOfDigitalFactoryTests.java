4dcc2be084ad91d3c19c844f9e6dfea188fa86d0
cfrith <cfrith@apriori.com>
2022-01-06 10:40:12 +0000

    task: added functionality to build token in test and pass it into util constructor; refactor: moved customisation to its own new util class;

--- microservices/cidapp-api/src/test/java/com/evaluate/ListOfDigitalFactoryTests.java
+++ microservices/cidapp-api/src/test/java/com/evaluate/ListOfDigitalFactoryTests.java
@@ -3,12 +3,13 @@ package com.evaluate;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import com.apriori.ats.utils.JwtTokenUtil;
 import com.apriori.cidappapi.entity.response.customizations.Customizations;
 import com.apriori.cidappapi.entity.response.customizations.DigitalFactories;
-import com.apriori.cidappapi.utils.CidAppTestUtil;
+import com.apriori.cidappapi.utils.CustomizationUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.enums.DigitalFactoryEnum;
-import com.apriori.utils.reader.file.user.UserCredentials;
+import com.apriori.utils.http.utils.ResponseWrapper;
 import com.apriori.utils.reader.file.user.UserUtil;
 
 import io.qameta.allure.Description;
@@ -17,23 +18,18 @@ import org.junit.Test;
 import java.util.stream.Collectors;
 
 public class ListOfDigitalFactoryTests {
-    private CidAppTestUtil cidAppTestUtil = new CidAppTestUtil();
 
-    UserCredentials currentUser;
-
-    public ListOfDigitalFactoryTests() {
-        super();
-    }
+    private CustomizationUtil customizationUtil;
 
     @Test
     @TestRail(testCaseId = {"5415"})
     @Description("Get List of Digital Factory")
-    public void getDigitalFactoryList() {
-        currentUser = UserUtil.getUser();
+    public void getListOfDigitalFactory() {
+        customizationUtil = new CustomizationUtil(new JwtTokenUtil(UserUtil.getUser()).retrieveJwtToken());
 
-        Customizations customizations = cidAppTestUtil.getCustomizations(currentUser);
+        ResponseWrapper<Customizations> customizations = customizationUtil.getCustomizations();
 
-        assertThat(customizations.getItems().stream()
+        assertThat(customizations.getResponseEntity().getItems().stream()
             .map(x -> x.getDigitalFactories().stream()
                 .map(DigitalFactories::getName))
             .findAny()
