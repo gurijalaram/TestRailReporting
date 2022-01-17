88e1fd011cc97a334d4f01414486c4c0f8b8c65f
nvalieieva <nvalieieva@apriori.com>
2022-01-17 18:16:47 +0200

    - deleted soft assertions

--- web/cas-ui/src/test/java/com/customer/AprioriInternalProfileTests.java
+++ web/cas-ui/src/test/java/com/customer/AprioriInternalProfileTests.java
@@ -1,5 +1,8 @@
 package com.customer;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import com.apriori.login.CasLoginPage;
 import com.apriori.newcustomer.CustomerProfilePage;
 import com.apriori.utils.TestRail;
@@ -7,7 +10,6 @@ import com.apriori.utils.reader.file.user.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
 import io.qameta.allure.Description;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,10 +29,6 @@ public class AprioriInternalProfileTests extends TestBase {
     @Description("Validate Edit button is disabled on customer profile tab for aPriori Internal")
     @TestRail(testCaseId = "10585")
     public void testEditButtonIsDisabledOnAprioriInternalProfile() {
-        SoftAssertions soft = new SoftAssertions();
-
-        soft.assertThat(customerProfilePage.canEdit())
-                .overridingErrorMessage("Expected Edit button to be disabled and no clickable")
-                .isFalse();
+        assertThat(customerProfilePage.canEdit(), is(false));
     }
 }
