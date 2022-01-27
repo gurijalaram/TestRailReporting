fd7e70b99d5d09ec81af2bdf8d859438870946ed
bhegan613 <bhegan@apriori.com>
2022-01-27 12:44:41 +0000

    Added another field to pogo and schema

--- microservices/acs-api/src/main/java/com/apriori/acs/entity/response/getsetuserpreferences/GetUserPreferencesResponse.java
+++ microservices/acs-api/src/main/java/com/apriori/acs/entity/response/getsetuserpreferences/GetUserPreferencesResponse.java
@@ -36,81 +36,58 @@ public class GetUserPreferencesResponse {
     @JsonProperty("prod.info.vpe")
     private String prodInfoVpe;
     private String selectionColor;
+    @JsonProperty("show.component.list.preview.panel")
+    private String showComponentListPreviewPanel;
     @JsonProperty("TolerancePolicyDefaults.bendAngleToleranceOverride")
     private String tolerancePolicyDefaultsBendAngleToleranceOverride;
-
     @JsonProperty("TolerancePolicyDefaults.cadToleranceReplacement")
     private String tolerancePolicyDefaultsCadToleranceReplacement;
-
     @JsonProperty("TolerancePolicyDefaults.circularityOverride")
     private String circularityOverride;
-
     @JsonProperty("TolerancePolicyDefaults.concentricityOverride")
     private String concentricityOverride;
-
     @JsonProperty("TolerancePolicyDefaults.cylindricityOverride")
     private String cylindricityOverride;
-
     @JsonProperty("TolerancePolicyDefaults.diamToleranceOverride")
     private String tolerancePolicyDefaultsDiamToleranceOverride;
-
     @JsonProperty("TolerancePolicyDefaults.flatnessOverride")
     private String tolerancePolicyDefaultsFlatnessOverride;
-
     @JsonProperty("TolerancePolicyDefaults.minCadToleranceThreshhold")
     private String tolerancePolicyDefaultsMinCadToleranceThreshhold;
-
     @JsonProperty("TolerancePolicyDefaults.parallelismOverride")
     private String tolerancePolicyDefaultsParallelismOverride;
-
     @JsonProperty("TolerancePolicyDefaults.perpendicularityOverride")
     private String tolerancePolicyDefaultsPerpendicularityOverride;
-
     @JsonProperty("TolerancePolicyDefaults.positionToleranceOverride")
     private String tolerancePolicyDefaultsPositionToleranceOverride;
-
     @JsonProperty("TolerancePolicyDefaults.profileOfSurfaceOverride")
     private String tolerancePolicyDefaultsProfileOfSurfaceOverride;
-
     @JsonProperty("TolerancePolicyDefaults.roughnessOverride")
     private String tolerancePolicyDefaultsRoughnessOverride;
-
     @JsonProperty("TolerancePolicyDefaults.roughnessRzOverride")
     private String tolerancePolicyDefaultsRoughnessRzOverride;
-
     @JsonProperty("TolerancePolicyDefaults.runoutOverride")
     private String tolerancePolicyDefaultsRunoutOverride;
-
     @JsonProperty("TolerancePolicyDefaults.straightnessOverride")
     private String tolerancePolicyDefaultsStraightnessOverride;
-
     @JsonProperty("TolerancePolicyDefaults.symmetryOverride")
     private String tolerancePolicyDefaultsSymmetryOverride;
-
     @JsonProperty("TolerancePolicyDefaults.toleranceMode")
     private String tolerancePolicyDefaultsToleranceMode;
-
     @JsonProperty("TolerancePolicyDefaults.toleranceOverride")
     private String tolerancePolicyDefaultsToleranceOverride;
-
     @JsonProperty("TolerancePolicyDefaults.totalRunoutOverride")
     private String tolerancePolicyDefaultsTotalRunoutOverride;
-
     @JsonProperty("TolerancePolicyDefaults.useCadToleranceThreshhold")
     private String tolerancePolicyDefaultsUseCadToleranceThreshhold;
-
     @JsonProperty("web.login")
     private String webLogin;
-
     @JsonProperty("workspace.filter.private.sort.column")
     private String workspaceFilterPrivateSortColumn;
-
     @JsonProperty("workspace.filter.private.sort.direction")
     private String workspaceFilterPrivateSortDirection;
-
     @JsonProperty("workspace.filter.public.sort.column")
     private String workspaceFilterPublicSortColumn;
-
     @JsonProperty("workspace.filter.public.sort.direction")
     private String workspaceFilterPublicSortDirection;
 }
