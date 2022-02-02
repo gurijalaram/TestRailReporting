cff2d72da82b392a13b9001ed96b21d73ea13899
cfrith <cfrith@apriori.com>
2022-02-02 16:41:24 +0000

    refactor: added missing entity; refactor: added missing fields;

--- microservices/cidapp-api/src/test/java/com/settings/DecimalPlaceTests.java
+++ microservices/cidapp-api/src/test/java/com/settings/DecimalPlaceTests.java
@@ -3,14 +3,20 @@ package com.settings;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.number.IsCloseTo.closeTo;
 
+import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
+import com.apriori.cidappapi.entity.response.componentiteration.AnalysisOfScenario;
+import com.apriori.cidappapi.entity.response.componentiteration.ComponentIteration;
 import com.apriori.cidappapi.utils.ComponentsUtil;
 import com.apriori.cidappapi.utils.IterationsUtil;
 import com.apriori.cidappapi.utils.ScenariosUtil;
 import com.apriori.cidappapi.utils.UserPreferencesUtil;
+import com.apriori.css.entity.response.Item;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.TestRail;
+import com.apriori.utils.enums.DigitalFactoryEnum;
 import com.apriori.utils.enums.PreferencesEnum;
 import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.http.utils.ResponseWrapper;
@@ -45,5 +51,37 @@ public class DecimalPlaceTests {
 
         ResponseWrapper<String> patchResponse = userPreferencesUtil.patchPreference(currentUser, PreferencesEnum.DECIMAL_PLACES, "2");
         assertThat(patchResponse.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
+
+        Item componentResponse = componentsUtil.postComponentQueryCSS(componentName, scenarioName, resourceFile, currentUser);
+
+        scenariosUtil.postCostScenario(
+            ComponentInfoBuilder.builder()
+                .componentName(componentName)
+                .scenarioName(scenarioName)
+                .componentId(componentResponse.getComponentIdentity())
+                .scenarioId(componentResponse.getScenarioIdentity())
+                .processGroup(processGroupEnum)
+                .digitalFactory(DigitalFactoryEnum.APRIORI_USA)
+                .mode("manual")
+                .material("Steel, Hot Worked, AISI 1010")
+                .user(currentUser)
+                .build());
+
+        ResponseWrapper<ComponentIteration> componentIterationResponse = iterationsUtil.getComponentIterationLatest(
+            ComponentInfoBuilder.builder()
+                .componentId(componentResponse.getComponentIdentity())
+                .scenarioId(componentResponse.getScenarioIdentity())
+                .user(currentUser)
+                .build());
+
+        AnalysisOfScenario analysisOfScenario = componentIterationResponse.getResponseEntity().getAnalysisOfScenario();
+
+        assertThat(analysisOfScenario.getFinishMass(), is(closeTo(5.309458, 1)));
+        assertThat(analysisOfScenario.getUtilization(), is(closeTo(81.163688, 1)));
+        assertThat(analysisOfScenario.getCycleTime(), is(closeTo(109.400000, 1)));
+        assertThat(analysisOfScenario.getMaterialCost(), is(closeTo(43.993885, 10)));
+        assertThat(analysisOfScenario.getPieceCost(), is(closeTo(51.043238, 10)));
+        assertThat(analysisOfScenario.getFullyBurdenedCost(), is(closeTo(51.043238, 10)));
+        assertThat(analysisOfScenario.getCapitalInvestment(), is(closeTo(0.000000, 1)));
     }
 }
