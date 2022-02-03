20d6fe026728db752d7700dc5ebea5ac4e390bfe
cfrith <cfrith@apriori.com>
2022-02-03 08:59:41 +0000

    refactor: modified schema as return type contains no properties only data; refactor: changed entity to extend arraylist;

--- microservices/acs-api/src/main/java/com/apriori/acs/entity/response/getpartprimaryprocessgroups/GetPartPrimaryProcessGroupsResponse.java
+++ microservices/acs-api/src/main/java/com/apriori/acs/entity/response/getpartprimaryprocessgroups/GetPartPrimaryProcessGroupsResponse.java
@@ -2,12 +2,17 @@ package com.apriori.acs.entity.response.getpartprimaryprocessgroups;
 
 import com.apriori.utils.http.enums.Schema;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Schema(location = "GetPartPrimaryProcessGroupsResponse.json")
-public class GetPartPrimaryProcessGroupsResponse {
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetPartPrimaryProcessGroupsResponse extends ArrayList {
     private List<String> processGroups;
 }
