6f107139b1f9e7afb0cb44efa55c4c29a70f3e4d
bhegan613 <bhegan@apriori.com>
2022-02-14 15:05:10 +0000

    Moved to proper location in project

--- web/cir-ui/src/test/java/com/ootbreports/newreportstests/dtcmetrics/castingdtc/CastingDtcReportTests.java
+++ web/cir-ui/src/test/java/com/ootbreports/newreportstests/dtcmetrics/castingdtc/CastingDtcReportTests.java
@@ -1,25 +1,30 @@
-package com.apriori.cir.tests.ootbreports.dtcmetrics.castingdtc;
+package com.ootbreports.newreportstests.dtcmetrics.castingdtc;
 
 import java.util.Arrays;
 
-import com.apriori.cir.tests.JasperReportTest;
 import com.apriori.cirapi.entity.JasperReportSummary;
 import com.apriori.cirapi.entity.request.ReportRequest;
 import com.apriori.cirapi.entity.response.ChartDataPoint;
 import com.apriori.cirapi.utils.JasperReportUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.enums.reports.ExportSetEnum;
+import com.apriori.utils.http.utils.WebDriverUtils;
+import com.apriori.utils.web.driver.DriverFactory;
+import com.apriori.utils.web.driver.WebDriverService;
 import io.qameta.allure.Description;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import testsuites.suiteinterface.ReportsTest;
 
 public class CastingDtcReportTests {
 
     @Test
-    @Category(JasperReportTest.class)
+    @Category(ReportsTest.class)
     @TestRail(testCaseId = {"1699"})
     @Description("Verify Currency Code input control functions correctly")
     public void testCurrencyCode() {
+        // use login endpoint to get authorisation
+
         ReportRequest reportRequest = ReportRequest.initFromJsonFile("ReportCastingDTCRequest");
         reportRequest.getParameters().getReportParameterByName("exportSet")
             .setValue(Arrays.asList("Export Set", "\"Export Set\"", ExportSetEnum.CASTING_DTC.getExportSetName()));
@@ -27,6 +32,7 @@ public class CastingDtcReportTests {
             .setValue(Arrays.asList("Currency Code", "\"Currency Code\"", "GBP"));
 
         // get jsessionid and pass into the init call below
+        // ask Vlad how to get jsession id
         /*JasperReportSummary jasperReportSummary =
             JasperReportUtil.init("")
                 .generateJasperReportSummary(reportRequest);*/
