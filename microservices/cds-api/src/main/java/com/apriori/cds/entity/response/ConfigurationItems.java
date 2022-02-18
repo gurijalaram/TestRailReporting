4a46727341fcae6ac9771e3230aae6489a66d510
cfrith <cfrith@apriori.com>
2022-02-18 10:14:11 +0000

    cleanup: replaced get/set with lombok features;

--- microservices/cds-api/src/main/java/com/apriori/cds/entity/response/ConfigurationItems.java
+++ microservices/cds-api/src/main/java/com/apriori/cds/entity/response/ConfigurationItems.java
@@ -2,46 +2,13 @@ package com.apriori.cds.entity.response;
 
 import com.apriori.utils.http.enums.Schema;
 
+import lombok.Data;
+
 @Schema(location = "ConfigurationSchema.json")
+@Data
 public class ConfigurationItems {
     private String identity;
     private String createdBy;
     private String createdAt;
     private String domain;
-
-    public String getIdentity() {
-        return identity;
-    }
-
-    public ConfigurationItems setIdentity(String identity) {
-        this.identity = identity;
-        return this;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public ConfigurationItems setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-        return this;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public ConfigurationItems setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public ConfigurationItems setDomain(String domain) {
-        this.domain = domain;
-        return this;
-    }
 }
