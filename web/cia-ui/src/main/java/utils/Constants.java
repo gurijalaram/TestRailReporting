1e1ac0a52b6f061e9c454a14596022c64e85bd7e
bhegan613 <bhegan@apriori.com>
2022-03-10 15:48:56 +0000

    Very minor test fix

--- web/cia-ui/src/main/java/utils/Constants.java
+++ web/cia-ui/src/main/java/utils/Constants.java
@@ -11,6 +11,6 @@ public class Constants {
     public static final String SCENARIO_EXPORT_CHAPTER_URL_PART_ONE = "https://www.apriori.com/Collateral/Documents/English-US/online_help/apriori-platform/";
     public static final String SCENARIO_EXPORT_CHAPTER_URL_PART_TWO = "CIA_UG";
     public static final String CIA_USER_GUIDE_TITLE = "Cost Insight Admin\nUser Guide";
-    public static final String SCENARIO_EXPORT_CHAPTER_PAGE_TITLE = "2 Scenario and System Data Exports";
+    public static final String SCENARIO_EXPORT_CHAPTER_PAGE_TITLE = "3 Scenario and System Data Exports";
     public static final String REPORTS_URL_SUFFIX = "jasperserver-pro/";
 }
