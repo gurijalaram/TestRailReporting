e376d15cab94da1b2012293fcb8ddd15ac4d6562
BhagyaSusiripala <bsusiripala@apriori.com>
2022-04-25 11:30:52 +0530

    COL-479 * Automated C12058 test case - Verify the fields included in the parts and assemblies table

---
+++ web/cis-ui/src/main/java/com/apriori/pageobjects/common/PartsAndAssemblyTableController.java
@@ -0,0 +1,38 @@
+package com.apriori.pageobjects.common;
+
+import com.apriori.utils.web.components.EagerPageComponent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+public class PartsAndAssemblyTableController extends EagerPageComponent<PartsAndAssemblyTableController> {
+
+    @FindBy(css = "div.MuiDataGrid-columnHeaders.css-qw65j7")
+    private WebElement tableHeaders;
+
+    public PartsAndAssemblyTableController(WebDriver driver) {
+        super(driver, log);
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+
+
+    }
+
+    /**
+     * Gets table headers
+     *
+     * @return list of string
+     */
+    public List<String> getTableHeaders() {
+        return Stream.of(tableHeaders.getAttribute("innerText").split("\n")).collect(Collectors.toList());
+    }
+}
--- web/cis-ui/src/main/java/com/apriori/pageobjects/navtoolbars/LeftHandNavigationBar.java
+++ web/cis-ui/src/main/java/com/apriori/pageobjects/navtoolbars/LeftHandNavigationBar.java
@@ -3,7 +3,7 @@ package com.apriori.pageobjects.navtoolbars;
 import com.apriori.pageobjects.pages.compare.ComparePage;
 import com.apriori.pageobjects.pages.explore.ExplorePage;
 import com.apriori.pageobjects.pages.help.HelpPage;
-import com.apriori.pageobjects.pages.myuser.MyUserPage;
+import com.apriori.pageobjects.pages.myuser.MyUserPage;import com.apriori.pageobjects.pages.partsandassemblies.PartsAndAssembliesPage;
 import com.apriori.utils.web.components.EagerPageComponent;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +15,13 @@ import org.slf4j.Logger;
 @Slf4j
 public class LeftHandNavigationBar extends CisHeaderBar {
 
+
+
     @FindBy(xpath = "//button[@data-testid='non-collapsed']")
     private WebElement hamburgerIcon;
+
+    @FindBy(xpath = "//div[@data-testid='list-subitem-text-left-menu.subTitle.parts-and-assemblies']")
+    private WebElement btnPartsAndAssemblies;
 
     public LeftHandNavigationBar(WebDriver driver) {
         this(driver, log);
@@ -40,4 +45,28 @@ public class LeftHandNavigationBar extends CisHeaderBar {
     public String getNavigationPanelDefaultState() {
         return getPageUtils().waitForElementToAppear(hamburgerIcon).getAttribute("data-testid");
     }
+
+    /**
+     * Collapse the navigation panel
+     *
+     * @return current page object
+     */
+
+    public LeftHandNavigationBar collapseNavigationPanel() {
+        getPageUtils().waitForElementToAppear(hamburgerIcon).click();
+        return this;
+    }
+
+    /**
+     * Navigate to parts and assemblies page
+     *
+     * @return new page object
+     */
+
+    public PartsAndAssembliesPage clickPartsAndAssemblies() {
+        getPageUtils().waitForElementToAppear(btnPartsAndAssemblies).click();
+        return new PartsAndAssembliesPage(getDriver());
+    }
+
+
 }
