2af01d706754a93aea3378e69819a065d4cee954
cfrith <cfrith@apriori.com>
2022-04-28 14:37:10 +0100

    refactor: minor text case change;

--- web/cidapp-ui/src/test/java/com/evaluate/assemblies/GroupEditAssemblies.java
+++ web/cidapp-ui/src/test/java/com/evaluate/assemblies/GroupEditAssemblies.java
@@ -39,7 +39,7 @@ public class GroupEditAssemblies extends TestBase {
     @Test
     @Category(SmokeTests.class)
     @TestRail(testCaseId = {"10882", "10890", "10893"})
-    @Description("group Edit sub Components")
+    @Description("Group edit subcomponents")
     public void editButtonAvailable() {
 
         String assemblyName = "Hinge assembly";
@@ -88,117 +88,8 @@ public class GroupEditAssemblies extends TestBase {
     }
 
     @Test
-    @TestRail(testCaseId = {"10883", "10884", "10885", "10894", "11140"})
-    @Description("group Edit sub Components")
-    public void editButtonUnavailable() {
-
-        String assemblyName = "Gym Bike";
-        final String assemblyExtension = ".iam";
-
-        List<String> subComponentNames = Arrays.asList("centre bolt", "centre washer", "display", "gasket", "Handle", "left paddle", "leg cover", "leg", "mechanism body", "paddle bar", "pin", "right paddle", "seat lock", "seat", "steer wheel support", "washer");
-        final ProcessGroupEnum subComponentProcessGroup = ProcessGroupEnum.PLASTIC_MOLDING;
-        final String subComponentExtension = ".ipt";
-
-        UserCredentials currentUser = UserUtil.getUser();
-        String scenarioName = new GenerateStringUtil().generateScenarioName();
-        String scenarioName2 = new GenerateStringUtil().generateScenarioName();
-
-        SoftAssertions softAssertions = new SoftAssertions();
-
-        ComponentInfoBuilder componentAssembly = assemblyUtils.associateAssemblyAndSubComponents(assemblyName,
-            assemblyExtension,
-            ASSEMBLY,
-            subComponentNames,
-            subComponentExtension,
-            subComponentProcessGroup,
-            scenarioName,
-            currentUser);
-        assemblyUtils.uploadSubComponents(componentAssembly)
-            .uploadAssembly(componentAssembly);
-        assemblyUtils.publishSubComponents(componentAssembly);
-
-        loginPage = new CidAppLoginPage(driver);
-        componentsListPage = loginPage.login(currentUser)
-            .navigateToScenario(componentAssembly)
-            .openComponents()
-            .multiSelectSubcomponents("centre bolt, " + scenarioName + "", "centre washer, " + scenarioName + "", "display, " + scenarioName + "", "gasket, " + scenarioName + "", "Handle, " + scenarioName + "", "left paddle, " + scenarioName + "", "leg cover, " + scenarioName + "", "leg, " + scenarioName + "", "mechanism body, " + scenarioName + "", "paddle bar, " + scenarioName + "", "pin, " + scenarioName + "");
-
-        softAssertions.assertThat(componentsListPage.isEditButtonEnabled()).isEqualTo(false);
-
-        componentsListPage.multiSelectSubcomponents("centre bolt, " + scenarioName + "")
-            .editSubcomponent(EditComponentsPage.class)
-            .renameScenarios()
-            .enterScenarioName(scenarioName2)
-            .clickContinue(EditScenarioStatusPage.class)
-            .close(ComponentsListPage.class)
-            .checkSubcomponentState(componentAssembly, "centre washer, display, gasket, Handle, left paddle, leg cover, leg, mechanism body, paddle bar, pin");
-
-        softAssertions.assertThat(componentsListPage.getRowDetails("centre washer", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("display", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("gasket", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("Handle", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("left paddle", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("leg cover", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("leg", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("mechanism body", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("paddle bar", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("pin", scenarioName2)).contains(StatusIconEnum.PRIVATE.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("centre bolt", scenarioName)).contains(StatusIconEnum.PUBLIC.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("seat", scenarioName)).contains(StatusIconEnum.PUBLIC.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("steer wheel support", scenarioName)).contains(StatusIconEnum.PUBLIC.getStatusIcon());
-        softAssertions.assertThat(componentsListPage.getRowDetails("washer", scenarioName)).contains(StatusIconEnum.PUBLIC.getStatusIcon());
-
-        softAssertions.assertAll();
-    }
-
-    @Test
-    @TestRail(testCaseId = {"10886", "10887", "10888"})
-    @Description("group Edit private sub Components disallowed")
-    public void cannotEditPrivateComponents() {
-
-        String assemblyName = "Gym Bike";
-        final String assemblyExtension = ".iam";
-
-        List<String> subComponentNames = Arrays.asList("centre bolt", "centre washer", "display", "gasket", "Handle", "left paddle", "leg cover", "leg", "mechanism body", "paddle bar", "pin", "right paddle", "seat lock", "seat", "steer wheel support", "washer");
-        final ProcessGroupEnum subComponentProcessGroup = ProcessGroupEnum.PLASTIC_MOLDING;
-        final String subComponentExtension = ".ipt";
-
-        UserCredentials currentUser = UserUtil.getUser();
-        String scenarioName = new GenerateStringUtil().generateScenarioName();
-
-        SoftAssertions softAssertions = new SoftAssertions();
-
-        ComponentInfoBuilder componentAssembly = assemblyUtils.associateAssemblyAndSubComponents(assemblyName,
-            assemblyExtension,
-            ASSEMBLY,
-            subComponentNames,
-            subComponentExtension,
-            subComponentProcessGroup,
-            scenarioName,
-            currentUser);
-        assemblyUtils.uploadSubComponents(componentAssembly)
-            .uploadAssembly(componentAssembly);
-
-        loginPage = new CidAppLoginPage(driver);
-        componentsListPage = loginPage.login(currentUser)
-            .navigateToScenario(componentAssembly)
-            .openComponents()
-            .multiSelectSubcomponents("centre bolt, " + scenarioName + "");
-
-        softAssertions.assertThat(componentsListPage.isEditButtonEnabled()).isEqualTo(false);
-
-        componentsListPage.multiSelectSubcomponents("centre washer, " + scenarioName + "", "display, " + scenarioName + "", "gasket, " + scenarioName + "", "Handle, " + scenarioName + "");
-        softAssertions.assertThat(componentsListPage.isEditButtonEnabled()).isEqualTo(false);
-
-        componentsListPage.multiSelectSubcomponents("left paddle, " + scenarioName + "", "leg cover, " + scenarioName + "", "steer wheel support, " + scenarioName + "", "mechanism body, " + scenarioName + "", "paddle bar, " + scenarioName + "");
-        softAssertions.assertThat(componentsListPage.isEditButtonEnabled()).isEqualTo(false);
-
-        softAssertions.assertAll();
-    }
-
-    @Test
     @TestRail(testCaseId = {"10891", "11132"})
-    @Description("group Edit sub Components")
+    @Description("Group edit subcomponents")
     public void overridePrivateSubComponent() {
 
         String assemblyName = "Hinge assembly";
@@ -246,7 +137,7 @@ public class GroupEditAssemblies extends TestBase {
 
     @Test
     @TestRail(testCaseId = {"10889", "11953"})
-    @Description("group Edit sub Components")
+    @Description("Group edit subcomponents")
     public void privateAndPublicSubComponents() {
 
         String assemblyName = "flange c";
