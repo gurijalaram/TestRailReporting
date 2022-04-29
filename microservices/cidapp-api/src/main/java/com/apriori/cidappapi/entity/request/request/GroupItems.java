b44b901cbdc28fb40c4406cab4524124c3adad3e
cfrith <cfrith@apriori.com>
2022-04-29 15:24:08 +0100

    refactor: added entity for group items; refactor: set default boolean value and added new field;

--- microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/request/request/GroupItems.java
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/request/request/GroupItems.java
@@ -5,15 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ForkRequest {
-    @Builder.Default
-    private Boolean override = false;
-    private String scenarioName;
-    private List<GroupItems> groupItems;
+public class GroupItems {
+    private String componentIdentity;
+    private String scenarioIdentity;
 }
