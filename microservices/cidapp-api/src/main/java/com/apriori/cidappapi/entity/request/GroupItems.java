167ae446fb1768297a22bd2202a3e46f4ebb649f
cfrith <cfrith@apriori.com>
2022-05-19 13:42:38 +0100

    refactor: reverted changes to entity;

--- microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/request/GroupItems.java
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/request/GroupItems.java
@@ -12,10 +12,4 @@ import lombok.NoArgsConstructor;
 public class GroupItems {
     private String componentIdentity;
     private String scenarioIdentity;
-    private String scenarioAssociationIdentity;
-    private String childScenarioIdentity;
-    private Boolean excluded;
-    private Boolean included;
-    private Integer occurrences;
-
 }
