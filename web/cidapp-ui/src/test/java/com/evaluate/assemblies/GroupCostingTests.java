01cd65b16b30e1eeb7c262f3a97c0c8a9637d480
cfrith <cfrith@apriori.com>
2022-07-14 14:48:37 +0100

    refactor: added testrail id to test;

--- web/cidapp-ui/src/test/java/com/evaluate/assemblies/GroupCostingTests.java
+++ web/cidapp-ui/src/test/java/com/evaluate/assemblies/GroupCostingTests.java
@@ -110,7 +110,7 @@ public class GroupCostingTests extends TestBase {
     }
 
     @Test
-    @TestRail(testCaseId = {"11097", "11090", "11092", "11097"})
+    @TestRail(testCaseId = {"11097", "11090", "11092", "11097", "11099"})
     @Description("Verify sub-components are group costed successfully.")
     public void groupCostSubComponentsTest() {
         final String retainText = "Retain Existing Input";
