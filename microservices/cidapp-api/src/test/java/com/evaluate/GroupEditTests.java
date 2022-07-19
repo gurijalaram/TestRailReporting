ad9181a69eb6190f9812b558135fc20c47cd25b9
cfrith <cfrith@apriori.com>
2022-07-19 09:28:23 +0100

    refactor: added testrail id, changed test name and added test for private subcomponents;

--- microservices/cidapp-api/src/test/java/com/evaluate/GroupEditTests.java
+++ microservices/cidapp-api/src/test/java/com/evaluate/GroupEditTests.java
@@ -27,9 +27,9 @@ public class GroupEditTests {
     private UserCredentials currentUser;
 
     @Test
-    @TestRail(testCaseId = {"10949"})
+    @TestRail(testCaseId = {"10949", "11851", "11853"})
     @Description("Edit multiple public sub-components with no Private counterparts (Overwrite)")
-    public void testGroupEditSubcomponents() {
+    public void testGroupEditPublicSubcomponents() {
         currentUser = UserUtil.getUser();
         String scenarioName = new GenerateStringUtil().generateScenarioName();
         String newScenarioName = new GenerateStringUtil().generateScenarioName();
@@ -74,5 +74,67 @@ public class GroupEditTests {
         SoftAssertions softAssertions = new SoftAssertions();
 
         groupEditResponse.getResponseEntity().getSuccesses().forEach(o -> softAssertions.assertThat(o.getScenarioName()).isEqualTo(newScenarioName));
+
+        ResponseWrapper<ScenarioSuccessesFailures> singleEditResponse = scenariosUtil.postEditGroupScenarios(componentAssembly, forkRequest,
+            JOINT + "," + scenarioName);
+
+        softAssertions.assertThat(singleEditResponse.getResponseEntity().getSuccesses()).isEqualTo(1);
+        singleEditResponse.getResponseEntity().getSuccesses().forEach(o -> softAssertions.assertThat(o.getScenarioName()).isEqualTo(newScenarioName));
+    }
+
+    @Test
+    @TestRail(testCaseId = {"11852", "11854"})
+    @Description("Publish a single private sub-component with no public counterpart Setting Override to false and supplying a scenario name")
+    public void testGroupEditPrivateSubcomponents() {
+        currentUser = UserUtil.getUser();
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        String newScenarioName = new GenerateStringUtil().generateScenarioName();
+
+        final String STAND = "stand";
+        final String DRIVE = "drive";
+        final String JOINT = "joint";
+        String assemblyName = "oldham";
+        final String assemblyExtension = ".asm.1";
+
+        List<String> subComponentNames = Arrays.asList(STAND, DRIVE, JOINT);
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.PLASTIC_MOLDING;
+        final String componentExtension = ".prt.1";
+
+        componentAssembly = assemblyUtils.associateAssemblyAndSubComponents(
+            assemblyName,
+            assemblyExtension,
+            ProcessGroupEnum.ASSEMBLY,
+            subComponentNames,
+            componentExtension,
+            processGroupEnum,
+            scenarioName,
+            currentUser);
+        assemblyUtils.uploadSubComponents(componentAssembly)
+            .uploadAssembly(componentAssembly);
+
+        assemblyUtils.uploadSubComponents(componentAssembly)
+            .uploadAssembly(componentAssembly);
+
+        assemblyUtils.costAssembly(componentAssembly);
+
+        assemblyUtils.publishSubComponents(componentAssembly);
+
+        ForkRequest forkRequest = ForkRequest.builder()
+            .scenarioName(newScenarioName)
+            .override(false)
+            .build();
+
+        ResponseWrapper<ScenarioSuccessesFailures> groupEditResponse = scenariosUtil.postEditGroupScenarios(componentAssembly, forkRequest,
+            STAND + "," + scenarioName, DRIVE + "," + scenarioName);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        groupEditResponse.getResponseEntity().getSuccesses().forEach(o -> softAssertions.assertThat(o.getScenarioName()).isEqualTo(newScenarioName));
+
+        ResponseWrapper<ScenarioSuccessesFailures> singleEditResponse = scenariosUtil.postEditGroupScenarios(componentAssembly, forkRequest,
+            JOINT + "," + scenarioName);
+
+        softAssertions.assertThat(singleEditResponse.getResponseEntity().getSuccesses()).isEqualTo(1);
+        singleEditResponse.getResponseEntity().getSuccesses().forEach(o -> softAssertions.assertThat(o.getScenarioName()).isEqualTo(newScenarioName));
     }
 }
