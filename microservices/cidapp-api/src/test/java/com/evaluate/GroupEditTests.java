b7846676c41401ad073f97496d352dea57f41650
cfrith <cfrith@apriori.com>
2022-07-26 11:18:13 +0100

    refactor: modified time in wait condition as logic was incorrect; refactor: set field as optional; refactor: added test to publish multiple subcomponents setting override to false;

--- microservices/cidapp-api/src/test/java/com/evaluate/GroupEditTests.java
+++ microservices/cidapp-api/src/test/java/com/evaluate/GroupEditTests.java
@@ -13,6 +13,7 @@ import com.apriori.utils.CssComponent;
 import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.enums.ProcessGroupEnum;
+import com.apriori.utils.enums.ScenarioStateEnum;
 import com.apriori.utils.http.utils.ResponseWrapper;
 import com.apriori.utils.reader.file.user.UserCredentials;
 import com.apriori.utils.reader.file.user.UserUtil;
@@ -213,6 +214,68 @@ public class GroupEditTests {
 
         List<ScenarioItem> jointItem = cssComponent.getSimpleCssComponent(JOINT, scenarioName, currentUser).getResponseEntity().getItems();
         softAssertions.assertThat(jointItem.stream().findFirst().get().getScenarioIterationKey().getWorkspaceId()).isEqualTo(0);
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @TestRail(testCaseId = {"11854"})
+    @Description("Publish multiple private sub-components with no public counterparts Setting Override to false and supplying a scenario name")
+    public void testGroupPublishPrivateSubcomponentsFalseOverride() {
+        currentUser = UserUtil.getUser();
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        String newScenarioName = new GenerateStringUtil().generateScenarioName();
+
+        final String STAND = "stand";
+        final String DRIVE = "drive";
+        final String JOINT = "joint";
+        String assemblyName = "oldham";
+        final String assemblyExtension = ".asm.1";
+
+        List<String> subComponentNames = Arrays.asList(STAND, DRIVE, JOINT);
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.PLASTIC_MOLDING;
+        final String componentExtension = ".prt.1";
+
+        componentAssembly = assemblyUtils.associateAssemblyAndSubComponents(
+            assemblyName,
+            assemblyExtension,
+            ProcessGroupEnum.ASSEMBLY,
+            subComponentNames,
+            componentExtension,
+            processGroupEnum,
+            scenarioName,
+            currentUser);
+        assemblyUtils.uploadSubComponents(componentAssembly)
+            .uploadAssembly(componentAssembly);
+
+        User user = new PeopleUtil().getCurrentUser(currentUser);
+
+        PublishRequest publishRequest = PublishRequest.builder()
+            .assignedTo(user.getIdentity())
+            .costMaturity("Initial".toUpperCase())
+            .override(false)
+            .status("New".toUpperCase())
+            .scenarioName(newScenarioName)
+            .build();
+
+        scenariosUtil.postPublishGroupScenarios(publishRequest, user.getCustomAttributes().getWorkspaceId(), componentAssembly,
+            STAND + "," + scenarioName, DRIVE + "," + scenarioName, JOINT + "," + scenarioName);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        componentAssembly.getSubComponents().forEach(scenario -> scenariosUtil.getScenarioRepresentation(scenario, ScenarioStateEnum.NOT_COSTED));
+
+        List<ScenarioItem> driveItem = cssComponent.getSimpleCssComponent(DRIVE, newScenarioName, currentUser).getResponseEntity().getItems();
+        softAssertions.assertThat(driveItem.stream().findFirst().get().getScenarioIterationKey().getWorkspaceId()).isEqualTo(0);
+        softAssertions.assertThat(driveItem.stream().findAny().get().getScenarioIterationKey().getWorkspaceId()).isNotEqualTo(user.getCustomAttributes().getWorkspaceId());
+
+        List<ScenarioItem> standItem = cssComponent.getSimpleCssComponent(STAND, newScenarioName, currentUser).getResponseEntity().getItems();
+        softAssertions.assertThat(standItem.stream().findFirst().get().getScenarioIterationKey().getWorkspaceId()).isEqualTo(0);
+        softAssertions.assertThat(standItem.stream().findAny().get().getScenarioIterationKey().getWorkspaceId()).isNotEqualTo(user.getCustomAttributes().getWorkspaceId());
+
+        List<ScenarioItem> jointItem = cssComponent.getSimpleCssComponent(JOINT, newScenarioName, currentUser).getResponseEntity().getItems();
+        softAssertions.assertThat(jointItem.stream().findFirst().get().getScenarioIterationKey().getWorkspaceId()).isEqualTo(0);
+        softAssertions.assertThat(standItem.stream().findAny().get().getScenarioIterationKey().getWorkspaceId()).isNotEqualTo(user.getCustomAttributes().getWorkspaceId());
 
         softAssertions.assertAll();
     }
