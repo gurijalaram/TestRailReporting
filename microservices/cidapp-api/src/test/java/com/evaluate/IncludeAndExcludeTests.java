f71888115169b7e56a8c749cb0d787288c3f742a
cfrith <cfrith@apriori.com>
2022-08-25 15:36:43 +0100

    refactor: added tests for include/exclude (C11928);

--- microservices/cidapp-api/src/test/java/com/evaluate/IncludeAndExcludeTests.java
+++ microservices/cidapp-api/src/test/java/com/evaluate/IncludeAndExcludeTests.java
@@ -185,4 +185,52 @@ public class IncludeAndExcludeTests {
 
         softAssertions.assertAll();
     }
+
+    @Test
+    @TestRail(testCaseId = {"11928"})
+    @Description("Include all sub-components from top-level assembly")
+    public void testIncludeAllSubcomponents() {
+        final String assemblyName = "Assembly01";
+        final String assemblyExtension = ".iam";
+        final ProcessGroupEnum assemblyProcessGroup = ProcessGroupEnum.ASSEMBLY;
+        final String PART_0001 = "Part0001";
+        final String PART_0002 = "Part0002";
+        final String PART_0003 = "Part0003";
+        final String PART_0004 = "Part0004";
+        final List<String> subComponentNames = Arrays.asList(PART_0001, PART_0002, PART_0003, PART_0004);
+        final String subComponentExtension = ".ipt";
+        final ProcessGroupEnum subComponentProcessGroup = ProcessGroupEnum.SHEET_METAL;
+
+        UserCredentials currentUser = UserUtil.getUser();
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+
+        ComponentInfoBuilder componentAssembly = assemblyUtils.associateAssemblyAndSubComponents(assemblyName,
+            assemblyExtension,
+            assemblyProcessGroup,
+            subComponentNames,
+            subComponentExtension,
+            subComponentProcessGroup,
+            scenarioName,
+            currentUser);
+
+        assemblyUtils.uploadSubComponents(componentAssembly)
+            .uploadAssembly(componentAssembly);
+
+        assemblyUtils.costSubComponents(componentAssembly)
+            .costAssembly(componentAssembly);
+
+        ResponseWrapper<AssociationSuccessesFailures> patchResponse = scenariosUtil.patchAssociations(componentAssembly, false, PART_0001 + ", " + scenarioName +
+            "", PART_0002 + ", " + scenarioName, PART_0003 + ", " + scenarioName, PART_0004 + ", " + scenarioName);
+
+        softAssertions.assertThat(patchResponse.getResponseEntity().getSuccesses().size()).isEqualTo(4);
+
+        scenariosUtil.postCostScenario(componentAssembly);
+
+        softAssertions.assertThat(scenariosUtil.isSubcomponentExcluded(componentAssembly, PART_0001, scenarioName)).isEqualTo(false);
+        softAssertions.assertThat(scenariosUtil.isSubcomponentExcluded(componentAssembly, PART_0002, scenarioName)).isEqualTo(false);
+        softAssertions.assertThat(scenariosUtil.isSubcomponentExcluded(componentAssembly, PART_0003, scenarioName)).isEqualTo(false);
+        softAssertions.assertThat(scenariosUtil.isSubcomponentExcluded(componentAssembly, PART_0004, scenarioName)).isEqualTo(false);
+
+        softAssertions.assertAll();
+    }
 }
