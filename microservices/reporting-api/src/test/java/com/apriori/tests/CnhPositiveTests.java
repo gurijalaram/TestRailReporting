770310bf127f2ffc7f5bc918b85a213d5cd5390a
Krzysztof Derezinski (Contractor) <krzysztof_derezinski@epam.com>
2022-08-31 12:38:03 +0200

    adding assertAll in soft assertion

--- microservices/reporting-api/src/test/java/com/apriori/tests/CnhPositiveTests.java
+++ microservices/reporting-api/src/test/java/com/apriori/tests/CnhPositiveTests.java
@@ -34,5 +34,6 @@ public class CnhPositiveTests {
 
         soft.assertThat(responseActual.getResponseEntity().getStatus()).isEqualTo("RUNNING");
         soft.assertThat(responseActual.getResponseEntity().getExecutionId()).isNotEmpty();
+        soft.assertAll();
     }
 }
