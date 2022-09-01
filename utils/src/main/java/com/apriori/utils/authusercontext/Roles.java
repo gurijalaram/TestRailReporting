86aa6cac1ba375d423063b66413551ebd21c4113
bhegan613 <bhegan@apriori.com>
2022-09-01 16:31:02 +0100

    Config yml updaates and other things

--- utils/src/main/java/com/apriori/utils/authusercontext/Roles.java
+++ utils/src/main/java/com/apriori/utils/authusercontext/Roles.java
@@ -1,11 +1,15 @@
 package com.apriori.utils.authusercontext;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @Builder
+@NoArgsConstructor
 public class Roles {
 }
