35671bb098116473a6a51b09e2c1b6fddcd9410e
cfrith <cfrith@apriori.com>
2022-09-07 15:32:55 +0100

    refactor: debugging;

--- web/cidapp-ui/src/test/java/com/evaluate/assemblies/LargeGroupAssemblyTests.java
+++ web/cidapp-ui/src/test/java/com/evaluate/assemblies/LargeGroupAssemblyTests.java
@@ -1,26 +1,17 @@
 package com.evaluate.assemblies;
 
-import static com.utils.PartNamesEnum.CENTRE_BOLT;
-import static com.utils.PartNamesEnum.PIN;
-
 import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
 import com.apriori.cidappapi.utils.AssemblyUtils;
 import com.apriori.pageobjects.pages.evaluate.components.ComponentsListPage;
 import com.apriori.pageobjects.pages.login.CidAppLoginPage;
-import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.TestRail;
-import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.reader.file.user.UserCredentials;
-import com.apriori.utils.reader.file.user.UserUtil;
 import com.apriori.utils.web.driver.TestBase;
 
-import com.utils.ButtonTypeEnum;
 import io.qameta.allure.Description;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class LargeGroupAssemblyTests extends TestBase {
 
@@ -36,29 +27,14 @@ public class LargeGroupAssemblyTests extends TestBase {
         super();
     }
 
-    // @BeforeClass
+    @BeforeClass
     public static void assemblySetup() {
-        final String assemblyName = "Gym Bike";
-        final String assemblyExtension = ".iam";
-        List<String> subComponentNames = Arrays.asList(CENTRE_BOLT.getPartName());
-        final String subComponentExtension = ".ipt";
+        int f = 1;
+        int r = 2;
 
-        final ProcessGroupEnum subComponentProcessGroup = ProcessGroupEnum.PLASTIC_MOLDING;
-        final ProcessGroupEnum assemblyProcessGroup = ProcessGroupEnum.ASSEMBLY;
-
-        currentUser = UserUtil.getUser();
-        scenarioName = new GenerateStringUtil().generateScenarioName();
-
-        componentAssembly = assemblyUtils.associateAssemblyAndSubComponents(assemblyName,
-            assemblyExtension,
-            assemblyProcessGroup,
-            subComponentNames,
-            subComponentExtension,
-            subComponentProcessGroup,
-            scenarioName,
-            currentUser);
-        assemblyUtils.uploadSubComponents(componentAssembly)
-            .uploadAssembly(componentAssembly);
+        if (f < r) {
+            throw new RuntimeException("Exception caught");
+        }
     }
 
     @Test
@@ -66,19 +42,6 @@ public class LargeGroupAssemblyTests extends TestBase {
     @Description("Publish button becomes unavailable when 11+ private sub-components selected")
     public void testPublishButtonAvailability() {
         loginPage = new CidAppLoginPage(driver);
-        componentsListPage = loginPage.login(currentUser)
-            .navigateToScenario(componentAssembly)
-            .openComponents()
-            .multiSelectSubcomponents(
-                CENTRE_BOLT.getPartName() + ", " + scenarioName);
-
-        softAssertions.assertThat(componentsListPage.isAssemblyTableButtonEnabled(ButtonTypeEnum.PUBLISH)).isEqualTo(true);
-
-        componentsListPage.multiSelectSubcomponents(PIN.getPartName() + ", " + scenarioName + "");
-
-        softAssertions.assertThat(componentsListPage.isAssemblyTableButtonEnabled(ButtonTypeEnum.PUBLISH)).isEqualTo(false);
-
-        softAssertions.assertAll();
     }
 
     @Test
