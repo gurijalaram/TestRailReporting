bdd0d0b432ecedb47896fabc38a0948f181c83c7
cfrith <cfrith@apriori.com>
2022-09-21 12:57:19 +0100

    refactor: changed logging to lombok;

--- web/cidapp-ui/src/main/java/com/apriori/pageobjects/navtoolbars/ScenarioPage.java
+++ web/cidapp-ui/src/main/java/com/apriori/pageobjects/navtoolbars/ScenarioPage.java
@@ -3,17 +3,15 @@ package com.apriori.pageobjects.navtoolbars;
 import com.apriori.pageobjects.common.ModalDialogController;
 import com.apriori.utils.PageUtils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class ScenarioPage extends LoadableComponent<ScenarioPage> {
-
-    private static final Logger logger = LoggerFactory.getLogger(ScenarioPage.class);
 
     @FindBy(css = ".modal-title")
     private WebElement dialogTitle;
@@ -35,7 +33,7 @@ public class ScenarioPage extends LoadableComponent<ScenarioPage> {
         this.driver = driver;
         this.pageUtils = new PageUtils(driver);
         this.modalDialogController = new ModalDialogController(driver);
-        logger.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
+        log.debug(pageUtils.currentlyOnPage(this.getClass().getSimpleName()));
         PageFactory.initElements(driver, this);
         this.get();
     }
