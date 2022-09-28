cf1087e14d06a7fc1a6dbfdade502a4b7ada612a
cfrith <cfrith@apriori.com>
2022-09-28 10:10:21 +0100

    refactor: added missing fields to entity;

--- microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/response/componentiteration/ScenarioRoutings.java
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/entity/response/componentiteration/ScenarioRoutings.java
@@ -17,6 +17,8 @@ public class ScenarioRoutings {
     private String digitalFactoryName;
     private String name;
     private Integer piecePartCost;
+    private Integer capitalInvestment;
+    private Integer fullyBurdenedCost;
     private String processGroupName;
     private String routingState;
     private Boolean userSelected;
