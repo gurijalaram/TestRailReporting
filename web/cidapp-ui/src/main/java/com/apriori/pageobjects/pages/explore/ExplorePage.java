e7fdb47de0e0796b12598fdd539894d1816d26c0
Ricky Yabsley <ryabsley@apriori.com>
2022-10-10 17:12:37 +0100

    Addition of filter test C6094 and fix to PreviewPanelTests.java

--- web/cidapp-ui/src/main/java/com/apriori/pageobjects/pages/explore/ExplorePage.java
+++ web/cidapp-ui/src/main/java/com/apriori/pageobjects/pages/explore/ExplorePage.java
@@ -57,6 +57,9 @@ public class ExplorePage extends ExploreToolbar {
     @FindBy(css = "[id='qa-scenario-list-filter-selector'] input")
     private WebElement filterInput;
 
+    @FindBy(css = "#qa-scenario-explorer-filter-selector div div div div[class='text-overflow']")
+    private WebElement currentFilter;
+
     @FindBy(css = "div.no-content.medium-no-content")
     private WebElement noScenariosMessage;
 
@@ -95,6 +98,15 @@ public class ExplorePage extends ExploreToolbar {
         pageUtils.typeAheadSelect(filterDropdown, "qa-scenario-explorer-filter-selector", filter);
         setPagination();
         return this;
+    }
+
+    /**
+     * Get Currently selected Filter
+     *
+     * @return The current filter name as a String
+     */
+    public String getCurrentFilter() {
+        return currentFilter.getText();
     }
 
     /**
