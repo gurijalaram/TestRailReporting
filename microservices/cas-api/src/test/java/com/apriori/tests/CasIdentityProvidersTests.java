bc0bc85ce332b0fe9d3a7d064a257625bfe9362c
nvalieieva <nvalieieva@apriori.com>
2022-10-11 12:19:54 +0300

    - changed identity provider tests for cas - added issue to cds idp tests

--- microservices/cas-api/src/test/java/com/apriori/tests/CasIdentityProvidersTests.java
+++ microservices/cas-api/src/test/java/com/apriori/tests/CasIdentityProvidersTests.java
@@ -5,10 +5,17 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
+import com.apriori.apibase.services.cas.Customer;
 import com.apriori.apibase.services.cas.IdentityProviders;
 import com.apriori.apibase.utils.TestUtil;
 import com.apriori.cas.enums.CASAPIEnum;
+import com.apriori.cas.utils.CasTestUtil;
+import com.apriori.cds.entity.response.IdentityProviderResponse;
+import com.apriori.cds.enums.CDSAPIEnum;
+import com.apriori.cds.utils.CdsTestUtil;
+import com.apriori.entity.response.CustomerUser;
 import com.apriori.entity.response.SingleIdp;
+import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.authorization.AuthorizationUtil;
 import com.apriori.utils.http.builder.request.HTTPRequest;
@@ -18,40 +25,76 @@ import com.apriori.utils.http.utils.ResponseWrapper;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
 import org.apache.http.HttpStatus;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class CasIdentityProvidersTests extends TestUtil {
 
     private String token;
+    private String customerIdentity;
+    private String userIdentity;
+    private String idpIdentity;
+    private CdsTestUtil cdsTestUtil = new CdsTestUtil();
+    private String customerName;
+    private String cloudRef;
+    private String email;
+    private String description;
+    private String userName;
+    private GenerateStringUtil generateStringUtil = new GenerateStringUtil();
 
     @Before
     public void getToken() {
         token = new AuthorizationUtil().getTokenAsString();
+        customerName = generateStringUtil.generateCustomerName();
+        userName = generateStringUtil.generateUserName();
+        cloudRef = generateStringUtil.generateCloudReference();
+        description = customerName + " Description";
+        email = customerName.toLowerCase();
+
+        ResponseWrapper<Customer> customer = CasTestUtil.addCustomer(customerName, cloudRef, description, email);
+        customerIdentity = customer.getResponseEntity().getIdentity();
+
+        ResponseWrapper<CustomerUser> user = CasTestUtil.addUser(customerIdentity, userName, customerName);
+        userIdentity = user.getResponseEntity().getIdentity();
+    }
+
+    @After
+    public void cleanUp() {
+        if (idpIdentity != null) {
+            cdsTestUtil.delete(CDSAPIEnum.SAML_BY_CUSTOMER_PROVIDER_IDS, customerIdentity, idpIdentity);
+        }
+        if (customerIdentity != null && userIdentity != null) {
+            cdsTestUtil.delete(CDSAPIEnum.USER_BY_CUSTOMER_USER_IDS, customerIdentity, userIdentity);
+        }
+        if (customerIdentity != null) {
+            cdsTestUtil.delete(CDSAPIEnum.CUSTOMER_BY_ID, customerIdentity);
+        }
     }
 
     @Test
-    @Ignore ("Will be fixed after fixing a bug IDS-851")
     @Issue("IDS-851")
     @TestRail(testCaseId = {"5646", "5647"})
     @Description("Get IDPs for customer and get IDP by identity")
     public void getIdpCustomer() {
+        String customerName = generateStringUtil.generateCustomerName();
+
+        ResponseWrapper<IdentityProviderResponse> postResponse = cdsTestUtil.addSaml(customerIdentity, userIdentity, customerName);
+        assertThat(postResponse.getStatusCode(), is(equalTo(HttpStatus.SC_CREATED)));
+        idpIdentity = postResponse.getResponseEntity().getIdentity();
 
         ResponseWrapper<IdentityProviders> response = HTTPRequest.build(RequestEntityUtil.init(CASAPIEnum.CUSTOMER, IdentityProviders.class)
             .token(token)
-            .inlineVariables("L2H992828N8M/identity-providers")).get();
+            .inlineVariables(customerIdentity + "/identity-providers")).get();
 
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
         assertThat(response.getResponseEntity().getResponse().getTotalItemCount(), is(greaterThanOrEqualTo(1)));
 
-        String identity = response.getResponseEntity().getResponse().getItems().get(0).getIdentity();
-
-        ResponseWrapper<SingleIdp> responseIdentity = HTTPRequest.build(RequestEntityUtil.init(CASAPIEnum.GET_CUSTOMER_DEPLOYMENT, SingleIdp.class)
+        ResponseWrapper<SingleIdp> responseIdentity = HTTPRequest.build(RequestEntityUtil.init(CASAPIEnum.CUSTOMER, SingleIdp.class)
             .token(token)
-            .inlineVariables(identity)).get();
+            .inlineVariables(customerIdentity + "/identity-providers/" + idpIdentity)).get();
 
         assertThat(responseIdentity.getStatusCode(), is(equalTo(HttpStatus.SC_OK)));
-        assertThat(responseIdentity.getResponseEntity().getResponse().getIdentity(), is(equalTo(identity)));
+        assertThat(responseIdentity.getResponseEntity().getResponse().getIdentity(), is(equalTo(idpIdentity)));
     }
 }
