f3327188217c42018eb8db2ce0e2488b478e5d31
bhegan613 <bhegan@apriori.com>
2022-10-14 10:19:58 +0100

    Fix for cloud context strict checking

--- utils/src/main/java/com/apriori/utils/authorization/AuthorizationUtil.java
+++ utils/src/main/java/com/apriori/utils/authorization/AuthorizationUtil.java
@@ -8,10 +8,15 @@ import com.apriori.utils.http.builder.common.entity.RequestEntity;
 import com.apriori.utils.http.builder.request.HTTPRequest;
 import com.apriori.utils.http.utils.RequestEntityUtil;
 import com.apriori.utils.http.utils.ResponseWrapper;
+import com.apriori.utils.json.utils.JsonManager;
 import com.apriori.utils.properties.PropertiesContext;
 import com.apriori.utils.reader.file.user.UserCredentials;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.Get;
 
 @Slf4j
 public class AuthorizationUtil {
@@ -19,6 +24,7 @@ public class AuthorizationUtil {
     private String email = PropertiesContext.get("${env}.ats.token_email");
     private String issuer = PropertiesContext.get("${env}.ats.token_issuer");
     private String subject = PropertiesContext.get("${customer}.token_subject");
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public AuthorizationUtil(UserCredentials userCredentials) {
         this.username = userCredentials.getUsername();
@@ -66,9 +72,9 @@ public class AuthorizationUtil {
      * @param userCredentials - UserCredentials instance containing user details to use in api call
      * @return Instance of GetDeploymentsResponse with ResponseWrapper
      */
-    private ResponseWrapper<GetDeploymentsResponse> getDeployments(UserCredentials userCredentials) {
+    private ResponseWrapper<String> getDeployments(UserCredentials userCredentials) {
         final RequestEntity requestEntity = RequestEntityUtil
-            .init(DeploymentsAPIEnum.DEPLOYMENTS, GetDeploymentsResponse.class)
+            .init(DeploymentsAPIEnum.DEPLOYMENTS, null)
             .token(userCredentials.getToken())
             .inlineVariables(
                 PropertiesContext.get("${env}.customer_identity"),
@@ -85,7 +91,12 @@ public class AuthorizationUtil {
      * @return GetDeploymentsResponse instance
      */
     private GetDeploymentsResponse getDeploymentsResponse(UserCredentials userCredentials) {
-        return getDeployments(userCredentials).getResponseEntity();
+        Gson gson = new Gson();
+        return gson.fromJson(
+            gson.toJson(
+                JsonParser.parseString(
+                    getDeployments(userCredentials).getBody()).getAsJsonObject().getAsJsonObject("response")),
+            GetDeploymentsResponse.class);
     }
 
     /**
