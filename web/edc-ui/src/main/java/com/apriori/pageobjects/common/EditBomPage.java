223bb6fa2a22dc3818456386c37ee73052975a97
Krzysztof Derezinski (Contractor) <krzysztof_derezinski@epam.com>
2022-10-14 11:20:50 +0200

    tests

--- web/edc-ui/src/main/java/com/apriori/pageobjects/common/EditBomPage.java
+++ web/edc-ui/src/main/java/com/apriori/pageobjects/common/EditBomPage.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 public class EditBomPage extends EagerPageComponent<EditBomPage> {
 
@@ -29,6 +32,18 @@ public class EditBomPage extends EagerPageComponent<EditBomPage> {
 
     @FindBy(id = "manufacturer-part-number")
     private WebElement partNumber;
+
+    @FindBy(css = "input[class='form-control is-invalid']")
+    private WebElement mountTypeError;
+
+    @FindBy(xpath = "//button[@class = 'btn btn-outline-primary btn-sm disabled']")
+    private WebElement saveBtnDisabled;
+
+    @FindBy(css = "input[type='radio'][name='mountType']")
+    private List<WebElement> mountTypeRadioButtons;
+
+    @FindBy(css = "[class='input-radio'] label")
+    private List<WebElement> mountTypeButtonsText;
 
     private DialogController dialogController = new DialogController(getDriver());
 
@@ -53,6 +68,49 @@ public class EditBomPage extends EagerPageComponent<EditBomPage> {
      */
     public boolean isSaveButtonEnabled() {
         return getPageUtils().waitForElementToAppear(disabledSaveButton).isEnabled();
+    }
+
+    /**
+     * Check if the disabled save button is displayed
+     *
+     * @return boolean
+     */
+    public boolean isSaveButtonDisabledDisplayed() {
+        return getPageUtils().waitForElementToAppear(saveBtnDisabled).isDisplayed();
+    }
+
+    /**
+     * Check if mount type warning message is displayed
+     *
+     * @return boolean
+     */
+    public boolean isMountTypeWarnMsgDisplayed() {
+        return getPageUtils().waitForElementToAppear(mountTypeError).isDisplayed();
+    }
+
+    /**
+     * get list of radio buttons labels for mount type
+     *
+     *
+     * @return list of label names for radio buttons for mount type
+     */
+    public List<String> getMountTypeBtnLabels() {
+
+        List<String> radioListText = mountTypeButtonsText
+            .stream()
+            .map(i -> i.getText()).collect(Collectors.toList());
+        return radioListText;
+    }
+
+    /**
+     * get the mount type radio button options
+     *
+     *
+     * @return int - size of elements' list
+     */
+
+    public int getMountTypeButtonsSize() {
+        return mountTypeRadioButtons.size();
     }
 
     /**
