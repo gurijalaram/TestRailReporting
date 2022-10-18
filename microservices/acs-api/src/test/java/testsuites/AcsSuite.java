82401b61a72a4a0370a64dd80420e7bae3e67d8e
smccaffreyapriori <smccaffrey@apriori.com>
2022-10-18 17:25:40 +0100

    Get Available Routings - Code Review changes

--- microservices/acs-api/src/test/java/testsuites/AcsSuite.java
+++ microservices/acs-api/src/test/java/testsuites/AcsSuite.java
@@ -2,10 +2,26 @@ package testsuites;
 
 import com.apriori.utils.ProjectRunID;
 import com.apriori.utils.runner.ConcurrentSuiteRunner;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import tests.acs.*;
+import tests.acs.AvailableRoutingsTests;
+import tests.acs.CreateMissingScenarioTests;
+import tests.acs.Get2DImageByScenarioIterationKeyTests;
+import tests.acs.GetActiveAxesByScenarioIterationKeyTests;
+import tests.acs.GetActiveDimensionsByScenarioIterationKeyTests;
+import tests.acs.GetArtifactTableInfoTests;
+import tests.acs.GetEnabledCurrencyRateVersionsTests;
+import tests.acs.GetPartPrimaryProcessGroupsTests;
+import tests.acs.GetScenariosInfoTests;
+import tests.acs.GetSetArtifactPropertiesTests;
+import tests.acs.GetSetDisplayUnitsTests;
+import tests.acs.GetSetProductionDefaultsTests;
+import tests.acs.GetSetProductionInfoTests;
+import tests.acs.GetSetTolerancePolicyDefaultsTests;
+import tests.acs.GetSetUserPreferenceByNameTests;
+import tests.acs.GetSetUserPreferencesTests;
+import tests.acs.GetUnitVariantSettingsTests;
+import tests.acs.SaveRoutingSelectionTests;
 
 @ProjectRunID("1347")
 @RunWith(ConcurrentSuiteRunner.class)
