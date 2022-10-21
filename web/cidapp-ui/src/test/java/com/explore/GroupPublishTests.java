b1614bf3c106771b666b750259a8defa58946519
Zlatina Marcheva <zmarcheva@apriori.com>
2022-10-21 10:55:46 +0300

    BA-2552 Group Publish Tests

---
+++ web/cidapp-ui/src/test/java/com/explore/GroupPublishTests.java
@@ -0,0 +1,338 @@
+package com.explore;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
+import com.apriori.cidappapi.utils.AssemblyUtils;
+import com.apriori.pageobjects.navtoolbars.AssignPage;
+import com.apriori.pageobjects.navtoolbars.InfoPage;
+import com.apriori.pageobjects.navtoolbars.PublishPage;
+import com.apriori.pageobjects.pages.evaluate.EvaluatePage;
+import com.apriori.pageobjects.pages.explore.ExplorePage;
+import com.apriori.pageobjects.pages.login.CidAppLoginPage;
+import com.apriori.utils.FileResourceUtil;
+import com.apriori.utils.GenerateStringUtil;
+import com.apriori.utils.TestRail;
+import com.apriori.utils.enums.ProcessGroupEnum;
+import com.apriori.utils.enums.ScenarioStateEnum;
+import com.apriori.utils.reader.file.user.UserCredentials;
+import com.apriori.utils.reader.file.user.UserUtil;
+import com.apriori.utils.web.driver.TestBase;
+
+import com.utils.MultiUpload;
+import io.qameta.allure.Description;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupPublishTests extends TestBase {
+
+    private UserCredentials currentUser;
+    private CidAppLoginPage loginPage;
+    private ExplorePage explorePage;
+    private ComponentInfoBuilder cidComponentItem;
+    private ComponentInfoBuilder cidComponentItemA;
+    private final SoftAssertions softAssertions = new SoftAssertions();
+    private final AssemblyUtils assemblyUtils = new AssemblyUtils();
+    private AssignPage assignPage;
+    private InfoPage infoPage;
+
+
+    @Test
+    @TestRail(testCaseId = {"14458"})
+    @Description("Publish multiple components")
+    public void testGroupPublishPartScenarios() {
+        final ProcessGroupEnum processGroupEnum1 = ProcessGroupEnum.PLASTIC_MOLDING;
+        String componentName1 = "titan charger lead";
+        final File resourceFile1 = FileResourceUtil.getCloudFile(processGroupEnum1, componentName1 + ".SLDPRT");
+        final String scenarioName1 = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        final ProcessGroupEnum processGroupEnum2 = ProcessGroupEnum.SHEET_METAL;
+        String componentName2 = "Part0004";
+        final File resourceFile2 = FileResourceUtil.getCloudFile(processGroupEnum2, componentName2 + ".ipt");
+        final String scenarioName2 = new GenerateStringUtil().generateScenarioName();
+
+        loginPage = new CidAppLoginPage(driver);
+
+        cidComponentItem = loginPage.login(currentUser)
+            .uploadComponent(componentName1, scenarioName1, resourceFile1, currentUser);
+
+        cidComponentItemA = new ExplorePage(driver).uploadComponent(componentName2, scenarioName2, resourceFile2, currentUser);
+        List<MultiUpload> multiComponents = new ArrayList<>();
+        multiComponents.add(new MultiUpload(resourceFile1, scenarioName1));
+        multiComponents.add(new MultiUpload(resourceFile2, scenarioName2));
+
+        explorePage = new ExplorePage(driver)
+            .refresh()
+            .multiSelectScenarios("" + componentName1 + ", " + scenarioName1 + "", "" + componentName2 + ", " + scenarioName2 + "")
+            .publishScenario(PublishPage.class)
+            .override()
+            .clickContinue(PublishPage.class)
+            .publish(PublishPage.class)
+            .close(ExplorePage.class);
+
+        multiComponents.forEach(component ->
+            assertThat(explorePage.getScenarioState(component.getResourceFile().getName().split("\\.")[0],
+                component.getScenarioName(), currentUser, ScenarioStateEnum.NOT_COSTED), is(ScenarioStateEnum.NOT_COSTED.getState())));
+
+        explorePage.refresh()
+                .selectFilter("Public");
+
+        softAssertions.assertThat(explorePage.getListOfScenarios(componentName1, scenarioName1)).isEqualTo(1);
+        softAssertions.assertThat(explorePage.getListOfScenarios(componentName2, scenarioName2)).isEqualTo(1);
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @TestRail(testCaseId = {"14463", "14460"})
+    @Description("Publish multiple assemblies")
+    public void testGroupPublishAssemblyScenarios() {
+        currentUser = UserUtil.getUser();
+        final String assemblyName1 = "Hinge assembly";
+        final String assemblyExtension1 = ".SLDASM";
+        final ProcessGroupEnum assemblyProcessGroup1 = ProcessGroupEnum.ASSEMBLY;
+        final List<String> subComponentNames1 = Arrays.asList("big ring", "Pin", "small ring");
+        final String subComponentExtension1 = ".SLDPRT";
+        final ProcessGroupEnum subComponentProcessGroup1 = ProcessGroupEnum.FORGING;
+
+        final String assemblyScenarioName1 = new GenerateStringUtil().generateScenarioName();
+
+        ComponentInfoBuilder componentAssembly1 = assemblyUtils.associateAssemblyAndSubComponents(
+            assemblyName1,
+            assemblyExtension1,
+            assemblyProcessGroup1,
+            subComponentNames1,
+            subComponentExtension1,
+            subComponentProcessGroup1,
+            assemblyScenarioName1,
+            currentUser);
+
+        assemblyUtils.uploadSubComponents(componentAssembly1).uploadAssembly(componentAssembly1);
+        assemblyUtils.costSubComponents(componentAssembly1).costAssembly(componentAssembly1);
+        assemblyUtils.publishSubComponents(componentAssembly1);
+
+        final String assemblyName2 = "flange c";
+        final String assemblyExtension2 = ".CATProduct";
+        final ProcessGroupEnum assemblyProcessGroup2 = ProcessGroupEnum.ASSEMBLY;
+        final List<String> subComponentNames2 = Arrays.asList("flange", "nut", "bolt");
+        final String subComponentExtension2 = ".CATPart";
+        final ProcessGroupEnum subComponentProcessGroup2 = ProcessGroupEnum.PLASTIC_MOLDING;
+
+        final String assemblyScenarioName2 = new GenerateStringUtil().generateScenarioName();
+
+        ComponentInfoBuilder componentAssembly2 = assemblyUtils.associateAssemblyAndSubComponents(
+            assemblyName2,
+            assemblyExtension2,
+            assemblyProcessGroup2,
+            subComponentNames2,
+            subComponentExtension2,
+            subComponentProcessGroup2,
+            assemblyScenarioName2,
+            currentUser);
+
+        assemblyUtils.uploadSubComponents(componentAssembly2).uploadAssembly(componentAssembly2);
+        assemblyUtils.costSubComponents(componentAssembly2).costAssembly(componentAssembly2);
+        assemblyUtils.publishSubComponents(componentAssembly2);
+
+        loginPage = new CidAppLoginPage(driver);
+        explorePage = loginPage.login(currentUser)
+            .multiSelectScenarios("" + assemblyName1 + ", " + assemblyScenarioName1 + "", "" + assemblyName2 + ", " + assemblyScenarioName2 + "")
+            .publishScenario(PublishPage.class)
+            .override()
+            .clickContinue(PublishPage.class)
+            .publish(PublishPage.class)
+            .close(ExplorePage.class)
+            .checkComponentStateRefresh(componentAssembly1, ScenarioStateEnum.COST_COMPLETE)
+            .checkComponentStateRefresh(componentAssembly2, ScenarioStateEnum.COST_COMPLETE)
+            .refresh()
+            .selectFilter("Public");
+
+        softAssertions.assertThat(explorePage.getListOfScenarios(assemblyName1, assemblyScenarioName1)).isEqualTo(1);
+        softAssertions.assertThat(explorePage.getListOfScenarios(assemblyName2, assemblyScenarioName2)).isEqualTo(1);
+
+        explorePage.multiSelectScenarios("" + assemblyName1 + ", " + assemblyScenarioName1 + "", "" + assemblyName2 + ", " + assemblyScenarioName2 + "");
+
+        softAssertions.assertThat(explorePage.isPublishButtonEnabled()).isEqualTo(false);
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @TestRail(testCaseId = {"14459"})
+    @Description("Publish a mix of components and assemblies")
+    public void testGroupPublishComponentAndAssembly() {
+        currentUser = UserUtil.getUser();
+        final String assemblyName1 = "Hinge assembly";
+        final String assemblyExtension1 = ".SLDASM";
+        final ProcessGroupEnum assemblyProcessGroup1 = ProcessGroupEnum.ASSEMBLY;
+        final List<String> subComponentNames1 = Arrays.asList("big ring", "Pin", "small ring");
+        final String subComponentExtension1 = ".SLDPRT";
+        final ProcessGroupEnum subComponentProcessGroup1 = ProcessGroupEnum.FORGING;
+
+        final String assemblyScenarioName1 = new GenerateStringUtil().generateScenarioName();
+
+        ComponentInfoBuilder componentAssembly1 = assemblyUtils.associateAssemblyAndSubComponents(
+            assemblyName1,
+            assemblyExtension1,
+            assemblyProcessGroup1,
+            subComponentNames1,
+            subComponentExtension1,
+            subComponentProcessGroup1,
+            assemblyScenarioName1,
+            currentUser);
+
+        assemblyUtils.uploadSubComponents(componentAssembly1).uploadAssembly(componentAssembly1);
+
+        loginPage = new CidAppLoginPage(driver);
+        explorePage = loginPage.login(currentUser)
+            .multiSelectScenarios("" + "big ring" + ", " + assemblyScenarioName1 + "");
+
+        softAssertions.assertThat(explorePage.isPublishButtonEnabled()).isEqualTo(true);
+
+        explorePage.multiSelectScenarios("" + assemblyName1 + ", " + assemblyScenarioName1 + "");
+
+        softAssertions.assertThat(explorePage.isPublishButtonEnabled()).isEqualTo(false);
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @TestRail(testCaseId = {"14464"})
+    @Description("Publish multiple components and set inputs in modal")
+    public void testGroupPublishWithInputs() {
+        final ProcessGroupEnum processGroupEnum1 = ProcessGroupEnum.PLASTIC_MOLDING;
+        String componentName1 = "titan charger lead";
+        final File resourceFile1 = FileResourceUtil.getCloudFile(processGroupEnum1, componentName1 + ".SLDPRT");
+        final String scenarioName1 = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        final ProcessGroupEnum processGroupEnum2 = ProcessGroupEnum.SHEET_METAL;
+        String componentName2 = "Part0004";
+        final File resourceFile2 = FileResourceUtil.getCloudFile(processGroupEnum2, componentName2 + ".ipt");
+        final String scenarioName2 = new GenerateStringUtil().generateScenarioName();
+
+        loginPage = new CidAppLoginPage(driver);
+
+        cidComponentItem = loginPage.login(currentUser)
+            .uploadComponent(componentName1, scenarioName1, resourceFile1, currentUser);
+
+        cidComponentItemA = new ExplorePage(driver).uploadComponent(componentName2, scenarioName2, resourceFile2, currentUser);
+        String scenarioAssignedTo = cidComponentItem.getScenarioItem().getScenarioCreatedByName();
+
+        List<MultiUpload> multiComponents = new ArrayList<>();
+        multiComponents.add(new MultiUpload(resourceFile1, scenarioName1));
+        multiComponents.add(new MultiUpload(resourceFile2, scenarioName2));
+
+        explorePage = new ExplorePage(driver).refresh()
+            .multiSelectScenarios("" + componentName1 + ", " + scenarioName1 + "", "" + componentName2 + ", " + scenarioName2 + "")
+            .publishScenario(PublishPage.class)
+            .override()
+            .clickContinue(PublishPage.class)
+            .selectStatus("Analysis")
+            .selectCostMaturity("Low")
+            .selectAssignee(currentUser)
+            .publish(PublishPage.class)
+            .close(ExplorePage.class);
+
+        multiComponents.forEach(component ->
+            assertThat(explorePage.getScenarioState(component.getResourceFile().getName().split("\\.")[0],
+                component.getScenarioName(), currentUser, ScenarioStateEnum.NOT_COSTED), is(ScenarioStateEnum.NOT_COSTED.getState())));
+
+        assignPage = explorePage.refresh()
+            .selectFilter("Public")
+            .openScenario(componentName1, scenarioName1)
+            .clickActions()
+            .assign();
+
+        softAssertions.assertThat(assignPage.isAssigneeDisplayed(scenarioAssignedTo)).isEqualTo(true);
+
+        infoPage = assignPage.cancel(EvaluatePage.class)
+            .clickActions()
+            .info();
+
+        softAssertions.assertThat(infoPage.getCostMaturity()).isEqualTo("Low");
+        softAssertions.assertThat(infoPage.getStatus()).isEqualTo("Analysis");
+
+        infoPage.cancel(EvaluatePage.class)
+            .clickExplore()
+            .openScenario(componentName2, scenarioName2)
+            .clickActions()
+            .assign();
+
+        softAssertions.assertThat(assignPage.isAssigneeDisplayed(scenarioAssignedTo)).isEqualTo(true);
+
+        infoPage = assignPage.cancel(EvaluatePage.class)
+            .clickActions()
+            .info();
+        softAssertions.assertThat(infoPage.getCostMaturity()).isEqualTo("Low");
+        softAssertions.assertThat(infoPage.getStatus()).isEqualTo("Analysis");
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @TestRail(testCaseId = {"14461", "14462"})
+    @Description("Attempt to publish more than 10 components/assemblies")
+    public void testGroupPublishMoreThanTenScenarios() {
+        currentUser = UserUtil.getUser();
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        List<MultiUpload> multiComponents = new ArrayList<>();
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.FORGING, "big ring.SLDPRT"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.PLASTIC_MOLDING, "titan charger lead.SLDPRT"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.FORGING, "small ring.SLDPRT"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.SHEET_METAL, "Part0004.ipt"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.SHEET_METAL, "bracket_basic.prt"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.POWDER_METAL, "PowderMetalShaft.stp"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.PLASTIC_MOLDING, "Push Pin.stp"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.CASTING_INVESTMENT, "piston cover_model1.prt"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.SHEET_METAL, "Part0005b.ipt"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.CASTING_INVESTMENT, "piston rod_model1.prt"), scenarioName));
+        multiComponents.add(new MultiUpload(FileResourceUtil.getCloudFile(ProcessGroupEnum.CASTING_INVESTMENT, "piston_model1.prt"), scenarioName));
+
+        loginPage = new CidAppLoginPage(driver);
+        explorePage = loginPage.login(currentUser)
+            .importCadFile()
+            .inputScenarioName(scenarioName)
+            .inputMultiComponents(multiComponents)
+            .submit()
+            .clickClose()
+            .setPagination()
+            .selectFilter("Recent")
+            .multiSelectScenarios("" + "piston rod_model1" + ", " + scenarioName + "",
+                    "" + "Part0005b" + ", " + scenarioName + "");
+
+        softAssertions.assertThat(explorePage.isPublishButtonEnabled()).isEqualTo(false);
+
+        multiComponents.forEach(component ->
+            assertThat(explorePage.getScenarioState(component.getResourceFile().getName().split("\\.")[0],
+                component.getScenarioName(), currentUser, ScenarioStateEnum.NOT_COSTED), is(ScenarioStateEnum.NOT_COSTED.getState())));
+
+        explorePage.refresh()
+            .setPagination()
+            .multiSelectScenarios("" + "piston rod_model1" + ", " + scenarioName + "",
+                "" + "Part0005b" + ", " + scenarioName + "",
+                "" + "piston cover_model1" + ", " + scenarioName + "",
+                "" + "Push Pin" + ", " + scenarioName + "",
+                "" + "PowderMetalShaft" + ", " + scenarioName + "",
+                "" + "bracket_basic" + ", " + scenarioName + "",
+                "" + "Part0004" + ", " + scenarioName + "",
+                "" + "small ring" + ", " + scenarioName + "",
+                "" + "titan charger lead" + ", " + scenarioName + "",
+                "" + "big ring" + ", " + scenarioName + "");
+
+        softAssertions.assertThat(explorePage.isPublishButtonEnabled()).isEqualTo(true);
+
+        explorePage.multiSelectScenarios("" + "piston_model1" + ", " + scenarioName + "");
+
+        softAssertions.assertThat(explorePage.isPublishButtonEnabled()).isEqualTo(false);
+
+        softAssertions.assertAll();
+    }
+}
