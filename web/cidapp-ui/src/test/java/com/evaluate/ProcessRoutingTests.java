74ff4ef42e776823d2707389da1138764e9b1b98
moyaparker <51705586+moyaparker@users.noreply.github.com>
2022-10-21 09:29:58 +0100

    Ported across remainder of old routing tests

--- web/cidapp-ui/src/test/java/com/evaluate/ProcessRoutingTests.java
+++ web/cidapp-ui/src/test/java/com/evaluate/ProcessRoutingTests.java
@@ -6,6 +6,7 @@ import com.apriori.pageobjects.pages.evaluate.MaterialSelectorPage;
 import com.apriori.pageobjects.pages.evaluate.designguidance.GuidanceIssuesPage;
 import com.apriori.pageobjects.pages.evaluate.inputs.RoutingSelectionPage;
 import com.apriori.pageobjects.pages.evaluate.materialprocess.MaterialProcessPage;
+import com.apriori.pageobjects.pages.explore.ExplorePage;
 import com.apriori.pageobjects.pages.login.CidAppLoginPage;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
@@ -103,6 +104,11 @@ public class ProcessRoutingTests extends TestBase {
             .costScenario();
 
         softAssertions.assertThat(evaluatePage.getProcessRoutingDetails()).contains("Reaction Injection Molding");
+
+        routingSelectionPage = evaluatePage.goToAdvancedTab()
+            .openRoutingSelection();
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("Injection Mold", "Reaction Injection Mold", "Structural Foam Mold");
+
         softAssertions.assertAll();
     }
 
@@ -196,7 +202,7 @@ public class ProcessRoutingTests extends TestBase {
         softAssertions.assertThat(evaluatePage.isCostLabel(NewCostingLabelEnum.COSTING_FAILED)).isEqualTo(true);
 
         evaluatePage.openDesignGuidance()
-                .selectIssueTypeGcd("Costing Failed", "Additive Manufacturing/Surface Treatment is infeasible", "Component:1");
+            .selectIssueTypeGcd("Costing Failed", "Additive Manufacturing/Surface Treatment is infeasible", "Component:1");
 
         softAssertions.assertThat(guidanceIssuesPage.getIssueDescription()).contains("This DMLS material is not compatible with Stereolithography.");
         softAssertions.assertAll();
@@ -374,6 +380,290 @@ public class ProcessRoutingTests extends TestBase {
             .openRoutingSelection();
 
         softAssertions.assertThat(routingSelectionPage.getRoutingStates("Injection Mold")).doesNotContain("MillTurn Routing", "Die Casting");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Be able to see basic breakdown of cycle time by process for problem identification.")
+    public void routingCycleTime() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.STOCK_MACHINING;
+
+        String componentName = "Push Pin";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".stp");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        materialProcessPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .openMaterialProcess()
+            .selectBarChart("Band Saw");
+
+        softAssertions.assertThat(materialProcessPage.getProcessPercentage("Band Saw")).contains("16.23s (52.51%)");
+        materialProcessPage.selectProcessTab();
+
+        softAssertions.assertThat(materialProcessPage.getProcessResult("Machine Name")).contains("DoAll 3613-1 Vert");
+        materialProcessPage.selectBarChart("2 Axis Lathe");
+
+        softAssertions.assertThat(materialProcessPage.getTotalResult("Cycle Time")).isCloseTo(Double.valueOf(15.99), Offset.offset(5.0));
+        softAssertions.assertThat(materialProcessPage.getProcessResult("Machine Name")).contains("Virtual 2 Axis Lathe - Small");
+
+        routingSelectionPage = materialProcessPage.closePanel()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("3 Axis Lathe Routing", "2AL+3AM Routing", "3 Axis Mill Routing", "4 Axis Mill Routing",
+            "5 Axis Mill Routing", "3AM+Drill Press Routing", "3AM+4AM Routing", "3AM+5AM Routing", "MillTurn Routing", "2AL+4AM Routing", "2AL+5AM Routing",
+            "2ABFL and 3AM routing", "3ABFL routing");
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Die Cast")
+    public void routingsDieCasting() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.CASTING_DIE;
+
+        String componentName = "SandCast";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".x_t");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("High Pressure Die Cast", "Gravity Die Cast");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Sand Cast")
+    public void routingsSandCasting() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.CASTING_SAND;
+
+        String componentName = "SandCast";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".x_t");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("VerticalAutomatic", "HorizontalAutomatic", "ManualStd", "ManualFloor", "ManualPit");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Forging")
+    public void routingsForging() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.FORGING;
+
+        String componentName = "ap_blow_molding_excerise_EL0000";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".STEP");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("Ring Rolled Forging", "Closed Die Forging");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Rapid Prototyping")
+    public void routingsRapidPrototyping() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.RAPID_PROTOTYPING;
+
+        String componentName = "Rapid Prototyping";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".stp");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("3D Printing", "Selective Laser Sintering", "Stereolithography");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Roto & Blow Moulding")
+    public void routingsRotoBlowMould() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.ROTO_BLOW_MOLDING;
+
+        String componentName = "Rapid Prototyping";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".stp");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("Blow Molding", "Rotational Molding");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Sheet Metal")
+    public void routingsSheetMetal() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.SHEET_METAL;
+
+        String componentName = "700-33770-01_A0";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".stp");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("Stage Tooling", "Prog Die", "[CTL]/Shear/Press", "[CTL]/Shear/Chemical Mill", "Tandem Die",
+            "[CTL]/Laser/[Bend]", "[CTL]/Laser Punch/[Bend]", "[CTL]/Plasma/[Deslag]/[Bend]", "[CTL]/Plasma Punch/[Deslag]/[Bend]", "[CTL]/Oxyfuel/[Deslag]/[Bend]",
+            "[CTL]/Waterjet/[Bend]", "[CTL]/Turret/[Bend]", "[CTL]/2 Axis Router/[Bend]", "[CTL]/[Bend]", "[CTL]/Fiber Laser/[Bend]");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Sheet Metal - Hydroforming")
+    public void routingsHydroforming() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.SHEET_METAL_HYDROFORMING;
+
+        String componentName = "Hydroforming";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".stp");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("Laser Cut - Fluid Cell Routing", "Router Cut - Fluid Cell Routing",
+            "Offline Blank - Fluid Cell Routing", "Laser Cut - Deep Draw Routing", "Router Cut - Deep Draw Routing", "Offline Blank - Deep Draw Routing");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Sheet Metal - Stretchforming")
+    public void routingsStretchforming() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.SHEET_METAL_STRETCH_FORMING;
+
+        String componentName = "Hydroforming";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".stp");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("Stretch Form Transverse", "Stretch Form Longitudinal");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate routings Sheet Plastic")
+    public void routingsSheetPlastic() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.SHEET_PLASTIC;
+
+        String componentName = "sheet_plastic";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".STEP");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        routingSelectionPage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario()
+            .goToAdvancedTab()
+            .openRoutingSelection();
+
+        softAssertions.assertThat(routingSelectionPage.getAvailableRoutings()).contains("Single Cavity Mold", "2x1-Cavity Mold", "2x2-Cavity Mold");
+        softAssertions.assertAll();
+    }
+
+
+    @Test
+    //@TestRail(testCaseId = {""}) TODO add testrail ID
+    @Description("Validate behaviour when Adding/Editing tolerances that may require additional machining.")
+    public void routingTolerances() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.CASTING_DIE;
+
+        String componentName = "DTCCastingIssues";
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".catpart");
+
+        loginPage = new CidAppLoginPage(driver);
+        evaluatePage = loginPage.login(currentUser)
+            .openSettings()
+            .goToToleranceTab()
+            .selectCad()
+            .submit(ExplorePage.class)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario();
+
+        softAssertions.assertThat(evaluatePage.getProcessRoutingDetails()).contains("Melting / High Pressure Die Casting / Trim / 5 Axis Mill / Cylindrical Grinder / Reciprocating Surface Grinder");
+
+        evaluatePage.goToAdvancedTab()
+            .openRoutingSelection()
+            .selectRoutingPreferenceByName("Gravity Die Cast")
+            .submit(EvaluatePage.class)
+            .costScenario();
+
+        softAssertions.assertThat(evaluatePage.getProcessRoutingDetails()).contains("Melting", "Coremaking", "Gravity Die Casting", "Core Refractory Coat", "Refractory Coat Oven Dry",
+            "Trim", "Cleaning", "Finishing", "Visual Inspection", "3 Axis Mill", "Drill Press", "Cylindrical Grinder", "Reciprocating Surface Grinder");
         softAssertions.assertAll();
     }
 }
