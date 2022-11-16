a059114c0cb8b50bbde6260007f5cfc261833829
cfrith <cfrith@apriori.com>
2022-11-16 10:43:04 +0000

    refactor: added missing commas;

--- microservices/css-api/src/main/java/com/apriori/entity/enums/CssSearch.java
+++ microservices/css-api/src/main/java/com/apriori/entity/enums/CssSearch.java
@@ -3,9 +3,9 @@ package com.apriori.entity.enums;
 public enum CssSearch {
     COMPONENT_NAME_EQ("componentName[EQ],"),
     COMPONENT_NAME_CN("componentName[CN],"),
-    COMPONENT_IDENTITY_EQ("componentIdentity[EQ]"),
+    COMPONENT_IDENTITY_EQ("componentIdentity[EQ],"),
     COMPONENT_TYPE_EQ("componentType[EQ],"),
-    SCENARIO_IDENTITY_EQ("scenarioIdentity[EQ]"),
+    SCENARIO_IDENTITY_EQ("scenarioIdentity[EQ],"),
     SCENARIO_NAME_EQ("scenarioName[EQ],"),
     SCENARIO_NAME_CN("scenarioName[CN],"),
     SCENARIO_STATE_EQ("scenarioState[EQ],"),
