03072c0078cd20c789cea399eb741a15a87692b4
cfrith <cfrith@apriori.com>
2022-11-21 11:38:49 +0000

    refactor: checkstyle fix;

--- microservices/cidapp-api/src/test/java/com/evaluate/RoutingsTests.java
+++ microservices/cidapp-api/src/test/java/com/evaluate/RoutingsTests.java
@@ -18,34 +18,34 @@ import org.junit.Test;
 import java.io.File;
 
 public class RoutingsTests {
-        private final ScenariosUtil scenariosUtil = new ScenariosUtil();
-        private SoftAssertions softAssertions;
+    private final ScenariosUtil scenariosUtil = new ScenariosUtil();
+    private SoftAssertions softAssertions;
 
-        @Test
-        @Description("Test routings")
-        public void cadFormatSLDPRT() {
-            final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.STOCK_MACHINING;
-            final String componentName = "Machined Box AMERICAS";
-            final File resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".SLDPRT");
-            final UserCredentials currentUser = UserUtil.getUser();
-            final String scenarioName = new GenerateStringUtil().generateScenarioName();
+    @Test
+    @Description("Test routings")
+    public void cadFormatSLDPRT() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.STOCK_MACHINING;
+        final String componentName = "Machined Box AMERICAS";
+        final File resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".SLDPRT");
+        final UserCredentials currentUser = UserUtil.getUser();
+        final String scenarioName = new GenerateStringUtil().generateScenarioName();
 
-            ScenarioResponse scenarioResponse = new DataCreationUtil(componentName, scenarioName, processGroupEnum, resourceFile, currentUser).createCostComponent();
+        ScenarioResponse scenarioResponse = new DataCreationUtil(componentName, scenarioName, processGroupEnum, resourceFile, currentUser).createCostComponent();
 
-            Routings routings = scenariosUtil.getRoutings(currentUser, new CssComponent().findFirst(componentName, scenarioName, currentUser).getComponentIdentity(),
-                scenarioResponse.getIdentity()).getResponseEntity();
+        Routings routings = scenariosUtil.getRoutings(currentUser, new CssComponent().findFirst(componentName, scenarioName, currentUser).getComponentIdentity(),
+            scenarioResponse.getIdentity()).getResponseEntity();
 
-            softAssertions = new SoftAssertions();
+        softAssertions = new SoftAssertions();
 
-            softAssertions.assertThat(routings.getItems().size()).isGreaterThan(0);
+        softAssertions.assertThat(routings.getItems().size()).isGreaterThan(0);
 
-            routings.getItems().forEach(routing -> {
-                softAssertions.assertThat(routing.getDigitalFactoryName()).isNotEmpty();
-                softAssertions.assertThat(routing.getDisplayName()).isNotEmpty();
-                softAssertions.assertThat(routing.getName()).isNotEmpty();
-                softAssertions.assertThat(routing.getProcessGroupName()).isNotEmpty();
-            });
+        routings.getItems().forEach(routing -> {
+            softAssertions.assertThat(routing.getDigitalFactoryName()).isNotEmpty();
+            softAssertions.assertThat(routing.getDisplayName()).isNotEmpty();
+            softAssertions.assertThat(routing.getName()).isNotEmpty();
+            softAssertions.assertThat(routing.getProcessGroupName()).isNotEmpty();
+        });
 
-            softAssertions.assertAll();
-        }
+        softAssertions.assertAll();
+    }
 }
