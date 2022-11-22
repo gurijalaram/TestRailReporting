86b3e7d48ee67a31c7c15211b76b670e877caa3a
gurijalaram16 <98057348+gurijalaram16@users.noreply.github.com>
2022-11-22 10:35:17 -0500

    Added DMS comment view endpoint and tests (#2050)

--- microservices/dms-api/src/main/java/entity/response/DmsDiscussionResponse.java
+++ microservices/dms-api/src/main/java/entity/response/DmsDiscussionResponse.java
@@ -12,19 +12,21 @@ import java.util.ArrayList;
 
 @Data
 @JsonRootName("response")
-@Schema(location = "DmsCommentResponseSchema.json")
-public class DmsCommentResponse {
+@Schema(location = "DmsDiscussionResponseSchema.json")
+public class DmsDiscussionResponse {
     private String identity;
     private String createdBy;
     @JsonDeserialize(using = DateTimeDeserializer_yyyyMMddTHHmmssSSSZ.class)
     private LocalDateTime createdAt;
+    private String description;
     private String status;
-    private String content;
-    private ArrayList<DmsParticipant> mentionedUsers;
-    @JsonDeserialize(using = DateTimeDeserializer_yyyyMMddTHHmmssSSSZ.class)
-    private LocalDateTime publishedAt;
-    private ArrayList<DmsCommentViewResponse> commentView;
+    private ArrayList<DmsCommentResponse> comments;
+    private ArrayList<DmsParticipant> participants;
     private String updatedBy;
     @JsonDeserialize(using = DateTimeDeserializer_yyyyMMddTHHmmssSSSZ.class)
     private LocalDateTime updatedAt;
+    @JsonDeserialize(using = DateTimeDeserializer_yyyyMMddTHHmmssSSSZ.class)
+    private LocalDateTime assignedAt;
+    private DmsAssignee assignee;
+    private DmsAttributes attributes;
 }
