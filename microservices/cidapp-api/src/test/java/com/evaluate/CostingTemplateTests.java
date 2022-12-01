d2237897032820edb9d4445bc5d1e389b95edc99
cfrith <cfrith@apriori.com>
2022-12-01 10:16:43 +0000

    refactor: added test for default costing template values;

--- microservices/cidapp-api/src/test/java/com/evaluate/CostingTemplateTests.java
+++ microservices/cidapp-api/src/test/java/com/evaluate/CostingTemplateTests.java
@@ -4,6 +4,7 @@ import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
 import com.apriori.cidappapi.entity.response.CostingTemplate;
 import com.apriori.cidappapi.entity.response.CostingTemplates;
 import com.apriori.cidappapi.utils.ComponentsUtil;
+import com.apriori.cidappapi.utils.IterationsUtil;
 import com.apriori.cidappapi.utils.ScenariosUtil;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
@@ -19,9 +20,10 @@ import java.io.File;
 
 public class CostingTemplateTests {
 
-    ComponentsUtil componentsUtil = new ComponentsUtil();
-    SoftAssertions softAssertions = new SoftAssertions();
-    ScenariosUtil scenariosUtil = new ScenariosUtil();
+    private final ComponentsUtil componentsUtil = new ComponentsUtil();
+    private final SoftAssertions softAssertions = new SoftAssertions();
+    private final ScenariosUtil scenariosUtil = new ScenariosUtil();
+    private final IterationsUtil iterationsUtil = new IterationsUtil();
 
     @Test
     public void testCostingTemplateId() {
@@ -95,6 +97,26 @@ public class CostingTemplateTests {
 
         softAssertions.assertThat(costingTemplates.getResponseEntity().getItems().size()).isGreaterThan(0);
         costingTemplates.getResponseEntity().getItems().forEach(o -> softAssertions.assertThat(o.getIdentity()).isNotEmpty());
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    public void testCostingTemplateDefaultValues() {
+        final UserCredentials currentUser = UserUtil.getUser();
+
+        ComponentInfoBuilder costingInfo = ComponentInfoBuilder.builder()
+            .costingTemplate(CostingTemplate.builder().build())
+            .user(currentUser)
+            .build();
+
+        CostingTemplate costingTemplate = new ScenariosUtil().postCostingTemplate(costingInfo);
+
+        softAssertions.assertThat(costingTemplate.getAnnualVolume()).isEqualTo(500);
+        softAssertions.assertThat(costingTemplate.getBatchSize()).isEqualTo(458);
+        softAssertions.assertThat(costingTemplate.getProductionLife()).isEqualTo(5.0);
+        softAssertions.assertThat(costingTemplate.getProductionLife()).isNotEqualTo(9.5);
+        softAssertions.assertThat(costingTemplate.getProcessGroupName()).isNull();
 
         softAssertions.assertAll();
     }
