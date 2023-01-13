0eda74f34b96dc34523485943fc231bc2cf1cfc9
Zlatina Marcheva <zmarcheva@apriori.com>
2023-01-13 12:08:44 +0200

    BA-2768 adding automated tests

--- web/cidapp-ui/src/test/java/com/evaluate/assemblies/IncludeAndExcludeNestedAssemblyTests.java
+++ web/cidapp-ui/src/test/java/com/evaluate/assemblies/IncludeAndExcludeNestedAssemblyTests.java
@@ -139,14 +139,21 @@ public class IncludeAndExcludeNestedAssemblyTests extends TestBase {
 
     @Test
     @Category(ExtendedRegression.class)
-    @TestRail(testCaseId = {"11827", "11826"})
+    @TestRail(testCaseId = {"11827", "11826", "11863", "11950"})
     @Description("Validate  the publish button will only be enabled in the tree view and now the list view")
     public void testPublishButtonEnabledInTreeView() {
         loginPage = new CidAppLoginPage(driver);
         componentsTreePage = loginPage.login(currentUser)
             .navigateToScenario(componentAssembly3)
-            .openComponents()
-            .multiSelectSubcomponents("3575135, " + scenarioName + "", "3574255, " + scenarioName + "", "3575134, " + scenarioName + "", "3575132, " + scenarioName + "", "3538968, " + scenarioName + "");
+            .openComponents();
+
+        softAssertions.assertThat(componentsTreePage.isComponentNameDisplayedInTreeView(SUB_SUB_ASSEMBLY)).isFalse();
+
+        componentsTreePage.expandSubAssembly(SUB_ASSEMBLY, scenarioName);
+
+        softAssertions.assertThat(componentsTreePage.isComponentNameDisplayedInTreeView(SUB_SUB_ASSEMBLY)).isTrue();
+
+        componentsTreePage.multiSelectSubcomponents("3575135, " + scenarioName + "", "3574255, " + scenarioName + "", "3575134, " + scenarioName + "", "3575132, " + scenarioName + "", "3538968, " + scenarioName + "");
 
         softAssertions.assertThat(componentsTreePage.isAssemblyTableButtonEnabled(ButtonTypeEnum.PUBLISH)).isEqualTo(true);
 
