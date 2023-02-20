096457cb7965058cc8bbeee44295b7ff0332fab7
cfrith <cfrith@apriori.com>
2023-02-20 10:03:34 +0000

    refactor: added method to remove subcomponent from assembly;

--- microservices/cidapp-api/src/main/java/com/apriori/cidappapi/utils/AssemblyUtils.java
+++ microservices/cidapp-api/src/main/java/com/apriori/cidappapi/utils/AssemblyUtils.java
@@ -10,6 +10,7 @@ import com.apriori.utils.reader.file.user.UserCredentials;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -259,10 +260,10 @@ public class AssemblyUtils {
     }
 
     /**
-     * Delete an assembly and all it's sub-components
+     * Delete an assembly and all it's subcomponents
      *
-     * @param componentAssembly - The Assembly ComponentIfo
-     * @param currentUser - The current user
+     * @param componentAssembly - the component assembly
+     * @param currentUser       - the current user
      * @return - Current Object
      */
     public AssemblyUtils deleteAssemblyAndComponents(ComponentInfoBuilder componentAssembly, UserCredentials currentUser) {
@@ -270,6 +271,18 @@ public class AssemblyUtils {
         componentAssembly.getSubComponents().forEach(
             subComponent -> scenariosUtil.deleteScenario(subComponent.getComponentIdentity(), subComponent.getScenarioIdentity(), currentUser));
 
+        return this;
+    }
+
+    /**
+     * Disassociates a subcomponent from an assembly
+     *
+     * @param componentAssembly - the component assembly
+     * @param subcomponents     - the list of subcomponents
+     * @return current object
+     */
+    public AssemblyUtils disassociateSubcomponents(ComponentInfoBuilder componentAssembly, String... subcomponents) {
+        Arrays.stream(subcomponents).forEach(subcomponent -> componentAssembly.getSubComponents().removeIf(o -> o.getComponentName().equalsIgnoreCase(subcomponent)));
         return this;
     }
 }
