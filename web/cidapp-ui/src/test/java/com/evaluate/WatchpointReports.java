594914bee10e9e0253c51598840ff3a277922d63
moyaparker <51705586+moyaparker@users.noreply.github.com>
2023-02-20 17:13:21 +0000

    Added a new test for part cost reports

---
+++ web/cidapp-ui/src/test/java/com/evaluate/WatchpointReports.java
@@ -0,0 +1,67 @@
+package com.evaluate;
+
+import com.apriori.pageobjects.pages.evaluate.EvaluatePage;
+import com.apriori.pageobjects.pages.login.CidAppLoginPage;
+import com.apriori.utils.FileResourceUtil;
+import com.apriori.utils.GenerateStringUtil;
+import com.apriori.utils.TestRail;
+import com.apriori.utils.enums.NewCostingLabelEnum;
+import com.apriori.utils.enums.ProcessGroupEnum;
+import com.apriori.utils.reader.file.user.UserCredentials;
+import com.apriori.utils.reader.file.user.UserUtil;
+import com.apriori.utils.web.driver.TestBase;
+
+import io.qameta.allure.Description;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import testsuites.suiteinterface.SmokeTests;
+
+import java.io.File;
+
+public class WatchpointReports extends TestBase {
+
+    private File resourceFile;
+    private CidAppLoginPage loginPage;
+    private EvaluatePage evaluatePage;
+    private SoftAssertions softAssertions = new SoftAssertions();
+
+    public WatchpointReports() {
+        super();
+    }
+
+    private UserCredentials currentUser;
+
+    @Test
+    @Category(SmokeTests.class)
+    @TestRail(testCaseId = {"21933", "21940"})
+    @Description("Generate and download a Part Cost Report")
+    public void partCostReport() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.POWDER_METAL;
+
+        String componentName = "PowderMetalShaft";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".stp");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        loginPage = new CidAppLoginPage(driver);
+        evaluatePage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .selectProcessGroup(processGroupEnum)
+            .costScenario();
+
+        softAssertions.assertThat(evaluatePage.isReportButtonEnabled()).isTrue();
+        evaluatePage.clickReportDropdown();
+
+        softAssertions.assertThat(evaluatePage.isDownloadButtonEnabled()).isFalse();
+
+        evaluatePage.clickReportDropdown()
+            .generateReport(EvaluatePage.class)
+            .waitForCostLabelNotContain(NewCostingLabelEnum.PROCESSING_REPORT_ACTION, 3);
+
+        evaluatePage.clickReportDropdown();
+        softAssertions.assertThat(evaluatePage.isDownloadButtonEnabled()).isTrue();
+
+        softAssertions.assertAll();
+    }
+}
