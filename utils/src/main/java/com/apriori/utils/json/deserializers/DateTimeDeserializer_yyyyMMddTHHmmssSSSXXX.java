ddc4e6bf5d5e03e897827dfad2b7e0f5d66dd8ea
3p-akrishnan <3p-akrishnan@apriori.com>
2023-03-01 18:17:30 +0530

    Date serializer support for long date

--- utils/src/main/java/com/apriori/utils/json/deserializers/DateTimeDeserializer_yyyyMMddTHHmmssSSSXXX.java
+++ utils/src/main/java/com/apriori/utils/json/deserializers/DateTimeDeserializer_yyyyMMddTHHmmssSSSXXX.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -20,12 +22,15 @@ public class DateTimeDeserializer_yyyyMMddTHHmmssSSSXXX extends JsonDeserializer
     public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
         throws IOException {
 
-        // making sure that date has max 3 trailing numbers after seconds
-        // example: 2018-06-28T05:50:59.533242Z --> 2018-06-28T05:50:59.000Z
-        String formattedTime = jsonParser.getText().substring(0, 20) + "000Z";
+
 
         if (jsonParser.getCurrentToken().equals(JsonToken.VALUE_STRING)) {
+            // making sure that date has max 3 trailing numbers after seconds
+            // example: 2018-06-28T05:50:59.533242Z --> 2018-06-28T05:50:59.000Z
+            String formattedTime = jsonParser.getText().substring(0, 20) + "000Z";
             return LocalDateTime.parse(formattedTime, formatter);
+        } else if (jsonParser.getCurrentToken().equals(JsonToken.VALUE_NUMBER_INT)) {
+            return Instant.ofEpochMilli(jsonParser.getLongValue()).atZone(ZoneId.systemDefault()).toLocalDateTime();
         }
         return null;
     }
