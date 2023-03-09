fed29de30fa78a1f3cb1a9292dc2244c5088329d
Ricky Yabsley <ryabsley@apriori.com>
2023-03-09 15:26:56 +0000

    Update testParameters to use ProcessGroupEnum Add basic Costing Template for costing to avoid selection of wrong PG

--- microservices/cidapp-api/src/test/java/com/apriori/tests/MaterialSelectionTests.java
+++ microservices/cidapp-api/src/test/java/com/apriori/tests/MaterialSelectionTests.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
+import com.apriori.cidappapi.entity.response.CostingTemplate;
 import com.apriori.cidappapi.entity.response.componentiteration.ComponentIteration;
 import com.apriori.cidappapi.utils.ComponentsUtil;
 import com.apriori.cidappapi.utils.IterationsUtil;
@@ -40,23 +41,23 @@ public class MaterialSelectionTests {
     private Object[] testParameters() {
         return new Object[] {
             new Object[] {ProcessGroupEnum.ADDITIVE_MANUFACTURING, MaterialNameEnum.ALUMINIUM_ALSI10MG.getMaterialName(), "ADD-LOW-001", ".SLDPRT"},
-            new Object[] {ProcessGroupEnum.BAR_TUBE_FAB, "Steel, Hot Worked, AISI 1010", "AP-000-006", ".step"},
-            new Object[] {ProcessGroupEnum.CASTING, "Aluminum, Cast, ANSI AL380.0", "CastedPart", ".CATPart"},
-            new Object[] {ProcessGroupEnum.CASTING_DIE, "Aluminum, Cast, ANSI AL380.0", "CurvedWall", ".CATPart"},
-            new Object[] {ProcessGroupEnum.CASTING_INVESTMENT, "Aluminum, Cast, ANSI AL380.0", "AP-000-506", ".prt.1"},
-            new Object[] {ProcessGroupEnum.CASTING_SAND, "Aluminum, Cast, ANSI AL380.0", "casting_q5_thinvalve", ".prt"},
-            new Object[] {ProcessGroupEnum.FORGING, "Steel, Cold Worked, AISI 1010", "big ring", ".SLDPRT"},
-            new Object[] {ProcessGroupEnum.PLASTIC_MOLDING, "ABS", "bolt", ".CATPart"},
-            new Object[] {ProcessGroupEnum.POWDER_METAL, "F-0005", "case_31_test_part_6_small", ".prt.2"},
+            new Object[] {ProcessGroupEnum.BAR_TUBE_FAB, MaterialNameEnum.STEEL_HOT_WORKED_AISI1010.getMaterialName(), "AP-000-006", ".step"},
+            new Object[] {ProcessGroupEnum.CASTING, MaterialNameEnum.ALUMINIUM_ANSI_AL380.getMaterialName(), "CastedPart", ".CATPart"},
+            new Object[] {ProcessGroupEnum.CASTING_DIE, MaterialNameEnum.ALUMINIUM_ANSI_AL380.getMaterialName(), "CurvedWall", ".CATPart"},
+            new Object[] {ProcessGroupEnum.CASTING_INVESTMENT, MaterialNameEnum.ALUMINIUM_ANSI_AL380.getMaterialName(), "AP-000-506", ".prt.1"},
+            new Object[] {ProcessGroupEnum.CASTING_SAND, MaterialNameEnum.ALUMINIUM_ANSI_AL380.getMaterialName(), "casting_q5_thinvalve", ".prt"},
+            new Object[] {ProcessGroupEnum.FORGING, MaterialNameEnum.STEEL_COLD_WORKED_AISI1010.getMaterialName(), "big ring", ".SLDPRT"},
+            new Object[] {ProcessGroupEnum.PLASTIC_MOLDING, MaterialNameEnum.ABS.getMaterialName(), "bolt", ".CATPart"},
+            new Object[] {ProcessGroupEnum.POWDER_METAL, MaterialNameEnum.STEEL_F0005.getMaterialName(), "case_31_test_part_6_small", ".prt.2"},
             new Object[] {ProcessGroupEnum.RAPID_PROTOTYPING, "Default", "Plastic moulded cap DFM", ".CATPart"},
-            new Object[] {ProcessGroupEnum.ROTO_BLOW_MOLDING, "Polyethylene, High Density (HDPE)", "Plastic moulded cap DFM", ".CATPart"},
-            new Object[] {ProcessGroupEnum.SHEET_METAL, "Steel, Cold Worked, AISI 1020", "3571050_cad", ".prt.1"},
-            new Object[] {ProcessGroupEnum.SHEET_METAL_HYDROFORMING, "Aluminum, Stock, ANSI 2017", "FlangedRound", ".SLDPRT"},
-            //new Object[] {ProcessGroupEnum.SHEET_METAL_ROLLFORMING, "Steel, Cold Worked, AISI 1020", "", ""},
-            new Object[] {ProcessGroupEnum.SHEET_METAL_STRETCH_FORMING, "Aluminum, Stock, ANSI 2024", "bracket_basic", ".prt"},
-            new Object[] {ProcessGroupEnum.SHEET_METAL_TRANSFER_DIE, "Steel, Cold Worked, AISI 1020", "SheetMetal", ".prt"},
-            new Object[] {ProcessGroupEnum.SHEET_PLASTIC, "Polyethylene, HDPE Extrusion Sheet", "r151294", ".prt.1"},
-            new Object[] {ProcessGroupEnum.STOCK_MACHINING, "Steel, Hot Worked, AISI 1010", "case_005_flat end mill contouring", ".SLDPRT"}
+            new Object[] {ProcessGroupEnum.ROTO_BLOW_MOLDING, MaterialNameEnum.POLYETHYLENE_HDPE.getMaterialName(), "Plastic moulded cap DFM", ".CATPart"},
+            new Object[] {ProcessGroupEnum.SHEET_METAL, MaterialNameEnum.STEEL_COLD_WORKED_AISI1020.getMaterialName(), "3571050_cad", ".prt.1"},
+            new Object[] {ProcessGroupEnum.SHEET_METAL_HYDROFORMING, MaterialNameEnum.ALUMINIUM_ANSI_2017.getMaterialName(), "FlangedRound", ".SLDPRT"},
+            //new Object[] {ProcessGroupEnum.SHEET_METAL_ROLLFORMING, MaterialNameEnum.STEEL_COLD_WORKED_AISI1020.getMaterialName(), "", ""},
+            new Object[] {ProcessGroupEnum.SHEET_METAL_STRETCH_FORMING, MaterialNameEnum.ALUMINIUM_ANSI_2024.getMaterialName(), "bracket_basic", ".prt"},
+            new Object[] {ProcessGroupEnum.SHEET_METAL_TRANSFER_DIE, MaterialNameEnum.STEEL_COLD_WORKED_AISI1020.getMaterialName(), "SheetMetal", ".prt"},
+            new Object[] {ProcessGroupEnum.SHEET_PLASTIC, MaterialNameEnum.POLYETHYLENE_HDPE_EXTRUSION_SHEET.getMaterialName(), "r151294", ".prt.1"},
+            new Object[] {ProcessGroupEnum.STOCK_MACHINING, MaterialNameEnum.STEEL_HOT_WORKED_AISI1010.getMaterialName(), "case_005_flat end mill contouring", ".SLDPRT"}
         };
     }
 
@@ -88,6 +89,9 @@ public class MaterialSelectionTests {
                 .scenarioIdentity(componentResponse.getScenarioIdentity())
                 .processGroup(pg)
                 .user(currentUser)
+                .costingTemplate(CostingTemplate.builder()
+                    .processGroupName(pg.getProcessGroup())
+                    .build())
                 .build());
 
         ResponseWrapper<ComponentIteration> getScenarioInfo = componentsUtil.getComponentIterationLatest(
