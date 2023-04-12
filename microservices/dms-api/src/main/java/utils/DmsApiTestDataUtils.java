495bb198654ce90687730dce586eb2f3f8128b85
3p-akrishnan <3p-akrishnan@apriori.com>
2023-04-12 10:08:31 +0530

    Replaced hard delay with new CssComponent().getWaitBaseCssComponents()

--- microservices/dms-api/src/main/java/utils/DmsApiTestDataUtils.java
+++ microservices/dms-api/src/main/java/utils/DmsApiTestDataUtils.java
@@ -1,11 +1,14 @@
 package utils;
 
+import static com.apriori.entity.enums.CssSearch.COMPONENT_NAME_EQ;
+import static com.apriori.entity.enums.CssSearch.SCENARIO_NAME_EQ;
+import static com.apriori.entity.enums.CssSearch.SCENARIO_STATE_EQ;
+
 import com.apriori.apibase.utils.TestUtil;
 import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
 import com.apriori.cidappapi.entity.response.scenarios.ScenarioResponse;
 import com.apriori.cidappapi.utils.ComponentsUtil;
 import com.apriori.cidappapi.utils.ScenariosUtil;
-import com.apriori.entity.enums.CssSearch;
 import com.apriori.entity.response.ScenarioItem;
 import com.apriori.qms.controller.QmsBidPackageResources;
 import com.apriori.qms.controller.QmsScenarioDiscussionResources;
@@ -17,6 +20,7 @@ import com.apriori.utils.CssComponent;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.enums.ProcessGroupEnum;
+import com.apriori.utils.enums.ScenarioStateEnum;
 import com.apriori.utils.reader.file.user.UserCredentials;
 import com.apriori.utils.reader.file.user.UserUtil;
 
@@ -30,7 +34,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 public abstract class DmsApiTestDataUtils extends TestUtil {
     protected static String bidPackageName;
@@ -47,49 +50,6 @@ public abstract class DmsApiTestDataUtils extends TestUtil {
     protected static UserCredentials currentUser = UserUtil.getUser();
 
     /**
-     * Create Component via Cid-app
-     *
-     * @param processGroupEnum - ProcessGroupEnum
-     * @param componentName    - component name
-     * @param currentUser      - UserCredentials
-     * @return ScenarioItem object
-     */
-    public static ScenarioItem createAndPublishComponentViaCidApp(ProcessGroupEnum processGroupEnum, String componentName, UserCredentials currentUser) {
-        final File resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".prt");
-        String scenarioName = new GenerateStringUtil().generateScenarioName();
-        ComponentInfoBuilder componentInfoBuilder = ComponentInfoBuilder.builder()
-            .componentName(componentName)
-            .scenarioName(scenarioName)
-            .resourceFile(resourceFile)
-            .user(currentUser)
-            .build();
-        new ComponentsUtil().postComponent(componentInfoBuilder);
-        //Delay to sync till component is created
-        try {
-            TimeUnit.SECONDS.sleep(30);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        ScenarioItem scenarioItem = new CssComponent().getBaseCssComponents(currentUser,
-            CssSearch.COMPONENT_NAME_EQ.getKey() + "Casting",
-            CssSearch.SCENARIO_NAME_EQ.getKey() + scenarioName).get(0);
-        componentInfoBuilder.setComponentIdentity(scenarioItem.getComponentIdentity());
-        componentInfoBuilder.setScenarioIdentity(scenarioItem.getScenarioIdentity());
-        new ScenariosUtil().publishScenario(componentInfoBuilder, ScenarioResponse.class, HttpStatus.SC_CREATED);
-        return scenarioItem;
-    }
-
-    /**
-     * Delete scenario via Cid-app
-     *
-     * @param scenarioItem the scenario item
-     * @param currentUser  the current user
-     */
-    public static void deleteScenarioViaCidApp(ScenarioItem scenarioItem, UserCredentials currentUser) {
-        new ScenariosUtil().deleteScenario(scenarioItem.getComponentIdentity(), scenarioItem.getScenarioIdentity(), currentUser);
-    }
-
-    /**
      * Create test data.
      */
     @BeforeClass
@@ -97,7 +57,7 @@ public abstract class DmsApiTestDataUtils extends TestUtil {
         bidPackageName = "BPN" + new GenerateStringUtil().getRandomNumbers();
         projectName = "PROJ" + new GenerateStringUtil().getRandomNumbers();
         contentDesc = RandomStringUtils.randomAlphabetic(12);
-        scenarioItem = createAndPublishComponentViaCidApp(ProcessGroupEnum.CASTING_DIE, "Casting", currentUser);
+        scenarioItem = createAndPublishScenarioViaCidApp(ProcessGroupEnum.CASTING_DIE, "Casting", currentUser);
         bidPackageResponse = QmsBidPackageResources.createBidPackage(bidPackageName, currentUser);
         bidPackageItemResponse = QmsBidPackageResources.createBidPackageItem(
             QmsBidPackageResources.bidPackageItemRequestBuilder(scenarioItem.getComponentIdentity(),
@@ -126,4 +86,39 @@ public abstract class DmsApiTestDataUtils extends TestUtil {
         deleteScenarioViaCidApp(scenarioItem, currentUser);
     }
 
+    /**
+     * Create & Publish Scenario via Cid-app
+     *
+     * @param processGroupEnum - ProcessGroupEnum
+     * @param componentName    - component name
+     * @param currentUser      - UserCredentials
+     * @return ScenarioItem object
+     */
+    private static ScenarioItem createAndPublishScenarioViaCidApp(ProcessGroupEnum processGroupEnum, String componentName, UserCredentials currentUser) {
+        final File resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".prt");
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        ComponentInfoBuilder componentInfoBuilder = ComponentInfoBuilder.builder()
+            .componentName(componentName)
+            .scenarioName(scenarioName)
+            .resourceFile(resourceFile)
+            .user(currentUser)
+            .build();
+        new ComponentsUtil().postComponent(componentInfoBuilder);
+        ScenarioItem scenarioItem = new CssComponent().getWaitBaseCssComponents(currentUser, COMPONENT_NAME_EQ.getKey() + componentInfoBuilder.getComponentName(),
+            SCENARIO_NAME_EQ.getKey() + componentInfoBuilder.getScenarioName(), SCENARIO_STATE_EQ.getKey() + ScenarioStateEnum.NOT_COSTED).get(0);
+        componentInfoBuilder.setComponentIdentity(scenarioItem.getComponentIdentity());
+        componentInfoBuilder.setScenarioIdentity(scenarioItem.getScenarioIdentity());
+        new ScenariosUtil().publishScenario(componentInfoBuilder, ScenarioResponse.class, HttpStatus.SC_CREATED);
+        return scenarioItem;
+    }
+
+    /**
+     * Delete scenario via Cid-app
+     *
+     * @param scenarioItem the scenario item
+     * @param currentUser  the current user
+     */
+    private static void deleteScenarioViaCidApp(ScenarioItem scenarioItem, UserCredentials currentUser) {
+        new ScenariosUtil().deleteScenario(scenarioItem.getComponentIdentity(), scenarioItem.getScenarioIdentity(), currentUser);
+    }
 }
--- microservices/qms-api/src/main/java/utils/QmsApiTestDataUtils.java
+++ microservices/qms-api/src/main/java/utils/QmsApiTestDataUtils.java
@@ -1,11 +1,14 @@
 package utils;
 
+import static com.apriori.entity.enums.CssSearch.COMPONENT_NAME_EQ;
+import static com.apriori.entity.enums.CssSearch.SCENARIO_NAME_EQ;
+import static com.apriori.entity.enums.CssSearch.SCENARIO_STATE_EQ;
+
 import com.apriori.apibase.utils.TestUtil;
 import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
 import com.apriori.cidappapi.entity.response.scenarios.ScenarioResponse;
 import com.apriori.cidappapi.utils.ComponentsUtil;
 import com.apriori.cidappapi.utils.ScenariosUtil;
-import com.apriori.entity.enums.CssSearch;
 import com.apriori.entity.response.ScenarioItem;
 import com.apriori.qms.controller.QmsBidPackageResources;
 import com.apriori.qms.controller.QmsScenarioDiscussionResources;
@@ -18,6 +21,7 @@ import com.apriori.utils.CssComponent;
 import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.enums.ProcessGroupEnum;
+import com.apriori.utils.enums.ScenarioStateEnum;
 import com.apriori.utils.reader.file.user.UserCredentials;
 import com.apriori.utils.reader.file.user.UserUtil;
 
@@ -28,7 +32,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 public abstract class QmsApiTestDataUtils extends TestUtil {
     protected static String bidPackageName;
@@ -50,7 +53,7 @@ public abstract class QmsApiTestDataUtils extends TestUtil {
         bidPackageName = "BPN" + new GenerateStringUtil().getRandomNumbers();
         projectName = "PROJ" + new GenerateStringUtil().getRandomNumbers();
         contentDesc = RandomStringUtils.randomAlphabetic(12);
-        scenarioItem = createAndPublishComponentViaCidApp(ProcessGroupEnum.CASTING_DIE, "Casting", currentUser);
+        scenarioItem = createAndPublishScenarioViaCidApp(ProcessGroupEnum.CASTING_DIE, "Casting", currentUser);
         bidPackageResponse = QmsBidPackageResources.createBidPackage(bidPackageName, currentUser);
         bidPackageItemResponse = QmsBidPackageResources.createBidPackageItem(
             QmsBidPackageResources.bidPackageItemRequestBuilder(scenarioItem.getComponentIdentity(),
@@ -71,14 +74,14 @@ public abstract class QmsApiTestDataUtils extends TestUtil {
     }
 
     /**
-     * Create Component via Cid-app
+     * Create & Publish Scenario via Cid-app
      *
      * @param processGroupEnum - ProcessGroupEnum
      * @param componentName    - component name
      * @param currentUser      - UserCredentials
      * @return ScenarioItem object
      */
-    public static ScenarioItem createAndPublishComponentViaCidApp(ProcessGroupEnum processGroupEnum, String componentName, UserCredentials currentUser) {
+    private static ScenarioItem createAndPublishScenarioViaCidApp(ProcessGroupEnum processGroupEnum, String componentName, UserCredentials currentUser) {
         final File resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".prt");
         String scenarioName = new GenerateStringUtil().generateScenarioName();
         ComponentInfoBuilder componentInfoBuilder = ComponentInfoBuilder.builder()
@@ -88,15 +91,8 @@ public abstract class QmsApiTestDataUtils extends TestUtil {
             .user(currentUser)
             .build();
         new ComponentsUtil().postComponent(componentInfoBuilder);
-        //Delay to sync till component is created
-        try {
-            TimeUnit.SECONDS.sleep(30);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        ScenarioItem scenarioItem = new CssComponent().getBaseCssComponents(currentUser,
-            CssSearch.COMPONENT_NAME_EQ.getKey() + "Casting",
-            CssSearch.SCENARIO_NAME_EQ.getKey() + scenarioName).get(0);
+        ScenarioItem scenarioItem = new CssComponent().getWaitBaseCssComponents(currentUser, COMPONENT_NAME_EQ.getKey() + componentInfoBuilder.getComponentName(),
+            SCENARIO_NAME_EQ.getKey() + componentInfoBuilder.getScenarioName(), SCENARIO_STATE_EQ.getKey() + ScenarioStateEnum.NOT_COSTED).get(0);
         componentInfoBuilder.setComponentIdentity(scenarioItem.getComponentIdentity());
         componentInfoBuilder.setScenarioIdentity(scenarioItem.getScenarioIdentity());
         new ScenariosUtil().publishScenario(componentInfoBuilder, ScenarioResponse.class, HttpStatus.SC_CREATED);
@@ -109,7 +105,7 @@ public abstract class QmsApiTestDataUtils extends TestUtil {
      * @param scenarioItem the scenario item
      * @param currentUser  the current user
      */
-    public static void deleteScenarioViaCidApp(ScenarioItem scenarioItem, UserCredentials currentUser) {
+    private static void deleteScenarioViaCidApp(ScenarioItem scenarioItem, UserCredentials currentUser) {
         new ScenariosUtil().deleteScenario(scenarioItem.getComponentIdentity(), scenarioItem.getScenarioIdentity(), currentUser);
     }
 
