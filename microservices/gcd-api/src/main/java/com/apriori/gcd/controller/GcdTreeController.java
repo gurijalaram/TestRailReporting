9770233c2145943a6ab3cdeff1f9bd1eaa1fb595
cfrith <cfrith@apriori.com>
2023-05-22 13:37:28 +0100

    Update the post gcd to a generic method. Update test to work with generic method.

--- microservices/gcd-api/src/main/java/com/apriori/gcd/controller/GcdTreeController.java
+++ microservices/gcd-api/src/main/java/com/apriori/gcd/controller/GcdTreeController.java
@@ -1,15 +1,12 @@
 package com.apriori.gcd.controller;
 
 import com.apriori.gcd.entity.enums.GCDAPIEnum;
-import com.apriori.gcd.entity.response.GcdTree;
 import com.apriori.utils.authusercontext.AuthUserContextUtil;
 import com.apriori.utils.http.builder.common.entity.RequestEntity;
 import com.apriori.utils.http.builder.request.HTTPRequest;
 import com.apriori.utils.http.utils.RequestEntityUtil;
 import com.apriori.utils.http.utils.ResponseWrapper;
 import com.apriori.utils.reader.file.user.UserCredentials;
-
-import org.apache.http.HttpStatus;
 
 public class GcdTreeController extends RequestEntityUtil {
 
@@ -18,15 +15,14 @@ public class GcdTreeController extends RequestEntityUtil {
      *
      * @param gcdTree     - the gcd tree
      * @param currentUser - the current user
-     * @return ResponseWrapper <GcdTree>
+     * @return a generic response object
      */
-    public GcdTree postGcdTree(String gcdTree, UserCredentials currentUser) {
-        final RequestEntity requestEntity = init(GCDAPIEnum.TREE_DIFF, GcdTree.class)
+    public <T> ResponseWrapper<T> postGcdTree(String gcdTree, UserCredentials currentUser, int expectedResponseCode, Class<T> klass) {
+        final RequestEntity requestEntity = init(GCDAPIEnum.TREE_DIFF, klass)
             .apUserContext(new AuthUserContextUtil().getAuthUserContext(currentUser.getEmail()))
             .customBody(gcdTree)
-            .expectedResponseCode(HttpStatus.SC_OK);
+            .expectedResponseCode(expectedResponseCode);
 
-        ResponseWrapper<GcdTree> gcdTreeResponse = HTTPRequest.build(requestEntity).post();
-        return gcdTreeResponse.getResponseEntity();
+        return HTTPRequest.build(requestEntity).post();
     }
 }
