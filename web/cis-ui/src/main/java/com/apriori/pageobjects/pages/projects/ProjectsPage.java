4b4f5b1945a51d2a9c8e6d46171d3f84bcb6071d
BhagyaSusiripala <bsusiripala@apriori.com>
2023-05-22 13:31:56 +0530

    COL-98-Create a new project page
    * Automated testcases : "22686","22687","22707"
    * Verify user can access create a new project page

--- web/cis-ui/src/main/java/com/apriori/pageobjects/pages/projects/ProjectsPage.java
+++ web/cis-ui/src/main/java/com/apriori/pageobjects/pages/projects/ProjectsPage.java
@@ -23,6 +23,33 @@ public class ProjectsPage extends EagerPageComponent<ProjectsPage> {
     @FindBy(xpath = "//div[@data-testid='loader']")
     private WebElement spinner;
 
+    @FindBy(id = "create-project-name-input")
+    private WebElement projectNameField;
+
+    @FindBy(id = "create-project-description-input")
+    private WebElement projectDescriptionField;
+
+    @FindBy(id = "create-project-add-part-and-assembly-btn")
+    private WebElement btnAddPartsAndAssemblies;
+
+    @FindBy(xpath = "//span[contains(text(),'Make Selection')]")
+    private WebElement inviteTeammatesField;
+
+    @FindBy(xpath = "//div[@data-testid='create-project-due-date']")
+    private WebElement dueDateField;
+
+    @FindBy(id = "create-project-submit-btn")
+    private WebElement btnProjectSubmit;
+
+    @FindBy(id = "create-project-cancel-btn")
+    private WebElement btnCancelProject;
+
+    @FindBy(xpath = "//div[contains(@class,'MuiCalendarPicker')]")
+    private WebElement dueDateDatePicker;
+
+    @FindBy(xpath = "//button[@title='Previous month']")
+    private WebElement previousMonthSelector;
+
     private PageUtils pageUtils;
 
     public ProjectsPage(WebDriver driver) {
@@ -66,5 +93,107 @@ public class ProjectsPage extends EagerPageComponent<ProjectsPage> {
      */
     public boolean isCreateNewProjectsOptionDisplayed() {
         return getPageUtils().isElementDisplayed(btnCreateNewProject);
+    }
+
+    /**
+     * clicks on create new project
+     *
+     * @return current page object
+     */
+    public ProjectsPage clickOnCreateNewProject() {
+        getPageUtils().waitForElementAndClick(btnCreateNewProject);
+        return this;
+    }
+
+    /**
+     * Checks if project name field displayed
+     *
+     * @return true/false
+     */
+    public boolean isProjectNameFieldDisplayed() {
+        return getPageUtils().isElementDisplayed(projectNameField);
+    }
+
+    /**
+     * Checks if project description field displayed
+     *
+     * @return true/false
+     */
+    public boolean isProjectDescriptionFieldDisplayed() {
+        return getPageUtils().isElementDisplayed(projectDescriptionField);
+    }
+
+    /**
+     * Checks if add parts and assemblies displayed
+     *
+     * @return true/false
+     */
+    public boolean isAddPartsAndAssembliesOptionDisplayed() {
+        return getPageUtils().isElementDisplayed(btnAddPartsAndAssemblies);
+    }
+
+    /**
+     * Checks if invite members list displayed
+     *
+     * @return true/false
+     */
+    public boolean isInviteTeamMembersFieldDisplayed() {
+        return getPageUtils().isElementDisplayed(inviteTeammatesField);
+    }
+
+    /**
+     * Checks if due date field displayed
+     *
+     * @return true/false
+     */
+    public boolean isDueDateFieldDisplayed() {
+        return getPageUtils().isElementDisplayed(dueDateField);
+    }
+
+    /**
+     * Checks if create project button displayed
+     *
+     * @return true/false
+     */
+    public boolean isCreateProjectButtonDisplayed() {
+        return getPageUtils().isElementDisplayed(btnProjectSubmit);
+    }
+
+    /**
+     * Checks if cancel project button displayed
+     *
+     * @return true/false
+     */
+    public boolean isCancelProjectCreationDisplayed() {
+        return getPageUtils().isElementDisplayed(btnCancelProject);
+    }
+
+    /**
+     * clicks on due date field
+     *
+     * @return current page object
+     */
+    public ProjectsPage clickOnDueDatePicker() {
+        getPageUtils().waitForElementAndClick(dueDateField);
+        return this;
+    }
+
+    /**
+     * gets previous month status
+     *
+     * @return a String
+     */
+    public String getMonthSelectorStatus(String month) {
+        return getPageUtils().waitForElementToAppear(By.xpath("//button[@title='" + month + "']")).getAttribute("class");
+    }
+
+    /**
+     * clicks on cancel project
+     *
+     * @return current page object
+     */
+    public ProjectsPage clickOnCancelProject() {
+        getPageUtils().waitForElementAndClick(btnCancelProject);
+        return this;
     }
 }
