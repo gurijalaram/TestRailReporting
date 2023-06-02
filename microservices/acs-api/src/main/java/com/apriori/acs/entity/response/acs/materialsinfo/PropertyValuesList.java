43c6ff22d7e0991e56adfca52a420aed7bfa8b40
smccaffreyapriori <smccaffrey@apriori.com>
2023-06-02 11:31:22 +0100

    Checkstyle update

--- microservices/acs-api/src/main/java/com/apriori/acs/entity/response/acs/materialsinfo/PropertyValuesList.java
+++ microservices/acs-api/src/main/java/com/apriori/acs/entity/response/acs/materialsinfo/PropertyValuesList.java
@@ -154,7 +154,6 @@ public class PropertyValuesList {
     private Double hexBarCarbonEmissionsFactor;
     @JsonProperty("iBeamCarbonEmissionsFactor")
     private Double iiBeamCarbonEmissionsFactor;
-//    private Double squareTubeCarbonEmissionsFactor;
     private Double rectangularTubeCarbonEmissionsFactor;
     private Double maximumExtrusionExitSpeed;
     private Double minimumExtrusionExitSpeed;
