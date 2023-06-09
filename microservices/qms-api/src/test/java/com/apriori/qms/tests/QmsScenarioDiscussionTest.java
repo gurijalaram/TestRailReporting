53a7091022d3729f40780508c5efabbfa16f0ba8
3p-akrishnan <3p-akrishnan@apriori.com>
2023-06-09 14:07:33 +0530

    checkstyle issues fixed

--- microservices/qms-api/src/test/java/com/apriori/qms/tests/QmsScenarioDiscussionTest.java
+++ microservices/qms-api/src/test/java/com/apriori/qms/tests/QmsScenarioDiscussionTest.java
@@ -23,6 +23,7 @@ import com.apriori.utils.http.utils.RequestEntityUtil;
 import com.apriori.utils.http.utils.ResponseWrapper;
 import com.apriori.utils.reader.file.user.UserCredentials;
 import com.apriori.utils.reader.file.user.UserUtil;
+
 import io.qameta.allure.Description;
 import io.qameta.allure.Link;
 import org.apache.http.HttpStatus;
