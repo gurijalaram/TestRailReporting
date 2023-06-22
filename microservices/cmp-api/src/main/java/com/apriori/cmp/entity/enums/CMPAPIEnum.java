54a697213a5cd010a9fccef7bec0210de4c6dc29
Ricky Yabsley <ryabsley@apriori.com>
2023-06-22 17:37:06 +0100

    Initial work on compare microservice

---
+++ microservices/cmp-api/src/main/java/com/apriori/cmp/entity/enums/CMPAPIEnum.java
@@ -0,0 +1,25 @@
+package com.apriori.cmp.entity.enums;
+
+import com.apriori.utils.http.enums.common.ExternalEndpointEnum;
+import com.apriori.utils.properties.PropertiesContext;
+
+public enum CMPAPIEnum implements ExternalEndpointEnum {
+
+    COMPARISON("/comparisons");
+
+    private final String endpoint;
+
+    CMPAPIEnum(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    @Override
+    public String getEndpointString() {
+        return endpoint;
+    }
+
+    @Override
+    public String getEndpoint(Object... variables) {
+        return PropertiesContext.get("cmp.api_url") + String.format(getEndpointString(), variables) + "?key=" + PropertiesContext.get("secret_key");
+    }
+}
---
+++ microservices/cmp-api/src/main/java/com/apriori/cmp/entity/response/PostComparisonResponse.java
@@ -0,0 +1,30 @@
+package com.apriori.cmp.entity.response;
+
+
+import com.apriori.utils.http.enums.Schema;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(location = "CreateComparisonResponse.json")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonRootName("response")
+public class PostComparisonResponse {
+    private String customerIdentity;
+    private String comparisonName;
+    private String comparisonType;
+    private String objectType;
+    private List<String> comparisonObjects;
+
+}
+
