fd3fbd4259c944aaf9e151a699168b322acbb8f3
smccaffreyapriori <smccaffrey@apriori.com>
2023-06-23 16:21:33 +0100

    Design Guidance Tests automation and assertion attempts

--- microservices/acs-api/src/test/java/tests/acs/DesignGuidanceTests.java
+++ microservices/acs-api/src/test/java/tests/acs/DesignGuidanceTests.java
@@ -1,18 +1,41 @@
 package tests.acs;
 
 import com.apriori.acs.entity.response.acs.designGuidance.DesignGuidanceResponse;
+import com.apriori.acs.entity.response.acs.designGuidance.InfosByTopics;
 import com.apriori.acs.entity.response.workorders.cost.costworkorderstatus.CostOrderStatusOutputs;
 import com.apriori.acs.utils.acs.AcsResources;
 import com.apriori.utils.TestRail;
 import com.apriori.utils.enums.ProcessGroupEnum;
 
 import io.qameta.allure.Description;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 import tests.workorders.WorkorderAPITests;
 
 public class DesignGuidanceTests {
     private AcsResources acsResources = new AcsResources();
     private WorkorderAPITests workorderAPITests = new WorkorderAPITests();
+
+    private void designGuidanceAssertion(DesignGuidanceResponse designGuidanceResponse, String guidanceTopics) {
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        InfosByTopics infosByTopics = designGuidanceResponse.getInfosByTopics();
+//        ProcessInstanceKey processInstanceKey = costResultsRootItem.getProcessInstanceKey();
+//        ResultMapBean resultMapBean = costResultsRootItem.getResultMapBean();
+//        PropertyValueMap propertyValueMap = resultMapBean.getPropertyValueMap();
+
+        softAssertions.assertThat(designGuidanceResponse.getInfosByTopics()).isNotNull();
+        softAssertions.assertThat(designGuidanceResponse.getCostingFailed()).isEqualTo(false);
+//        softAssertions.assertThat(infosByTopics.getDTC_MESSAGES()).isEqualTo(guidanceTopics);
+
+        softAssertions.assertThat(designGuidanceResponse.getInfosByTopics()).toString().contains(guidanceTopics);
+
+//        softAssertions.assertThat(costResultsRootItem.getCostingFailed()).isEqualTo(false);
+//        softAssertions.assertThat(costResultsRootItem.getDepth()).isEqualTo("ROOT");
+//        softAssertions.assertThat(costResultsRootItem.getSecondaryProcess()).isEqualTo(false);
+        softAssertions.assertAll();
+    }
 
     @Test
     @TestRail(testCaseId = "")
@@ -22,5 +45,8 @@ public class DesignGuidanceTests {
 
         CostOrderStatusOutputs costOutputs = acsResources.uploadAndCost(processGroup, "DTCCastingIssues.catpart", workorderAPITests.setupProductionInfoInputs());
         DesignGuidanceResponse designGuidanceResponse = acsResources.getDesignGuidance(costOutputs.getScenarioIterationKey(), "DTC_MESSAGES");
+
+        designGuidanceAssertion(designGuidanceResponse, "DTC_MESSAGES");
+
     }
 }
