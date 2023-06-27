93793caa4593fb969fc8fe3bc4f0ebdb04e07e39
3p-akrishnan <3p-akrishnan@apriori.com>
2023-06-27 17:12:13 +0530

    Checkstyle issues fixed

--- microservices/qms-api/src/test/java/com/apriori/qms/tests/BidPackageProjectUserTest.java
+++ microservices/qms-api/src/test/java/com/apriori/qms/tests/BidPackageProjectUserTest.java
@@ -21,6 +21,7 @@ import com.apriori.utils.authusercontext.AuthUserContextUtil;
 import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.reader.file.user.UserCredentials;
 import com.apriori.utils.reader.file.user.UserUtil;
+
 import io.qameta.allure.Description;
 import org.apache.http.HttpStatus;
 import org.assertj.core.api.SoftAssertions;
