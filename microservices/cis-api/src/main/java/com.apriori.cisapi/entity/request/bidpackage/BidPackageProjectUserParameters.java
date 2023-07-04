523c1c352577cb48b9953398667641423251a8a6
3p-akrishnan <3p-akrishnan@apriori.com>
2023-07-04 09:45:46 +0530

    https://jira.apriori.com/browse/BA-3127 aPW_v1.2.0 - COL-1926 - Test case Automation

--- microservices/cis-api/src/main/java/com.apriori.cisapi/entity/request/bidpackage/BidPackageProjectUserParameters.java
+++ microservices/cis-api/src/main/java/com.apriori.cisapi/entity/request/bidpackage/BidPackageProjectUserParameters.java
@@ -1,5 +1,6 @@
 package com.apriori.cisapi.entity.request.bidpackage;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BidPackageProjectUserParameters {
     private String userIdentity;
     private String userEmail;
     private String role;
+    private String customerIdentity;
+    private String identity;
 }
---
+++ microservices/cis-api/src/main/java/com.apriori.cisapi/entity/response/bidpackage/BidPackageProjectUsersDeleteResult.java
@@ -0,0 +1,10 @@
+package com.apriori.cisapi.entity.response.bidpackage;
+
+import lombok.Data;
+
+@Data
+@SuppressWarnings("unused")
+public class BidPackageProjectUsersDeleteResult {
+    private String identity;
+    private String error;
+}
