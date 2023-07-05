f0e0f49462a312976f2a53a33b806ca7d57a35d8
Ricky Yabsley <ryabsley@apriori.com>
2023-07-05 09:31:32 +0100

    Updates to CMP microservice tests

--- microservices/cmp-api/src/main/java/com/apriori/cmp/utils/ComparisonUtils.java
+++ microservices/cmp-api/src/main/java/com/apriori/cmp/utils/ComparisonUtils.java
@@ -2,6 +2,9 @@ package com.apriori.cmp.utils;
 
 import com.apriori.cmp.entity.enums.CMPAPIEnum;
 import com.apriori.cmp.entity.request.CreateComparison;
+import com.apriori.cmp.entity.request.UpdateComparison;
+import com.apriori.cmp.entity.response.ErrorResponse;
+import com.apriori.cmp.entity.response.GetComparisonResponse;
 import com.apriori.cmp.entity.response.PostComparisonResponse;
 import com.apriori.utils.authusercontext.AuthUserContextUtil;
 import com.apriori.utils.http.builder.common.entity.RequestEntity;
@@ -11,6 +14,14 @@ import com.apriori.utils.reader.file.user.UserCredentials;
 
 public class ComparisonUtils {
 
+    /**
+     * Post to the Create Comparison endpoint
+     *
+     * @param comparison - Create Comparison object
+     * @param currentUser - UserCredentials object
+     *
+     * @return PostComparisonResponse
+     */
     public PostComparisonResponse createComparison(CreateComparison comparison, UserCredentials currentUser) {
         RequestEntity requestEntity =
             RequestEntityUtil.init(CMPAPIEnum.COMPARISON, PostComparisonResponse.class)
@@ -19,5 +30,61 @@ public class ComparisonUtils {
                 .expectedResponseCode(201);
 
         return (PostComparisonResponse) HTTPRequest.build(requestEntity).post().getResponseEntity();
+    }
+
+    /**
+     * Post to the Create Comparison endpoint expecting an error response
+     *
+     * @param comparison - Create Comparison object
+     * @param currentUser - UserCredentials object
+     *
+     * @return PostComparisonResponse
+     */
+    public ErrorResponse createComparisonExpectingError(CreateComparison comparison, UserCredentials currentUser) {
+        RequestEntity requestEntity =
+            RequestEntityUtil.init(CMPAPIEnum.COMPARISON, ErrorResponse.class)
+                .apUserContext(new AuthUserContextUtil().getAuthUserContext(currentUser.getEmail()))
+                .body("comparison", comparison)
+                .expectedResponseCode(400);
+
+        return (ErrorResponse) HTTPRequest.build(requestEntity).post().getResponseEntity();
+    }
+
+    /**
+     * Update Comparison details given a Comparison ID
+     *
+     * @param comparisonID - String of the requested Comparison ID
+     * @param comparison - Update Comparison Object
+     * @param currentUser - UserCredentials object
+     *
+     * @return GetComparisonResponse
+     */
+    public GetComparisonResponse updateComparison(String comparisonID, UpdateComparison comparison, UserCredentials currentUser) {
+        RequestEntity requestEntity =
+            RequestEntityUtil.init(CMPAPIEnum.COMPARISON_BY_IDENTITY, GetComparisonResponse.class)
+                .inlineVariables(comparisonID)
+                .apUserContext(new AuthUserContextUtil().getAuthUserContext(currentUser.getEmail()))
+                .body("comparison", comparison)
+                .expectedResponseCode(200);
+
+        return (GetComparisonResponse) HTTPRequest.build(requestEntity).patch().getResponseEntity();
+    }
+
+    /**
+     * Get Comparison details given a Comparison ID
+     *
+     * @param comparisonID - String of the requested Comparison ID
+     * @param currentUser - UserCredentials object
+     *
+     * @return GetComparisonResponse
+     */
+    public GetComparisonResponse getComparison(String comparisonID, UserCredentials currentUser) {
+        RequestEntity requestEntity =
+            RequestEntityUtil.init(CMPAPIEnum.COMPARISON_BY_IDENTITY, GetComparisonResponse.class)
+                .inlineVariables(comparisonID)
+                .apUserContext(new AuthUserContextUtil().getAuthUserContext(currentUser.getEmail()))
+                .expectedResponseCode(200);
+
+        return (GetComparisonResponse) HTTPRequest.build(requestEntity).get().getResponseEntity();
     }
 }
--- microservices/cmp-api/src/test/java/com/apriori/cmp/tests/CmpSaveComparisonTests.java
+++ microservices/cmp-api/src/test/java/com/apriori/cmp/tests/CmpSaveComparisonTests.java
@@ -1,12 +1,17 @@
 package com.apriori.cmp.tests;
 
+import com.apriori.cidappapi.entity.builder.ComponentInfoBuilder;
+import com.apriori.cidappapi.utils.ComponentsUtil;
 import com.apriori.cmp.entity.builder.ComparisonObjectBuilder;
 import com.apriori.cmp.entity.request.CreateComparison;
+import com.apriori.cmp.entity.request.UpdateComparison;
+import com.apriori.cmp.entity.response.GetComparisonResponse;
 import com.apriori.cmp.entity.response.PostComparisonResponse;
 import com.apriori.cmp.utils.ComparisonUtils;
+import com.apriori.utils.FileResourceUtil;
 import com.apriori.utils.GenerateStringUtil;
 import com.apriori.utils.TestRail;
-import com.apriori.utils.http.utils.ResponseWrapper;
+import com.apriori.utils.enums.ProcessGroupEnum;
 import com.apriori.utils.reader.file.user.UserCredentials;
 import com.apriori.utils.reader.file.user.UserUtil;
 
@@ -14,35 +19,75 @@ import io.qameta.allure.Description;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CmpSaveComparisonTests {
 
-    ComparisonUtils comparisonUtils = new ComparisonUtils();
+    private ComparisonUtils comparisonUtils = new ComparisonUtils();
+    private ComponentsUtil componentsUtil = new ComponentsUtil();
     private UserCredentials currentUser;
     private String comparisonName;
+    private String componentName1 = "big ring";
+    private String componentName2 = "small ring";
+    private String componentName3 = "pin";
+    private String componentExt = ".SLDPRT";
+
+    private File resourceFile1 = FileResourceUtil.getCloudFile(ProcessGroupEnum.FORGING, componentName1 + componentExt);
+    private File resourceFile2 = FileResourceUtil.getCloudFile(ProcessGroupEnum.FORGING, componentName2 + componentExt);
+    private File resourceFile3 = FileResourceUtil.getCloudFile(ProcessGroupEnum.FORGING, componentName3 + componentExt);
+
     private SoftAssertions softAssertions = new SoftAssertions();
 
     @Test
-    @TestRail(testCaseId = {"25931"})
+    @TestRail(testCaseId = {"25931", "25932"})
     @Description("Create a Comparison via CMP-API")
-    public void testCreateComparison() {
+    public void testCreateThenUpdateComparison() {
+
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        currentUser = UserUtil.getUser();
+
+        ComponentInfoBuilder component1 = componentsUtil.postComponentQueryCID(
+            ComponentInfoBuilder.builder()
+                .componentName(componentName1)
+                .scenarioName(scenarioName)
+                .resourceFile(resourceFile1)
+                .user(currentUser)
+                .build()
+        );
+        ComponentInfoBuilder component2 = componentsUtil.postComponentQueryCID(
+            ComponentInfoBuilder.builder()
+                .componentName(componentName2)
+                .scenarioName(scenarioName)
+                .resourceFile(resourceFile2)
+                .user(currentUser)
+                .build()
+        );
+        ComponentInfoBuilder component3 = componentsUtil.postComponentQueryCID(
+            ComponentInfoBuilder.builder()
+                .componentName(componentName3)
+                .scenarioName(scenarioName)
+                .resourceFile(resourceFile3)
+                .user(currentUser)
+                .build()
+        );
 
         currentUser = UserUtil.getUser();
         comparisonName = new GenerateStringUtil().generateComparisonName();
+        String updatedComparisonName = new GenerateStringUtil().generateComparisonName();
 
         ComparisonObjectBuilder baseScenario = ComparisonObjectBuilder.builder()
-            .externalIdentity("123456789")
+            .externalIdentity(component1.getScenarioIdentity())
             .position(1)
             .basis(true)
             .build();
         ComparisonObjectBuilder comparisonScenario1 = ComparisonObjectBuilder.builder()
-            .externalIdentity("987654321")
+            .externalIdentity(component2.getScenarioIdentity())
             .position(2)
             .build();
         ComparisonObjectBuilder comparisonScenario2 = ComparisonObjectBuilder.builder()
-            .externalIdentity("147258369")
+            .externalIdentity(component3.getScenarioIdentity())
             .position(3)
             .build();
 
@@ -58,9 +103,43 @@ public class CmpSaveComparisonTests {
             .objectsToCompare(comparisonObjectsList)
             .build();
 
-        PostComparisonResponse savedComparison = comparisonUtils.createComparison(comparison, currentUser);
+        PostComparisonResponse savedComparisonResponse = comparisonUtils.createComparison(comparison, currentUser);
 
-        softAssertions.assertThat(savedComparison.getComparisonName()).as("Ensure same comparison name returned").isEqualTo(comparisonName);
+        softAssertions.assertThat(savedComparisonResponse.getComparisonName()).as("Ensure same comparison name returned").isEqualTo(comparisonName);
+
+        GetComparisonResponse getComparisonDetails =comparisonUtils.getComparison(savedComparisonResponse.getIdentity(), currentUser);
+
+        softAssertions.assertThat(getComparisonDetails.getObjectsToCompare().size()).as("Verify number of Comparison Objects")
+            .isEqualTo(3);
+        softAssertions.assertThat(getComparisonDetails.getObjectsToCompare().get(0).getIdentity()).as("Verify first object is Component 1")
+            .isEqualTo(component1.getScenarioIdentity());
+        softAssertions.assertThat(getComparisonDetails.getObjectsToCompare().get(0).getBasis()).as("Verify first object is the basis")
+            .isTrue();
+
+        List<ComparisonObjectBuilder> comparisonObjects = comparison.getObjectsToCompare();
+
+        comparisonObjects.get(0).setBasis(false);
+        comparisonObjects.get(0).setPosition(2);
+        comparisonObjects.get(1).setPosition(3);
+        comparisonObjects.get(2).setPosition(1);
+        comparisonObjects.get(0).setBasis(true);
+
+        UpdateComparison updateComparison = UpdateComparison.builder()
+            .comparisonName(updatedComparisonName)
+            .objectsToCompare(comparisonObjects)
+            .build();
+
+        GetComparisonResponse updatedComparison = comparisonUtils.updateComparison(getComparisonDetails.getIdentity(), updateComparison, currentUser);
+
+        softAssertions.assertThat(savedComparisonResponse.getComparisonName()).as("Ensure same comparison name returned")
+            .isEqualTo(updatedComparisonName);
+        softAssertions.assertThat(getComparisonDetails.getObjectsToCompare().size()).as("Verify number of Comparison Objects")
+            .isEqualTo(3);
+        softAssertions.assertThat(getComparisonDetails.getObjectsToCompare().get(0).getIdentity()).as("Verify first object is Component 2")
+            .isEqualTo(component1.getScenarioIdentity());
+        softAssertions.assertThat(getComparisonDetails.getObjectsToCompare().get(0).getBasis()).as("Verify first object is the basis")
+            .isTrue();
+
         softAssertions.assertAll();
 
     }
