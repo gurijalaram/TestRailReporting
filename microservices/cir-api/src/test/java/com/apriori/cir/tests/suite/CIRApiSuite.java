c05af85b3fc458fcf1ed5b3abd1d85352c81e21b
cfrith <cfrith@apriori.com>
2023-07-26 17:06:19 +0100

    Fix errors in cir module.

--- microservices/cir-api/src/test/java/com/apriori/cir/tests/suite/CIRApiSuite.java
+++ microservices/cir-api/src/test/java/com/apriori/cir/tests/suite/CIRApiSuite.java
@@ -1,16 +1,7 @@
 package com.apriori.cir.tests.suite;
 
-import com.apriori.cir.tests.JasperReportTest;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("") // TODO should be added ID
-@RunWith(ConcurrentSuiteRunner.class)
-@Suite.SuiteClasses({
-    JasperReportTest.class
-})
+@Suite
 public class CIRApiSuite {
 }
