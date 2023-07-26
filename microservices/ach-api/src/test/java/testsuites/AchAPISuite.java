c1ad8851f2b38d8f5a1faafb857157c2e2df618a
cfrith <cfrith@apriori.com>
2023-07-26 16:52:22 +0100

    Fix errors in module.

--- microservices/ach-api/src/test/java/testsuites/AchAPISuite.java
+++ microservices/ach-api/src/test/java/testsuites/AchAPISuite.java
@@ -1,26 +1,7 @@
 package testsuites;
 
-import com.apriori.ach.tests.AchApplicationDataTests;
-import com.apriori.ach.tests.AchCurrentUserTests;
-import com.apriori.ach.tests.AchCustomersTests;
-import com.apriori.ach.tests.AchNotificationsTests;
-import com.apriori.ach.tests.AchPeopleTests;
-import com.apriori.ach.tests.AchUserPreferencesTests;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("2353")
-@RunWith(ConcurrentSuiteRunner.class)
-@Suite.SuiteClasses({
-    AchApplicationDataTests.class,
-    AchCurrentUserTests.class,
-    AchCustomersTests.class,
-    AchNotificationsTests.class,
-    AchPeopleTests.class,
-    AchUserPreferencesTests.class
-})
+@Suite
 public class AchAPISuite {
 }
