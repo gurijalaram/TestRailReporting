ca38c70e6dc6ffa76883547ad5be6f91b6651270
cfrith <cfrith@apriori.com>
2023-07-26 17:08:00 +0100

    Fix errors in cmp module.

--- microservices/cmp-api/src/test/java/com/apriori/cmp/testsuites/CmpRegressionTestSuite.java
+++ microservices/cmp-api/src/test/java/com/apriori/cmp/testsuites/CmpRegressionTestSuite.java
@@ -1,19 +1,7 @@
 package com.apriori.cmp.testsuites;
 
-import com.apriori.cmp.tests.CmpComparisonTests;
-import com.apriori.cmp.tests.CmpSaveComparisonTests;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.CategorySuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("2766")
-@RunWith(CategorySuiteRunner.class)
-@Suite.SuiteClasses({
-    CmpSaveComparisonTests.class,
-    CmpComparisonTests.class
-})
-
+@Suite
 public class CmpRegressionTestSuite {
 }
