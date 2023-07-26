0598754e587a6be2baa84ee525605e59da2ab3a2
cfrith <cfrith@apriori.com>
2023-07-26 17:14:54 +0100

    Fix errors in gdc module.

--- microservices/gcd-api/src/test/java/testsuites/GcdRegressionTestSuite.java
+++ microservices/gcd-api/src/test/java/testsuites/GcdRegressionTestSuite.java
@@ -1,17 +1,7 @@
 package testsuites;
 
-import com.apriori.gcd.tests.GcdTreesTests;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.CategorySuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("2766")
-@RunWith(CategorySuiteRunner.class)
-@Suite.SuiteClasses({
-    GcdTreesTests.class
-})
-
+@Suite
 public class GcdRegressionTestSuite {
 }
