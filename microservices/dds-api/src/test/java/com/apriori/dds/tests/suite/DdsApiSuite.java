3e26ebfd1927e0ff874be8512e6c3a6ab953afdf
cfrith <cfrith@apriori.com>
2023-07-26 17:10:47 +0100

    Fix errors in dds module.

--- microservices/dds-api/src/test/java/com/apriori/dds/tests/suite/DdsApiSuite.java
+++ microservices/dds-api/src/test/java/com/apriori/dds/tests/suite/DdsApiSuite.java
@@ -1,18 +1,7 @@
 package com.apriori.dds.tests.suite;
 
-import com.apriori.dds.tests.CommentsTest;
-import com.apriori.dds.tests.DiscussionTest;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("1713")
-@RunWith(ConcurrentSuiteRunner.class)
-@Suite.SuiteClasses({
-    DiscussionTest.class,
-    CommentsTest.class
-})
+@Suite
 public class DdsApiSuite {
 }
