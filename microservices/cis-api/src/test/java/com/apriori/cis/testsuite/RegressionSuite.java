2533f7bc0f0272e7f5ca1e19b38a49cfec788380
cfrith <cfrith@apriori.com>
2023-07-26 17:07:11 +0100

    Fix errors in cis module.

--- microservices/cis-api/src/test/java/com/apriori/cis/testsuite/RegressionSuite.java
+++ microservices/cis-api/src/test/java/com/apriori/cis/testsuite/RegressionSuite.java
@@ -1,27 +1,8 @@
 package com.apriori.cis.testsuite;
 
-import com.apriori.cis.tests.CisBidPackageItemTest;
-import com.apriori.cis.tests.CisBidPackageProjectUsersTest;
-import com.apriori.cis.tests.CisBidPackageProjectsTest;
-import com.apriori.cis.tests.CisBidPackageTest;
-import com.apriori.cis.tests.CisComponentTest;
-import com.apriori.cis.tests.CisUserPreferencesTest;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("867")
-@RunWith(ConcurrentSuiteRunner.class)
-@Suite.SuiteClasses({
-    CisUserPreferencesTest.class,
-    CisBidPackageTest.class,
-    CisBidPackageItemTest.class,
-    CisBidPackageProjectsTest.class,
-    CisComponentTest.class,
-    CisBidPackageProjectUsersTest.class
-})
+@Suite
 public class RegressionSuite {
 }
 
