07ea556217be3a7462ad300a190d4974986bd508
cfrith <cfrith@apriori.com>
2023-07-26 17:11:36 +0100

    Fix errors in dms module.

--- microservices/dms-api/src/test/java/testsuites/DmsApiSuite.java
+++ microservices/dms-api/src/test/java/testsuites/DmsApiSuite.java
@@ -1,24 +1,7 @@
 package testsuites;
 
-import com.apriori.dms.tests.DmsCommentViewTest;
-import com.apriori.dms.tests.DmsCommentsTest;
-import com.apriori.dms.tests.DmsDiscussionParticipantTest;
-import com.apriori.dms.tests.DmsDiscussionProjectItemTest;
-import com.apriori.dms.tests.DmsDiscussionTest;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("1714")
-@RunWith(ConcurrentSuiteRunner.class)
-@Suite.SuiteClasses({
-    DmsDiscussionTest.class,
-    DmsCommentsTest.class,
-    DmsCommentViewTest.class,
-    DmsDiscussionParticipantTest.class,
-    DmsDiscussionProjectItemTest.class
-})
+@Suite
 public class DmsApiSuite {
 }
