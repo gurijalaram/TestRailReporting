b1579db39bdf9629bf1171e6752f147cf962a058
cfrith <cfrith@apriori.com>
2023-07-26 17:14:07 +0100

    Fix errors in edc module.

--- microservices/edc-api/src/test/java/com/apriori/edcapi/testsuites/RegressionSuite.java
+++ microservices/edc-api/src/test/java/com/apriori/edcapi/testsuites/RegressionSuite.java
@@ -1,26 +1,7 @@
 package com.apriori.edcapi.testsuites;
 
-import com.apriori.edcapi.tests.AccountsControllerTest;
-import com.apriori.edcapi.tests.BillOfMaterialsTest;
-import com.apriori.edcapi.tests.LineItemsTest;
-import com.apriori.edcapi.tests.PartsTest;
-import com.apriori.edcapi.tests.ReportsTest;
-import com.apriori.edcapi.tests.UsersTest;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("262")
-@RunWith(ConcurrentSuiteRunner.class)
-@Suite.SuiteClasses({
-    BillOfMaterialsTest.class,
-    LineItemsTest.class,
-    PartsTest.class,
-    UsersTest.class,
-    AccountsControllerTest.class,
-    ReportsTest.class
-})
+@Suite
 public class RegressionSuite {
 }
