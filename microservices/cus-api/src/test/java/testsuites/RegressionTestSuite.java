55c696dc47a753866378d25c29f3938752ee1417
cfrith <cfrith@apriori.com>
2023-07-26 17:09:55 +0100

    Fix errors in cus module.

--- microservices/cus-api/src/test/java/testsuites/RegressionTestSuite.java
+++ microservices/cus-api/src/test/java/testsuites/RegressionTestSuite.java
@@ -1,18 +1,7 @@
 package testsuites;
 
-import com.apriori.tests.UserPreferencesTests;
-import com.apriori.tests.UserTests;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.CategorySuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("768")
-@RunWith(CategorySuiteRunner.class)
-@Suite.SuiteClasses({
-    UserTests.class,
-    UserPreferencesTests.class
-})
+@Suite
 public class RegressionTestSuite {
 }
