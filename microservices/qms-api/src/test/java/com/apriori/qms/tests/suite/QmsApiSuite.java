deb31afb6bc7d399676d404ea8f4f288da3a0448
cfrith <cfrith@apriori.com>
2023-07-26 17:17:13 +0100

    Fix errors in qms module.

--- microservices/qms-api/src/test/java/com/apriori/qms/tests/suite/QmsApiSuite.java
+++ microservices/qms-api/src/test/java/com/apriori/qms/tests/suite/QmsApiSuite.java
@@ -1,43 +1,7 @@
 package com.apriori.qms.tests.suite;
 
-import com.apriori.qms.tests.BidPackageProjectItemTest;
-import com.apriori.qms.tests.BidPackageProjectUserTest;
-import com.apriori.qms.tests.BidPackageProjectsTest;
-import com.apriori.qms.tests.LayoutConfigurationTest;
-import com.apriori.qms.tests.QmsBidPackageItemTest;
-import com.apriori.qms.tests.QmsBidPackageTest;
-import com.apriori.qms.tests.QmsComponentTest;
-import com.apriori.qms.tests.QmsProjectItemTest;
-import com.apriori.qms.tests.QmsProjectsFilteredTest;
-import com.apriori.qms.tests.QmsProjectsTest;
-import com.apriori.qms.tests.QmsScenarioDiscussionFilteredTest;
-import com.apriori.qms.tests.QmsScenarioDiscussionTest;
-import com.apriori.qms.tests.QmsScenarioSharingTest;
-import com.apriori.qms.tests.QmsUserPreferenceTest;
-import com.apriori.utils.ProjectRunID;
-import com.apriori.utils.runner.ConcurrentSuiteRunner;
+import org.junit.platform.suite.api.Suite;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@ProjectRunID("1712")
-@RunWith(ConcurrentSuiteRunner.class)
-@Suite.SuiteClasses({
-    BidPackageProjectItemTest.class,
-    BidPackageProjectsTest.class,
-    BidPackageProjectUserTest.class,
-    LayoutConfigurationTest.class,
-    QmsBidPackageItemTest.class,
-    QmsBidPackageTest.class,
-    QmsComponentTest.class,
-    QmsProjectItemTest.class,
-    QmsProjectsTest.class,
-    QmsProjectsFilteredTest.class,
-    QmsScenarioSharingTest.class,
-    QmsUserPreferenceTest.class,
-    QmsScenarioDiscussionTest.class,
-    QmsScenarioDiscussionFilteredTest.class
-})
-
+@Suite
 public class QmsApiSuite {
 }
