95ec1e3ab3ebf0baea1aaec7a49593d9376de445
cfrith <cfrith@apriori.com>
2023-08-04 12:42:05 +0100

    Update schema location and add missing schemas.

--- microservices/cds-api/src/main/java/com/apriori/cds/objects/response/ApVersions.java
+++ microservices/cds-api/src/main/java/com/apriori/cds/objects/response/ApVersions.java
@@ -10,7 +10,7 @@ import lombok.Data;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Schema(location = "common/ApVersionsSchema.json")
+@Schema(location = "ApVersionsSchema.json")
 @JsonRootName("response")
 @Data
 public class ApVersions extends Pagination {
