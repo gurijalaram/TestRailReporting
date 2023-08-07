9b6d9913bdf11b441a388fad001d3895a45d0f16
cfrith <cfrith@apriori.com>
2023-08-07 12:22:12 +0100

    Add missing api fields to model and schema. Fix assertions in test. Fix ref to schema. Remove duplicate pagination schema.

--- microservices/cas-api/src/main/java/com/apriori/cas/models/response/CustomerUser.java
+++ microservices/cas-api/src/main/java/com/apriori/cas/models/response/CustomerUser.java
@@ -48,4 +48,6 @@ public class CustomerUser {
     private String updatedByName;
     private List<Object> licenseAssignments = null;
     private String userType;
+    private UserEnablements userEnablements;
+    private Boolean hasCompleteProfile;
 }
