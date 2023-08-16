a5e459d1c7914655b88825a9bc67d8383ef1cbee
bhegan613 <bhegan@apriori.com>
2023-08-16 15:32:04 +0100

    Changed suite

--- web/cir-ui/src/test/java/testsuites/ReportingSuite.java
+++ web/cir-ui/src/test/java/testsuites/ReportingSuite.java
@@ -4,16 +4,12 @@ import static com.apriori.testconfig.TestSuiteType.TestSuite.ON_PREM;
 import static com.apriori.testconfig.TestSuiteType.TestSuite.REPORTS;
 
 import com.ootbreports.general.assemblydetails.AssemblyDetailsReportTests;
-import org.junit.jupiter.api.Tag;
-import org.junit.platform.suite.api.ExcludeTags;
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
-import org.junit.platform.suite.api.IncludePackages;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@ExcludeTags(REPORTS)
+@IncludeTags(ON_PREM)
 @SelectClasses({
     AssemblyDetailsReportTests.class
 })
