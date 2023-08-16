d86f80906861d591b660bc1e2cf6b27ca915dd16
bhegan613 <bhegan@apriori.com>
2023-08-16 14:41:53 +0100

    Temp fix

--- web/cir-ui/src/test/java/testsuites/ReportingSuite.java
+++ web/cir-ui/src/test/java/testsuites/ReportingSuite.java
@@ -1,13 +1,22 @@
 package testsuites;
 
+import static com.apriori.testconfig.TestSuiteType.TestSuite.ON_PREM;
 import static com.apriori.testconfig.TestSuiteType.TestSuite.REPORTS;
 
+import com.ootbreports.general.assemblydetails.AssemblyDetailsReportTests;
+import org.junit.jupiter.api.Tag;
+import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.IncludePackages;
 import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludePackages("com.ootbreports")
-@IncludeTags(REPORTS)
+@ExcludeTags(REPORTS)
+@SelectClasses({
+    AssemblyDetailsReportTests.class
+})
 public class ReportingSuite {
 }
