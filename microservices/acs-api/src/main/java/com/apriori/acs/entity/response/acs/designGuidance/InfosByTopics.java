dec140a25fa45b9133a8331beb93f7a296e667b6
smccaffreyapriori <smccaffrey@apriori.com>
2023-09-13 14:52:00 +0100

    Updated to use List Specification

--- microservices/acs-api/src/main/java/com/apriori/acs/entity/response/acs/designGuidance/InfosByTopics.java
+++ microservices/acs-api/src/main/java/com/apriori/acs/entity/response/acs/designGuidance/InfosByTopics.java
@@ -3,10 +3,10 @@ package com.apriori.acs.entity.response.acs.designGuidance;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class InfosByTopics {
     @JsonProperty("DTC_MESSAGES")
-    private ArrayList<GenericGuidanceTopics> DTC_MESSAGES;
+    private List<GenericGuidanceTopics> DTC_MESSAGES;
 }
