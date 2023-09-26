5e5b837cbf18efedec40758bbe552ab4640d2755
Krzysztof Derezinski (Contractor) <krzysztof_derezinski@epam.com>
2023-09-26 11:59:36 +0200

    addind test

--- web/cidapp-ui/src/test/java/com/apriori/explore/SustainabilityFieldsTests.java
+++ web/cidapp-ui/src/test/java/com/apriori/explore/SustainabilityFieldsTests.java
@@ -4,6 +4,10 @@ import static com.utils.ColumnsEnum.LOGISTICS_CARBON;
 import static com.utils.ColumnsEnum.MATERIAL_CARBON;
 import static com.utils.ColumnsEnum.PROCESS_CARBON;
 
+import com.apriori.enums.ProcessGroupEnum;
+import com.apriori.http.utils.FileResourceUtil;
+import com.apriori.http.utils.GenerateStringUtil;
+import com.apriori.pageobjects.compare.ComparePage;
 import com.apriori.pageobjects.explore.ExplorePage;
 import com.apriori.pageobjects.login.CidAppLoginPage;
 import com.apriori.reader.file.user.UserCredentials;
@@ -11,11 +15,14 @@ import com.apriori.reader.file.user.UserUtil;
 import com.apriori.testconfig.TestBaseUI;
 import com.apriori.testrail.TestRail;
 
+import com.utils.ColumnsEnum;
 import com.utils.DirectionEnum;
+import com.utils.SortOrderEnum;
 import io.qameta.allure.Description;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.List;
 
 public class SustainabilityFieldsTests extends TestBaseUI {
@@ -23,6 +30,9 @@ public class SustainabilityFieldsTests extends TestBaseUI {
     private CidAppLoginPage loginPage;
     private ExplorePage explorePage;
     private final SoftAssertions softAssertions = new SoftAssertions();
+    private File resourceFile;
+    private File resourceFile2;
+    private ComparePage comparePage;
 
     @Test
     @TestRail(id = 24429)
@@ -40,6 +50,38 @@ public class SustainabilityFieldsTests extends TestBaseUI {
 
         List<String> headers = explorePage.getTableHeaders();
         softAssertions.assertThat(headers).contains("Material Carbon","Process Carbon","Logistics Carbon");
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @TestRail(id = 24101)
+    @Description("Verify sustainability fields In comparison view")
+    public void sustainabilityFieldsComparisonView() {
+        final ProcessGroupEnum processGroupEnum = ProcessGroupEnum.PLASTIC_MOLDING;
+
+        String componentName = "M3CapScrew";
+        String componentName2 = "Push Pin";
+        resourceFile = FileResourceUtil.getCloudFile(processGroupEnum, componentName + ".CATPart");
+        resourceFile2 = FileResourceUtil.getCloudFile(processGroupEnum, componentName2 + ".stp");
+        currentUser = UserUtil.getUser();
+        String scenarioName = new GenerateStringUtil().generateScenarioName();
+        String scenarioName2 = new GenerateStringUtil().generateScenarioName();
+
+        loginPage = new CidAppLoginPage(driver);
+        comparePage = loginPage.login(currentUser)
+            .uploadComponentAndOpen(componentName, scenarioName, resourceFile, currentUser)
+            .uploadComponentAndOpen(componentName2, scenarioName2, resourceFile2, currentUser)
+            .clickExplore()
+            .selectFilter("Recent")
+            .sortColumn(ColumnsEnum.CREATED_AT, SortOrderEnum.DESCENDING)
+            .multiSelectScenarios("" + componentName + ", " + scenarioName + "", "" + componentName2 + ", " + scenarioName2 + "")
+            .createComparison()
+            .selectManualComparison();
+
+        comparePage.getSustainabilityHeaderDetails();
+        softAssertions.assertThat(comparePage.getCardHeader()).contains("Sustainability");
+        softAssertions.assertThat(comparePage.getSustainabilityHeaderDetails())
+            .contains("Material Carbon","Process Carbon","Logistics Carbon","Total Carbon","Annual Manufacturing Carbon");
         softAssertions.assertAll();
     }
 }
