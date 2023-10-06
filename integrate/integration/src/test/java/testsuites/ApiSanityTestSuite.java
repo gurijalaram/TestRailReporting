cdc595ed427ba9b9a3417b7e8c7c4ba1c02b34b2
cfrith <cfrith@apriori.com>
2023-10-06 17:28:32 +0100

    Add dependency on in build file. Add classes to testsuite.

--- integrate/integration/src/test/java/testsuites/ApiSanityTestSuite.java
+++ integrate/integration/src/test/java/testsuites/ApiSanityTestSuite.java
@@ -2,12 +2,53 @@ package testsuites;
 
 import static com.apriori.testconfig.TestSuiteType.TestSuite.API_SANITY;
 
+import com.apriori.AtsAuthorization;
+import com.apriori.BatchPartTest;
+import com.apriori.CasCustomersTests;
+import com.apriori.CasUsersTests;
+import com.apriori.CdsCustomersTests;
+import com.apriori.CicAgentTest;
+import com.apriori.CmpComparisonTests;
+import com.apriori.ComponentsTest;
+import com.apriori.CssSearchTests;
+import com.apriori.DigitalFactoriesTest;
+import com.apriori.EmailsTests;
+import com.apriori.FileManagementControllerTest;
+import com.apriori.GcdTreesTests;
+import com.apriori.LineItemsTest;
+import com.apriori.ProcessGroupsTest;
+import com.apriori.UserTests;
+import com.apriori.ach.tests.AchCurrentUserTests;
+import com.apriori.evaluate.DataCreationTests;
+import com.apriori.workorders.WorkorderAPITests;
+
 import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages("com.apriori")
+@SelectClasses({
+    AtsAuthorization.class,
+    AchCurrentUserTests.class,
+    WorkorderAPITests.class,
+    AtsAuthorization.class,
+    BatchPartTest.class,
+    CasCustomersTests.class,
+    CasUsersTests.class,
+    CdsCustomersTests.class,
+    CicAgentTest.class,
+    DataCreationTests.class,
+    CmpComparisonTests.class,
+    CssSearchTests.class,
+    UserTests.class,
+    LineItemsTest.class,
+    FileManagementControllerTest.class,
+    GcdTreesTests.class,
+    EmailsTests.class,
+    ComponentsTest.class,
+    DigitalFactoriesTest.class,
+    ProcessGroupsTest.class
+    })
 @IncludeTags(API_SANITY)
 public class ApiSanityTestSuite {
 }
