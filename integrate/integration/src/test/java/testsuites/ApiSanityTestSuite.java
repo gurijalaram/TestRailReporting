7f96a57c9bdca6f039f0b49db4763792edbde8d1
TurboFry3 <vautourj@gmail.com>
2023-10-11 22:51:03 -0400

    Update ApiSanityTestSuite.java

--- integrate/integration/src/test/java/testsuites/ApiSanityTestSuite.java
+++ integrate/integration/src/test/java/testsuites/ApiSanityTestSuite.java
@@ -38,7 +38,36 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({
-    EmailsTests.class
+    AtsAuthorization.class,
+    AchCurrentUserTests.class,
+    AllMaterialStocksInfoTests.class,
+    AtsAuthorization.class,
+    BatchPartTest.class,
+    BidPackageProjectUserTest.class,
+    QdsBidPackageProjectsTest.class,
+    QmsBidPackageProjectsTest.class,
+    CasCustomersTests.class,
+    CasUsersTests.class,
+    CdsCustomersTests.class,
+    CicAgentTest.class,
+    CisBidPackageProjectsTest.class,
+    CostAllCadTests.class,
+    CommentsTest.class,
+    DataCreationTests.class,
+    DiscussionTest.class,
+    DmsCommentsTest.class,
+    CmpComparisonTests.class,
+    CssSearchTests.class,
+    UserTests.class,
+    LineItemsTest.class,
+    FileManagementControllerTest.class,
+    GcdTreesTests.class,
+    EmailsTests.class,
+    ComponentsTest.class,
+    DigitalFactoriesTest.class,
+    ProcessGroupsTest.class,
+    QmsScenarioDiscussionTest.class,
+    SheetMetalDtcReportTests.class
     })
 @IncludeTags(API_SANITY)
 public class ApiSanityTestSuite {
