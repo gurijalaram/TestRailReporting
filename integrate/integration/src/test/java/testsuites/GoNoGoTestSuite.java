69a4c8f9481ec585bbf4ad0e33967d7bc056e2fa
gurijalaram16 <98057348+gurijalaram16@users.noreply.github.com>
2023-10-16 10:52:36 -0400

    Added tests to gonogo suite (#2567)

--- integrate/integration/src/test/java/testsuites/GoNoGoTestSuite.java
+++ integrate/integration/src/test/java/testsuites/GoNoGoTestSuite.java
@@ -1,7 +1,19 @@
 package testsuites;
 
+import com.integration.tests.CIAIntegrationTests;
+import com.integration.tests.CICIntegrationTests;
+import com.integration.tests.CIDIntegrationTests;
+import com.integration.tests.CIRIntegrationTests;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
+@SelectClasses({
+    CICIntegrationTests.class,
+    CIDIntegrationTests.class,
+    CIAIntegrationTests.class,
+    CIRIntegrationTests.class
+})
+
 public class GoNoGoTestSuite {
 }
