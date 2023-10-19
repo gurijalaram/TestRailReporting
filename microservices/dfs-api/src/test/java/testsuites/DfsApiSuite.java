e1c0d52dbf92aceffac681931ba6725f68acc04b
cfrith <cfrith@apriori.com>
2023-10-19 16:20:55 +0100

    Add class to test suite.

--- microservices/dfs-api/src/test/java/testsuites/DfsApiSuite.java
+++ microservices/dfs-api/src/test/java/testsuites/DfsApiSuite.java
@@ -1,10 +1,12 @@
 package testsuites;
 
+import com.aprioi.DigitalFactoriesTests;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({
+    DigitalFactoriesTests.class
 })
 public class DfsApiSuite {
 }
