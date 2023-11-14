a5e55672fd04fe643855956bab67d7548400c590
Vitaliy Krutas <vkrutas@apriori.com>
2023-11-14 12:41:32 +0200

    COST-1066, COST-1067, COST-1068, COST-1069 - refactor code for testing DF end points, address requested TODOs

--- microservices/dfs-api/src/main/java/com/apriori/dfs/api/enums/DFSApiEnum.java
+++ microservices/dfs-api/src/main/java/com/apriori/dfs/api/enums/DFSApiEnum.java
@@ -9,7 +9,9 @@ public enum DFSApiEnum implements ExternalEndpointEnum {
 
     // DIGITAL FACTORY
     DIGITAL_FACTORIES("digital-factories"),
-    DIGITAL_FACTORIES_BY_PATH_PARAMETER("digital-factories/%s");
+    DIGITAL_FACTORIES_WITH_KEY_PARAM("digital-factories?key=%s"),
+    DIGITAL_FACTORIES_BY_PATH("digital-factories/%s"),
+    DIGITAL_FACTORIES_BY_PATH_WITH_KEY_PARAM("digital-factories/%s?key=%s");
 
     private final String endpoint;
 
@@ -24,16 +26,19 @@ public enum DFSApiEnum implements ExternalEndpointEnum {
 
     @Override
     public String getEndpoint(Object... variables) {
+
         String endpoint = PropertiesContext.get("dfs.api_url") + String.format(getEndpointString(), variables);
-        String sharedSecretKey = "key=";
-        if (Arrays.stream(variables).anyMatch(o -> o.toString().contains(sharedSecretKey) || o.toString().isEmpty())) {
-            return endpoint;
-        }
-        return endpoint + this.addQuery(getEndpointString());
+        boolean dontAddSharedSecret = Arrays.stream(variables)
+            .map(Object::toString)
+            .anyMatch(String::isEmpty);
+
+        return dontAddSharedSecret ? endpoint : endpoint + this.addQuery(getEndpointString());
     }
 
     @Override
     public String addQuery(String endpointString) {
-        return endpointString.contains("?") ? "&" : "?" + "key=" + PropertiesContext.get("dfs.authorization_key");
+        // TODO: use when DFS fixed to pull secret from the param store
+        // return endpointString.contains("?") ? "&" : "?key=" + getSecretKey();
+        return endpointString.contains("?") ? "&" : "?key=" + PropertiesContext.get("dfs.authorization_key");
     }
 }
