c4db73c0c37e3d907c9d3b65796c35cbae61c298
Vladyslav Zarovnyi <vzarovnyi@apriori.com>
2023-12-08 16:23:43 +0200

    Updated profile

--- shared-util/src/main/java/com/apriori/shared/util/http/utils/AwsUtil.java
+++ shared-util/src/main/java/com/apriori/shared/util/http/utils/AwsUtil.java
@@ -40,7 +40,7 @@ public class AwsUtil {
             .credentialsProvider(
                 // System.getenv("AWS_ACCESS_KEY_ID") != null
                 //? EnvironmentVariableCredentialsProvider.create()
-                ProfileCredentialsProvider.create("apriori-central")
+                ProfileCredentialsProvider.create()
             )
             .build();
     }
