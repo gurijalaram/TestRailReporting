684ecc7a41e0dc87b98b5722125a9d2b693636f0
smccaffreyapriori <smccaffrey@apriori.com>
2024-01-11 16:36:25 +0000

    Updated MaterialProperties.java

--- microservices/dfs-api/src/main/java/com/apriori/dfs/api/models/response/MaterialProperties.java
+++ microservices/dfs-api/src/main/java/com/apriori/dfs/api/models/response/MaterialProperties.java
@@ -1,18 +1,22 @@
 package com.apriori.dfs.api.models.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class MaterialProperties {
     private Double costPerKG;
     private Integer ultimateTensileStrength;
-    private Double k;
+    @JsonProperty("k")
+    private Double thermalConductivity;
     private Double shearStrength;
     private Integer youngModulus;
     private Integer tensileYieldStrength;
-    private Double n;
+    @JsonProperty("n")
+    private Double strainHardeningExponent;
     private Double possionRatio;
-    private Double r;
+    @JsonProperty("r")
+    private Double plasticStrainSatio;
     private Double scrapCostPercent;
     private String costUnits;
     private String sourceName;
