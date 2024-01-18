6d249674eb11a562997a59735e5ac6d9826c8708
cfrith <cfrith@apriori.com>
2024-01-18 15:40:02 +0000

    Fix checkstyle.

--- web/cid-ui/src/test/java/com/apriori/cid/ui/tests/evaluate/assemblies/IncludeAndExcludeTests.java
+++ web/cid-ui/src/test/java/com/apriori/cid/ui/tests/evaluate/assemblies/IncludeAndExcludeTests.java
@@ -304,7 +304,7 @@ public class IncludeAndExcludeTests extends TestBaseUI {
     }
 
     @Test
-    @Issues( {@Issue("AP-74028")})
+    @Issues({@Issue("AP-74028")})
     @TestRail(id = {12135, 12052, 12138})
     @Description("Missing sub-component automatically included on update - test with alternate CAD file for Assembly with additional components not on system")
     public void testMissingSubcomponentIncludedOnUpdate() {
