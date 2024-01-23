32867513ad04bcc530d3eba61a8d5844a22f9bcb
Vladyslav Zarovnyi <vzarovnyi@apriori.com>
2024-01-23 18:36:04 +0200

    code style

--- web/ach-ui/src/main/java/com/apriori/qa/ach/ui/pageobjects/applications/AppStreamPage.java
+++ web/ach-ui/src/main/java/com/apriori/qa/ach/ui/pageobjects/applications/AppStreamPage.java
@@ -37,7 +37,7 @@ public class AppStreamPage extends LoadableComponent<AppStreamPage> {
             pageUtils.waitFor(Duration.ofMinutes(3).toMillis()); // give a time for the streaming resource to appear
             pageUtils.waitForElementAndClick(By.xpath("//button[@id='modal-alert-retry']"));
 
-            if(isDisplayedResourceNotAvailableWarning(resourceNotAvailableWarning)) {
+            if (isDisplayedResourceNotAvailableWarning(resourceNotAvailableWarning)) {
                 log.warn("'{}' Message. Skipped aP Pro (AppStream) validation.", resourceNotAvailableWarning);
                 return;
             }
@@ -45,10 +45,10 @@ public class AppStreamPage extends LoadableComponent<AppStreamPage> {
 
         pageUtils.waitForElementToAppear(
             By.xpath("//div[@id='toolbar-content' and @ng-show='toolbarOptions.isVisible']"),
-            Duration.ofMinutes(3));
+            Duration.ofMinutes(4));
     }
 
     public Boolean isDisplayedResourceNotAvailableWarning(final String messageToCheck) {
-        return pageUtils.waitForElementToAppear(By.xpath("//h1[contains(text(), '" + messageToCheck + "')]")).isDisplayed();
+        return pageUtils.waitForWebElement(driver.findElement(By.xpath("//h1[contains(text(), '" + messageToCheck + "')]")));
     }
 }
