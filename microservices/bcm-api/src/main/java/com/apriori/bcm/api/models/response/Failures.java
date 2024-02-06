d3d737786f100851e03bf4a9492ba4559b0b51ce
Krzysztof Derezinski (Contractor) <krzysztof_derezinski@epam.com>
2024-02-06 10:50:33 +0100

    adding tests

---
+++ microservices/bcm-api/src/main/java/com/apriori/bcm/api/models/response/Failures.java
@@ -0,0 +1,14 @@
+package com.apriori.bcm.api.models.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Failures {
+    private String inputRowIdentity;
+}
