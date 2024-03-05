f02acd5057d44c2c39dcdee1cba48f2bf6e7f6fc
Krzysztof Derezinski (Contractor) <krzysztof_derezinski@epam.com>
2024-03-05 22:03:53 +0100

    adding test to CID UI

--- web/cid-ui/src/main/java/com/apriori/cid/ui/pageobjects/projects/ProjectsPage.java
+++ web/cid-ui/src/main/java/com/apriori/cid/ui/pageobjects/projects/ProjectsPage.java
@@ -3,8 +3,14 @@ package com.apriori.cid.ui.pageobjects.projects;
 
 import com.apriori.web.app.util.PageUtils;
 
+import org.apache.groovy.util.Arrays;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.LoadableComponent;
+
+import java.util.List;
 
 public class ProjectsPage extends LoadableComponent<ProjectsPage> {
 
@@ -29,5 +35,11 @@ public class ProjectsPage extends LoadableComponent<ProjectsPage> {
 
     public boolean isOnProjectsPage() {
         return driver.getCurrentUrl().contains("/bulk-analysis");
+    }
+
+    public boolean isListOfWorksheetsPresent() {
+        List<WebElement> listOfWorksheetItems =
+            pageUtils.waitForElementsToAppear(By.xpath("//div[@data-testid = 'table-body']/div"));
+        return !(listOfWorksheetItems.isEmpty());
     }
 }
