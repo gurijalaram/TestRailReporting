8352668b3cc6efddcd5f3f2b079a5505cfac5403
cfrith <cfrith@apriori.com>
2024-05-21 15:41:16 +0100

    Fix error after merge. Add simple comments to explain new implementation.

--- shared-util/src/main/java/com/apriori/shared/util/enums/RolesEnum.java
+++ shared-util/src/main/java/com/apriori/shared/util/enums/RolesEnum.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 @Getter
 public enum RolesEnum {
 
-    APRIORI_DEVELOPER("APRIORI_DEVELOPER"),
+    APRIORI_DEVELOPER("APRIORI_DEVELOPER"), //equal to old access level 'admin'
     APRIORI_EXPERT("APRIORI_EXPERT"),
     APRIORI_CONTRIBUTOR("APRIORI_CONTRIBUTOR"),
     APRIORI_ANALYST("APRIORI_ANALYST"),
-    APRIORI_DESIGNER("APRIORI_DESIGNER"),
+    APRIORI_DESIGNER("APRIORI_DESIGNER"), //equal to old access level 'common'
     APRIORI_EDC("APRIORI_EDC");
 
     private final String role;
