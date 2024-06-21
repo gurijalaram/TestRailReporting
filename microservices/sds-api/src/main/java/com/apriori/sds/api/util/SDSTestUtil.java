7240e9eee0c0b2123780d0fc77da9eb4a6c96caa
cfrith <cfrith@apriori.com>
2024-06-21 11:33:25 +0100

    Add todo.

--- microservices/sds-api/src/main/java/com/apriori/sds/api/util/SDSTestUtil.java
+++ microservices/sds-api/src/main/java/com/apriori/sds/api/util/SDSTestUtil.java
@@ -500,6 +500,7 @@ public abstract class SDSTestUtil extends TestUtil {
 
         final RequestEntity requestEntity =
             RequestEntityUtil_Old.init(SDSAPIEnum.POST_PUBLISH_SCENARIO_BY_COMPONENT_SCENARIO_IDs, klass)
+                // TODO: 21/06/2024 cn - workaround for now so will be reworked with changes to requestentity
                 .apUserContext(new AuthUserContextUtil().getAuthUserContext(componentInfoBuilder.getUser().getEmail()))
                 .inlineVariables(componentInfoBuilder.getComponentIdentity(), componentInfoBuilder.getScenarioIdentity())
                 .body("scenario", shallowPublishRequest)
