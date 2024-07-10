642e2d5d4325aaf9f86c65f637daa41867bafa40
cfrith <cfrith@apriori.com>
2024-07-10 10:30:44 +0100

    Remove redundant call to userUtil. Modify test description. Convert fields to local var.

--- microservices/cds-api/src/test/java/com/apriori/cds/api/tests/CdsAccessAuthorizationsTests.java
+++ microservices/cds-api/src/test/java/com/apriori/cds/api/tests/CdsAccessAuthorizationsTests.java
@@ -1,7 +1,5 @@
 package com.apriori.cds.api.tests;
 
-import static com.apriori.shared.util.enums.RolesEnum.APRIORI_DEVELOPER;
-
 import com.apriori.cds.api.enums.CDSAPIEnum;
 import com.apriori.cds.api.models.IdentityHolder;
 import com.apriori.cds.api.models.response.AccessAuthorization;
@@ -13,8 +11,6 @@ import com.apriori.cds.api.models.response.StatusAccessAuthorizations;
 import com.apriori.cds.api.utils.CdsTestUtil;
 import com.apriori.cds.api.utils.Constants;
 import com.apriori.cds.api.utils.CustomerUtil;
-import com.apriori.shared.util.file.user.UserCredentials;
-import com.apriori.shared.util.file.user.UserUtil;
 import com.apriori.shared.util.http.utils.GenerateStringUtil;
 import com.apriori.shared.util.http.utils.RequestEntityUtil;
 import com.apriori.shared.util.http.utils.ResponseWrapper;
@@ -42,7 +38,6 @@ public class CdsAccessAuthorizationsTests {
     private GenerateStringUtil generateStringUtil = new GenerateStringUtil();
     private CdsTestUtil cdsTestUtil;
     private CustomerUtil customerUtil;
-    private ResponseWrapper<Customer> customer;
     private String customerName;
     private String cloudRef;
     private String emailPattern;
@@ -51,14 +46,11 @@ public class CdsAccessAuthorizationsTests {
     private String associationIdentity;
     private String apStaffIdentity;
     private String url;
-    private ResponseWrapper<CustomerAssociationResponse> customerAssociationResponse;
-    private ResponseWrapper<AssociationUserItems> associationUser;
     private SoftAssertions soft = new SoftAssertions();
-    private UserCredentials currentUser = UserUtil.getUser(APRIORI_DEVELOPER);
 
     @BeforeEach
     public void setDetails() {
-        RequestEntityUtil requestEntityUtil = TestHelper.initUser().useTokenInRequests();
+        RequestEntityUtil requestEntityUtil = TestHelper.initUser().useTokenInRequests();;
         cdsTestUtil = new CdsTestUtil(requestEntityUtil);
         customerUtil = new CustomerUtil(requestEntityUtil);
 
@@ -66,14 +58,14 @@ public class CdsAccessAuthorizationsTests {
         customerName = generateStringUtil.generateAlphabeticString("Customer", 6);
         cloudRef = generateStringUtil.generateCloudReference();
         emailPattern = "\\S+@".concat(customerName);
-        apStaffIdentity = currentUser.getUserDetails().getIdentity();
+        apStaffIdentity = requestEntityUtil.getEmbeddedUser().getUserDetails().getIdentity();
 
-        customer = customerUtil.addCASCustomer(customerName, cloudRef, emailPattern);
+        ResponseWrapper<Customer> customer = customerUtil.addCASCustomer(customerName, cloudRef, emailPattern);
         customerIdentity = customer.getResponseEntity().getIdentity();
-        apCustomerIdentity = currentUser.getUserDetails().getCustomerIdentity();
-        customerAssociationResponse = cdsTestUtil.getCommonRequest(CDSAPIEnum.CUSTOMERS_ASSOCIATIONS, CustomerAssociationResponse.class, HttpStatus.SC_OK, apCustomerIdentity);
+        apCustomerIdentity = requestEntityUtil.getEmbeddedUser().getUserDetails().getCustomerIdentity();
+        ResponseWrapper<CustomerAssociationResponse> customerAssociationResponse = cdsTestUtil.getCommonRequest(CDSAPIEnum.CUSTOMERS_ASSOCIATIONS, CustomerAssociationResponse.class, HttpStatus.SC_OK, apCustomerIdentity);
         associationIdentity = customerAssociationResponse.getResponseEntity().getItems().stream().filter(target -> target.getTargetCustomerIdentity().equals(customerIdentity)).toList().get(0).getIdentity();
-        associationUser = customerUtil.addCustomerAssociationUser(apCustomerIdentity, associationIdentity, apStaffIdentity);
+        ResponseWrapper<AssociationUserItems> associationUser = customerUtil.addCustomerAssociationUser(apCustomerIdentity, associationIdentity, apStaffIdentity);
         customerAssociationUserIdentity = associationUser.getResponseEntity().getIdentity();
         customerAssociationUserIdentityEndpoint = String.format(url, String.format("customers/%s/customer-associations/%s/customer-association-users/%s", apCustomerIdentity, associationIdentity, customerAssociationUserIdentity));
     }
@@ -97,7 +89,7 @@ public class CdsAccessAuthorizationsTests {
 
     @Test
     @TestRail(id = {13115, 13116})
-    @Description("Creating a new access authorization for customer and getting it")
+    @Description("Creating a new access authorization for customer and get it")
     public void addAccessAuthorization() {
         ResponseWrapper<AccessAuthorization> accessAuthorization = cdsTestUtil.addAccessAuthorization(customerIdentity, apStaffIdentity, "service-account.1");
         soft.assertThat(accessAuthorization.getResponseEntity().getUserIdentity()).isEqualTo(apStaffIdentity);
@@ -115,7 +107,7 @@ public class CdsAccessAuthorizationsTests {
 
     @Test
     @TestRail(id = {13117})
-    @Description("Getting status report of access authorizations")
+    @Description("Get status report of access authorizations")
     public void getStatusOfAccessAuthorization() {
         ResponseWrapper<AccessAuthorization> accessAuthorization = cdsTestUtil.addAccessAuthorization(customerIdentity, apStaffIdentity, "service-account.1");
         LocalDateTime creationDate = accessAuthorization.getResponseEntity().getCreatedAt();
@@ -134,7 +126,7 @@ public class CdsAccessAuthorizationsTests {
 
     @Test
     @TestRail(id = {13118, 13119})
-    @Description("Getting status report of access authorizations")
+    @Description("Get status report of access authorizations by id")
     public void getAccessAuthorizationByID() {
         ResponseWrapper<AccessAuthorization> accessAuthorization = cdsTestUtil.addAccessAuthorization(customerIdentity, apStaffIdentity, "service-account.1");
         String authorizationIdentity = accessAuthorization.getResponseEntity().getIdentity();
